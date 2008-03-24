b96e207d0a7a211276b92df985b2dbbfc9024a14
ptarra <Unknown>
2008-03-24 16:07:40 +0000

    Account Invoice Series: Adds different invoicing series
    Signed-off-by: Pedro Tarrafeta <pedro@haddock.acysos>

---
+++ extra_addons/account_invoice_series/__terp__.py
@@ -0,0 +1,13 @@
+{
+	"name" : "Account Invoice Series",
+	"version" : "1.0",
+	"author" : "ACYSOS S.L.",
+	"category" : "Accounting",
+	"depends" : ["account"],
+	"demo_xml" : [],
+	"update_xml" : [
+		"invoice_series_view.xml"
+	],
+	"active": True,
+	"installable": True
+}
