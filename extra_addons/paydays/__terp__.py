a919a7fb03e83579e11c35b51167ec2fba147de1
ejordi <Unknown>
2008-03-25 12:50:11 +0000

    Descripción del módulo paydays

--- extra_addons/paydays/__terp__.py
+++ extra_addons/paydays/__terp__.py
@@ -2,7 +2,9 @@
     "name" : "Paydays",
     "version" : "1.0",
     "author" : "ACYSOS, S.L. y Pablo Rocandio",
-    "description": """Este módulo permite definir los días de pago""",
+    "description": """Este módulo permite definir los días de pago.
+
+En el campo días de pago del formulario de partners se introducen los días de pago separados por cualquier carácter no numérico. El módulo convierte los días de pago a un formato separado por guiones (por ejemplo 5-15-25).""",
     "depends" : ["base", "account"],
     "init_xml" : [],
     "update_xml" : ['paydays_view.xml',],
