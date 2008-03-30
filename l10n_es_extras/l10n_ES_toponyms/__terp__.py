4a8f6fdcd0c23e8527f30489ed1762c45e00c7e5
ejordi <Unknown>
2008-03-30 18:34:29 +0000

    Creación módulo l10n_ES_toponyms: Provincias, municipios y códigos postales de España.

---
+++ l10n_es_extras/l10n_ES_toponyms/__terp__.py
@@ -0,0 +1,14 @@
+{
+		"name" : "Topónimos de España",
+		"version" : "1.0",
+		"author" : "Zikzakmedia SL",
+		"website" : "http://zikzakmedia.com",
+		"category" : "Localisations/Europe",
+		"description": """Provincias, municipios y códigos postales de España. Permite rellenar automáticamente los campos ciudad y provincia a partir del código postal.""",
+		"depends" : ["base"],
+		"init_xml" : ["l10n_ES_toponyms_paises.xml", "l10n_ES_toponyms_provincias.xml", ],
+		"demo_xml" : [ ],
+		"update_xml" : ["l10n_ES_toponyms_wizard.xml" ],
+		"active": False,
+		"installable": True
+} 
