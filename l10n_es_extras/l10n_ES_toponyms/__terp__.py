240a87cd6f088dd2b4fc3f6fd961af4f8209e9f8
ejordi <Unknown>
2008-04-04 18:44:37 +0000

    Modificación módulo l10n_ES_toponyms: Obtención de códigos postales y municipios desde un fichero .csv externo

--- l10n_es_extras/l10n_ES_toponyms/__terp__.py
+++ l10n_es_extras/l10n_ES_toponyms/__terp__.py
@@ -4,7 +4,9 @@
 		"author" : "Zikzakmedia SL",
 		"website" : "http://zikzakmedia.com",
 		"category" : "Localisations/Europe",
-		"description": """Provincias, municipios y códigos postales del Estado español. Permite rellenar automáticamente los campos ciudad y provincia a partir del código postal.""",
+		"description": """Provincias, municipios y códigos postales del Estado español.
+		
+Cambia Spain por España y añade las 52 provincias del Estado español. Mediante un asistente se añaden los valores por defecto de las provincias y municipios referentes a cada código postal. Esto permite rellenar automáticamente los campos ciudad y provincia del formulario contactos a partir del código postal.""",
 		"depends" : ["base"],
 		"init_xml" : ["l10n_ES_toponyms_paises.xml", "l10n_ES_toponyms_provincias.xml", ],
 		"demo_xml" : [ ],
