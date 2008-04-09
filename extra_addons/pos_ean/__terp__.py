eb060ba155cdd22b5e2aee97726e0856f0dd51a9
ejordi <Unknown>
2008-04-09 10:54:10 +0000

    Nuevo módulo pos_ean to extend the point of sale module to read quickly the products from their EAN13 code

---
+++ extra_addons/pos_ean/__terp__.py
@@ -0,0 +1,17 @@
+{
+	"name" : "Point Of Sale. EAN13 extension",
+	"version" : "1.0",
+	"author" : "Zikzakmedia SL",
+	
+	"description": """
+Extends point of sale module to read quickly the products from their EAN13 code
+	""",
+
+	"category" : "Generic Modules/Sales & Purchases",
+	"depends" : ["pos"],
+	"init_xml" : [],
+	"demo_xml" : [],
+	"update_xml" : ["pos_view.xml"],
+	"active": False,
+	"installable": True,
+}
