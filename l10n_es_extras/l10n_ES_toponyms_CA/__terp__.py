4834bd5120a1dc917330eb45ce808a7d63c64f24
ejordi <Unknown>
2008-04-11 14:30:49 +0000

    Añadido nuevo módulo l10n_ES_toponyms_CA: Comunidades autónomas del estado español

---
+++ l10n_es_extras/l10n_ES_toponyms_CA/__terp__.py
@@ -0,0 +1,18 @@
+{
+		"name" : "Comunidades Autónomas de España",
+		"version" : "1.0",
+		"author" : "Zikzakmedia SL",
+		"website" : "http://zikzakmedia.com",
+		"category" : "Localisations/Others",
+		"description": """Comunidades Autónomas del Estado Español
+
+  * Añade un nuevo campo de comunidades autónomas al formulario de empresas y contactos.
+  * Inserta todas las Comunidades Autónomas del Estado Español.
+  * Proporciona un assistente para dar de alta las comunidades autónomas por defecto asociadas a los codigos postales. Permite rellenar automaticamente el campo comunidad autónoma del formulario de las empresas y contactos a partir del codigo postal.""",
+		"depends" : ["base"],
+		"init_xml" : ["l10n_ES_toponyms_CA_data.xml"],
+		"demo_xml" : [ ],
+		"update_xml" : ["l10n_ES_toponyms_CA_view.xml", "l10n_ES_toponyms_CA_wizard.xml"],
+		"active": False,
+		"installable": True
+} 
