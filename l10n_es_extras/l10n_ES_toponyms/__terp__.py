0f973ad06f2587d854cd12d2de5ada2c2418b4a0
ejordi <Unknown>
2008-05-24 15:12:26 +0000

    Mòdulo l10n_ES_toponyms: Pequeñas mejoras en la documentación i menú del wizard

--- l10n_es_extras/l10n_ES_toponyms/__terp__.py
+++ l10n_es_extras/l10n_ES_toponyms/__terp__.py
@@ -4,9 +4,13 @@
 		"author" : "Zikzakmedia SL",
 		"website" : "http://zikzakmedia.com",
 		"category" : "Localisations/Europe",
-		"description": """Provincias, municipios y códigos postales del Estado español.
-		
-Cambia Spain por España y añade las 52 provincias del Estado español. Mediante un asistente se añaden los valores por defecto de las provincias y municipios referentes a cada código postal. Esto permite rellenar automáticamente los campos ciudad y provincia del formulario contactos a partir del código postal.""",
+		"description": """Provincias, municipios y códigos postales del Estado Español
+
+  * Traduce el país Spain por España
+  * Añade las 52 provincias actuales del Estado Español
+  * Proporciona un asistente para dar de alta los municipios y provincias por defecto asociados a los 15839 códigos postales del Estado Español. Permite rellenar automáticamente los campos ciudad y provincia del formulario de empresa y contacto a partir del código postal.
+
+Los datos han sido obtenidos de los datos públicos del Instituto Nacional de Estadística (INE).""",
 		"depends" : ["base"],
 		"init_xml" : ["l10n_ES_toponyms_paises.xml", "l10n_ES_toponyms_provincias.xml", ],
 		"demo_xml" : [ ],
