36160063f594c4cc11229dd7587dda58f725ce61
ptarra <Unknown>
2008-09-02 14:28:14 +0000

    Modificada dependencia errónea

--- l10n_es_extras/partner_seq_es/__terp__.py
+++ l10n_es_extras/partner_seq_es/__terp__.py
@@ -4,7 +4,7 @@
 	"author" : "Pablo Rocandio",
 	"description": """Este módulo vincula una secuencia al campo de codigo de partner
 	""",
-	"depends" : ["base","account","l10n_es_chart","partner_data_es"],
+	"depends" : ["base","account","l10n_chart_ES","partner_data_es"],
 	"init_xml" : [],
 	"update_xml" : ['partner_seq_sequence.xml', 'partner_seq_wizard.xml'],
 	"active": False,
