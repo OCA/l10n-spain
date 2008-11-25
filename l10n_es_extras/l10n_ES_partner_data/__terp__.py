b475c8b88d48c6012608e380e13eea15ff2896b8
Jordi Esteve <jesteve@alba>
2008-11-25 01:21:44 +0100

    Módulo l10n_ES_partner_data (antiguo partner_data): Añadido categorías de empresas, cargos de contactos, bancos. Eliminado provincias (ya está incluido en l10n_ES_toponyms)

---
+++ l10n_es_extras/l10n_ES_partner_data/__terp__.py
@@ -0,0 +1,22 @@
+# -*- encoding: utf-8 -*-
+{
+    "name" : "Datos iniciales para módulo base",
+    "version" : "1.0",
+    "author" : "Pablo Rocandio, Jordi Esteve (Zikzakmedia SL)",
+    "category" : "Localisation/Europe",
+    "depends" : [
+        "base",
+        ],
+    "init_xml" : [
+        "data/data_partner_events.xml",     # Canales y estados de ánimo
+        "data/data_partner_categories.xml", # Categorías de empresas
+        "data/data_partner_functions.xml",  # Cargos de contactos
+        "data/data_banks.xml",              # Bancos
+        ],
+    "demo_xml" : [],
+    "update_xml" :[],
+    "active": False,
+    "installable": True
+}
+
+
