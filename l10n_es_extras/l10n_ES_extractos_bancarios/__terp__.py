a846f2f38da32d13f11abfbe4865cfafa13e2525
Jordi Esteve <jesteve@alba>
2008-11-30 23:37:45 +0100

    Módulo l10n_ES_extractos_bancarios: Eliminación dependencia con l10n_ES_partner

--- l10n_es_extras/l10n_ES_extractos_bancarios/__terp__.py
+++ l10n_es_extras/l10n_ES_extractos_bancarios/__terp__.py
@@ -11,8 +11,10 @@ Permite definir las cuentas contables por defecto que se asociarán a los concep
 La búsqueda de la empresa se hace a partir de:
     1) La referencia2 del registro del extracto que acostumbra a ser la referencia de la operación que da la empresa. Se busca un apunte no conciliado con la misma referencia.
     2) El CIF/NIF que se encuentra en:
-      - Los 9 primeros caracteres de la referencia1 del registro del extracto (Banc Sabadell)
+      - Los 9 primeros caracteres de l['referencia1'] del registro del extracto (Banc Sabadell)
+      - Los 9 primeros caracteres de l['conceptos'] del registro del extracto (La Caixa)
       - Los caracteres [21:30] de l['conceptos'] (Caja Rural del Jalón)
+      - Si otros bancos o cajas guardan el CIF en otro lugar contactar con el autor/equipo de localización para poderlo añadir o implementar una forma flexible de definirlo.
     3) Búsqueda en los apuntes no conciliados por importe
 Si no se encuentra la empresa se asigna la cuenta contable que se haya definido por defecto para el concepto de ese registro.
 
@@ -20,8 +22,10 @@ Elimina el precálculo del importe de la línea del extracto bancario cuando se 
 """,
     "website" : "www.zikzakmedia.com",
     "license" : "GPL-2",
-    "depends" : ["base","account","l10n_ES_partner","l10n_chart_ES",],
-    "init_xml" : ['extractos_conceptos.xml',],
+    "depends" : ["base","account","l10n_chart_ES",],
+    "init_xml" : [
+        "extractos_conceptos.xml",
+        ],
     "demo_xml" : [],
     "update_xml" : [
         "extractos_view.xml",
