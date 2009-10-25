d46ffe4bb8a39f509aa7261ff772c105bfd3d3dc
Jordi Esteve <jesteve@alba>
2009-10-25 22:01:25 +0100

    [ADD] Módulo l10n_ES_account_balance_report: Añadidos informes balance_abreviado, balance_normal, pyg_abreviado y pyg_normal

--- l10n_es/l10n_ES_account_balance_report/__terp__.py
+++ l10n_es/l10n_ES_account_balance_report/__terp__.py
@@ -26,17 +26,24 @@
         "version" : "0.1",
         "author" : "Pexego, Zikzakmedia",
         "website" : "http://www.pexego.es",
-        "category" : "Enterprise Specific Modules",
+        "category" : "Localisation/Accounting",
         "description": """
 Spanish official account balance reports:
 
 - BALANCE PYMES (PGCE 2008)
 - CUENTA DE PÉRDIDAS Y GANANCIAS PYMES (PGCE 2008)
+- BALANCE ABREVIADO (PGCE 2008)
+- CUENTA DE PÉRDIDAS Y GANANCIAS ABREVIADO (PGCE 2008)
+- BALANCE NORMAL (PGCE 2008)
+- CUENTA DE PÉRDIDAS Y GANANCIAS NORMAL (PGCE 2008)
 
 Based on the annual accounts deposit models for the "Registro Mercantil":
+
 NORMAL: http://www.mjusticia.es/cs/Satellite?blobcol=urldocumento&blobheader=application%2Fpdf&blobkey=id&blobnocache=true&blobtable=Documento&blobwhere=1161679576351&ssbinary=true
-PYMES: http://www.mjusticia.es/cs/Satellite?blobcol=urldocumento&blobheader=application%2Fpdf&blobkey=id&blobnocache=true&blobtable=Documento&blobwhere=1161679576359&ssbinary=true
+
 ABREVIADO: http://www.mjusticia.es/cs/Satellite?blobcol=urldocumento&blobheader=application%2Fpdf&blobkey=id&blobnocache=true&blobtable=Documento&blobwhere=1161679576346&ssbinary=true
+
+PYMES: http://www.mjusticia.es/cs/Satellite?blobcol=urldocumento&blobheader=application%2Fpdf&blobkey=id&blobnocache=true&blobtable=Documento&blobwhere=1161679576359&ssbinary=true
             """,
         "depends" : [
                 'base',
@@ -50,7 +57,10 @@ ABREVIADO: http://www.mjusticia.es/cs/Satellite?blobcol=urldocumento&blobheader=
         "update_xml" : [
                 'data/balance_pymes.xml',
                 'data/pyg_pymes.xml',
-                #'data/balance_normal.xml',
+                'data/balance_abreviado.xml',
+                'data/pyg_abreviado.xml',
+                'data/balance_normal.xml',
+                'data/pyg_normal.xml',
             ],
         "installable": True
 }
