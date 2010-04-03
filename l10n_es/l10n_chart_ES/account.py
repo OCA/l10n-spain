b3d018de0b5ce1b0ab6c72a632cec6bf1c3640dd
Albert Cervera i Areny <albert@nan-tic.com>
2010-04-03 13:09:52 +0200

    [IMP] l10n_chart_ES: Mejorada la funcion 'search' para permitir la búsqueda de cuentas con expressiones del tipo '7.5' que buscará la cuenta con la expresión regular '^70+5$'. Esto facilita la búsqueda de cuentas sin necesidad de poner todos los ceros de la cuenta.

--- l10n_es/l10n_chart_ES/account.py
+++ l10n_es/l10n_chart_ES/account.py
@@ -8,6 +8,26 @@ entre PGCE 2008 y PGCE PYMES).
 
 from osv import fields, osv
 
+class account_account(osv.osv):
+    _inherit = "account.account"
+
+    def search(self, cr, uid, args, offset=0, limit=None, order=None, context=None, count=False):
+        args = args[:]
+        pos = 0
+        while pos < len(args):
+            if args[pos][0] == 'code' and args[pos][1] in ('like', 'ilike', '=like') and args[pos][2]:
+                query = args[pos][2].replace('%','')
+                if '.' in query:
+                    query = query.partition('.')
+                    cr.execute("SELECT id FROM account_account WHERE type <> 'view' AND code ~ ('^' || %s || '0+' || %s || '$')", (query[0], query[2]))
+                    ids = [x[0] for x in cr.fetchall()]
+                    if ids:
+                        args[pos] = ('id', 'in', ids)
+            pos += 1
+        return super(account_account,self).search(cr, uid, args, offset, limit, order, context, count)
+account_account()
+
+
 class account_account_template(osv.osv):
     """Extiende la plantillas de cuentas para añadir el nombre de plantilla"""
     _inherit = "account.account.template"
@@ -71,3 +91,5 @@ class account_fiscal_position_account_template(osv.osv):
         'template_name': fields.char('Template', size=32, select=True),
     }
 account_fiscal_position_account_template()
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
