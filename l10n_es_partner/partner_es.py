ff6902e9770695748cef7949a98a8e2cea614f0c
Jordi Esteve <jesteve@zikzakmedia.com>
2010-11-10 21:14:26 +0100

    [FIX+IMP] l10n_es_partner: Varios:
    [FIX] Renombrado de los archivos de traducción catalanas de ca_ES.po a ca.po
    [IMP] Eliminadas reglas de seguridad en modelos osv.osv_memory
    [IMP] Añadido website en la definición del módulo
    [IMP] Actualizado listado bancos y cajas españoles con los últimos datos disponibles del Banco de España: http://www.bde.es/webbde/es/secciones/servicio/regis/regent_excel.html
    [IMP] Improved the introduction of the VAT (uppercase conversion).
    [RFR] Cambio de licencia de GPL3 a AGPL3 en el resto de módulos de localización española, con el consentimiento de todos los autores expresado vía lista de correo del equipo de localización openerp-spain@googlegroups.com

--- l10n_es_partner/partner_es.py
+++ l10n_es_partner/partner_es.py
@@ -8,16 +8,16 @@
 #    $Id$
 #
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
+#    it under the terms of the GNU Affero General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+#    GNU Affero General Public License for more details.
 #
-#    You should have received a copy of the GNU General Public License
+#    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
@@ -110,6 +110,13 @@ class res_partner(osv.osv):
     _columns = {
         'comercial': fields.char('Trade name', size=128, select=True), # Nombre Comercial del Partner
     }
+
+    def vat_change(self, cr, uid, ids, value, context=None):
+        result = super(res_partner,self).vat_change(cr, uid, ids, value, context = context)
+        if value:
+            result['value']['vat'] = value.upper()
+        return result
+
 res_partner()
 
 
