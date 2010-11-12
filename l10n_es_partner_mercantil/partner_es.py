ad2332031218bbd5e4daa042363b529e0ae1c47d
Jordi Esteve <jesteve@zikzakmedia.com>
2010-11-12 13:51:14 +0100

    [IMP] Añadidos avisos NO ADAPTADO TODAVÍA A LA VERSIÓN 6.0 a varios módulos, limpieza l10n_es_partner_mercantil

--- l10n_es_partner_mercantil/partner_es.py
+++ l10n_es_partner_mercantil/partner_es.py
@@ -25,13 +25,13 @@ from osv import osv, fields
 class res_partner(osv.osv):
     _inherit = 'res.partner'
     _columns = {
-	'es_libro': fields.char('Book', size=128), #libro
-	'es_registro_mercantil': fields.char('Commercial Registry', size=128), # Registro Mercantil
-	'es_hoja': fields.char('Sheet', size=128), #hoja
-	'es_folio': fields.char('Page', size=128), #folio
-	'es_seccion': fields.char('Section', size=128),# seccion
-	'es_tomo': fields.char('Volume', size=128),# tomo
-	
+    	'es_libro': fields.char('Book', size=128), #libro
+	    'es_registro_mercantil': fields.char('Commercial Registry', size=128), # Registro Mercantil
+	    'es_hoja': fields.char('Sheet', size=128), #hoja
+	    'es_folio': fields.char('Page', size=128), #folio
+	    'es_seccion': fields.char('Section', size=128),# seccion
+	    'es_tomo': fields.char('Volume', size=128),# tomo
     }
+
 res_partner()
 
