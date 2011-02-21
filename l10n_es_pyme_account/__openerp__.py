40373a79cc7cbc2e0aab47312bf44670e2681332
Jordi Esteve <jesteve@zikzakmedia.com>
2011-02-21 21:36:41 +0100

    [REF] l10n_es_pyme_account: Refactorizado para funcionar con la versión 6.0. De momento se ha dejado comentadas las dependencias de módulos localización que todavía no han sido portados a la v 6.0.

--- l10n_es_pyme_account/__openerp__.py
+++ l10n_es_pyme_account/__openerp__.py
@@ -2,8 +2,9 @@
 ##############################################################################
 #
 #    OpenERP, Open Source Management Solution
-#    Copyright (c) 2009 Zikzakmedia S.L. (http://zikzakmedia.com) All Rights Reserved.
+#    Copyright (c) 2011 Zikzakmedia S.L. (http://zikzakmedia.com) All Rights Reserved.
 #                       Jordi Esteve <jesteve@zikzakmedia.com>
+#                       Jesús Martín <jmartin@zikzakmedia.com>
 #    $Id$
 #
 #    This program is free software: you can redistribute it and/or modify
@@ -27,24 +28,42 @@
     "author" : "Zikzakmedia SL",
     "category" : "Generic Modules/Others",
     "website": "www.zikzakmedia.com",
-    "description": """NOTA: NO ADAPTADO TODAVÍA A LA VERSIÓN 6.0
+    "description": """
 Instalación de los módulos contables de OpenERP para una PYME estándar incluyendo los módulos de localización española y algunos datos de configuración iniciales.
 
 Instala los módulos habituales para la gestión contable de una PYME del Estado Español: contabilidad y facturación, pagos, remesas de recibos, plan contable 2008, topónimos, datos de bancos españoles y su validación, importación de extractos bancarios, cierre de ejercicio, informes contables, ...
 
 Cuando se ejecute el asistente de configuración de contabilidad (account) deberá omitir el paso, pués la selección del plan contable y la creación de ejercicios y períodos fiscales lo realiza este módulo de forma automática.
 
-Después de instalar este módulo y todas sus dependencias, deberá crear los topónimos del Estado Español (crear las provincias mediante el asistente que se ejecuta automáticamente) y las cuentas contables a partir de la plantilla (mediante el menú "Gestión financiera/Configuración/Contabilidad financiera/Plantillas/Generar plan contable a partir de una plantilla de plan contable").
+Después de instalar este módulo y todas sus dependencias, deberá crear los topónimos del Estado Español (crear las provincias mediante el asistente que se ejecuta automáticamente) y las cuentas contables a partir de la plantilla de plan contable (con el asistente que se ejecuta automáticamente o mediante el menú "Contabilidad/Configuración/Contabilidad financiera/Configuración financiera para nueva compañía").
 
-Posteriormente podrá crear los bancos españoles mediante el menú "Empresas/Configuración/Bancos/Asistente de importación de todos los bancos del Estado Español" y los conceptos de extractos bancarios mediante el menú "Gestión financiera/Configuración/Extractos bancarios C43/Asistente de importación de conceptos de extractos".
+Posteriormente podrá crear los bancos españoles mediante el menú "Ventas/Configuración/Libreta de direcciones/Bancos/Asistente de importación de todos los bancos del Estado Español" y los conceptos de extractos bancarios mediante el menú "Contabilidad/Configuración/Varios/Extractos bancarios C43/Asistente de importación de conceptos de extractos".
 """,
     "license" : "GPL-3",
-    "depends" : ["base", "account", "account_payment", "account_payment_extension", "account_renumber", "account_financial_report", "l10n_es", "l10n_es_toponyms", "l10n_es_partner", "l10n_es_partner_seq", "l10n_es_payment_extension", "l10n_es_bank_statement", "l10n_es_fiscal_year_closing", "l10n_es_account_balance_report", "l10n_es_aeat_mod347"],
-    "init_xml" : ["pyme_data.xml"],
+    "depends" : [
+        "account",
+        "account_financial_report",
+        "account_payment",
+        "account_payment_extension",
+#        "account_renumber",
+        "l10n_es",
+        "l10n_es_account",
+        "l10n_es_account_balance_report",
+#        "l10n_es_aeat_mod347",
+#        "l10n_es_bank_statement",
+#        "l10n_es_fiscal_year_closing",
+        "l10n_es_partner",
+        "l10n_es_partner_seq",
+        "l10n_es_payment_order",
+        "l10n_es_toponyms",
+    ],
+    "init_xml" : [
+        "pyme_data.xml",
+    ],
     "demo_xml" : [],
     "update_xml" : [
     ],
     "active": False,
     "installable": True
 }
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+
