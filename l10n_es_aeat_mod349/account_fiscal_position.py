1de3691eb1e73ca58cbbd6528c02b2cbd769fde5
Omar (pexego) <omar@pexego.es>
2011-03-04 18:52:36 +0100

    [ADD] l10n_es_aeat, l10n_es_aeat_347, l10n_es_aeat_349: Adds a generic module for aeat models, 347 module was portedto v6.0 and adds new module to print AEAT model 349.

---
+++ l10n_es_aeat_mod349/account_fiscal_position.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2004-2011
+#        Pexego Sistemas Informáticos. (http://pexego.es) All Rights Reserved
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+__author__ = "Luis Manuel Angueira Blanco (Pexego)"
+
+
+from osv import osv, fields
+
+
+class account_fiscal_position(osv.osv):
+    """
+    Inheritance of Account fiscal position to add field 'include_in_mod349'.
+    This fields let us map fiscal position, taxes and accounts to create an AEAT 349 Report
+    """
+    _inherit = 'account.fiscal.position'
+
+    _columns = {
+        'intracommunity_operations' : fields.boolean('Intra-Community operations'),
+    }
+
+account_fiscal_position()
