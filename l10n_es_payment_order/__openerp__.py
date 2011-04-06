512b2ebd4965f47bba3a259b6e0fde781100ec50
Jordi Esteve <jesteve@zikzakmedia.com>
2011-04-06 14:57:54 +0200

    [IMP] l10n_es_payment_order: * Añadidas las formas de pago mediante pagaré, cheques y pago certificado de la norma CSB 34, así como mejorada la descripción del módulo. * Mejora de la exportación de la norma 34 añadiendo los tipos de datos 012, 013, 014, 015, 018, 101, 102, 103, 910 (Pagarés y cheques con y sin carta). * En los pagos con carta: Añadido los tipos de dato opcionales 018 (incluye el CIF para cheques nómina), 101, 102, 103 (líneas de carta) y 910 (incluye la fecha). Nota: Se mantiene compatibilidad con la exportación CSB 34 anterior.

--- l10n_es_payment_order/__openerp__.py
+++ l10n_es_payment_order/__openerp__.py
@@ -24,24 +24,24 @@
 ##############################################################################
 
 {
-    "name" : "Remesas de recibos CSB 19 y CSB 58",
+    "name" : "Exportación de ficheros bancarios CSB 19, CSB 32, CSB 34 y CSB 58",
     "version" : "1.5",
     "author" : "Acysos SL, Zikzakmedia SL, Pablo Rocandio, NaN",
     "category" : "Localisation/Accounting",
-    "description" : """Módulo para la exportación de remesas de recibos según las normas CSB 19 (recibos domiciliados), CSB 58 (anticipos de créditos) y CSB 34 (emisión de transferencias) para poder ser enviados a la entidad bancaria.
+    "description" : """Módulo para la exportación de ficheros bancarios según las normas CSB 19 (recibos domiciliados), CBS 32 (descuento comercial), CSB 58 (anticipos de créditos) y CSB 34 (emisión de transferencias, nóminas, cheques, pagarés y pagos certificados) para poder ser enviados a la entidad bancaria.
 
 Crea un tipo de pago "Recibo domiciliado" con el código RECIBO_CSB. Este código es importante pues permite ejecutar el asistente de creación del fichero de remesas cuando se presiona el botón "Realizar pagos" en la orden de pagos o remesa.
 
-También crea el tipo de pago "Transferencia" con el código TRANSFERENCIA_CSB.
+También crea los tipos de pago "Transferencia" (TRANSFERENCIA_CSB), "Pagaré" (PAGARE_CSB), "Cheque" (CHEQUE_CSB), "Pago certificado" (PAGO_CERTIFICADO_CSB).
 
-Antes de generar un fichero de remesas habrá que definir un modo de pago que use el tipo de pago anterior y donde se defina la forma de pago (CSB 19, CSB 58 o CSB 34), la compañía que emite el fichero y el sufijo y nombre de compañia a incluir en el fichero (lo que en la versión 4.2 se definía como la cuenta de remesas).
+Antes de generar un fichero bancario CSB habrá que definir un modo de pago que use el tipo de pago anterior y donde se defina la forma de pago (CSB 19, CSB 32, CSB 34 o CSB 58), la compañía que emite el fichero y el sufijo y nombre de compañia a incluir en el fichero.
 
-Al crear el fichero de remesa:
+Al crear el fichero bancario CSB:
 
   * Se pueden agrupar o no los pagos de una misma empresa y cuenta bancaria
   * El fichero creado se guarda como adjunto de la orden de pagos. Se puede volver a crear el fichero de remesas siempre que sea necesario (puede tener que renombrar el anterior fichero adjunto si tienen el mismo nombre).
 
-También se proporciona un informe para imprimir un listado de los recibos de la remesa.
+También se proporciona un informe para imprimir un listado de los pagos/cobros de la orden de pago/cobro (remesa).
 """,
     "website" : "www.zikzakmedia.com",
     "license" : "GPL-3",
