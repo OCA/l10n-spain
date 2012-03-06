0d7c1801297989081307013eb1e6a12e46ceb511
Albert Cervera i Areny <albert@nan-tic.com>
2012-03-06 20:54:35 +0100

    l10n_es_aeat_mod347: Actualizado el módulo para realizar el nuevo modelo con el detalle de los trimestes obligatorio para la declaración del 2011.

--- l10n_es_aeat_mod347/wizard/calculate_mod347_records.py
+++ l10n_es_aeat_mod347/wizard/calculate_mod347_records.py
@@ -3,6 +3,8 @@
 #
 #    Copyright (C) 2004-2011
 #        Pexego Sistemas Informáticos. (http://pexego.es) All Rights Reserved
+#    Copyright (C) 2012
+#        NaN·tic  (http://www.nan-tic.com) All Rights Reserved
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
