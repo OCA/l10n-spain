a7c7ae9fd8668320622f2cb412a716f27ea9c910
Ana Juaristi <ajuaristio@gmail.com>
2012-03-09 17:21:34 +0100

    [ADD] l10n_es_account_asset, l10n_es_prev_tesoreria: Incluidos modulos prevision tesoreria y activos/amortizaciones.

---
+++ l10n_es_prev_tesoreria/__openerp__.py
@@ -0,0 +1,46 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Avanzosc - Avanced Open Source Consulting
+#    Copyright (C) 2010 - 2011 Avanzosc <http://www.avanzosc.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see http://www.gnu.org/licenses/.
+#
+##############################################################################
+
+{
+    "name": "Previsión de Tesorería",
+    "version": "1.0",
+    "depends": [
+                "account",
+                "account_payment",
+                ],
+    "author": "Avanzosc S.L.",
+    "website": "http://www.avanzosc.com",
+    "category": "Accounting",
+    "description": """
+    Este modulo contiene :
+        Formularios para realizar las previsiones de tesoreria
+    """,
+    "init_xml": [],
+    'update_xml': [
+                   "wizard/wiz_crear_factura_view.xml",
+                   "prev_tesoreria_view.xml",
+                   "prev_tesoreria_plantilla_view.xml",
+                   ],
+    'demo_xml': [],
+    'installable': True,
+    'active': False,
+#    'certificate': 'certificate',
+}
