aacdeba6cf49cfd56e7e0ad33b2635278afe1ae5
Ignacio Ibeas - Acysos S.L <ignacio@acysos.com>
2012-04-25 12:09:21 +0200

    [FIX] l10n_es_aeat_mod347: Exclusión del IRPF del total de las facturas. Necesario para las Sociedades Irregulares, Sociedades Civiles y Comunidad de Bienes sin regimen de módulos en el IRPF. Al resto de las empresas no les afecta ya que no facturan con IRPF. l10n_es_aeat_mod340: Correción de los autores en el modelo 340

--- l10n_es_aeat_mod347/__openerp__.py
+++ l10n_es_aeat_mod347/__openerp__.py
@@ -22,9 +22,9 @@
 {
     "name" : "AEAT Model 347",
     "version" : "1.0",
-    "author" : "Pexego, ASR-OSS, NaN·tic",
+    "author" : "Pexego, ASR-OSS, NaN·tic, Acysos",
     "license" : "AGPL-3",
-    "website" : "http://www.pexego.es, www.asr-oss.com, www.nan-tic.com",
+    "website" : "http://www.pexego.es, www.asr-oss.com, www.nan-tic.com, www.acysos.com",
     "category" : "Localisation/Accounting",
     "description" : """
     Módulo para la presentación del Modelo AEAT 347 (Declaración Anual de Operaciones con Terceros)
@@ -32,14 +32,21 @@
 Basado en la Orden EHA/3012/2008, de 20 de Octubre, por el que se aprueban los diseños físicos y lógicos del 347.
 
 De acuerdo con la normativa de la Hacienda Española, están obligados a presentar el modelo 347:
-    * Todas aquellas personas físicas o jurídicas, de naturaleza pública o
-        privada que desarrollen actividades empresariales o profesionales,
-        siempre y cuando hayan realizado operaciones que, en su conjunto,
-        respecto de otra persona o Entidad, cualquiera que sea su naturaleza
-        o carácter, hayan superado la cifra de 3.005,06€ durante el año natural
-        al que se refiere la declaración. Para el cálculo de la cifra de
-        3.005,06 € se computan de forma separada las entregas de bienes
-        y servicios y las adquisiciones de los mismos.
+    * Todas aquellas personas físicas o jurídicas que no esten acogidas al regimen
+        de módulos en el IRPF, de naturaleza pública o privada que desarrollen
+        actividades empresariales o profesionales, siempre y cuando hayan
+        realizado operaciones que, en su conjunto, respecto de otra persona
+        o Entidad, cualquiera que sea su naturaleza o carácter, hayan superado
+        la cifra de 3.005,06€ durante el año natural al que se refiere la
+        declaración. Para el cálculo de la cifra de 3.005,06 € se computan de
+        forma separada las entregas de biene y servicios y las adquisiciones
+        de los mismos.
+    * En el caso de Sociedades Irregulares, Sociedades Civiles y Comunidad de Bienes
+        no acogidas el regimen de módulos en el IRPF, deben incluir las facturas sin
+        incluir la cuantía del IRPF.
+    * En el caso de facturas de proveedor con IRPF, no deben ser presentadas en este
+        modelo. Se presentan en el modelo 190. Desactivar en la ficha del proveedor
+        la opción de "Incluir en el informe 347".
 
 De acuerdo con la normativa no están obligados a presentar el modelo 347:
     * Quienes realicen en España actividades empresariales o profesionales sin
