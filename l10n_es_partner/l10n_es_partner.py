df2a8eed181fade61e4e9cc7e42b7182cbabf76a
Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
2013-06-24 15:19:01 +0200

    l10n_es_partner: [IMP] Refactorización de código [IMP] Asistente de importación puesto como res.config.installer que se ejecuta al instalar el módulo. [IMP] Limpieza del archivo XML de bancos. [FIX] Adaptación de las vistas a la versión 7.0.

--- l10n_es_partner/l10n_es_partner.py
+++ l10n_es_partner/l10n_es_partner.py
@@ -24,90 +24,86 @@
 #
 ##############################################################################
 
-from osv import osv, fields
+from osv import orm, fields
 from tools.translate import _
 import tools
 import os
 
-def _CRC(cTexto):
-    """Calculo el CRC de un número de 10 dígitos
-    ajustados con ceros por la izquierda"""
-    factor=(1,2,4,8,5,10,9,7,3,6)
-    # Cálculo CRC
-    nCRC=0
-    for n in range(10):
-        nCRC += int(cTexto[n])*factor[n]
-    # Reducción del CRC a un dígito
-    nValor=11 - nCRC%11
-    if nValor==10: nValor=1
-    elif nValor==11: nValor=0
-    return nValor
-
-def CalcCC(cBanco,cSucursal,cCuenta):
-    """Cálculo del Código de Control Bancario"""
-    cTexto="00%04d%04d" % (int(cBanco),int(cSucursal))
-    DC1=_CRC(cTexto)
-    cTexto="%010d" % long(cCuenta)
-    DC2=_CRC(cTexto)
-    return "%1d%1d" % (DC1,DC2)
-
-def checkBankAccount(account):
-    number = ""
-    for i in account:
-        if i.isdigit():
-            number += i
-    if len(number) != 20:
-        return 'invalid-size'
-    bank = number[0:4]
-    office = number[4:8]
-    dc = number[8:10]
-    account = number[10:20]
-    if dc != CalcCC(bank, office, account):
-        return 'invalid-dc'
-    return '%s %s %s %s' % (bank, office, dc, account)
-
-class res_partner_bank(osv.osv):
+class res_partner_bank(orm.Model):
     _inherit = 'res.partner.bank'
     _columns = {
         'acc_country_id': fields.many2one("res.country", 'Bank country', help="If the country of the bank is Spain, it validates the bank code. It only reads the digit characters of the bank code:\n- If the number of digits is 18, computes the two digits of control.\n- If the number of digits is 20, computes the two digits of control and ignores the current ones.\n- If the number of digits is different from 18 or 20, it leaves the bank code unaltered.\nThe result is shown in the '1234 5678 06 1234567890' format."),
     }
+
+    def _crc(self, cTexto):
+        """Calculo el CRC de un número de 10 dígitos
+        ajustados con ceros por la izquierda"""
+        factor = (1, 2, 4, 8, 5, 10, 9, 7, 3, 6)
+        # Cálculo CRC
+        nCRC = 0
+        for n in range(10):
+            nCRC += int(cTexto[n]) * factor[n]
+        # Reducción del CRC a un dígi9to
+        nValor = 11 - nCRC % 11
+        if nValor == 10: 
+            nValor = 1
+        elif nValor == 11: 
+            nValor = 0
+        return nValor
+    
+    def calcCC(self, cBanco, cSucursal, cCuenta):
+        """Cálculo del código de control bancario"""
+        cTexto = "00%04d%04d" % (int(cBanco), int(cSucursal))
+        dc1 = self._crc(cTexto)
+        cTexto = "%010d" % long(cCuenta)
+        dc2 = self._crc(cTexto)
+        return "%1d%1d" % (dc1, dc2)
+    
+    def checkBankAccount(self, account):
+        number = ""
+        for i in account:
+            if i.isdigit():
+                number += i
+        if len(number) != 20:
+            return 'invalid-size'
+        bank = number[0:4]
+        office = number[4:8]
+        dc = number[8:10]
+        account = number[10:20]
+        if dc != self.calcCC(bank, office, account):
+            return 'invalid-dc'
+        return '%s %s %s %s' % (bank, office, dc, account)
+    
     def onchange_banco(self, cr, uid, ids, account, country_id, context):
-        # No se por qué motivo, al añadir un nuevo banco, en ocasiones
-        # la función onchange_banco se ejecuta con el valor account=False
-        # dando el error: TypeError: 'bool' object is not iterable
-        # El problema se resuelve con las tres siguientes líneas
-        if type(account) <> str or type(country_id) <> int:
-            return {'value':{}}
-        country = self.pool.get('res.country').browse(cr, uid, country_id, context)
-        if country.code.upper() in ('ES', 'CT'):
-            number = checkBankAccount( account )
-            if number == 'invalid-size':
-                        return { 'warning': { 'title': _('Warning'), 'message': _('Bank account should have 20 digits.') } }
-            if number == 'invalid-dc':
-                return { 'warning': { 'title': _('Warning'), 'message': _('Invalid bank account.') } }
-
-            bank_ids = self.pool.get('res.bank').search(cr, uid, [('code','=',number[:4])], context=context)
-            if bank_ids:
-                return {'value':{'acc_number': number, 'bank': bank_ids[0]}}
-            else:
-                return {'value':{'acc_number': number}}
+        if account and country_id:
+            country = self.pool.get('res.country').browse(cr, uid, country_id, context)
+            if country.code.upper() == 'ES':
+                number = self.checkBankAccount(account)
+                if number == 'invalid-size':
+                    return { 'warning': { 'title': _('Warning'), 'message': _('Bank account should have 20 digits.') } }
+                if number == 'invalid-dc':
+                    return { 'warning': { 'title': _('Warning'), 'message': _('Invalid bank account.') } }
+    
+                bank_ids = self.pool.get('res.bank').search(cr, uid, [('code', '=', number[:4])], context=context)
+                if bank_ids:
+                    return {'value':{'acc_number': number, 'bank': bank_ids[0]}}
+                else:
+                    return {'value':{'acc_number': number}}
         return {'value':{}}
-res_partner_bank()
 
 
-class Bank(osv.osv):
+class res_bank(orm.Model):
     _inherit = 'res.bank'
     _columns = {
         'code': fields.char('Code', size=64),
         'lname': fields.char('Long name', size=128),
-        'vat': fields.char('VAT code',size=32 ,help="Value Added Tax number"),
-        'website': fields.char('Website',size=64),
+        'vat': fields.char('VAT code', size=32, help="Value Added Tax number"),
+        'website': fields.char('Website', size=64),
         'code': fields.char('Code', size=64),
     }
-Bank()
 
 
-class res_partner(osv.osv):
+class res_partner(orm.Model):
     _inherit = 'res.partner'
     _columns = {
         'comercial': fields.char('Trade name', size=128, select=True), # Nombre Comercial del Partner
@@ -126,28 +122,11 @@ class res_partner(osv.osv):
             ids = self.search(cr, uid, [('comercial', operator, name)] + args,
                                                 limit=limit, context=context)
         return self.name_get(cr, uid, ids, context)
-      
 
     def vat_change(self, cr, uid, ids, value, context=None):
         result = super(res_partner,self).vat_change(cr, uid, ids, value, context = context)
         if value:
             result['value']['vat'] = value.upper()
         return result
-
-res_partner()
-
-
-class l10n_es_partner_import_wizard(osv.osv_memory):
-    _name = 'l10n.es.partner.import.wizard'
-
-    def action_import(self, cr, uid, ids, context=None):
-        try:
-            fp = tools.file_open(os.path.join('l10n_es_partner', 'data_banks.xml'))
-        except IOError, e:
-            return {}
-        idref = {}
-        tools.convert_xml_import(cr, 'l10n_es_partner', fp,  idref, 'init', noupdate=True)
-        return {}
-l10n_es_partner_import_wizard()
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
