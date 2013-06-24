ae0957567990f86dbdfc1216fd042d3bbed8d9aa
Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
2013-06-24 15:19:01 +0200

    [FIX+IMP] l10n_es_partner: Several things:
    [IMP] Refactorización de código
    [IMP] Asistente de importación puesto como res.config.installer que se ejecuta al instalar el módulo.
    [IMP] Limpieza del archivo XML de bancos.
    [FIX] Adaptación de las vistas a la versión 7.0.
    [IMP] PEP8 + mejoras en la herencia de vistas
    
    Launchpad automatic translations update.

--- l10n_es_partner/wizard/__init__.py
+++ l10n_es_partner/wizard/__init__.py
@@ -2,9 +2,6 @@
 ##############################################################################
 #
 #    OpenERP, Open Source Management Solution
-#    Copyright (c) 2008 Spanish Localization Team
-#    Copyright (c) 2009 Zikzakmedia S.L. (http://zikzakmedia.com) All Rights Reserved.
-#                       Jordi Esteve <jesteve@zikzakmedia.com>
 #    Copyright (c) 2013 Serv. Tecnol. Avanzados (http://www.serviciosbaeza.com)
 #                       Pedro Manuel Baeza <pedro.baeza@serviciosbaeza.com>
 #    $Id$
@@ -24,6 +21,5 @@
 #
 ##############################################################################
 
-import l10n_es_partner
-import wizard
+import l10n_es_partner_wizard
 
