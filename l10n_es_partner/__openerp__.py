f35ad1fd960552284a003bed32fc42cfb225b251
Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
2013-06-24 15:19:01 +0200

    l10n_es_partner: [IMP] Refactorización de código [IMP] Asistente de importación puesto como res.config.installer que se ejecuta al instalar el módulo. [IMP] Limpieza del archivo XML de bancos. [FIX] Adaptación de las vistas a la versión 7.0.
    Launchpad automatic translations update.

--- l10n_es_partner/__openerp__.py
+++ l10n_es_partner/__openerp__.py
@@ -7,6 +7,8 @@
 #                       Jordi Esteve <jesteve@zikzakmedia.com>
 #    Copyright (c) 2013 Acysos S.L. (http://acysos.com) All Rights Reserved.
 #                       Ignacio Ibeas <ignacio@acysos.com>
+#    Copyright (c) 2013 Servicios Tecnológicos Avanzados (http://www.serviciosbaeza.com)
+#                       Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
 #    $Id$
 #
 #    This program is free software: you can redistribute it and/or modify
@@ -25,27 +27,25 @@
 ##############################################################################
 
 {
-    "name" : "Adaptación de partner para Estado Español",
+    "name" : "Adaptación de los terceros para España",
     "version" : "1.0",
-    "author" : "Spanish Localization Team",
+    "author" : "Spanish localization team",
     "website" : "https://launchpad.net/openerp-spain",
     "category" : "Localisation/Europe",
-    "description": """Funcionalidades:
-    * Añade el campo *Nombre Comercial* a las empresas
-    * Añades campos nombre largo, CIF y web a los bancos
-    * Añade datos de 191 bancos y cajas españolas extraídos del registro oficial del Banco de España
-    * Permite validar las cuentas bancarias, para ello añade un campo de país a los bancos de las empresas
+    "description": """
+Funcionalidades:
+    * Añade el campo *Nombre comercial* a las empresas y permite buscar por él.
+    * Convierte el NIF a mayúsculas.
+    * Añade los campos nombre largo, NIF y web a los bancos.
+    * Añade datos de 191 bancos y cajas españolas extraídos del registro oficial del Banco de España.
+    * Permite validar las cuentas bancarias, para ello añade un campo de país a los bancos de las empresas.
 
 Funcionamiento de la validación de la cuenta bancaria:
     * Se descartan todos los caracteres que no sean dígitos del campo número de cuenta.
-    * Si los dígitos son 18 calcula los dos dígitos de control
-    * Si los dígitos son 20 calcula los dos dígitos de control e ignora los actuales
-        Presenta el resultado con el formato "1234 5678 06 1234567890"
-    * Si el número de dígitos es diferente de 18 0 20 deja el valor inalterado
-NOTA
-Se ha eliminado la validación de CIF/NIF españoles, pues el módulo base_vat de OpenERP 5.0 añade un campo CIF/NIF en la pestaña de contabilidad de las empresas y la validación automática de los CIF de 27 paises europeos. Los CIFs deben introducirse añadiendo al principio los 2 caracteres que identifican cada país en mayúsculas (ES para España), por ejemplo ESB64425879
-
-NOTA: Éste módulo añade un asistente en Empresas/Configuración/Bancos para la importación de todos los bancos y cajas de España. Antes de ejecutar éste asistente deberá tener importadas las provincias disponibles en el módulo l10n_es_toponyms.
+    * Si los dígitos son 18, calcula los dos dígitos de control.
+    * Si los dígitos son 20, calcula los dos dígitos de control e ignora los actuales.
+    * Presenta el resultado con el formato "1234 5678 06 1234567890"
+    * Si el número de dígitos es diferente de 18 0 20, deja el valor inalterado.
 """,
     "license" : "AGPL-3",
     "depends" : [
@@ -53,14 +53,11 @@ NOTA: Éste módulo añade un asistente en Empresas/Configuración/Bancos para l
         "base_iban",
         "l10n_es_toponyms",
         ],
-    "init_xml" : [],
-    "update_xml" : [
-        "partner_es_view.xml",
+    "data" : [
+        "l10n_es_partner_view.xml",
+        "wizard/l10n_es_partner_wizard.xml"
         ],
     "active": False,
     "installable": True
 }
-
-
-
 
