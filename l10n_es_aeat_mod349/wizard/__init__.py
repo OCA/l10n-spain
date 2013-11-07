db964a91d60337443fb740f4f7743176c7f3a61f
unknown <infoweb@topconsultant.es>
2013-11-07 16:36:14 +0100

    [REF] cambiados los osv por orm.Model y orm.TransientModel, añadido en os import de objetos de OpenERP, el namespace openerp, refactorizado a PEP-8 y adaptacion vistas a formato 7

--- l10n_es_aeat_mod349/wizard/__init__.py
+++ l10n_es_aeat_mod349/wizard/__init__.py
@@ -4,6 +4,9 @@
 #    Copyright (C) 2004-2011
 #        Pexego Sistemas Informáticos. (http://pexego.es) All Rights Reserved
 #
+#    Migración OpenERP 7.0. Top Consultant Software Creations S.L. (http://www.topconsultant.es/) 2013
+#        Ignacio Martínez y Miguel López.
+#
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
 #    published by the Free Software Foundation, either version 3 of the
@@ -21,5 +24,5 @@
 
 __author__ = "Luis Manuel Angueira Blanco (Pexego)"
 
-import calculate_mod349_records
-import export_mod349_to_boe
+from . import calculate_mod349_records
+from . import export_mod349_to_boe
