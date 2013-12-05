9ebdcdf19750caf9d76c3b4206986eeb62cb9d60
Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
2013-12-05 15:50:56 +0100

    [FIX] l10n_es_toponyms: 3 cosas:
    * Refactorización PEP8 y convenciones v7
    * Arreglado generador de códigos postales a partir de archivo de www.codigospostales.com.
    * Quitado atributo type para quitar warning.

--- l10n_es_toponyms/wizard/l10n_es_toponyms_wizard.py
+++ l10n_es_toponyms/wizard/l10n_es_toponyms_wizard.py
@@ -4,9 +4,8 @@
 #    OpenERP, Open Source Management Solution
 #    Copyright (c) 2009 Zikzakmedia S.L. (http://zikzakmedia.com) All Rights Reserved.
 #                       Jordi Esteve <jesteve@zikzakmedia.com>
-#    Copyright (c) 2013 Serv. Tecnol. Avanzados (http://www.serviciosbaeza.com) All Rights Reserved.
+#    Copyright (c) 2013 Serv. Tecnol. Avanzados (http://www.serviciosbaeza.com)
 #                       Pedro Manuel Baeza <pedro.baeza@serviciosbaeza.com>
-#    $Id$
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
@@ -22,62 +21,65 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from osv import fields,osv
 import os
-import pooler
-import tools
+import openerp.tools
+from openerp.osv import fields, orm
 
-class config_es_toponyms(osv.osv_memory):
+class config_es_toponyms(orm.TransientModel):
     _name='config.es.toponyms'
     _inherit = 'res.config.installer'
 
     _columns = {
         'name':fields.char('Name', size=64),
-        'state': fields.selection([('official','Official'),('spanish','Spanish'),('both','Both')], 'State names', required=True),
-        'city_info': fields.selection([('yes','Yes'),('no','No')], 'City information', required=True),
+        'state': fields.selection([('official', 'Official'),
+                                   ('spanish', 'Spanish'),
+                                   ('both', 'Both')], 'State names',
+                                  required=True),
+        'city_info': fields.selection([('yes', 'Yes'), ('no','No')],
+                                      'City information', required=True),
     }
 
     _defaults={
-        'state': lambda *args: 'official',
-        'city_info': lambda *args: 'yes',
+        'state': 'official',
+        'city_info': 'yes',
     }
     
     def create_states(self, cr, uid, state_type, context=None):
-		"""Import spanish states information through an XML file."""
-		file_name = 'l10n_es_toponyms_states_%s.xml' %state_type
-		try:
-			fp = tools.file_open(os.path.join('l10n_es_toponyms', os.path.join('wizard', file_name)))
-		except IOError, e:
-			fp = None
-		if fp:
-			idref = {}
-			tools.convert_xml_import(cr, 'l10n_es_toponyms', fp,  idref, 'init', noupdate=True)
-			cr.commit()
-			return True
-		return False
-	
-    def create_zipcodes(self, cr, uid, context=None):
-        """Import spanish zipcodes information through an XML file."""
-        file_name = 'l10n_es_toponyms_zipcodes.xml'
+        """Import spanish states information through an XML file."""
+        file_name = 'l10n_es_toponyms_states_%s.xml' %state_type
         try:
-            fp = tools.file_open(os.path.join('l10n_es_toponyms', os.path.join('wizard', file_name)))
+            fp = openerp.tools.file_open(os.path.join('l10n_es_toponyms',
+                                            os.path.join('wizard', file_name)))
         except IOError, e:
             fp = None
         if fp:
             idref = {}
-            tools.convert_xml_import(cr, 'l10n_es_toponyms', fp,  idref, 'init', noupdate=True)
-            cr.commit()
+            openerp.tools.convert_xml_import(cr, 'l10n_es_toponyms', fp, idref,
+                                             'init', noupdate=True)
+            return True
+        return False
+
+    def create_zipcodes(self, cr, uid, context=None):
+        """Import spanish zipcodes information through an XML file."""
+        file_name = 'l10n_es_toponyms_zipcodes.xml'
+        try:
+            fp = openerp.tools.file_open(os.path.join('l10n_es_toponyms',
+                                            os.path.join('wizard', file_name)))
+        except IOError, e:
+            fp = None
+        if fp:
+            idref = {}
+            openerp.tools.convert_xml_import(cr, 'l10n_es_toponyms', fp, idref,
+                                             'init', noupdate=True)
             return True
         return False
 
     def execute(self, cr, uid, ids, context=None):
-		if context is None: context = {}
-		super(config_es_toponyms, self).execute(cr, uid, ids, context=context)
-		res = self.read(cr, uid, ids)[0]
-		# Import spanish states (official, Spanish or both)
-		self.create_states(cr, uid, res['state'], context=context)
-		# Import spanish cities and zip codes
-		if res['city_info'] == 'yes':
-			self.create_zipcodes(cr, uid, context=context)
-
-config_es_toponyms()
+        if context is None: context = {}
+        super(config_es_toponyms, self).execute(cr, uid, ids, context=context)
+        res = self.read(cr, uid, ids)[0]
+        # Import spanish states (official, Spanish or both)
+        self.create_states(cr, uid, res['state'], context=context)
+        # Import spanish cities and zip codes
+        if res['city_info'] == 'yes':
+            self.create_zipcodes(cr, uid, context=context)
