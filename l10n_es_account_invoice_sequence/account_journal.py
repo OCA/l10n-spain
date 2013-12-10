6c0b922718deea9d5d25af969add42ce4ef50491
Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
2013-12-10 10:58:01 +0100

    [FIX] l10n_es_account_invoice_sequence: Restaurada eliminación del contenido del campo internal_number. [IMP] l10n_es_account_invoice_sequence: PEP8 y convenciones v7.

--- l10n_es_account_invoice_sequence/account_journal.py
+++ l10n_es_account_invoice_sequence/account_journal.py
@@ -20,12 +20,9 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
+from openerp.osv import orm, fields
 
-from osv import osv
-from osv import fields
-from tools.translate import _
-
-class account_journal(osv.osv):
+class account_journal(orm.Model):
     _inherit = 'account.journal'
 
     _columns = {
