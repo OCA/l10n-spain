c3760b9ef31bc7bee5484c8dc14c083865c4d30e
Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
2014-01-17 02:03:03 +0100

    [IMP] l10n_es_aeat: Algo de PEP8 y refactorización de código [MIG] l10n_es_aeat_mod303: Migración a v7 del módulo con las siguientes mejoras: - Convenciones de la comunidad. - PEP8 en la mayoría de código. - Icono para el módulo. - Adaptación de vistas a las posibilidades de la v7. - Restringidas las cuentas bancarias a las de la compañía.

---
+++ l10n_es_aeat_mod303/__openerp__.py
@@ -0,0 +1,46 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution    
+#    Copyright (c) All rights reserved:
+#       2013      Guadaltech (http://www.guadaltech.es)
+#                 Alberto Martín Cortada
+#       2014      Serv. Tecnol. Avanzados (http://www.serviciosbaeza.com)
+#                 Pedro M. Baeza <pedro.baeza@serviciobaeza.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see http://www.gnu.org/licenses/.
+#
+##############################################################################
+
+{
+    "name" : "AEAT modelo 303",
+    "version" : "1.0",
+    "author" : "GuadalTech",
+    "license" : "AGPL-3",
+    "website" : "http://www.guadaltech.es",
+    'contributors': ["Pedro M. Baeza <pedro.baeza@serviciosbaeza.com"],
+    "category" : "Localisation/Accounting",
+    "description" : """
+Módulo para la presentación del modelo 303. Exportación a formato AEAT.
+        """,
+    "depends" : [
+        "l10n_es_aeat",
+    ],
+    "data" : [
+        "mod303_view.xml",
+        "mod303_workflow.xml",
+        "security/ir.model.access.csv",
+    ],
+    "installable" : True,
+}
