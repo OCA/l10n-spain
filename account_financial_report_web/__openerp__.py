b962c9e166fec834331fd0ef534cad5cd889a5bb
Alberto Garcia <alberto.garcia@factorlibre.com>
2014-02-28 14:09:11 +0100

    Libro Mayor Excel para openerp 6.0

---
+++ account_financial_report_web/__openerp__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2013 Factor Libre.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    "name": "Account Financial Report Web",
+    "version": "0.1",
+    "author": "Factor Libre",
+    "category": "Others",
+    "website": "http://www.factorlibre.com",
+    "description": "Update Account Financial Report (Factor Libre Project)",
+    "depends": ["base", "account_financial_report"],
+    "init_xml": [],
+    "update_xml": ["account_report_report.xml","account_report_wizard.xml",],
+    "active": False,
+    "installable": True,
+}
