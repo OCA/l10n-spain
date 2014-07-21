8bd92b219b0c5cc0a4ba59a4112cb48f18d918fe
Angel Moya - Domatix <angel.moya@domatix.com>
2014-07-21 19:52:16 +0200

    [MIG] migrado a la nueva api de la v8

---
+++ l10n_es_account_invoice_sequence/account_journal.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright (c) 2011 NaN Projectes de Programari Lliure, S.L. All Rights Reserved.
+#                    http://www.NaN-tic.com
+#    Copyright (c) 2013 Serv. Tecnol. Avanzados (http://www.serviciosbaeza.com)
+#                       Pedro Manuel Baeza <pedro.baeza@serviciosbaeza.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from openerp.osv import orm, fields
+
+class account_journal(orm.Model):
+    _inherit = 'account.journal'
+
+    _columns = {
+        'invoice_sequence_id': fields.many2one('ir.sequence',
+                'Invoice sequence',
+                domain="[('company_id','=',company_id)]",
+                help="The sequence used for invoice numbers in this journal.",
+                ondelete='restrict'),
+    }
+
+    def _check_company(self, cr, uid, ids):
+        for journal in self.browse(cr, uid, ids):
+            if (journal.invoice_sequence_id and
+                    journal.invoice_sequence_id.company_id !=
+                    journal.company_id):
+                return False
+        return True
+
+    _constraints = [
+        (_check_company,
+        'Journal company and invoice sequence company do not match.',
+        ['company_id','invoice_sequence_id'])
+    ]
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
