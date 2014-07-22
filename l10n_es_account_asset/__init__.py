3c2034a3280c59d9700eba677943bf64b2bb0f0a
Roberto Lizana <robertolizana@trey.es>
2014-07-22 21:20:42 +0200

    [FIX] PEP8 a __init__.py

--- l10n_es_account_asset/__init__.py
+++ l10n_es_account_asset/__init__.py
@@ -1,6 +1,6 @@
 # -*- encoding: utf-8 -*-
 ##############################################################################
-#    
+#
 #    OpenERP, Open Source Management Solution
 #    Copyright (C) 2011-2012 Wariasc.
 #
@@ -15,10 +15,9 @@
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
 import account_asset
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
-
