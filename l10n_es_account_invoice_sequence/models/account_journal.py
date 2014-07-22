fb68477c0b0d94731b3e4fa992dd5a442aa0afd5
Pedro M. Baeza <pedro.baeza@gmail.com>
2014-07-22 02:14:09 +0200

    [FIX] Arreglados errores de los tests + PEP8

--- l10n_es_account_invoice_sequence/models/account_journal.py
+++ l10n_es_account_invoice_sequence/models/account_journal.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-# Copyright (c) 2011 NaN Projectes de Programari Lliure, S.L. All Rights Reserved.
+#    Copyright (c) 2011 NaN Projectes de Programari Lliure, S.L.
 #                    http://www.NaN-tic.com
 #    Copyright (c) 2013 Serv. Tecnol. Avanzados (http://www.serviciosbaeza.com)
 #                       Pedro Manuel Baeza <pedro.baeza@serviciosbaeza.com>
 #
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
@@ -22,15 +22,17 @@
 ##############################################################################
 from openerp.osv import orm, fields
 
+
 class account_journal(orm.Model):
     _inherit = 'account.journal'
 
     _columns = {
-        'invoice_sequence_id': fields.many2one('ir.sequence',
-                'Invoice sequence',
-                domain="[('company_id','=',company_id)]",
-                help="The sequence used for invoice numbers in this journal.",
-                ondelete='restrict'),
+        'invoice_sequence_id': fields.many2one(
+            'ir.sequence',
+            'Invoice sequence',
+            domain="[('company_id','=',company_id)]",
+            help="The sequence used for invoice numbers in this journal.",
+            ondelete='restrict'),
     }
 
     def _check_company(self, cr, uid, ids):
@@ -43,8 +45,6 @@ class account_journal(orm.Model):
 
     _constraints = [
         (_check_company,
-        'Journal company and invoice sequence company do not match.',
-        ['company_id','invoice_sequence_id'])
+         'Journal company and invoice sequence company do not match.',
+         ['company_id', 'invoice_sequence_id'])
     ]
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
