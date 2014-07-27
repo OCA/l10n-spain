105ccbc4634f01079fec59f88c07f17141042214
Pedro M. Baeza <pedro.baeza@gmail.com>
2014-07-27 05:16:03 +0200

    [IMP] l10n_es_partner: PEP8

--- l10n_es_partner/wizard/__init__.py
+++ l10n_es_partner/wizard/__init__.py
@@ -1,14 +1,9 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    OpenERP, Open Source Management Solution
-#    Copyright (c) 2013 Serv. Tecnol. Avanzados (http://www.serviciosbaeza.com)
-#                       Pedro Manuel Baeza <pedro.baeza@serviciosbaeza.com>
-#    $Id$
-#
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
@@ -21,5 +16,4 @@
 #
 ##############################################################################
 
-import l10n_es_partner_wizard
-
+from . import l10n_es_partner_wizard
