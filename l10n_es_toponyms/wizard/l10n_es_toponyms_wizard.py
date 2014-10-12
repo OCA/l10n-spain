ba681149c7d7f93841b447935e56d321bbadbfa2
Roberto Lizana <robertolizana@trey.es>
2014-10-12 11:15:19 +0200

    [IMP] l10n_es_toponyms: Pruebas unitarias + aplicar API 8.0

--- l10n_es_toponyms/wizard/l10n_es_toponyms_wizard.py
+++ l10n_es_toponyms/wizard/l10n_es_toponyms_wizard.py
@@ -21,58 +21,54 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
+
+from openerp import models, fields, tools
 import os
-import openerp.tools
-from openerp.osv import fields, orm
 
 
-class ConfigEsToponyms(orm.TransientModel):
+class ConfigEsToponyms(models.TransientModel):
     _name = 'config.es.toponyms'
     _inherit = 'res.config.installer'
 
-    _columns = {
-        'name': fields.char('Name', size=64),
-        'state': fields.selection([('official', 'Official'),
-                                   ('spanish', 'Spanish'),
-                                   ('both', 'Both')], 'State names',
-                                  required=True),
-        'city_info': fields.selection([('yes', 'Yes'), ('no', 'No')],
-                                      'City information', required=True),
-    }
-
-    _defaults = {
-        'state': 'official',
-        'city_info': 'yes',
-    }
+    name = fields.Char('Name', size=64)
+    state = fields.Selection(
+        selection=[
+            ('official', 'Official'),
+            ('spanish', 'Spanish'),
+            ('both', 'Both')
+        ],
+        required=True,
+        string='State names',
+        default='official')
+    city_info = fields.Selection(
+        selection=[
+            ('yes', 'Yes'),
+            ('no', 'No')
+        ],
+        required=True,
+        string='City information',
+        default='yes')
 
     def create_states(self, cr, uid, state_type, context=None):
         """Import spanish states information through an XML file."""
         file_name = 'l10n_es_toponyms_states_%s.xml' % state_type
-        try:
-            fp = openerp.tools.file_open(os.path.join('l10n_es_toponyms',
-                                         os.path.join('wizard', file_name)))
-        except IOError:
-            fp = None
-        if fp:
-            idref = {}
-            openerp.tools.convert_xml_import(cr, 'l10n_es_toponyms', fp, idref,
-                                             'init', noupdate=True)
+        path = os.path.join('l10n_es_toponyms', 'wizard', file_name)
+        with tools.file_open(path) as fp:
+            tools.convert_xml_import(cr, 'l10n_es_toponyms', fp, {},
+                                     'init', noupdate=True)
             return True
+
         return False
 
     def create_zipcodes(self, cr, uid, context=None):
         """Import spanish zipcodes information through an XML file."""
         file_name = 'l10n_es_toponyms_zipcodes.xml'
-        try:
-            fp = openerp.tools.file_open(os.path.join('l10n_es_toponyms',
-                                         os.path.join('wizard', file_name)))
-        except IOError:
-            fp = None
-        if fp:
-            idref = {}
-            openerp.tools.convert_xml_import(cr, 'l10n_es_toponyms', fp, idref,
-                                             'init', noupdate=True)
+        path = os.path.join('l10n_es_toponyms', 'wizard', file_name)
+        with tools.file_open(path) as fp:
+            tools.convert_xml_import(cr, 'l10n_es_toponyms', fp, {},
+                                     'init', noupdate=True)
             return True
+
         return False
 
     def execute(self, cr, uid, ids, context=None):
