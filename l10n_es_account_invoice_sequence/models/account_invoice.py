fa3ef127b0ed74fd04c4baa32966b51276429665
Roberto Lizana <robertolizana@trey.es>
2014-10-17 18:01:17 +0200

    [IMP+FIX] l10n_es_account_invoice_sequence
    * Tests
    * Asigna la misma secuencia para la factura y el asiento
    * Nueva API
    * al cancelar y validar una factura se le asigna otro numero
    * with_context, quitar browse e 'internal_number' solo si se necesita
    * Cuando Valida -> Cancela -> Valida no se asigna correctamente el num. asiento contable
    * Permitir eliminar facturas en borrador o canceladas
    * Descripción del módulo ampliada y extraída a RST

--- l10n_es_account_invoice_sequence/models/account_invoice.py
+++ l10n_es_account_invoice_sequence/models/account_invoice.py
@@ -22,39 +22,42 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from openerp import models, fields, api, _
-from openerp.exceptions import except_orm
+from openerp import models, fields, api, _, exceptions
 
 
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
-    def copy(self, cr, uid, id, default=None, context=None):
-        if not default:
-            default = {}
-        default['invoice_number'] = False
-        default['number'] = False
-        return super(AccountInvoice, self).copy(cr, uid, id, default,
-                                                context=context)
-    number = fields.Char('Invoice Number', size=32, readonly=True,
-                         help="Unique number of the invoice, computed "
-                         "automatically when the invoice is created.")
+    number = fields.Char(related=False, size=32, copy=False)
+    invoice_number = fields.Char(copy=False)
 
     @api.multi
     def action_number(self):
-        sequence_obj = self.pool['ir.sequence']
-        period_obj = self.pool['account.period']
         for inv in self:
-            sequence = inv.journal_id.invoice_sequence_id
-            if not sequence:
-                raise except_orm(_('Error!'),
-                                 _('Journal %s has no sequence defined for '
-                                   'invoices.') % inv.journal_id.name)
-            ctx = self.env.context.copy()
-            period = period_obj.browse(self.env.cr, self.env.uid,
-                                       inv.period_id.id)
-            ctx['fiscalyear_id'] = period.fiscalyear_id.id
-            number = sequence_obj.next_by_id(self.env.cr, self.env.uid,
-                                             sequence.id, ctx)
-            inv.write({'number': number})
-        return super(AccountInvoice, self).action_number()
+            if not inv.invoice_number:
+                sequence = inv.journal_id.invoice_sequence_id
+                if not sequence:
+                    raise exceptions.Warning(
+                        (_('Journal %s has no sequence defined for invoices.')
+                         % inv.journal_id.name))
+                number = sequence.with_context({
+                    'fiscalyear_id': inv.period_id.fiscalyear_id.id
+                }).next_by_id(sequence.id)
+                inv.write({
+                    'number': number,
+                    'invoice_number': number
+                })
+            else:
+                inv.write({
+                    'number': inv.invoice_number,
+                })
+        re = super(AccountInvoice, self).action_number()
+        for inv in self:
+            inv.write({'internal_number': inv.move_id.name})
+
+        return re
+
+    @api.multi
+    def unlink(self):
+        self.write({'internal_number': False})
+        return super(AccountInvoice, self).unlink()
