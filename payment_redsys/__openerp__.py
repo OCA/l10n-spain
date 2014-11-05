2991c0364d979e3e2b179ed8b77c058572b85e1e
sergio-incaser <sergio@incaser.net>
2014-11-05 19:05:29 +0100

    [ADD] Nuevo modulo para pagos con tajeta (sermepa-redsys)

---
+++ payment_redsys/__openerp__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Redsys Payment Acquirer',
+    'category': 'Hidden',
+    'summary': 'Payment Acquirer: Redsys Implementation',
+    'version': '1.0',
+    'author': 'Incaser Informatica S.L.',
+    'depends': ['payment'],
+    'data': [
+        'views/redsys.xml',
+        'views/payment_acquirer.xml',
+        'data/redsys.xml',
+    ],
+    'installable': True,
+}
