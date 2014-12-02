1187828ed072f6f4746ac258feb00193bd21fc70
Ismael Calvo <ismael.calvo@factorlibre.com>
2014-12-02 14:55:33 +0100

    [FIX] l10n_es_partner: ajustes para PEP8

--- l10n_es_partner/gen_src/gen_data_banks.py
+++ l10n_es_partner/gen_src/gen_data_banks.py
@@ -3,12 +3,12 @@
 #
 #    OpenERP, Open Source Management Solution
 #    Copyright (c) 2014 Factor Libre S.L (http://www.factorlibre.com)
-#                       Ismael Calvo <ismael.calvo@factorlibre.com> 
+#                       Ismael Calvo <ismael.calvo@factorlibre.com>
 #    $Id$
 #
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
@@ -35,7 +35,8 @@ if __name__ == "__main__":
         sheet = book.sheet_by_index(0)
 
         # Prepara el archivo resultante
-        output = codecs.open("../wizard/data_banks.xml", mode='w', encoding='utf-8')
+        output = codecs.open("../wizard/data_banks.xml", mode='w',
+                             encoding='utf-8')
         output.write("<?xml version='1.0' encoding='UTF-8'?>\n")
         output.write("<openerp>\n")
         output.write("    <data noupdate='1'>\n")
@@ -43,20 +44,33 @@ if __name__ == "__main__":
         # Genera los nuevos registros de los bancos
         for row_index in range(1, sheet.nrows):
             row = sheet.row_values(row_index)
-            name = row[40].lower().replace(' ', '_').replace(u'\xf1', 'n')
-            output.write('        <record id="city_bank_%s" model="city.city">\n' %name)
-            output.write('            <field name="name">%s<field/>\n' %row[40])
-            output.write('            <field name="lname">%s<field/>\n' %row[4])
-            output.write('            <field name="code">%s<field/>\n' %row[1])
-            output.write('            <field name="vat">%s<field/>\n' %row[6])
-            output.write('            <field name="street">%s<field/>\n' %(row[7]+'. '+row[8]+', '+row[9]+' '+row[10]))
-            output.write('            <field name="city">%s<field/>\n' %row[12])
-            output.write('            <field name="zip">%s<field/>\n' %row[11])
-            output.write('            <field name="phone">%s<field/>\n' %row[16])
-            output.write('            <field name="fax">%s<field/>\n' %row[18])
+            name = "city_bank_" + row[40].lower().replace(
+                ' ', '_').replace(u'\xf1', 'n')
+            street = row[7] + '. ' + row[8] + ', ' + row[9] + ' ' + row[10]
+            output.write('        <record id="%s" model="city.city">\n' % name)
+            output.write('            <field name="name">%s<field/>\n' % (
+                         row[40]))
+            output.write('            <field name="lname">%s<field/>\n' % (
+                         row[4]))
+            output.write('            <field name="code">%s<field/>\n' % (
+                         row[1]))
+            output.write('            <field name="vat">%s<field/>\n' % (
+                         row[6]))
+            output.write('            <field name="street">%s<field/>\n' % (
+                         street))
+            output.write('            <field name="city">%s<field/>\n' % (
+                         row[12]))
+            output.write('            <field name="zip">%s<field/>\n' % (
+                         row[11]))
+            output.write('            <field name="phone">'
+                         '%s<field/>\n' % row[16])
+            output.write('            <field name="fax">%s<field/>\n' % (
+                         row[18]))
             output.write('            <field eval="1" name="active"/>\n')
-            output.write('            <field name="state" ref="l10n_es_topynyms.ES28"/>\n')
-            output.write('            <field name="country_id" ref="base.es"/>\n')
+            output.write('            <field name="state"'
+                         ' ref="l10n_es_topynyms.ES28"/>\n')
+            output.write('            <field name="country_id"'
+                         ' ref="base.es"/>\n')
             output.write('        </record>\n')
         output.write("    </data>\n")
         output.write("</openerp>\n")
