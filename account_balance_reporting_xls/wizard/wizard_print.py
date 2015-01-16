eb39e26f2c60f2ad7e270c3ff8e6c7503ec325e5
Alejandro Santana <alejandrosantana@anubia.es>
2015-01-16 14:28:49 +0100

    [IMP] account_balance_reporting_xls: Removed unnecesary code.

--- account_balance_reporting_xls/wizard/wizard_print.py
+++ account_balance_reporting_xls/wizard/wizard_print.py
@@ -40,35 +40,3 @@ class print_wizard(osv.osv_memory):
                         'datas': datas,
                         }
         return {'type': 'ir.actions.act_window_close'}
-
-    def print_report(self, cr, uid, ids, context=None):
-        if context is None:
-            context = {}
-
-        data = self.read(cr, uid, ids)[-1]
-        datas = {
-            'report_id': data.get('report_id') and \
-                         data['report_id'][0] or None,
-            'report_name': data.get('report_id') and \
-                           data['report_id'][1] or None,
-            'report_design': data.get('report_xml_id') and \
-                             data['report_xml_id'][1] or None,
-            'ids': context.get('active_ids', []),
-            'model': 'account.balance.reporting',
-            'form': data,
-        }
-        rpt_facade = self.pool['ir.actions.report.xml']
-        report_xml = None
-        if data.get('report_xml_id'):
-            report_xml_id = data['report_xml_id']
-            report_xml_ids = rpt_facade.search(cr, uid,
-                                               [('id', '=', report_xml_id[0])])
-            report_xml_id = report_xml_ids and report_xml_ids[0] or None
-            if report_xml_id:
-                report_xml = rpt_facade.browse(cr, uid, [report_xml_id])[0]
-            if report_xml:
-                return {'type': 'ir.actions.report.xml',
-                        'report_name': report_xml.report_name,
-                        'datas': datas,
-                        }
-        return {'type': 'ir.actions.act_window_close'}
