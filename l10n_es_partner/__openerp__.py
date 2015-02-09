1cc87025ba69054666c812effeb6ff0f582fe838
Pedro M. Baeza <pedro.baeza@gmail.com>
2015-02-09 02:33:01 +0100

    [IMP] l10n_es_partner: Descripción en README.rst

--- l10n_es_partner/__openerp__.py
+++ l10n_es_partner/__openerp__.py
@@ -32,45 +32,7 @@
     "version": "1.2",
     "author": "Spanish localization team",
     "website": "https://github.com/OCA/l10n-spain",
-    "contributors": [
-        'Jordi Esteve <jesteve@zikzakmedia.com>',
-        'Ignacio Ibeas <ignacio@acysos.com>',
-        'Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>',
-        'Sergio Teruel <sergio@incaser.es>',
-    ],
     "category": "Localisation/Europe",
-    "description": """
-Funcionalidad:
---------------
-
- * Añade el campo *Nombre comercial* a las empresas y permite buscar por él.
- * Convierte el NIF a mayúsculas.
- * Añade los campos nombre largo, NIF y web a los bancos.
- * Añade los datos de los bancos españoles extraídos del registro oficial del
-   Banco de España (http://goo.gl/mtx6ic). Ult. actualización: 2015-02-06.
- * Permite validar las cuentas bancarias españolas. Para ello, se añade un
-   campo de país a las cuentas bancarias de las empresas y se realizan
-   validaciones cuando el país es España.
-
-
-Funcionamiento de la validación de la cuenta bancaria para cuentas españolas:
------------------------------------------------------------------------------
-
- * Se comprueba si la cuenta consta de 20 dígitos (con o sin espacios).
- * Si no los tiene, se devuelve un error de longitud incorrecta.
- * Si los dígitos son 20, se calculan los dos dígitos de control (DC) y se
-   comprueba que coinciden con los introducidos.
- * Si el DC no coincide, se devuelve un error de que la cuenta es incorrecta.
- * Si el DC es correcto, presenta el resultado con el formato
-   "1234 5678 06 1234567890"
-
-
-Funcionamiento de la validación de la cuenta bancaria para cuentas IBAN:
-------------------------------------------------------------------------
-
- * Se limpia la cuenta de espacios.
- * Se divide lo introducido en bloques de 4 caracteres.
-""",
     "license": "AGPL-3",
     "depends": [
         "base",
