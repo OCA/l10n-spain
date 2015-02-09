ffc7561b9e9e710a0a172e95b517f3950c0c89ab
Pedro M. Baeza <pedro.baeza@gmail.com>
2015-02-09 01:18:08 +0100

    [FIX] New PEP8 checks

--- account_balance_reporting/wizard/__init__.py
+++ account_balance_reporting/wizard/__init__.py
@@ -20,11 +20,4 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-
-"""
-Account balance reporting engine wizards
-"""
-__author__ = "Borja López Soilán (Pexego) - borjals@pexego.es"
-
-
 from . import wizard_print
