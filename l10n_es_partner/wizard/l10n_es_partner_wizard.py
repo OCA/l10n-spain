ff335a25454a59744d549f506f3d028db3b2021c
Pedro M. Baeza <pedro.baeza@gmail.com>
2015-02-16 01:30:10 +0100

    [IMP] l10n_es_partner: Permitir importación directa desde Internet Se ha corregido además el script de importación para que ya no cause más problemas
    OCA Transbot updated translations from Transifex

--- l10n_es_partner/wizard/l10n_es_partner_wizard.py
+++ l10n_es_partner/wizard/l10n_es_partner_wizard.py
@@ -19,8 +19,10 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from openerp import models, api
+from openerp import models, fields, api, _
 from openerp import tools
+from ..gen_src.gen_data_banks import gen_bank_data_xml
+import tempfile
 import os
 
 
@@ -28,11 +30,50 @@ class L10nEsPartnerImportWizard(models.TransientModel):
     _name = 'l10n.es.partner.import.wizard'
     _inherit = 'res.config.installer'
 
+    import_fail = fields.Boolean(default=False)
+
     @api.multi
-    def execute(self):
+    def import_local(self):
         res = super(L10nEsPartnerImportWizard, self).execute()
         path = os.path.join('l10n_es_partner', 'wizard', 'data_banks.xml')
         with tools.file_open(path) as fp:
             tools.convert_xml_import(
-                self._cr, 'l10n_es_partner', fp, {}, 'init', noupdate=True)
+                self._cr, 'l10n_es_partner', fp, {}, 'init', noupdate=False)
         return res
+
+    @api.multi
+    def execute(self):
+        import urllib2
+        try:
+            xlsfile = urllib2.urlopen(
+                'http://www.bde.es/f/webbde/IFI/servicio/regis/ficheros/es/'
+                'REGBANESP_CONESTAB_A.XLS')
+            # Read XLS
+            src_file = tempfile.NamedTemporaryFile(delete=False)
+            src_file.write(xlsfile.read())
+            src_file.close()
+            # Prepare XML dest file
+            dest_file = tempfile.NamedTemporaryFile('w', delete=False)
+            dest_file.close()
+            # Generate XML and reopen it
+            gen_bank_data_xml(src_file.name, dest_file.name)
+            tools.convert_xml_import(
+                self._cr, 'l10n_es_partner', dest_file.name, {}, 'init',
+                noupdate=False)
+        except:
+            self.import_fail = True
+            return {
+                'name': _('Import spanish bank data'),
+                'type': 'ir.actions.act_window',
+                'res_model': 'l10n.es.partner.import.wizard',
+                'view_id': self.env.ref("l10n_es_partner."
+                                        "l10n_es_partner_import_wizard").id,
+                'view_type': 'form',
+                'view_mode': 'form',
+                'res_id': self.id,
+                'target': 'new',
+            }
+        finally:
+            dest_file.close()
+            os.remove(src_file.name)
+            os.remove(dest_file.name)
