9e41fb032ec9fd11571508dc1b246c2e2cc4c0c2
Pedro M. Baeza <pedro.baeza@gmail.com>
2015-02-20 04:16:12 +0100

    [ADD] l10n_es_account_bank_statement_import_n43: Sustituye a l10n_es_bank_statement para importar extractos bancarios N43

---
+++ l10n_es_account_bank_statement_import_n43/__openerp__.py
@@ -0,0 +1,36 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Copyright(c) 2013-2015 Serv. Tecnol. Avanzados (http://serviciosbaeza.com)
+#                           Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Importación de extractos bancarios españoles (Norma 43)',
+    'category': 'Accounting & Finance',
+    'version': '4.0',
+    'license': 'AGPL-3',
+    'author': 'Spanish Localization Team,'
+              'Serv. Tecnol. Avanzados - Pedro M. Baeza,'
+              'Antiun Ingenería S.L.,'
+              'Odoo Community Association (OCA)',
+    "website": "https://github.com/OCA/l10n-spain",
+    'depends': [
+        'account_bank_statement_import',
+    ],
+    'data': [],
+    'installable': True,
+}
