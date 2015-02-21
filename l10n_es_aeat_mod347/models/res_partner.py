1a4a5e4a011d863f195db33bf23613fa20e98b2b
Pedro M. Baeza <pedro.baeza@gmail.com>
2015-02-21 03:32:55 +0100

    [MIG] Mergear cambios de la 7 y recolocar

--- l10n_es_aeat_mod347/models/res_partner.py
+++ l10n_es_aeat_mod347/models/res_partner.py
@@ -15,23 +15,19 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
+from openerp.osv import fields, orm
 
 
-from openerp.osv import fields
-from openerp.osv import orm
-
-
-class res_partner(orm.Model):
+class ResPartner(orm.Model):
     _inherit = "res.partner"
     _columns = {
-        'not_in_mod347': fields.boolean(u"Not included in 347 report",
+        'not_in_mod347': fields.boolean(
+            "Not included in 347 report",
             help="If you mark this field, this partner will not be included "
-                 "in any AEAT 347 model report, independently from the "
-                 "total amount of its operations."),
+            "in any AEAT 347 model report, independently from the total "
+            "amount of its operations."),
     }
 
     _defaults = {
         'not_in_mod347': False,
     }
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
