c1e6536b4621408ecb4ae8bd733db5e13f266435
Pedro M. Baeza <pedro.baeza@gmail.com>
2015-02-21 06:27:46 +0100

    [MIG] l10n_es_aeat_mod347: Migrado a nueva API

--- l10n_es_aeat_mod347/__init__.py
+++ l10n_es_aeat_mod347/__init__.py
@@ -15,20 +15,13 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from openerp import fields, models, api
+from . import models
+from . import wizard
+from openerp import SUPERUSER_ID
 
 
-class ResPartner(models.Model):
-    _inherit = "res.partner"
-
-    not_in_mod347 = fields.Boolean(
-        "Not included in 347 report",
-        help="If you mark this field, this partner will not be included in "
-             "any AEAT 347 model report, independently from the total "
-             "amount of its operations.", default=False)
-
-    @api.model
-    def _commercial_fields(self):
-        res = super(ResPartner, self)._commercial_fields()
-        res += ['not_in_mod347']
-        return res
+def post_init_hook(cr, registry):
+    # Assign quarters on first time
+    period_obj = registry['account.period']
+    period_ids = period_obj.search(cr, SUPERUSER_ID, [])
+    period_obj.assign_quarter(cr, SUPERUSER_ID, period_ids)
