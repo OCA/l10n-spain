069ebe53ca73d7b569d959053a104ec98d65780f
Pedro M. Baeza <pedro.baeza@gmail.com>
2015-02-21 03:32:55 +0100

    [MIG] Mergear cambios de la 7 y recolocar

--- l10n_es_aeat_mod347/wizard/__init__.py
+++ l10n_es_aeat_mod347/wizard/__init__.py
@@ -18,9 +18,6 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-
-
-import calculate_mod347_records
 import export_mod347_to_boe
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
