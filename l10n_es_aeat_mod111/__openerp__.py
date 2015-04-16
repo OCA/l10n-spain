d8ce75f1db746ab80e47072a8ce2553907639f09
agaldona <ainaragaldona@avanzosc.es>
2015-04-16 09:51:54 +0200

    [ADD] l10n_es_aeat_mod111: modulo para sacar el modelo 111

---
+++ l10n_es_aeat_mod111/__openerp__.py
@@ -0,0 +1,35 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#  OpenERP, Open Source Management Solution.
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as
+#  published by the Free Software Foundation, either version 3 of the
+#  License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'AEAT modelo 111',
+    'version': '1.0',
+    'category': "Localisation/Accounting",
+    'author': "Spanish Localization Team,Odoo Community Association (OCA)",
+    'website': "https://github.com/OCA/l10n-spain",
+    'license': 'AGPL-3',
+    'depends': ['l10n_es_aeat'],
+    'data': [
+        'wizard/export_mod111_to_boe.xml',
+        'views/mod111_view.xml',
+        'security/ir.model.access.csv'],
+    'active': False,
+    'installable': True,
+}
