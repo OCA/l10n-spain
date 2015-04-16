2f82a431c4ffb160d0d1a9a47bf06eb715050d16
agaldona <ainaragaldona@avanzosc.es>
2015-04-16 16:13:09 +0200

    [ADD] l10n_es_aeat_mod115: módulo para al cálculo del modelo 115

---
+++ l10n_es_aeat_mod115/models/mod115.py
@@ -0,0 +1,133 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#  OpenERP, Open Source Management Solution.
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as
+#  published by the Free Software Foundation, either version 3 of the
+#  License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import fields, models, api, exceptions, _
+
+
+class L10nEsAeatMod115Report(models.Model):
+
+    _description = 'AEAT 115 report'
+    _inherit = 'l10n.es.aeat.report'
+    _name = 'l10n.es.aeat.mod115.report'
+
+    number = fields.Char(default='115')
+    casilla_01 = fields.Integer('Casilla [01]',
+                                help='Retenciones e ingresos a cuenta. '
+                                'Número perceptores.')
+    casilla_02 = fields.Float('Casilla [02]',
+                              help='Retenciones e ingresos a cuenta. '
+                              'Base retenciones e ingresos a cuenta.')
+    casilla_03 = fields.Float('Casilla [03]',
+                              help='Retenciones e ingresos a cuenta. '
+                              'Retenciones e ingresos a cuenta.')
+    casilla_04 = fields.Integer('Casilla [04]',
+                                help='Retenciones e ingresos a cuenta. '
+                                'Resultado anteriores declaraciones.')
+    casilla_05 = fields.Integer('Casilla [05]',
+                                help='Retenciones e ingresos a cuenta. '
+                                'Resultado a ingresar.',
+                                compute='get_casilla05')
+    currency_id = fields.Many2one('res.currency', string='Moneda',
+                                  related='company_id.currency_id', store=True)
+    period_id = fields.Many2one('account.period', 'Periodo', readonly=True,
+                                states={'draft': [('readonly', False)]},
+                                required=True)
+    move_lines_02 = fields.Many2many(comodel_name='account.move.line',
+                                     relation='mod115_account_move_line02_rel',
+                                     column1='mod115',
+                                     column2='account_move_line')
+    move_lines_03 = fields.Many2many(comodel_name='account.move.line',
+                                     relation='mod115_account_move_line03_rel',
+                                     column1='mod115',
+                                     column2='account_move_line')
+
+    @api.one
+    @api.depends('casilla_03', 'casilla_04')
+    def get_casilla05(self):
+        self.casilla_05 = self.casilla_03 - self.casilla_04
+
+    def __init__(self, pool, cr):
+        self._aeat_number = '115'
+        super(L10nEsAeatMod115Report, self).__init__(pool, cr)
+
+    @api.multi
+    def _calc_prev_trimesters_data(self):
+        self.ensure_one()
+        periods = self.env['account.period'].search([('fiscalyear_id', '=',
+                                                      self.fiscalyear_id.id),
+                                                     ('special', '=', False)])
+        amount = 0
+        for period in periods:
+            if period == self.period_id:
+                break
+            report_ids = self.search(
+                [('period_id', '=', period.id),
+                 ('fiscalyear_id', '=', self.fiscalyear_id.id),
+                 ('company_id', '=', self.company_id.id)])
+            if not report_ids:
+                raise exceptions.Warning(
+                    "No se ha encontrado la "
+                    "declaracion mod. 115 para el periodo %s. No se "
+                    "puede continuar el calculo si no existe dicha "
+                    "declaracion." % period.code)
+            amount = report_ids[0].casilla_05 or 0
+        return amount
+
+    @api.multi
+    def show_move_lines(self):
+        move_lines = []
+        if self.env.context.get('move_lines02', False):
+            move_lines = self.move_lines_02.ids
+        elif self.env.context.get('move_lines03', False):
+            move_lines = self.move_lines_03.ids
+        return {'type': 'ir.actions.act_window',
+                'name': _('Account Move Lines'),
+                'view_mode': 'tree,form',
+                'view_type': 'form',
+                'res_model': 'account.move.line',
+                'domain': [('id', 'in', move_lines)]
+                }
+
+    @api.multi
+    def calculate(self):
+        self.ensure_one()
+        tax_code_obj = self.env['account.tax.code']
+        move_line_obj = self.env['account.move.line']
+        code_base_02 = tax_code_obj.search([('code', '=', 'RBI'),
+                                            ('company_id', '=',
+                                             self.company_id.id)])
+        code_cuota_03 = tax_code_obj.search([('code', '=', 'RLC115'),
+                                             ('company_id', '=',
+                                              self.company_id.id)])
+        move_lines02 = move_line_obj.search(
+            [('tax_code_id', 'child_of', code_base_02.id),
+             ('period_id', '=', self.period_id.id),
+             ('company_id', '=', self.company_id.id)])
+        move_lines03 = move_line_obj.search(
+            [('tax_code_id', 'child_of', code_cuota_03.id),
+             ('period_id', '=', self.period_id.id),
+             ('company_id', '=', self.company_id.id)])
+        self.move_lines_02 = move_lines02.ids
+        self.move_lines_03 = move_lines03.ids
+        self.casilla_02 = sum([x.tax_amount for x in move_lines02])
+        self.casilla_03 = sum([x.tax_amount for x in move_lines03])
+        self.casilla_04 = self._calc_prev_trimesters_data()
+        self.casilla_01 = len(set([x.partner_id for x in (move_lines02 +
+                                                          move_lines03)]))
---
+++ l10n_es_aeat_mod115/wizard/export_mod115_to_boe.py
@@ -0,0 +1,91 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#  OpenERP, Open Source Management Solution.
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as
+#  published by the Free Software Foundation, either version 3 of the
+#  License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import models, api
+
+
+class L10nEsAeatMod115ExportToBoe(models.TransientModel):
+
+    _inherit = 'l10n.es.aeat.report.export_to_boe'
+    _name = 'l10n.es.aeat.mod115.export_to_boe'
+
+    @api.multi
+    def _get_formatted_declaration_record(self, report):
+        res = ''
+        #  Inicio del identificador de modelo y página: Constante "<T"
+        res += self._formatString('<T', 2)
+        # Modelo: Constante "115"
+        res += self._formatString('115', 3)
+        # Página: Constante "01"
+        res += self._formatString('01', 2)
+        # Fin de identificador de modelo: Constante ">"
+        res += self._formatString('000>', 4)
+        # Reservado página complementaria
+        res += self._formatString('C' if report.type == 'C' else ' ', 1)
+        # Tipo de declaración: I (ingreso), U (domiciliación),
+        #                      G (ingreso a anotar en CCT), N (negativa)
+        res += self._formatString(report.company_vat, 9)
+        # Identificación. Sujeto pasivo. Denominación (o Apellidos y Nombre)
+        res += self._formatString(report.company_name, 60)
+        # Reservado para la Administración
+        res += self._formatString('', 20)
+        # Num Identificación. Ejercicio
+        res += self._formatNumber(report.fiscalyear_id.code, 4)
+        # Identificación. Periodo: "01" ... "12" o "1T" … "4T"
+        res += self._formatString(report.period_id.code[0:2], 2)
+        return res
+
+    @api.multi
+    def _get_formatted_main_record(self, report):
+        res = ''
+        # Retenciones e ingresos a cuenta
+        # N.º perceptores (15).
+        res += self._formatNumber(report.casilla_01, 15)
+        # Retenciones e ingresos a cuenta
+        # Base retenciones e ingresos a cuenta (17).
+        res += self._formatNumber(report.casilla_02, 17)
+        # Retenciones e ingresos a cuenta
+        # Retenciones e ingresos a cuenta (17).
+        res += self._formatNumber(report.casilla_03, 17)
+        # Retenciones e ingresos a cuenta
+        # Resultado anteriores declaraciones (17).
+        res += self._formatNumber(report.casilla_04, 17)
+        # Retenciones e ingresos a cuenta
+        # Resultado a ingresar (17).
+        res += self._formatNumber(report.casilla_05, 17)
+        return res
+
+    @api.multi
+    def _get_formatted_other_records(self, report):
+        res = ''
+        # Declaración complementaria (1).
+        res += self._formatString('X' if report.type == 'C' else ' ', 1)
+        # Número de justificante de la declaración anterior (13).
+        res += self._formatString(report.previous_number
+                                  if report.type == 'C' else '', 13)
+        # Domicialización IBAN (34).
+        res += self._formatString('', 34)
+        # Reservado AEAT (236).
+        res += self._formatString('', 236)
+        # Reservado para la administración. Sello electronico (13).
+        res += self._formatString('', 13)
+        # Indicador de fin de registro (12).
+        res += self._formatString('</T11501000>', 12)
+        return res
