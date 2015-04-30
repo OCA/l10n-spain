12be1ce890eb3c7fcef3eff3bdfdfed6ba5fd2be
Endika Iglesias <endikaig@antiun.com>
2015-04-30 11:24:39 +0200

    [ADD] Add l10n_es_pos_simplified_invoice

---
+++ l10n_es_pos_simplified_invoice/__init__.py
@@ -0,0 +1,23 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    This module copyright :
+#        (c) 2014 Antiun Ingenieria S.L. (Madrid, Spain, http://www.antiun.com)
+#                 Endika Iglesias <endikaig@antiun.com>
+#                 Antonio Espinosa <antonioea@antiun.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
---
+++ l10n_es_pos_simplified_invoice/static/src/js/main.js
@@ -0,0 +1,4 @@
+openerp.l10n_es_pos_simplified_invoice = function (instance) {
+    var module = instance.point_of_sale;
+    l10n_es_pos_simplified_invoice_screens(instance, module);
+};
