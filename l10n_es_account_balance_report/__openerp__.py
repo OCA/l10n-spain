e023075b7442c719216ada6bb664191bf9d470ae
Pedro M. Baeza <pedro.baeza@gmail.com>
2015-06-11 08:09:34 +0200

    [FIX] PEP8 length

--- l10n_es_account_balance_report/__openerp__.py
+++ l10n_es_account_balance_report/__openerp__.py
@@ -44,20 +44,20 @@ Informes de cuentas anuales oficiales españoles.
 Incluye las siguientes plantillas para el motor de informes de cuentas provisto
 por el módulo *account_balance_reporting*:
 
-    * Balance PYMEs (PGCE 2008)
-    * Cuenta de pérdidas y ganancias PYMEs (PGCE 2008)
-    * Balance abreviado (PGCE 2008)
-    * Cuenta de pérdidas y ganancias abreviado (PGCE 2008)
-    * Balance normal (PGCE 2008)
-    * Cuenta de pérdidas y ganancias completo (PGCE 2008)
-    * Estado de ingresos y gastos reconocidos (PGCE 2008)
+* Balance PYMEs (PGCE 2008)
+* Cuenta de pérdidas y ganancias PYMEs (PGCE 2008)
+* Balance abreviado (PGCE 2008)
+* Cuenta de pérdidas y ganancias abreviado (PGCE 2008)
+* Balance normal (PGCE 2008)
+* Cuenta de pérdidas y ganancias completo (PGCE 2008)
+* Estado de ingresos y gastos reconocidos (PGCE 2008)
 
 Las plantillas están basadas en los modelos para el depósito de cuentas anuales
 del Registro Mercantil:
 
-    * *Normal*: http://www.mjusticia.gob.es/cs/Satellite/Portal/1292427306020
-    * *Abreviado*: http://www.mjusticia.gob.es/cs/Satellite/Portal/1292427306005
-    * *PYMEs*: http://www.mjusticia.gob.es/cs/Satellite/Portal/1292427306035
+* *Normal*: http://www.mjusticia.gob.es/cs/Satellite/Portal/1292427306020
+* *Abreviado*: http://www.mjusticia.gob.es/cs/Satellite/Portal/1292427306005
+* *PYMEs*: http://www.mjusticia.gob.es/cs/Satellite/Portal/1292427306035
     """,
     "depends": [
         'l10n_es',
