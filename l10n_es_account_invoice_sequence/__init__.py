ed46110346f744cb457d2424ede48fcafcceb68e
Pedro M. Baeza <pedro.baeza@gmail.com>
2015-08-20 18:02:25 +0200

    [IMP] l10n_es_account_invoice_sequence: Configuración automática de diarios + tests

--- l10n_es_account_invoice_sequence/__init__.py
+++ l10n_es_account_invoice_sequence/__init__.py
@@ -1,19 +1,36 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# (c) 2015 Pedro M. Baeza
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
+from .constants import ALLOWED_JOURNAL_TYPES
 from . import models
+from . import wizard
+
+
+def fill_invoice_sequences(cr, registry):
+    from openerp import SUPERUSER_ID, _
+    company_obj = registry['res.company']
+    journal_obj = registry['account.journal']
+    model_data_obj = registry['ir.model.data']
+    sequence_obj = registry['ir.sequence']
+    company_ids = company_obj.search(cr, SUPERUSER_ID, [])
+    for company in company_obj.browse(cr, SUPERUSER_ID, company_ids):
+        if company.country_id and company.country_id.code != 'ES':
+            # Discard non spanish companies (by the country of the address)
+            # Companies with no country are not discarded
+            continue
+        journal_ids = journal_obj.search(
+            cr, SUPERUSER_ID, [('company_id', '=', company.id)])
+        generic_journal_seq_id = model_data_obj.get_object_reference(
+            cr, SUPERUSER_ID, 'l10n_es_account_invoice_sequence',
+            'sequence_spanish_journal')[1]
+        journal_seq_id = sequence_obj.copy(
+            cr, SUPERUSER_ID, generic_journal_seq_id,
+            {'name': _('Journal Entries Sequence'),
+             'company_id': company.id})
+        for journal in journal_obj.browse(cr, SUPERUSER_ID, journal_ids):
+            vals = {'sequence_id': journal_seq_id}
+            if journal.type in ALLOWED_JOURNAL_TYPES:
+                vals['invoice_sequence_id'] = journal.sequence_id.id
+            journal_obj.write(
+                cr, SUPERUSER_ID, journal.id, vals)
