0741caa741558620b2e0260ed51e2c0f2d55678a
Pedro M. Baeza <pedro.baeza@gmail.com>
2015-09-06 19:39:22 +0200

    [IMP] l10n_es_aeat_vat_prorrate: Enfoque definitivo
    ======================
    AEAT - Prorrata de IVA
    ======================
    
    Módulo para gestionar la prorrata del IVA en las declaraciones de la AEAT (por
    el momento, en el modelo 303), según el artículo 92 de la Ley 37/1992, de 28 de
    diciembre, del Impuesto sobre el Valor Añadido.
    
    Uso
    ===
    
    Realizando el modelo 303, se puede seleccionar si se desea aplicar prorrata de
    IVA.
    
    Prorrata general
    ----------------
    
    En el caso de seleccionar "Prorrata general", aparece un nuevo campo llamado
    "Porcentaje de prorrata de IVA" que puede ser rellenado a mano.
    
    Además, cuando se realice el asiento de regularización, la parte proporcional
    de la cuota no deducida se compensará como gasto.
    
    Se ha incluido un calculador del porcentaje de prorrata, utilizable en 2 casos:
    
    * Al comienzo de un nuevo ejercicio, para determinar el porcentaje provisional
      a aplicar hasta el último periodo de ese ejercicio.
    * En la última declaración del ejercicio, para determinar el porcentaje
      definitivo de prorrata, y sobre el que se compensará la diferencia.
    
    Problemas conocidos / Hoja de ruta
    ==================================
    
    * La prorrata especial de IVA no está contemplada aún.

--- l10n_es_aeat_mod303/models/mod303.py
+++ l10n_es_aeat_mod303/models/mod303.py
@@ -34,6 +34,53 @@ class L10nEsAeatMod303Report(models.Model):
         return self.env['account.account'].search(
             [('code', 'like', '4750%'), ('type', '!=', 'view')])[:1]
 
+    @api.multi
+    @api.depends('tax_lines', 'tax_lines.amount')
+    def _compute_total_devengado(self):
+        casillas_devengado = (3, 6, 9, 11, 13, 15, 18, 21, 24, 26)
+        for report in self:
+            tax_lines = report.tax_lines.filtered(
+                lambda x: x.field_number in casillas_devengado)
+            report.total_devengado = sum(tax_lines.mapped('amount'))
+
+    @api.multi
+    @api.depends('tax_lines', 'tax_lines.amount')
+    def _compute_total_deducir(self):
+        casillas_deducir = (29, 31, 33, 35, 37, 39, 41, 42, 43, 44)
+        for report in self:
+            tax_lines = report.tax_lines.filtered(
+                lambda x: x.field_number in casillas_deducir)
+            report.total_deducir = sum(tax_lines.mapped('amount'))
+
+    @api.multi
+    @api.depends('total_devengado', 'total_deducir')
+    def _compute_casilla_46(self):
+        for report in self:
+            report.casilla_46 = report.total_devengado - report.total_deducir
+
+    @api.multi
+    @api.depends('porcentaje_atribuible_estado', 'casilla_46')
+    def _compute_atribuible_estado(self):
+        for report in self:
+            report.atribuible_estado = (
+                report.casilla_46 * report.porcentaje_atribuible_estado / 100)
+
+    @api.multi
+    @api.depends('atribuible_estado', 'cuota_compensar',
+                 'regularizacion_anual')
+    def _compute_casilla_69(self):
+        for report in self:
+            report.casilla_69 = (
+                report.atribuible_estado + report.cuota_compensar +
+                report.regularizacion_anual)
+
+    @api.multi
+    @api.depends('casilla_69', 'previous_result')
+    def _compute_resultado_liquidacion(self):
+        for report in self:
+            report.resultado_liquidacion = (
+                report.casilla_69 - report.previous_result)
+
     currency_id = fields.Many2one(
         comodel_name='res.currency', string='Currency',
         related='company_id.currency_id', store=True)
@@ -45,11 +92,15 @@ class L10nEsAeatMod303Report(models.Model):
     devolucion_mensual = fields.Boolean(
         string="Devolución mensual", states={'done': [('readonly', True)]},
         help="Inscrito en el Registro de Devolución Mensual")
-    total_devengado = fields.Float(string="[27] IVA devengado", readonly=True)
-    total_deducir = fields.Float(string="[45] IVA a deducir", readonly=True)
+    total_devengado = fields.Float(
+        string="[27] IVA devengado", readonly=True,
+        compute="_compute_total_devengado", store=True)
+    total_deducir = fields.Float(
+        string="[45] IVA a deducir", readonly=True,
+        compute="_compute_total_deducir", store=True)
     casilla_46 = fields.Float(
-        string="[46] Resultado régimen general", readonly=True,
-        help="(IVA devengado - IVA deducible)")
+        string="[46] Resultado régimen general", readonly=True, store=True,
+        help="(IVA devengado - IVA deducible)", compute="_compute_casilla_46")
     porcentaje_atribuible_estado = fields.Float(
         string="[65] % atribuible al Estado",
         states={'done': [('readonly', True)]},
@@ -60,7 +111,8 @@ class L10nEsAeatMod303Report(models.Model):
              "común. Los demás sujetos pasivos consignarán en esta casilla el "
              "100%", default=100)
     atribuible_estado = fields.Float(
-        string="[66] Atribuible a la Administración", readonly=True)
+        string="[66] Atribuible a la Administración", readonly=True,
+        compute="_compute_atribuible_estado", store=True)
     cuota_compensar = fields.Float(
         string="[67] Cuotas a compensar", default=0,
         states={'done': [('readonly', True)]},
@@ -77,16 +129,17 @@ class L10nEsAeatMod303Report(models.Model):
              "Comunidad Autónoma del País Vasco y el Convenio Económico entre "
              "el Estado y la Comunidad Foral de Navarra.""")
     casilla_69 = fields.Float(
-        string="[69] Resultado", readonly=True,
+        string="[69] Resultado", readonly=True, compute="_compute_casilla_69",
         help="Atribuible a la Administración [66] - Cuotas a compensar [67] + "
-             "Regularización anual [68]""")
+             "Regularización anual [68]""", store=True)
     previous_result = fields.Float(
         string="[70] A deducir",
         help="Resultado de la anterior o anteriores declaraciones del mismo "
              "concepto, ejercicio y periodo",
         states={'done': [('readonly', True)]})
     resultado_liquidacion = fields.Float(
-        string="[71] Result. liquidación", readonly=True)
+        string="[71] Result. liquidación", readonly=True,
+        compute="_compute_resultado_liquidacion", store=True)
     result_type = fields.Selection(
         selection=[('I', 'A ingresar'),
                    ('D', 'A devolver'),
@@ -135,31 +188,6 @@ class L10nEsAeatMod303Report(models.Model):
     def onchange_result_type(self):
         if self.result_type != 'B':
             self.compensate = False
-
-    @api.multi
-    def calculate(self):
-        res = super(L10nEsAeatMod303Report, self).calculate()
-        for mod303 in self:
-            total_devengado = mod303.tax_lines.filtered(
-                lambda x: x.field_number == 27).amount
-            total_deducir = mod303.tax_lines.filtered(
-                lambda x: x.field_number == 45).amount
-            casilla_46 = total_devengado - total_deducir
-            atribuible_estado = (
-                casilla_46 * mod303.porcentaje_atribuible_estado / 100)
-            casilla_69 = (atribuible_estado - mod303.cuota_compensar +
-                          mod303.regularizacion_anual)
-            resultado_liquidacion = casilla_69 - mod303.previous_result
-            vals = {
-                'total_devengado': total_devengado,
-                'total_deducir': total_deducir,
-                'casilla_46': casilla_46,
-                'atribuible_estado': atribuible_estado,
-                'casilla_69': casilla_69,
-                'resultado_liquidacion': resultado_liquidacion,
-            }
-            mod303.write(vals)
-        return res
 
     @api.multi
     def button_confirm(self):
