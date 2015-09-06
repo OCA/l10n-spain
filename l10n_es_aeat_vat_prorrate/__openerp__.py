51b40e979a5d5aa8449239690e166a1efd18bfd6
Pedro M. Baeza <pedro.baeza@gmail.com>
2015-09-06 19:39:22 +0200

    [IMP] l10n_es_aeat_vat_prorrate: Enfoque definitivo
    ======================
    AEAT - Prorrata de IVA
    ======================
    
    Módulo para gestionar la prorrata del IVA en las declaraciones de la AEAT (por
    el momento, en el modelo 303), según el artículo 92 de la Ley 37/1992, de 28 de
    diciembre, del Impuesto sobre el Valor Añadido.
    
    Uso
    ===
    
    Realizando el modelo 303, se puede seleccionar si se desea aplicar prorrata de
    IVA.
    
    Prorrata general
    ----------------
    
    En el caso de seleccionar "Prorrata general", aparece un nuevo campo llamado
    "Porcentaje de prorrata de IVA" que puede ser rellenado a mano.
    
    Además, cuando se realice el asiento de regularización, la parte proporcional
    de la cuota no deducida se compensará como gasto.
    
    Se ha incluido un calculador del porcentaje de prorrata, utilizable en 2 casos:
    
    * Al comienzo de un nuevo ejercicio, para determinar el porcentaje provisional
      a aplicar hasta el último periodo de ese ejercicio.
    * En la última declaración del ejercicio, para determinar el porcentaje
      definitivo de prorrata, y sobre el que se compensará la diferencia.
    
    Problemas conocidos / Hoja de ruta
    ==================================
    
    * La prorrata especial de IVA no está contemplada aún.
    
    OCA Transbot updated translations from Transifex

---
+++ l10n_es_aeat_vat_prorrate/__openerp__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# (c) 2015 Antiun Ingeniería S.L. - Pedro M. Baeza
+# (c) 2015 AvanzOSC - Ainara Galdona
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "AEAT - Prorrata de IVA",
+    "version": "8.0.2.0.0",
+    "license": "AGPL-3",
+    "author": "AvanzOSC, "
+              "Antiun Ingeniería S.L., "
+              "Serv. Tecnol. Avanzados - Pedro M. Baeza, "
+              "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-spain",
+    "category": "Accounting",
+    "depends": [
+        'l10n_es_aeat_mod303',
+    ],
+    "data": [
+        "data/tax_code_map_mod303_data.xml",
+        "data/aeat_export_mod303_data.xml",
+        'wizard/l10n_es_aeat_compute_vat_prorrate_view.xml',
+        'views/mod303_view.xml'
+    ],
+    "installable": True,
+}
