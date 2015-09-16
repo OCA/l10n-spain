d319426ed16563678d37adb47e3be4852e4a0a34
Omar <omarcs7r@gmail.com>
2015-09-16 14:17:57 +0200

    l10n_es_facturae: Añadimos el módulo de facturae para la versión 8.0

---
+++ l10n_es_facturae/partner.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (c) 2015 Omar Castiñeira Savedra (http://www.pexego.es)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the Affero GNU General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the Affero GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import models, fields
+
+
+class ResPartner(models.Model):
+
+    _inherit = "res.partner"
+
+    facturae = fields.Boolean('Factura electrónica')
+    organo_gestor = fields.Char('Órgano gestor')
+    unidad_tramitadora = fields.Char('Unidad tramitadora')
+    oficina_contable = fields.Char('Oficina contable')
