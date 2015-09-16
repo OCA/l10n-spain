27abb95a4ab0bc9a5fd4829e358f56d429660ac9
Omar <omarcs7r@gmail.com>
2015-09-16 20:52:50 +0200

    l10n_es_facturae: Mejoras codificación

--- l10n_es_facturae/country.py
+++ l10n_es_facturae/country.py
@@ -3,7 +3,6 @@
 #
 #    OpenERP, Open Source Management Solution
 #    Copyright (c) 2009 Alejandro Sanchez (http://www.asr-oss.com)
-#    $Id$
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the Affero GNU General Public License as published
@@ -27,26 +26,6 @@ class Country(models.Model):
 
     _inherit = 'res.country'
 
-    code_3166 = fields.Char('Country Code', size=3, required=True,
+    code_3166 = fields.Char('Code ISO 3166', size=3, required=True,
                             help='El código del país ISO 3166 (3 caractéres). '
                                  'Para España: "ESP".')
-
-    def _auto_init(self, cr, context=None):
-        res = super(Country, self)._auto_init(cr, context=context)
-        cr.execute("select count(*) from pg_class as c inner join "
-                   "pg_attribute as a on a.attrelid = c.oid where "
-                   "a.attname = 'code_3166' and c.relkind = 'r' and "
-                   "c.relname = 'res_country'")
-        noupdate = True
-        if not cr.rowcount:
-            noupdate = False
-        if noupdate:
-            cr.execute("select code_3166 from res_country where code = 'ES'")
-            code = cr.fetchone()
-            if not code or not code[0]:
-                noupdate = False
-        if not noupdate:
-            cr.execute("update ir_model_data set noupdate=false where "
-                       "module = 'base' and model = 'res.country'")
-
-        return res
