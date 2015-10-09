1bcb5217395e014ef89f9c0ae55b2592404cf8a6
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2015-10-09 10:01:41 +0200

    [UPD] prefix versions with 8.0

--- l10n_es_subcontractor_certificate/__openerp__.py
+++ l10n_es_subcontractor_certificate/__openerp__.py
@@ -23,7 +23,7 @@
 
 {
     "name": "Certificado de subcontratista",
-    "version": "1.0",
+    "version": "8.0.1.0.0",
     "author": "Domatix,Odoo Community Association (OCA)",
     "website": "www.domatix.com",
     "category": "Accounting",
