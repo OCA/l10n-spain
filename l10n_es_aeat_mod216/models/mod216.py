99a12043e0762a7d4caa80de0cee597834b438c9
Pedro M. Baeza <pedro.baeza@gmail.com>
2015-10-18 18:50:48 +0200

    [IMP] l10n_es_aeat_*: Cálculo de importes de impuestos por cadena en lugar de recordset + gestionar grupos de compañías

--- l10n_es_aeat_mod216/models/mod216.py
+++ l10n_es_aeat_mod216/models/mod216.py
@@ -84,23 +84,12 @@ class L10nEsAeatMod216Report(models.Model):
     @api.multi
     def calculate(self):
         self.ensure_one()
-        tax_code_obj = self.env['account.tax.code']
-        tax_code = tax_code_obj.search([('code', '=', 'IRPBI')])
-        if not tax_code:
-            raise exceptions.Warning(
-                _('Tabla de impuestos desactualizada.')
-            )
         move_lines_base = self._get_tax_code_lines(
-            tax_code, periods=self.period_id)
-        tax_code = tax_code_obj.search([('code', '=', 'ITRPC')])
-        if not tax_code:
-            raise exceptions.Warning(
-                _('Tabla de impuestos desactualizada.')
-            )
+            ['IRPBI'], periods=self.period_id)
         move_lines_cuota = self._get_tax_code_lines(
-            tax_code, periods=self.period_id)
+            ['ITRPC'], periods=self.period_id)
         partner_lst = set([x.partner_id for x in
                            (move_lines_base + move_lines_cuota)])
         self.casilla_01 = len(partner_lst)
-        self.casilla_02 = sum([x.tax_amount for x in move_lines_base])
-        self.casilla_03 = sum([x.tax_amount for x in move_lines_cuota])
+        self.casilla_02 = sum(move_lines_base.mapped('tax_amount'))
+        self.casilla_03 = sum(move_lines_cuota.mapped('tax_amount'))
