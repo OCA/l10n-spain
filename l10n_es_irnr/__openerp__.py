50575d8781adb4e1205e46f264da7d3dbf90ccbe
Antonio Espinosa <antespi@gmail.com>
2015-10-22 14:21:01 +0200

    [ADD] l10n_es_irnr addon

---
+++ l10n_es_irnr/__openerp__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3: Antiun Ingenieria S.L. - Antonio Espinosa
+# See README.rst file on addon root folder for more details
+
+{
+    'name': "Retenciones IRNR (No residentes)",
+    'category': 'Localization/Account Charts',
+    'version': '8.0.1.0.0',
+    'depends': [
+        'l10n_es',
+    ],
+    'external_dependencies': {},
+    'data': [
+        'data/tax_codes_irnr.xml',
+        'data/taxes_irnr.xml',
+        'data/fiscal_positions_irnr.xml',
+        'data/fiscal_position_taxes_irnr.xml',
+    ],
+    'author': 'Antiun Ingeniería S.L., '
+              'Odoo Community Association (OCA)',
+    'website': 'http://www.antiun.com',
+    'license': 'AGPL-3',
+    'demo': [],
+    'test': [],
+    'installable': True,
+}
