b301817bd4a280ca17505329f7b9dde592b02352
David Diz <daviddiz@gmail.com>
2015-10-27 18:08:44 +0100

    Módulo de IGIC

---
+++ l10n_es_igic/__openerp__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2004-2011 Pexego Sistemas Informáticos. All Rights Reserved
+#    $Omar Castiñeira Saavedra$
+#
+#   This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    "name": "IGIC (Impuesto General Indirecto Canario)",
+    "version": "8.0.0.1",
+    "author": "David Diz Martínez <daviddiz@gmail.com>",
+    "website": "https://github.com/daviddiz/igic",
+    "category": "Localisation/Europe",
+    "depends": [
+        "l10n_es",
+    ],
+    "license": "AGPL-3",
+    "data": [
+        "data/taxes_igic.xml",
+    ],
+    'images': [],
+    "demo": [],
+    'test': [],
+    'installable': True
+}
