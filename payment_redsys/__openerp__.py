f2bf05f00b24df39d9c7b009c8ee5a2750aa5ac8
sergio-incaser <sergio@incaser.es>
2015-11-13 13:33:44 +0100

    [IMP] payment_redsys: Migracion a HMAC SHA-256

--- payment_redsys/__openerp__.py
+++ payment_redsys/__openerp__.py
@@ -7,9 +7,16 @@
     'version': '8.0.1.0.0',
     'author': "Incaser Informatica S.L.,Odoo Community Association (OCA)",
     'depends': ['payment'],
+    "external_dependencies": {
+        "python": [
+            "Crypto.Cipher.DES3",
+        ],
+        "bin": [],
+    },
     'data': [
         'views/redsys.xml',
         'views/payment_acquirer.xml'
     ],
+    'license': 'AGPL-3',
     'installable': True,
 }
