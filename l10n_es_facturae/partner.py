432884a8a20439d5edc67c8b2f2b0d964c895eba
Omar <omarcs7r@gmail.com>
2015-12-04 17:48:19 +0100

    [IMP] l10n_es_facturae: Se permite opcionalmente registrar el cuarto nivel de los centros administrativos en el xml de facturae, el organo proponente

--- l10n_es_facturae/partner.py
+++ l10n_es_facturae/partner.py
@@ -30,3 +30,4 @@ class ResPartner(models.Model):
     organo_gestor = fields.Char('Órgano gestor', size=10)
     unidad_tramitadora = fields.Char('Unidad tramitadora', size=10)
     oficina_contable = fields.Char('Oficina contable', size=10)
+    organo_proponente = fields.Char('Órgano proponente', size=10)
