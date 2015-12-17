574da26aa63fc8345434aac961a58460411c9b6b
sergio-incaser <sergio@incaser.es>
2015-12-17 18:50:31 +0100

    [IMP] payment_redsys: Show order information after payment

--- payment_redsys/models/redsys.py
+++ payment_redsys/models/redsys.py
@@ -85,6 +85,9 @@ class AcquirerRedsys(models.Model):
     send_quotation = fields.Boolean('Send quotation', default=True)
 
     def _prepare_merchant_parameters(self, acquirer, tx_values):
+        base_url = self.env['ir.config_parameter'].get_param('web.base.url')
+        sale_order = self.env['sale.order'].search(
+            [('name', '=', tx_values['reference'])])
         values = {
             'Ds_Sermepa_Url': (
                 self._get_redsys_urls(acquirer.environment)[
@@ -116,8 +119,12 @@ class AcquirerRedsys(models.Model):
                 acquirer.redsys_merchant_description[:125]),
             'Ds_Merchant_ConsumerLanguage': (
                 acquirer.redsys_merchant_lang or '001'),
-            'Ds_Merchant_UrlOk': acquirer.redsys_url_ok or '',
-            'Ds_Merchant_UrlKo': acquirer.redsys_url_ko or '',
+            'Ds_Merchant_UrlOk':
+            '%s/payment/redsys/result/%s?order_id=%s' % (
+                base_url, acquirer.redsys_url_ok, sale_order.id),
+            'Ds_Merchant_UrlKo':
+            '%s/payment/redsys/result/%s?order_id=%s' % (
+                base_url, acquirer.redsys_url_ko, sale_order.id),
             'Ds_Merchant_Paymethods': acquirer.redsys_pay_method or 'T',
         }
         return self._url_encode64(json.dumps(values))
