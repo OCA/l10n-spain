8915146238c5e6837444dccbee4a3978f77a2be3
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-01-18 21:58:22 +0100

    [FIX] payment_redsys: Bump version number

--- payment_redsys/__openerp__.py
+++ payment_redsys/__openerp__.py
@@ -4,7 +4,7 @@
     'name': 'Redsys Payment Acquirer',
     'category': 'Hidden',
     'summary': 'Payment Acquirer: Redsys Implementation',
-    'version': '8.0.1.0.1',
+    'version': '8.0.1.0.2',
     'author': "Incaser Informatica S.L.,Odoo Community Association (OCA)",
     'depends': ['payment'],
     "external_dependencies": {
