8f6a67e2b7b1b25e53103e6b4be960193a364290
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-01-20 23:30:43 +0100

    [FIX+IMP] l10n_es_aeat_mod349: 3 things:
    * IMP: Fiscal position onchange in new API + speed imp + refactoring
    * FIX: l10n_es_aeat_mod349: Remove check of names
    
      There's no need of a check about the name validity, because l10n_es_aeat
      already transform all the exported strings to valid ASCII characters
      (though unidecode), so I remove it entirely this check.
    
    * FIX: Use first period year in export
    
      Relying in fiscal year code for getting the fiscal year is not safe,
      because you can use that code for defining your journal sequences (for
      example, with the module account_auto_fy_sequence).
    
      We can't use also the year of the fiscal year, because you can have
      split fiscal years (i. e. like the academic courses). What we uses then
      is the year of the first period of the declaration, as there's no
      legal possibility of having mixed periods from different years.

--- l10n_es_aeat_mod349/models/account_invoice.py
+++ l10n_es_aeat_mod349/models/account_invoice.py
@@ -2,9 +2,7 @@
 ##############################################################################
 # For copyright and license notices, see __openerp__.py file in root directory
 ##############################################################################
-from openerp import models, fields, api, exceptions, _
-from datetime import datetime, date
-import calendar
+from openerp import models, fields, api
 
 OPERATION_KEYS = [
     ('E', 'E - Intra-Community supplies'),
@@ -36,118 +34,14 @@ class AccountInvoice(models.Model):
                 # Establecer a adquisición si es de compra
                 return 'A'
 
-    def _get_year_from_fy_month(self, fy, month):
-        fy_start = fields.Date.from_string(fy.date_start)
-        fy_stop = fields.Date.from_string(fy.date_stop)
-        if fy_start.month < month:
-            year = fy_start.year
-        elif fy_stop.month > month:
-            year = fy_stop.year
-        else:
-            raise exceptions.Warning(
-                _('Cannot get invoices.\nProvided month is not included on '
-                  'selected fiscal year'))
-        return year
-
-    @api.model
-    def _get_invoices_by_type(
-            self, partner, operation_key, fiscalyear=None,
-            period_id=None, month=None, period_selection=None):
-        """
-        Returns invoices ids by type (supplier/customer) for a fiscal
-        year, period or month.
-        """
-        assert period_selection, 'There is no period selected'
-        # Set type of invoice
-        invoice_type = ('in_invoice', 'out_invoice', 'in_refund', 'out_refund')
-        domain = [('partner_id', 'child_of', partner.id),
-                  ('state', 'in', ['open', 'paid']),
-                  ('type', 'in', invoice_type),
-                  ('operation_key', '=', operation_key)]
-        # Invoices by fiscalyear (Annual)
-        if period_selection == '0A':
-            if not fiscalyear:
-                raise exceptions.Warning(
-                    _('Cannot get invoices.\nThere is no fiscal year '
-                      'selected'))
-            domain.append(('period_id', 'in',
-                           [period.id for period in fiscalyear.period_ids
-                            if not period.special]))
-        # Invoices by period
-        elif period_selection in ['1T', '2T', '3T', '4T']:
-            if not period_id:
-                raise exceptions.Warning(
-                    _('Cannot get invoices.\nThere is no period selected'))
-            domain.append(('period_id', 'in', period_id))
-        # Invoices by month
-        else:
-            if not month and not fiscalyear:
-                raise exceptions.Warning(
-                    _('Cannot get invoices.\nThere is no month and/or fiscal '
-                      'year selected'))
-            month = int(month)
-            year = self._get_year_from_fy_month(fiscalyear, month)
-            month_last_day = calendar.monthrange(year, month)[1]
-            date_start = datetime(year=year, month=month, day=1)
-            date_stop = datetime(year=year, month=month, day=month_last_day)
-            domain.append(
-                ('date_invoice', '>=', fields.Date.to_string(date_start)))
-            domain.append(
-                ('date_invoice', '<=', fields.Date.to_string(date_stop)))
-        return self.search(domain)
-
     @api.multi
-    def clean_refund_invoices(
-            self, partner, fiscalyear=None, periods=None, month=None,
-            period_selection=None):
-        """Separate refunds from invoices"""
-        invoices = self.env['account.invoice']
-        refunds = self.env['account.invoice']
-        for inv in self:
-            if inv.type in ('in_refund', 'out_refund'):
-                if not inv.origin_invoices_ids:
-                    invoices += inv
-                    continue
-                for origin_line in inv.origin_invoices_ids:
-                    if (origin_line.state in ('open', 'paid') and
-                            origin_line.partner_id.commercial_partner_id ==
-                            partner):
-                        if period_selection == '0A':
-                            if (origin_line.period_id.id not in
-                                    [period.id for period in
-                                     fiscalyear.period_ids if not
-                                     period.special]):
-                                refunds += inv
-                            else:
-                                invoices += inv
-                        elif period_selection in ['1T', '2T', '3T', '4T']:
-                            if origin_line.period_id not in periods:
-                                refunds += inv
-                            else:
-                                invoices += inv
-                        else:
-                            month = int(month)
-                            year = self._get_year_from_fy_month(fiscalyear,
-                                                                month)
-                            if (fields.Date.from_string(
-                                    origin_line.date_invoice) <
-                                    date(year=year, month=month, day=1)):
-                                refunds += inv
-                            else:
-                                invoices += inv
-                        break
-            else:
-                invoices += inv
-        return invoices, refunds
-
-    @api.multi
-    def on_change_fiscal_position(self, fiscal_position, invoice_type):
+    @api.onchange('fiscal_position')
+    def onchange_fiscal_position_l10n_es_aeat_mod349(self):
         """Suggest an operation key when fiscal position changes."""
-        res = {'operation_key': False}
-        if fiscal_position and invoice_type:
-            fp = self.env['account.fiscal.position'].browse(fiscal_position)
-            res['operation_key'] = self._get_operation_key(fp, invoice_type)
-        return {'value': res}
+        for invoice in self:
+            if invoice.fiscal_position:
+                invoice.operation_key = self._get_operation_key(
+                    invoice.fiscal_position, invoice.type)
 
     @api.model
     def create(self, vals):
