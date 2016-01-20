e1f7d2b10542e9c51f935fdc00a252251ff9d6a7
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-01-20 23:30:43 +0100

    [FIX+IMP] l10n_es_aeat_mod349: 3 things:
    * IMP: Fiscal position onchange in new API + speed imp + refactoring
    * FIX: l10n_es_aeat_mod349: Remove check of names
    
      There's no need of a check about the name validity, because l10n_es_aeat
      already transform all the exported strings to valid ASCII characters
      (though unidecode), so I remove it entirely this check.
    
    * FIX: Use first period year in export
    
      Relying in fiscal year code for getting the fiscal year is not safe,
      because you can use that code for defining your journal sequences (for
      example, with the module account_auto_fy_sequence).
    
      We can't use also the year of the fiscal year, because you can have
      split fiscal years (i. e. like the academic courses). What we uses then
      is the year of the first period of the declaration, as there's no
      legal possibility of having mixed periods from different years.

--- l10n_es_aeat_mod349/__openerp__.py
+++ l10n_es_aeat_mod349/__openerp__.py
@@ -25,7 +25,7 @@
 
 {
     "name": "Modelo 349 AEAT",
-    "version": "8.0.2.0.0",
+    "version": "8.0.2.1.0",
     "author": "Pexego, "
               "Top Consultant, "
               "Serv. Tecnol. Avanzados - Pedro M. Baeza, "
