6216c190cb53a99a42effd6ce9c1e5dc5c2727af
Antonio Espinosa <antespi@gmail.com>
2016-02-01 17:59:53 +0100

    Use separate and explicit fields for each NUTS level

--- l10n_es_location_nuts/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# © 2015 Antiun Ingenieria S.L. - Antonio Espinosa
-# © 2015 Antiun Ingenieria S.L. - Jairo Llopis
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
-from . import res_partner
