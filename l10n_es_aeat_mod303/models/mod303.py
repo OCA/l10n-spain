e155820bacd07c2fa9388f6186f337d97aff68de
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-02-11 17:28:32 +0100

    [FIX] l10n_es_aeat_*: Mejor herencia de impuestos
    El anterior método utilizado, al tener una clase y un many2one genérico,
    no permitía poner los datos en múltiples modelos hijos, ya que se repetían
    los IDs, y cogía los datos de un modelo en otros.
    
    Con este nuevo sistema, se sigue utilizando una tabla, pero con doble
    índice: el modelo y el ID, parecido a un campo reference, pero gestionado
    por código propio en el método de cálculo.
    
    El 303 está adaptado a este nuevo sistema, y se incluye un script de
    migración que conserve los datos.

--- l10n_es_aeat_mod303/models/mod303.py
+++ l10n_es_aeat_mod303/models/mod303.py
@@ -1,25 +1,14 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see http://www.gnu.org/licenses/.
-#
-##############################################################################
+# -*- coding: utf-8 -*-
+# © 2013 - Guadaltech - Alberto Martín Cortada
+# © 2015 - AvanzOSC - Ainara Galdona
+# © 2014-2016 - Serv. Tecnol. Avanzados - Pedro M. Baeza
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 from openerp import models, fields, api, _
 
 
 class L10nEsAeatMod303Report(models.Model):
-    _inherit = "l10n.es.aeat.report"
+    _inherit = "l10n.es.aeat.report.tax.mapping"
     _name = "l10n.es.aeat.mod303.report"
     _description = "AEAT 303 Report"
 
