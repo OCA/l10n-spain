17ea32649f1c1d3b1dbd32be332c1434e69ec324
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-02-16 19:23:32 +0100

    [IMP+FIX] l10n_es_account_asset: New API + fix start depreciation date + remove # depreciations compensation. Fixes #146 +  Delete empty last depreciation line

--- l10n_es_account_asset/models/__init__.py
+++ l10n_es_account_asset/models/__init__.py
@@ -2,4 +2,4 @@
 # © 2012-2015 Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from . import models
+from . import account_asset
