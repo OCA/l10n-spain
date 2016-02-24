c3e41d2df05341cf166da4def7ca63ed51d146e1
Omar <omarcs7r@gmail.com>
2016-02-24 12:13:29 +0100

    [REN][IMP] l10n_es_account_banking_sepa_fsdd: Se renombra el módulo para incluir el prefijo de la localización, se cambia la configuración del cobro financiado de la orden al modo de pago y la acción al tiempo de exportación

---
+++ l10n_es_account_banking_sepa_fsdd/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# © 2016 Comunitea Servicios Tecnológicos <omar@comunitea.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Account Banking Sepa - FSDD (Anticipos de crédito)",
+    "version": "8.0.1.0.0",
+    "author": "Comunitea,Odoo Community Association (OCA)",
+    "website": "http://www.comunitea.com",
+    "category": "Banking addons",
+    "depends": [
+        "account_banking_sepa_direct_debit"
+    ],
+    "demo": [],
+    "data": [
+        "views/payment_mode_view.xml",
+    ],
+    "installable": True,
+}
