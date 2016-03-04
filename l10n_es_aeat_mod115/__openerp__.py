d87c5cce68f83e1b5bd2b82ea2e1e9f694fa916e
Jose Maria Alzaga <jose.alzaga@aselcis.com>
2016-03-04 17:26:15 +0100

    Fix inherit problem in Mods 115,216 and 296

--- l10n_es_aeat_mod115/__openerp__.py
+++ l10n_es_aeat_mod115/__openerp__.py
@@ -20,7 +20,7 @@
 
 {
     'name': 'AEAT modelo 115',
-    'version': '8.0.1.1.0',
+    'version': '8.0.1.2.0',
     'category': "Localisation/Accounting",
     'author': "Serv. Tecnol. Avanzados - Pedro M. Baeza,"
               "AvanzOSC,"
