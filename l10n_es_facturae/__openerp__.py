6cced87eda27a09ae9b517f3364103d60cff1753
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-03-13 02:53:13 +0100

    l10n_es_facturae: Use report_xml
    Using report_xml engine, the output is more readable and the maintenance
    of the exporter in case of any update is far better than existing one.

--- l10n_es_facturae/__openerp__.py
+++ l10n_es_facturae/__openerp__.py
@@ -2,7 +2,6 @@
 # © 2009 Alejandro Sanchez <alejandro@asr-oss.com>
 # © 2015 Ismael Calvo <ismael.calvo@factorlibre.com>
 # © 2015 Tecon
-# © 2015 Juanjo Algaz (MalagaTIC)
 # © 2015 Omar Castiñeira (Comunitea)
 # © 2016 Serv. Tecnol. Avanzados - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
@@ -13,8 +12,6 @@
     "author": "ASR-OSS, "
               "FactorLibre, "
               "Tecon, "
-              "Pexego, "
-              "Malagatic, "
               "Comunitea, "
               "Serv. Tecnol. Avanzados - Pedro M. Baeza, "
               "Odoo Community Association (OCA)",
@@ -24,11 +21,14 @@
     "depends": [
         "account_payment_partner",
         "base_iso3166",
+        "base_vat",
+        "report_xml",
     ],
     "data": [
         "views/res_partner_view.xml",
         "views/res_company.xml",
         "views/payment_mode_view.xml",
+        "views/report_facturae.xml",
         "wizard/create_facturae_view.xml",
     ],
     "installable": True,
