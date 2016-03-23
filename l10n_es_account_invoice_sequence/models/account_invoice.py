b9ccc13be1c6baa39ca2d9b306a00c88a0d2374f
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2016-03-23 19:46:55 +0100

    pep8

--- l10n_es_account_invoice_sequence/models/account_invoice.py
+++ l10n_es_account_invoice_sequence/models/account_invoice.py
@@ -22,7 +22,7 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from openerp import models, fields, api, _, exceptions
+from openerp import models, fields, api
 
 
 class AccountInvoice(models.Model):
