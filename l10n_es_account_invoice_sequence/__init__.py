f041321b0e7f76d0668cafcd95df7ccd5d1109d2
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-03-28 17:32:14 +0200

    [MIG] l10n_es_account_invoice_sequence: Migration to 9.0
    * Full tests
    * All flows covered
    * Journal fix on install

--- l10n_es_account_invoice_sequence/__init__.py
+++ l10n_es_account_invoice_sequence/__init__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
-# © 2015 Pedro M. Baeza
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
+from .hooks import post_init_hook
 from . import models
-from . import wizard
-from .hooks import fill_invoice_sequences
+from . import wizards
