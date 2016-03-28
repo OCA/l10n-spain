8250a0a2d2df4f1fb0ff871d70f2a4b63ca8b6c6
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-03-28 17:32:14 +0200

    [MIG] l10n_es_account_invoice_sequence: Migration to 9.0
    * Full tests
    * All flows covered
    * Journal fix on install

--- l10n_es_account_invoice_sequence/constants.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# (c) 2015 Pedro M. Baeza
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-
-ALLOWED_JOURNAL_TYPES = ('sale', 'sale_refund', 'purchase', 'purchase_refund')
