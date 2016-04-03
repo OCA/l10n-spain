2ac00eba4faeadb3f47b24fac4e8e266558c13a4
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-04-03 21:48:22 +0200

    [IMP] l10n_es_account_invoice_sequence: Tests in Python + performance

--- l10n_es_account_invoice_sequence/__openerp__.py
+++ l10n_es_account_invoice_sequence/__openerp__.py
@@ -1,29 +1,11 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-# Copyright (c) 2011 NaN Projectes de Programari Lliure, S.L.
-#                    http://www.NaN-tic.com
-#    Copyright (c) 2013 Serv. Tecnol. Avanzados (http://www.serviciosbaeza.com)
-#                       Pedro Manuel Baeza <pedro.baeza@serviciosbaeza.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2011 NaN Projectes de Programari Lliure, S.L.
+# © 2013-2016 Pedro M. Baeza
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
     "name": "Secuencia para facturas separada de la secuencia de asientos",
-    "version": "8.0.1.2.1",
+    "version": "8.0.1.3.0",
     "author": "Spanish Localization Team, "
               "NaN·Tic, "
               "Trey, "
@@ -38,10 +20,6 @@
     "data": [
         'data/sequence_data.xml',
         'views/account_view.xml',
-    ],
-    "demo": [],
-    "test": [
-        'test/sequence.yml'
     ],
     "post_init_hook": "fill_invoice_sequences",
     "installable": True,
