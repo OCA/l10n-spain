51a5673d06e90b92cd8a2fbf57295a40877a07ee
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-04-09 19:01:10 +0200

    [IMP] l10n_es_aeat_vat_prorrate: Include analytics + tests

--- l10n_es_aeat_vat_prorrate/__openerp__.py
+++ l10n_es_aeat_vat_prorrate/__openerp__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # (c) 2015 Antiun Ingeniería S.L. - Pedro M. Baeza
 # (c) 2015 AvanzOSC - Ainara Galdona
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0
 
 {
     "name": "AEAT - Prorrata de IVA",
