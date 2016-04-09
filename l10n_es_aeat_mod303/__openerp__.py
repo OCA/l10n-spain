9685ec85a938b5633467d876b6c6e9f818667b36
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-04-09 19:01:10 +0200

    [IMP] l10n_es_aeat_vat_prorrate: Include analytics + tests

--- l10n_es_aeat_mod303/__openerp__.py
+++ l10n_es_aeat_mod303/__openerp__.py
@@ -1,28 +1,8 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (c)
-#       2013      Guadaltech (http://www.guadaltech.es)
-#                 Alberto Martín Cortada
-#       2014-2015 Serv. Tecnol. Avanzados (http://www.serviciosbaeza.com)
-#                 Pedro M. Baeza <pedro.baeza@serviciobaeza.com>
-#       2015      AvanzOSC (http://www.avanzosc.es)
-#                 Ainara Galdona
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see http://www.gnu.org/licenses/.
-#
-##############################################################################
+# -*- coding: utf-8 -*-
+# © 2013 Alberto Martín Cortada (Guadaltech)
+# © 2014-2015 Pedro M. Baeza
+# © 2015 AvanzOSC - Ainara Galdona
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0
 
 {
     "name": "AEAT modelo 303",
