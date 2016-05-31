add829e6cf317b026a22251d46c447bee4287afb
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-05-31 18:52:57 +0200

    [FIX] l10n_es_account_bank_statement_import_n43: Populate correctly partner_id

--- l10n_es_account_bank_statement_import_n43/__openerp__.py
+++ l10n_es_account_bank_statement_import_n43/__openerp__.py
@@ -1,30 +1,15 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    Copyright(c) 2013-2015 Serv. Tecnol. Avanzados (http://serviciosbaeza.com)
-#                           Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# -*- coding: utf-8 -*-
+# © 2013-2015 Serv. Tecnol. Avanzados - Pedro M. Baeza
+# © 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 {
     'name': 'Importación de extractos bancarios españoles (Norma 43)',
     'category': 'Accounting & Finance',
-    'version': '8.0.4.0.1',
+    'version': '8.0.4.0.2',
     'license': 'AGPL-3',
     'author': 'Spanish Localization Team,'
-              'Serv. Tecnol. Avanzados - Pedro M. Baeza,'
+              'Tecnativa,'
               'Antiun Ingenería S.L.,'
               'Odoo Community Association (OCA)',
     "website": "https://github.com/OCA/l10n-spain",
