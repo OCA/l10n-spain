2b0b25e85305610aac6d6989f14891e4d3ab5010
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-08-14 15:10:31 +0200

    [MIG] l10n_es_aeat: Initial migration

--- l10n_es_aeat/__manifest__.py
+++ l10n_es_aeat/__manifest__.py
@@ -1,43 +1,23 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (C) 2004-2011
-#        Pexego Sistemas Informáticos. (http://pexego.es)
-#        Luis Manuel Angueira Blanco
-#    Copyright (C) 2013
-#        Ignacio Ibeas - Acysos S.L. (http://acysos.com)
-#        Migración a OpenERP 7.0
-#    Copyright (C) 2013-2015
-#        Serv. Tecnol. Avanzados - Pedro M. Baeza (www.serviciosbaeza.com)
-#    Copyright (C) 2015
-#        Ainara Galdona - AvanzOSC (https://www.avanzosc.es)
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2004-2011 Luis Manuel Angueira Blanco - Pexego
+# Copyright 2013 Ignacio Ibeas - Acysos S.L. (http://acysos.com)
+# Copyright 2015 Ainara Galdona <agaldona@avanzosc.com>
+# Copyright 2013-2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl
+
 {
     'name': "AEAT Base",
-    'version': "8.0.1.9.0",
+    'version': "9.0.1.0.0",
     'author': "Pexego,"
-              "Serv. Tecnol. Avanzados - Pedro M. Baeza,"
+              "Acysos,"
               "AvanzOSC,"
+              "Tecnativa,"
               "Odoo Community Association (OCA)",
     'license': "AGPL-3",
     'contributors': [
         'Ignacio Ibeas (Acysos S.L.)',
         'Ainara Galdona',
-        'Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>',
+        'Pedro M. Baeza <pedro.baeza@tecnativa.com>',
         'Santi Argüeso <santi@comunitea.com>',
         'cubells <info@obertix.net>',
     ],
@@ -52,7 +32,6 @@
     'data': [
         'security/aeat_security.xml',
         'security/ir.model.access.csv',
-        'data/aeat_sequence_type.xml',
         'data/aeat_partner.xml',
         'wizard/export_to_boe_wizard.xml',
         'views/aeat_menuitem.xml',
@@ -62,5 +41,5 @@
         'views/aeat_tax_code_mapping_view.xml',
         'views/account_move_line_view.xml',
     ],
-    'installable': False,
+    'installable': True,
 }
