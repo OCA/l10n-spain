c9ca1a81ed0e79370b1a4fbc8cf1a31595bd9ee7
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-10-18 20:33:39 +0200

    [FIX] l10n_es_aeat: Bump version

--- l10n_es_aeat/__openerp__.py
+++ l10n_es_aeat/__openerp__.py
@@ -28,7 +28,7 @@
 ##############################################################################
 {
     'name': "AEAT Base",
-    'version': "8.0.1.8.0",
+    'version': "8.0.1.9.0",
     'author': "Pexego,"
               "Serv. Tecnol. Avanzados - Pedro M. Baeza,"
               "AvanzOSC,"
