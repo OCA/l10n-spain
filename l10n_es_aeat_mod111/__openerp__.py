478b7f589fc6d2fea110298b96328706ffc7622d
Antonio Espinosa <antespi@gmail.com>
2016-10-20 15:33:43 +0200

    l10n_es_aeat_mod111: Migration to 9.0
    * Added tests

--- l10n_es_aeat_mod111/__openerp__.py
+++ l10n_es_aeat_mod111/__openerp__.py
@@ -1,37 +1,26 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#  This program is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU Affero General Public License as
-#  published by the Free Software Foundation, either version 3 of the
-#  License, or (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-#  GNU Affero General Public License for more details.
-#
-#  You should have received a copy of the GNU Affero General Public License
-#  along with this program. If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2016 Antonio Espinosa <antonio.espinosa@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl
 
 {
     'name': 'AEAT modelo 111',
-    'version': '8.0.2.0.0',
+    'version': '9.0.1.0.0',
     'category': "Localisation/Accounting",
     'author': "AvanzOSC,"
-              "Serv. Tecnol. Avanzados - Pedro M. Baeza,"
-              "Antiun Ingeniería S.L.,"
-              "MálagaTIC,"
-              "G. Vermon,"
+              "RGB Consulting SL,"
+              "Tecnativa,"
               "Odoo Community Association (OCA)",
     'website': "https://github.com/OCA/l10n-spain",
     'license': 'AGPL-3',
-    'depends': ['l10n_es_aeat'],
+    'depends': [
+        'l10n_es',
+        'l10n_es_aeat',
+    ],
     'data': [
         'data/aeat_export_mod111_data.xml',
+        'data/tax_code_map_mod111_data.xml',
         'views/mod111_view.xml',
-        'security/ir.model.access.csv'],
+        'security/ir.model.access.csv',
+    ],
     'installable': True,
 }
