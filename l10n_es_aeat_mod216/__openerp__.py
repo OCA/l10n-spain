503ca902701dc57ffc9d4f0f6964a13be7e41883
Antonio Espinosa <antespi@gmail.com>
2016-10-28 11:50:18 +0200

    [MIG] l10n_es_aeat_mod216: Migration to 9.0

--- l10n_es_aeat_mod216/__openerp__.py
+++ l10n_es_aeat_mod216/__openerp__.py
@@ -1,40 +1,27 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#  This program is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU Affero General Public License as
-#  published by the Free Software Foundation, either version 3 of the
-#  License, or (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-#  GNU Affero General Public License for more details.
-#
-#  You should have received a copy of the GNU Affero General Public License
-#  along with this program. If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# -*- coding: utf-8 -*-
+# Copyright 2015 AvanzOSC - Ainara Galdona
+# Copyright 2015 Pedro M. Baeza
+# Copyright 2016 Antonio Espinosa <antonio.espinosa@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'AEAT modelo 216',
-    'version': '8.0.1.2.0',
+    'version': '9.0.1.0.0',
     'category': "Localisation/Accounting",
-    'author': "Serv. Tecnol. Avanzados - Pedro M. Baeza,"
-              "AvanzOSC,"
-              "Antiun Ingeniería S.L.,"
+    'author': "AvanzOSC,"
+              "Tecnativa,"
               "Odoo Community Association (OCA)",
     'website': "https://github.com/OCA/l10n-spain",
     'license': 'AGPL-3',
     'depends': [
-        'l10n_es_aeat',
         'l10n_es_aeat_mod111',
     ],
     'data': [
-        'wizard/export_mod216_to_boe.xml',
+        'security/ir.model.access.csv',
+        'data/tax_code_map_mod216_data.xml',
+        'data/aeat_export_mod216_data.xml',
         'views/mod216_view.xml',
         'views/res_partner_view.xml',
-        'security/ir.model.access.csv'
     ],
-    'installable': False,
+    'installable': True,
 }
