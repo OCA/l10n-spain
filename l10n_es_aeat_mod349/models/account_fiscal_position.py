0d61deea258f10bfd1063d1f633f945d33216695
Angel Moya <amoyapardo@gmail.com>
2016-11-08 22:51:12 +0100

    [MIG] l10n_es_aeat_mod349

--- l10n_es_aeat_mod349/models/account_fiscal_position.py
+++ l10n_es_aeat_mod349/models/account_fiscal_position.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-# For copyright and license notices, see __openerp__.py file in root directory
-##############################################################################
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from openerp import models, fields
 
 
