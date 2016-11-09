6dbfc51a41974ce8882b76fe1242b0aebc1fcbeb
Enrique Zanardi <ezanardi@atlantux.com>
2016-11-09 13:20:24 +0000

    Añadida posición fiscal
    para Importaciones a Canarias y Exportaciones desde Canarias

--- l10n_es_igic/__openerp__.py
+++ l10n_es_igic/__openerp__.py
@@ -23,9 +23,10 @@
     "name": "IGIC (Impuesto General Indirecto Canario)",
     "version": "8.0.0.1",
     "author": "David Diz Martínez <daviddiz@gmail.com>,"
+              "Atlantux Consultores - Enrique Zanardi,",
               "Odoo Community Association (OCA)",
     "website": "https://github.com/daviddiz/igic",
-    "category": "Localisation/Europe",
+    "category": "Localization/Europe",
     "depends": [
         "l10n_es",
     ],
@@ -36,6 +37,7 @@
         "data/tax_codes_igic.xml",
         "data/taxes_igic.xml",
         "data/account_chart_template_igic_post.xml",
+        "data/fiscal_positions_igic.xml",
     ],
     'images': [],
     "demo": [],
