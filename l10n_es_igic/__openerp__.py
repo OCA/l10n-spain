b5178d2e3c6efd128ed8c6ab5deb1207f37b0bc0
Enrique Zanardi <ezanardi@atlantux.com>
2016-11-11 12:30:15 +0000

    typo

--- l10n_es_igic/__openerp__.py
+++ l10n_es_igic/__openerp__.py
@@ -22,8 +22,8 @@
 {
     "name": "IGIC (Impuesto General Indirecto Canario)",
     "version": "8.0.0.1",
-    "author": "David Diz Martínez <daviddiz@gmail.com>,"
-              "Atlantux Consultores - Enrique Zanardi,",
+    "author": "David Diz Martínez <daviddiz@gmail.com>, "
+              "Atlantux Consultores - Enrique Zanardi, "
               "Odoo Community Association (OCA)",
     "website": "https://github.com/daviddiz/igic",
     "category": "Localization/Europe",
