3465832a9064a31481d88e75a1836749c9810f20
Carlos Dauden <carlos.dauden@tecnativa.com>
2016-11-15 16:50:34 +0100

    l10n_es_partner: Return commercial name in name_get with configurable pattern

--- l10n_es_partner/models/res_partner.py
+++ l10n_es_partner/models/res_partner.py
@@ -2,15 +2,16 @@
 # © 2009 Jordi Esteve <jesteve@zikzakmedia.com>
 # © 2012-2014 Ignacio Ibeas <ignacio@acysos.com>
 # © 2016 Pedro M. Baeza
+# © 2016 Carlos Dauden <carlos.dauden@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3).
 
-from openerp import fields, models
+from openerp import api, fields, models
 
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    comercial = fields.Char('Trade name', size=128, select=True)
+    comercial = fields.Char('Trade name', size=128, index=True)
 
     def name_search(self, cr, uid, name, args=None, operator='ilike',
                     context=None, limit=100):
@@ -24,3 +25,19 @@ class ResPartner(models.Model):
             ids = self.search(cr, uid, [('comercial', operator, name)] + args,
                               limit=limit, context=context)
         return self.name_get(cr, uid, ids, context=context)
+
+    @api.multi
+    @api.depends('name', 'comercial')
+    def name_get(self):
+        result = []
+        name_pattern = self.env['ir.config_parameter'].get_param(
+            'l10n_es_partner.name_pattern', default='')
+        orig_name = dict(super(ResPartner, self).name_get())
+        for partner in self:
+            name = orig_name[partner.id]
+            comercial = partner.commercial_partner_id.comercial
+            if comercial and name_pattern:
+                name = name_pattern % {'name': name,
+                                       'comercial_name': comercial}
+            result.append((partner.id, name))
+        return result
