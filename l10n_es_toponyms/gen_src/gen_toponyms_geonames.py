781d467ce9bfa3ef8c2f6c4737c0b6eaa5b782b2
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-12-01 00:08:48 +0100

    [MIG] l10n_es_toponyms: Migration to 10.0

--- l10n_es_toponyms/gen_src/gen_toponyms_geonames.py
+++ l10n_es_toponyms/gen_src/gen_toponyms_geonames.py
@@ -1,24 +1,7 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (c) 2013 Serv. Tecnol. Avanzados (http://www.serviciosbaeza.com)
-#                       Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# -*- coding: utf-8 -*-
+# Copyright 2013-2017 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 import os.path
 import requests
 import tempfile
@@ -28,9 +11,8 @@ import zipfile
 
 
 def filter_city(originalName):
-    """
-    Pone correctamente la capitalización de palabras como 'De', 'Las', 'Los',
-    cuando son palabras intermedias en el nombre de la ciudad.
+    """Pone correctamente la capitalización de palabras como 'De', 'Las',
+    'Los', cuando son palabras intermedias en el nombre de la ciudad.
     """
     return originalName.replace(' De ', ' de ').replace(' Del ', ' del ').\
         replace(' La ', ' la ').replace(' Las ', ' las ').\
@@ -53,25 +35,27 @@ if __name__ == "__main__":
     print 'Generando XML...'
     output = open("l10n_es_toponyms_zipcodes.xml", 'w')
     output.write('<?xml version="1.0" encoding="UTF-8"?>\n')
-    output.write('<openerp>\n')
-    output.write(' ' * 4 + '<data noupdate="1">\n')
+    output.write('<odoo noupdate="1">\n')
     cont = 0
-    for row in unicodecsv.reader(
-            data_file, encoding='utf-8', delimiter='	'):
+    reader = unicodecsv.reader(data_file, encoding='utf-8', delimiter='	')
+    for row in reader:
         cont += 1
         output.write(' ' * 8 +
                      '<record id="city_ES_%s" model="res.better.zip">\n' %
                      cont)
-        output.write(' ' * 12 + '<field name="state_id" ref="'
-                                'l10n_es_toponyms.ES%s"/>\n' % row[1][:2])
+        state = row[6].lower()
+        if state == 'me':
+            state = 'ml'  # FIXME: While Odoo changes this
+        output.write(
+            ' ' * 12 +
+            '<field name="state_id" ref="base.state_es_{}"/>\n'.format(state)
+        )
         output.write(' ' * 12 + '<field name="city">%s</field>\n' %
                      filter_city(row[2]).encode('utf-8'))
         output.write(' ' * 12 + '<field name="name">%s</field>\n' % row[1])
         output.write(' ' * 12 + '<field name="country_id" ref="base.es"/>\n')
         output.write(' ' * 8 + '</record>\n')
     data_file.close()
-    output.write(' ' * 4 + '</data>\n')
-    output.write('</openerp>\n')
-    # Cerrar archivo
+    output.write('</odoo>\n')
     output.close()
     print "Proceso terminado"
