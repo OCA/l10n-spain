e5d34eadd33621574cc85b4d66a30bf63e1337fb
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-11-30 08:41:17 +0100

    [MIG] l10n_es_toponyms: Migration to 9.0
    * Update static l10n_es_toponyms_zipcodes.xml
    * Test in Python + improve speed

--- l10n_es_toponyms/__openerp__.py
+++ l10n_es_toponyms/__openerp__.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 # © 2009 Jordi Esteve <jesteve@zikzakmedia.com>
-# © 2013-2016 Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
+# © 2013-2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Topónimos españoles",
-    "version": "8.0.2.0.1",
+    "version": "9.0.1.0.0",
     "author": "Spanish Localization Team, "
               "ZikZakMedia, "
-              "Serv. Tecnol. Avanzados - Pedro M. Baeza, "
+              "Tecnativa, "
               "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-spain",
     "category": "Localisation/Europe",
@@ -17,13 +17,14 @@
     ],
     'contributors': [
         'Jordi Esteve <jesteve@zikzakmedia.com>',
-        'Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>',
+        'Pedro M. Baeza <pedro.baeza@tecnativa.com>',
     ],
     "license": "AGPL-3",
     "data": [
         "wizard/l10n_es_toponyms_wizard.xml",
     ],
-    'images': ['images/l10n_es_toponyms_config.png'],
-    "demo": [],
-    "installable": True
+    'images': [
+        'images/l10n_es_toponyms_config.png'
+    ],
+    'installable': True
 }
