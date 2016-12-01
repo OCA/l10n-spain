60dec0408bc725033963d4253fd3f29d86b10803
Sergio Teruel Albert <sergio.teruel@tecnativa.com>
2016-12-01 19:17:20 +0100

    [MIG] payment_redsys: Migration to v9
    Includes to allow show order for unauthenticated users

--- payment_redsys/__openerp__.py
+++ payment_redsys/__openerp__.py
@@ -4,8 +4,9 @@
     'name': 'Redsys Payment Acquirer',
     'category': 'Hidden',
     'summary': 'Payment Acquirer: Redsys Implementation',
-    'version': '8.0.1.0.2',
-    'author': "Incaser Informatica S.L.,Odoo Community Association (OCA)",
+    'version': '9.0.1.0.0',
+    'author': "Tecnativa,"
+              "Odoo Community Association (OCA)",
     'depends': ['payment'],
     "external_dependencies": {
         "python": [
@@ -15,7 +16,8 @@
     },
     'data': [
         'views/redsys.xml',
-        'views/payment_acquirer.xml'
+        'views/payment_acquirer.xml',
+        'data/payment_redsys.xml'
     ],
     'license': 'AGPL-3',
     'installable': True,
