751502c2274a9c705e6778e7b1657fb1a42772b5
Pedro M. Baeza <pedro.baeza@gmail.com>
2016-12-06 16:24:07 +0100

    [MIG] l10n_es_account_bank_statement_import_n43: Migration to 10.0

--- l10n_es_account_statement_import_n43/tests/test_l10n_es_account_bank_statement_import_n43.py
+++ l10n_es_account_statement_import_n43/tests/test_l10n_es_account_bank_statement_import_n43.py
@@ -2,8 +2,8 @@
 # © 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp.tests import common
-from openerp.modules.module import get_module_resource
+from odoo.tests import common
+from odoo.modules.module import get_module_resource
 
 
 class L10nEsAccountBankStatementImportN43(common.TransactionCase):
--- l10n_es_account_statement_import_n43/wizards/account_bank_statement_import_n43.py
+++ l10n_es_account_statement_import_n43/wizards/account_bank_statement_import_n43.py
@@ -3,7 +3,7 @@
 # © 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, fields, api, exceptions, _
+from odoo import models, fields, api, exceptions, _
 from datetime import datetime
 
 account_mapping = {
