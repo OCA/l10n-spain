5bbba27b3706bb0e34692200611e4af59c60c7be
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-01-23 02:03:35 +0100

    [MIG] l10n_es_aeat/mod303: Migration to 10.0

--- l10n_es_aeat_mod303/models/mod303.py
+++ l10n_es_aeat_mod303/models/mod303.py
@@ -6,7 +6,7 @@
 # Copyright 2016 Antonio Espinosa <antonio.espinosa@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields, api, _
+from odoo import models, fields, api, _
 
 
 class L10nEsAeatMod303Report(models.Model):
