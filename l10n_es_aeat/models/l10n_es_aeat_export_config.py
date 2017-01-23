163e6dde76c93ba62c6a89af1b6e98bda1a39d57
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-01-23 02:03:35 +0100

    [MIG] l10n_es_aeat/mod303: Migration to 10.0

--- l10n_es_aeat/models/l10n_es_aeat_export_config.py
+++ l10n_es_aeat/models/l10n_es_aeat_export_config.py
@@ -2,7 +2,7 @@
 # Copyright 2016 Antonio Espinosa <antonio.espinosa@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl
 
-from openerp import models, fields
+from odoo import models, fields
 
 
 class AeatModelExportConfig(models.Model):
@@ -12,8 +12,8 @@ class AeatModelExportConfig(models.Model):
     model_number = fields.Char(string='Model number', size=3)
     model_id = fields.Many2one(
         comodel_name='ir.model', string="Odoo model", oldname='model')
-    date_start = fields.Date(string='Start date')
-    date_end = fields.Date(string='End date')
+    date_start = fields.Date(string='Starting date')
+    date_end = fields.Date(string='Ending date')
     config_line_ids = fields.One2many(
         comodel_name='aeat.model.export.config.line', oldname='config_lines',
         inverse_name='export_config_id', string='Lines')
