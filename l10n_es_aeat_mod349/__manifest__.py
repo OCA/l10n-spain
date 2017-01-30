33e6b6202f269111c3f900baa638bbc088a5841a
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-01-30 19:51:15 +0100

    [FIX+IMP] l10n_es_aeat_mod349
    * Pylint
    * Use new API
    
    [skip ci]

--- l10n_es_aeat_mod349/__manifest__.py
+++ l10n_es_aeat_mod349/__manifest__.py
@@ -6,43 +6,19 @@
 #                     - Pedro M. Baeza (http://www.serviciosbaeza.com)
 # Copyright 2016 - Tecnativa - Angel Moya <odoo@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-##############################################################################
-#
-#    Copyright (C)
-#        2004-2011: Pexego Sistemas Informáticos. (http://pexego.es)
-#        2013:      Top Consultant Software Creations S.L.
-#                   (http://www.topconsultant.es/)
-#        2014-2015: Serv. Tecnol. Avanzados - Pedro M. Baeza
-#                   (http://www.serviciosbaeza.com)
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
 
 {
     "name": "Modelo 349 AEAT",
     "version": "9.0.1.0.0",
     "author": "Pexego, "
               "Top Consultant, "
-              "Serv. Tecnol. Avanzados - Pedro M. Baeza, "
-              "Antiun Ingeniería S.L.,"
+              "Tecnativa,"
               "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": 'Localisation/Accounting',
     'website': 'https://github.com/OCA/l10n-spain',
     "depends": [
-        "account_refund_original",
+        "account_invoice_refund_link",
         "l10n_es_aeat",
     ],
     'data': [
@@ -56,6 +32,6 @@
         "security/ir.model.access.csv",
         "security/mod_349_security.xml",
     ],
-    'post_init_hook': '_assign_invoice_operation_keys',
+    'post_init_hook': 'post_init_hook',
     'installable': True,
 }
