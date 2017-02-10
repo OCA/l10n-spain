61c0e095892aadac04f576f5cf458eb2e3e4b940
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-02-10 01:35:44 +0100

    [IMP] account_refund_original: Use better method
    Odoo already includes a method for preparing the refund, so using it
    we don't have to make any guess about the returned domain and the
    code also serves for refunds created directly by code.

--- account_refund_original/__init__.py
+++ account_refund_original/__init__.py
@@ -3,4 +3,3 @@
 # For copyright and license notices, see __openerp__.py file in root directory
 ##############################################################################
 from . import models
-from . import wizard
--- account_refund_original/models/account_invoice.py
+++ account_refund_original/models/account_invoice.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-# For copyright and license notices, see __openerp__.py file in root directory
-##############################################################################
+# Copyright 2004-2011 Pexego Sistemas Informáticos. (http://pexego.es)
+# Copyright 2014-2017 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, fields
+from openerp import api, fields, models
 
 
 class AccountInvoice(models.Model):
@@ -14,4 +14,16 @@ class AccountInvoice(models.Model):
         comodel_name='account.invoice', column1='refund_invoice_id',
         column2='original_invoice_id', relation='account_invoice_refunds_rel',
         string='Refund invoice', help='Links to original invoice which is '
-                                      'referred by current refund invoice')
+                                      'referred by current refund invoice',
+    )
+
+    @api.model
+    def _prepare_refund(self, invoice, date=None, period_id=None,
+                        description=None, journal_id=None):
+        res = super(AccountInvoice, self)._prepare_refund(
+            invoice, date=date, period_id=period_id,
+            description=description, journal_id=journal_id,
+        )
+        res['origin_invoices_ids'] = [(6, 0, invoice.ids)]
+        res['refund_invoices_description'] = description
+        return res
