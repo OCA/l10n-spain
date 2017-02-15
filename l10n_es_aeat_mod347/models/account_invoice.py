240e1554dc24ef1d57f11d141524eec4f1558adf
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-02-15 09:18:26 +0100

    [IMP] l10n_es_aeat_mod347: General code check

--- l10n_es_aeat_mod347/models/account_invoice.py
+++ l10n_es_aeat_mod347/models/account_invoice.py
@@ -6,18 +6,25 @@ from openerp import fields, models, api
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
-    @api.one
-    @api.depends('amount_untaxed_signed', 'tax_line_ids.amount')
-    def _get_amount_total_wo_irpf(self):
-        self.amount_total_wo_irpf = self.amount_untaxed_signed
-        for tax_line_ids in self.tax_line_ids:
-            if 'IRPF' not in tax_line_ids.name:
-                self.amount_total_wo_irpf += tax_line_ids.amount
-
     amount_total_wo_irpf = fields.Float(
-        compute="_get_amount_total_wo_irpf",
-        string="Total amount without IRPF taxes")
+        compute="_compute_amount_total_wo_irpf", store=True,
+        string="Total amount without IRPF taxes",
+    )
     not_in_mod347 = fields.Boolean(
         "Not included in 347 report",
         help="If you mark this field, this invoice will not be included in "
-             "any AEAT 347 model report.", default=False)
+             "any AEAT 347 model report.", default=False,
+    )
+
+    @api.multi
+    @api.depends('amount_untaxed_signed', 'tax_line_ids.amount')
+    def _compute_amount_total_wo_irpf(self):
+        for invoice in self:
+            invoice.amount_total_wo_irpf = (
+                # TODO: Make match by tax itself, not tax name
+                invoice.amount_untaxed + sum(
+                    invoice.tax_line_ids.filtered(
+                        lambda t: 'IRPF' not in t.name
+                    ).mapped('amount')
+                )
+            )
