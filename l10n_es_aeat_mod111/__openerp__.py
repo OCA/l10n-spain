94ac58f64bb1ea24d56e82dc272eaf35c9bf74d0
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-02-15 17:40:21 +0100

    l10n_es_aeat_mod111: Add multi-company security rule

--- l10n_es_aeat_mod111/__openerp__.py
+++ l10n_es_aeat_mod111/__openerp__.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 # Copyright 2016 Antonio Espinosa <antonio.espinosa@tecnativa.com>
+# Copyright 2017 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl
 
 {
     'name': 'AEAT modelo 111',
-    'version': '9.0.1.0.0',
+    'version': '9.0.1.1.0',
     'category': "Localisation/Accounting",
     'author': "AvanzOSC,"
               "RGB Consulting SL,"
@@ -21,6 +22,7 @@
         'data/tax_code_map_mod111_data.xml',
         'views/mod111_view.xml',
         'security/ir.model.access.csv',
+        'security/l10n_es_aeat_mod111_security.xml',
     ],
     'installable': True,
 }
