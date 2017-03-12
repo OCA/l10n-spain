7927cb0fab9033d810dbe3efc9ad5370a93da4b3
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2017-03-12 11:28:09 +0100

    [UPD] fix website url in manifest

--- l10n_es_subcontractor_certificate/__openerp__.py
+++ l10n_es_subcontractor_certificate/__openerp__.py
@@ -25,7 +25,7 @@
     "name": "Certificado de subcontratista",
     "version": "8.0.1.0.0",
     "author": "Domatix,Odoo Community Association (OCA)",
-    "website": "www.domatix.com",
+    "website": "http://www.domatix.com/",
     "category": "Accounting",
     "depends": ["purchase"],
     "data": ["views/subcontractor_certificate_view.xml"],
