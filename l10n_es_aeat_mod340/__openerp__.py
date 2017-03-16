d9220341f9d0ba450ffbf1b143d4279a0d85302c
agaldona <ainaragaldona@avanzosc.es>
2017-03-16 09:58:04 +0100

    [IMP] l10n_es_aeat_mod340: Error when invoice amount is 0

--- l10n_es_aeat_mod340/__openerp__.py
+++ l10n_es_aeat_mod340/__openerp__.py
@@ -23,7 +23,7 @@
 
 {
     'name': 'Generación de fichero modelo 340 y libro de IVA',
-    'version': '8.0.2.2.0',
+    'version': '8.0.2.2.1',
     "author": "Spanish Localization Team,"
               # "Acysos S.L., "
               # "Ting, "
