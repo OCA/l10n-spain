ea0d3365cfd7a76637ca1e50c67b9136abf2c3ad
Pedro Rodríguez Gil <pedro@otherway.es>
2017-03-16 10:21:37 +0100

    Corrección número nueva versión del módulo
    Nuevo número debe ser 8.0.1.8.0 ya que requiere actualización

--- l10n_es_aeat_mod303/__openerp__.py
+++ l10n_es_aeat_mod303/__openerp__.py
@@ -6,7 +6,7 @@
 
 {
     "name": "AEAT modelo 303",
-    "version": "8.0.1.7.1",
+    "version": "8.0.1.8.0",
     'category': "Accounting & Finance",
     'author': "Guadaltech,"
               "AvanzOSC,"
