f5a9f3ec41e889038b18ee77faa21a454759cc3d
Pedro Rodríguez Gil <pedro@otherway.es>
2017-03-16 10:49:57 +0100

    [FIX] aeat_mod_303. Corrección tipos de exportación (#465)

--- l10n_es_aeat_mod303/__openerp__.py
+++ l10n_es_aeat_mod303/__openerp__.py
@@ -6,7 +6,7 @@
 
 {
     "name": "AEAT modelo 303",
-    "version": "8.0.1.7.0",
+    "version": "8.0.1.8.0",
     'category': "Accounting & Finance",
     'author': "Guadaltech,"
               "AvanzOSC,"
