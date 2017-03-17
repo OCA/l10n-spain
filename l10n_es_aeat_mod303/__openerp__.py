004412b0cc6adeb832b10b84d5fe3b2b412d4730
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-03-17 18:25:13 +0100

    [FIX] l10n_es_aeat_mod303: Show compensate flag

--- l10n_es_aeat_mod303/__openerp__.py
+++ l10n_es_aeat_mod303/__openerp__.py
@@ -7,7 +7,7 @@
 
 {
     "name": "AEAT modelo 303",
-    "version": "9.0.1.3.0",
+    "version": "9.0.1.4.0",
     'category': "Accounting & Finance",
     'author': "Guadaltech,"
               "AvanzOSC,"
