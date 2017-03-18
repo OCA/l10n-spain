7e7ec027745c93d04510e1596678c06f61cacae9
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-03-18 14:07:59 +0100

    [MIG] l10n_es_partner: Migration to 10.0

--- l10n_es_partner/models/account_journal.py
+++ l10n_es_partner/models/account_journal.py
@@ -2,7 +2,7 @@
 # © 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3).
 
-from openerp import api, fields, models, _
+from odoo import api, fields, models, _
 
 
 class AccountJournal(models.Model):
