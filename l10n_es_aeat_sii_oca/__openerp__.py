0fd85218c9b2e54c5a3d5daefbf4287c0f4a6f1d
Rubén <ruben.cerda.roig@diagram.es>
2017-03-21 17:52:18 +0100

    l10n_es_aeat_sii: Carga directa del certificado

--- l10n_es_aeat_sii_oca/__openerp__.py
+++ l10n_es_aeat_sii_oca/__openerp__.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 Ignacio Ibeas <ignacio@acysos.com>
+# (c) 2017 Diagram Software S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Suministro Inmediato de Información en el IVA",
     "version": "8.0.1.0.1",
     "category": "Accounting & Finance",
     "website": "https://odoo-community.org/",
-    "author": "Acysos S.L., Odoo Community Association (OCA)",
+    "author": "Acysos S.L., Odoo Community Association (OCA), Diagram Software S.L.",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
@@ -21,6 +22,8 @@
     "data": [
         "data/ir_config_parameter.xml",
         "views/res_company_view.xml",
-        "views/account_invoice_view.xml"
+        "views/account_invoice_view.xml",
+        "views/aeat_sii_view.xml",
+        "wizard/aeat_sii_password_view.xml",
     ],
 }
