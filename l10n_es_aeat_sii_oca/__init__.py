c560497ef5f418b33b804f37130f0f2e6581ed10
Rubén <ruben.cerda.roig@diagram.es>
2017-03-21 17:52:18 +0100

    l10n_es_aeat_sii: Carga directa del certificado

--- l10n_es_aeat_sii_oca/__init__.py
+++ l10n_es_aeat_sii_oca/__init__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import models
-# from . import wizards
+from . import wizard
 # from . import controllers
 # from . import report
 # from .hooks import pre_init_hook, post_load, post_init_hook, uninstall_hook
--- l10n_es_aeat_sii_oca/models/__init__.py
+++ l10n_es_aeat_sii_oca/models/__init__.py
@@ -4,3 +4,4 @@
 
 from . import res_company
 from . import account_invoice
+from . import aeat_sii
