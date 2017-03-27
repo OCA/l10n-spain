613265278d15e1e2eb218100794285c37518de49
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-03-27 19:26:30 +0200

    [FIX] l10n_es_aeat_mod303: Bump version

--- l10n_es_aeat_mod303/__openerp__.py
+++ l10n_es_aeat_mod303/__openerp__.py
@@ -6,7 +6,7 @@
 
 {
     "name": "AEAT modelo 303",
-    "version": "8.0.1.8.0",
+    "version": "8.0.1.9.0",
     'category': "Accounting & Finance",
     'author': "Guadaltech,"
               "AvanzOSC,"
