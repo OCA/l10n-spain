611be597d81f42841477168042c3e67f62198056
Joaquin Gutierrez <joaquin.gutierrez@grupoancana.com>
2017-03-31 12:39:46 +0200

    [FIX]:Arreglo de errores OCA Guideline

---
+++ l10n_es_account_hierarchy_report/README.rst
@@ -0,0 +1,45 @@
+Account Hierarchy Report
+=========
+
+Módulo para la explotacion de la informacion contable de forma jerarquizada,
+dicho modulo incluye:
+
+* Campos calculados a nivel de cuentas contables para extraer los digitos.
+* Etiquetas para los distintos niveles 1 digito, 2 digitos y 3 digitos.
+* Informe BI que por defecto presenta la informacion a 1,2,3 digitos.
+* Posibilidad de filtrar por asientos asentados o borrados, y por aquellas
+  cuentas que tengan el balance a 0.
+* Se incluyen grupos por empresa, diario, tipo de cuenta, moneda.
+
+Configuración
+=============
+
+No se required configuracion.
+
+Problemas conocidos / Hoja de ruta
+==================================
+
+* Actualmente no hay problemas conocidos
+
+Créditos
+========
+
+Contribudores
+-------------
+
+* Joaquin Gutierrez Pedrosa (https://www.gutierrezweb.es)
+
+Maintainer
+----------
+
+.. image:: http://odoo-community.org/logo.png
+   :alt: Odoo Community Association
+   :target: http://odoo-community.org
+
+This module is maintained by the OCA.
+
+OCA, or the Odoo Community Association, is a nonprofit organization whose
+mission is to support the collaborative development of Odoo features and
+promote its widespread use.
+
+To contribute to this module, please visit http://odoo-community.org.
--- l10n_es_account_hierarchy_report/__manifest__.py
+++ l10n_es_account_hierarchy_report/__manifest__.py
@@ -3,30 +3,25 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Account Hierarchy Report',
-    'version': '10.0.0.8.0',
-    'summary': 'Account Hierarchy Report',
+    'name': "Account Hierarchy Report",
+    'version': "10.0.0.8.0",
+    'summary': "Account Hierarchy Report",
     'sequence': 51,
-    'description': """
-    Extrae de los codigos de cuenta los distintos niveles usados en los
-    informes contables (1,2,3).
-    Mediante etiquetas para los distintos niveles crea un informe utilizando
-    la funcionalidad BI para explotar la informacion contable jerarquizada.
-    """,
-    'category': 'Accounting',
-    'author': 'Joaquin Gutierrez Pedrosa'
-              'Odoo Community Association (OCA)',
-    'website': 'https://www.gutierrezweb.es',
+    'category': "Accounting",
+    'author': "Joaquin Gutierrez Pedrosa,"
+              "Odoo Community Association (OCA)",
+    'license': "AGPL-3",
+    'website': "https://www.gutierrezweb.es",
     'images': [
     ],
     'depends': [
-        'account',
+        "account",
     ],
     'data': [
-        'security/ir.model.access.csv',
-        'data/account.xml',
-        'views/account_views.xml',
-        'reports/bi_reporting_account_hierarchy.xml',
+        "security/ir.model.access.csv",
+        "data/account.xml",
+        "views/account_views.xml",
+        "reports/bi_reporting_account_hierarchy.xml",
     ],
     'demo': [
     ],
