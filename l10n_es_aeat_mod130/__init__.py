9aab6291107dbe244be1e8a1a2393ca0c20f4007
Hugo Santos <hugo.santos@factorlibre.com>
2017-04-11 13:39:39 +0200

    [IMP] Añadir reglas de registro multicompañía a informes financieros e informes AEAT

--- l10n_es_aeat_mod130/__init__.py
+++ l10n_es_aeat_mod130/__init__.py
@@ -1,4 +1,3 @@
-# -*- encoding: utf-8 -*-
 ##############################################################################
 #
 #    This program is free software: you can redistribute it and/or modify
