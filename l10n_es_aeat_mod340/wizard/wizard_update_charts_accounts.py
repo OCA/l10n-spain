e87221b8dfd8c18b813954a9e6b60bd5fb24cb6b
Pedro Rodríguez Gil <pedro@otherway.es>
2017-04-20 10:56:27 +0200

    Revisión de Travis

--- l10n_es_aeat_mod340/wizard/wizard_update_charts_accounts.py
+++ l10n_es_aeat_mod340/wizard/wizard_update_charts_accounts.py
@@ -35,11 +35,12 @@ class WizardUpdateChartsAccounts(models.TransientModel):
             tax_code_template, mapping_tax_codes)
         res['mod340'] = tax_code_template.mod340
         return res
-    
+
     def _is_different_tax(self, tax, tax_template, mapping_taxes,
                           mapping_tax_codes, mapping_accounts):
         notes = super(WizardUpdateChartsAccounts, self)._is_different_tax(
-            tax, tax_template, mapping_taxes,mapping_tax_codes, mapping_accounts)
+            tax, tax_template, mapping_taxes, mapping_tax_codes, 
+            mapping_accounts)
         if tax.is_340_reserve_charge != tax_template.is_340_reserve_charge:
             notes += _("The field is 340 reverse charge is different.\n")
         return notes
@@ -47,6 +48,6 @@ class WizardUpdateChartsAccounts(models.TransientModel):
     def _prepare_tax_vals(self, tax_template, mapping_tax_codes,
                           mapping_taxes):
         res = super(WizardUpdateChartsAccounts, self)._prepare_tax_vals(
-            tax_template, mapping_tax_codes,mapping_taxes)
+            tax_template, mapping_tax_codes, mapping_taxes)
         res['is_340_reserve_charge'] = tax_template.is_340_reserve_charge
         return res
