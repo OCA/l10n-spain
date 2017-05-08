a134e26426a2c2a06db31207b4b546df938bea48
Ignacio Ibeas - Acysos S.L <ignacio@acysos.com>
2017-05-08 09:05:30 +0200

    l10n_es_aeat_sii: se añade mapeo de impuestos, traducciones y permisos
    l10n_es_aeat_sii: añadidas facturas de bienes de inversión e intracomunitarias. Se traspasa la configuración del WSDL a parametros

--- l10n_es_aeat_sii_oca/models/res_company.py
+++ l10n_es_aeat_sii_oca/models/res_company.py
@@ -17,51 +17,12 @@ class ResCompany(models.Model):
                 _('The module "Connector" is not installed. You have '
                   'to install it to activate this option'))
 
+    sii_enabled = fields.Boolean(string='Enable SII')
     sii_test = fields.Boolean(string='Test Enviroment')
-    sii_version = fields.Char(string='SII Version', default='0.6')
+    chart_template_id = fields.Many2one(
+        comodel_name='account.chart.template', string='Chart Template',
+        required=True)
     use_connector = fields.Boolean(
         string='Use connector',
         help='Check it to use connector instead to send the invoice '
         'when it is validated')
-    wsdl_out = fields.Char(
-        string='WSDL Invoice Out',
-        default='http://www.agenciatributaria.es/static_files/AEAT/'
-        'Contenidos_Comunes/La_Agencia_Tributaria/Modelos_y_formularios/'
-        'Suministro_inmediato_informacion/FicherosSuministros/V_06/'
-        'SuministroFactEmitidas.wsdl')
-    wsdl_in = fields.Char(
-        string='WSDL Invoice In',
-        default='http://www.agenciatributaria.es/static_files/AEAT/'
-        'Contenidos_Comunes/La_Agencia_Tributaria/Modelos_y_formularios/'
-        'Suministro_inmediato_informacion/FicherosSuministros/V_06/'
-        'SuministroFactRecibidas.wsdl')
-    wsdl_pi = fields.Char(
-        string='WSDL Property Investment',
-        default='http://www.agenciatributaria.es/static_files/AEAT/'
-        'Contenidos_Comunes/La_Agencia_Tributaria/Modelos_y_formularios/'
-        'Suministro_inmediato_informacion/FicherosSuministros/V_06/'
-        'SuministroBienesInversion.wsdl')
-    wsdl_ic = fields.Char(
-        string='WSDL Intra-Community',
-        default='http://www.agenciatributaria.es/static_files/AEAT/'
-        'Contenidos_Comunes/La_Agencia_Tributaria/Modelos_y_formularios/'
-        'Suministro_inmediato_informacion/FicherosSuministros/V_06/'
-        'SuministroOpIntracomunitarias.wsdl')
-    wsdl_pr = fields.Char(
-        string='WSDL Payment Received',
-        default='http://www.agenciatributaria.es/static_files/AEAT/'
-        'Contenidos_Comunes/La_Agencia_Tributaria/Modelos_y_formularios/'
-        'Suministro_inmediato_informacion/FicherosSuministros/V_06/'
-        'SuministroCobrosEmitidas.wsdl')
-    wsdl_prm = fields.Char(
-        string='WSDL Money Payment Received',
-        default='http://www.agenciatributaria.es/static_files/AEAT/'
-        'Contenidos_Comunes/La_Agencia_Tributaria/Modelos_y_formularios/'
-        'Suministro_inmediato_informacion/FicherosSuministros/V_06/'
-        'SuministroCobrosMetalico.wsdl')
-    wsdl_ps = fields.Char(
-        string='WSDL Payment Sent',
-        default='http://www.agenciatributaria.es/static_files/AEAT/'
-        'Contenidos_Comunes/La_Agencia_Tributaria/Modelos_y_formularios/'
-        'Suministro_inmediato_informacion/FicherosSuministros/V_06/'
-        'SuministroPagosRecibidas.wsdl')
