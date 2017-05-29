dfe45ffec9dcac06b4603979933d2afc4d40343e
Pablo Fuentes <pablo@studio73.es>
2017-05-29 12:25:29 +0200

    l10n_es_aeat_sii: Mejoras del certificado + otros
    * Ocultar la contraseña del certificado
    * Plantilla de cuentas solo obligatorio si "activar SII" activado
    * Habilitar un certificado por compañía
    * Permisos de acceso
    * Tener en cuenta las fechas de los certificados
    
    [IMP] l10n_es_aeat_sii: No utilizar los parámetros del sistema y buscar solo certificados activos

--- l10n_es_aeat_sii/models/res_company.py
+++ l10n_es_aeat_sii/models/res_company.py
@@ -2,7 +2,8 @@
 # Copyright 2017 Ignacio Ibeas <ignacio@acysos.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, fields, api
+from openerp import models, fields, api, _
+from openerp.exceptions import Warning
 
 
 class ResCompany(models.Model):
@@ -10,18 +11,17 @@ class ResCompany(models.Model):
     
     @api.onchange('use_connector')
     def _check_connector_installed(self):
-        module = self.env['ir.module'].search(
+        module = self.env['ir.module.module'].search(
             [('name', '=', 'connector'), ('state', '=', 'installed')])
         if not module:
-            raise exceptions.Warning(
+            raise Warning(
                 _('The module "Connector" is not installed. You have '
                   'to install it to activate this option'))
 
     sii_enabled = fields.Boolean(string='Enable SII')
     sii_test = fields.Boolean(string='Test Enviroment')
     chart_template_id = fields.Many2one(
-        comodel_name='account.chart.template', string='Chart Template',
-        required=True)
+        comodel_name='account.chart.template', string='Chart Template')
     use_connector = fields.Boolean(
         string='Use connector',
         help='Check it to use connector instead to send the invoice '
