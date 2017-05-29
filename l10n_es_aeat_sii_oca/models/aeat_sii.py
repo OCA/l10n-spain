d2c1474fc0bfcc6d1c3cbea0de19470af0bdd835
Pablo Fuentes <pablo@studio73.es>
2017-05-29 12:25:29 +0200

    l10n_es_aeat_sii: Mejoras del certificado + otros
    * Ocultar la contraseña del certificado
    * Plantilla de cuentas solo obligatorio si "activar SII" activado
    * Habilitar un certificado por compañía
    * Permisos de acceso
    * Tener en cuenta las fechas de los certificados
    
    [IMP] l10n_es_aeat_sii: No utilizar los parámetros del sistema y buscar solo certificados activos

--- l10n_es_aeat_sii_oca/models/aeat_sii.py
+++ l10n_es_aeat_sii_oca/models/aeat_sii.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # (c) 2017 Diagram Software S.L.
+# (c) 2017 Consultoría Informática Studio 73 S.L.
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from openerp import api, models, fields, _
@@ -19,6 +20,12 @@ class l10nEsAeatSii(models.Model):
     date_end = fields.Date(string="End Date")
     public_key = fields.Char(string="Public Key", readonly=True)
     private_key = fields.Char(string="Private Key", readonly=True)
+    company_id = fields.Many2one(
+        comodel_name="res.company",
+        string="Compañía",
+        required=True,
+        default=lambda self: self.env.user.company_id.id
+    )
 
     @api.multi
     def load_password_wizard(self):
@@ -36,13 +43,8 @@ class l10nEsAeatSii(models.Model):
     @api.multi
     def action_active(self):
         self.ensure_one()
-        if self.public_key:
-            sii_crt = self.env.ref('l10n_es_aeat_sii.config_parameter_sii_crt')
-            sii_crt.value = self.public_key
-        if self.private_key:
-            sii_key = self.env.ref('l10n_es_aeat_sii.config_parameter_sii_key')
-            sii_key.value = self.private_key
-        other_configs = self.search([('id', '!=', self.id)])
+        other_configs = self.search([('id', '!=', self.id),
+                                     ('company_id', '=', self.company_id.id)])
         for config_id in other_configs:
             config_id.state = 'draft'
         self.state = 'active'
