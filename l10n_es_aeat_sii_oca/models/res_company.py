42c92ca62ad09d6e8bd34517966efd24d2c956ed
Omar <omarcs7r@gmail.com>
2017-05-31 18:23:42 +0200

    [MIG] l10n_es_aeat_sii: Migración a la 10.0

--- l10n_es_aeat_sii_oca/models/res_company.py
+++ l10n_es_aeat_sii_oca/models/res_company.py
@@ -5,7 +5,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from datetime import datetime, timedelta
-from openerp import fields, models
+from odoo import fields, models
 import pytz
 
 
