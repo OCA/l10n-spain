4f7872cb0546392859ecb49981f76b5a1ebd3110
Omar <omarcs7r@gmail.com>
2017-05-31 18:23:42 +0200

    [MIG] l10n_es_aeat_sii: Migración a la 10.0

--- l10n_es_aeat_sii_oca/models/aeat_sii_mapping_registration_keys.py
+++ l10n_es_aeat_sii_oca/models/aeat_sii_mapping_registration_keys.py
@@ -2,7 +2,7 @@
 # Copyright 2017 MINORISA (http://www.minorisa.net)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import api, fields, models
+from odoo import api, models, fields
 
 
 class AeatSiiMappingRegistrationKeys(models.Model):
