b267d2c61afeebf2fc2bda287255ae2c0252e42e
Pablo Fuentes <pablo@studio73.es>
2017-06-03 18:18:21 +0200

    l10n_es_aeat_sii: Trabajos readonly, limitar campos, poder re-encolar y retardo configurable

--- l10n_es_aeat_sii_oca/models/res_company.py
+++ l10n_es_aeat_sii_oca/models/res_company.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 Ignacio Ibeas <ignacio@acysos.com>
+# (c) 2017 Studio73 - Pablo Fuentes <pablo@studio73.es>
+# (c) 2017 Studio73 - Jordi Tolsà <jordi@studio73.es>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from datetime import datetime, timedelta
 
 from openerp import models, fields, api, _
 from openerp.exceptions import Warning
@@ -26,3 +29,30 @@ class ResCompany(models.Model):
         string='Use connector',
         help='Check it to use connector instead to send the invoice '
         'when it is validated')
+
+    send_mode = fields.Selection(string="Send mode",
+                                 selection=[('auto', 'On validate'),
+                                            ('fixed', 'At fixed time'),
+                                            ('delayed', 'With delay')],
+                                 default='auto')
+    sent_time = fields.Float(string="Sent time")
+    delay_time = fields.Float(string="Delay time")
+
+    def _get_sii_eta(self):
+        if self.send_mode == 'fixed':
+            now = datetime.now()
+
+            hour, minute = divmod(self.sent_time, 1)
+            hour = int(hour)
+            minute = int(minute*60)
+
+            if now.date > hour or (now.hour == hour and now.minute > minute):
+                now += timedelta(days=1)
+
+            return now.replace(hour=hour, minute=minute)
+
+        elif self.send_mode == 'delayed':
+            return datetime.now() + timedelta(seconds=self.delay_time * 60 * 60)
+
+        else:
+            return None
