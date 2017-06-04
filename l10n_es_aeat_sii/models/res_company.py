63b2712cc08da014fc1813980d09ebd839e0841d
Ignacio Ibeas - Acysos S.L <ignacio@acysos.com>
2017-06-04 18:54:37 +0200

    l10n_es_aeat_sii: copy fix, send button, method send, new filter and translations

--- l10n_es_aeat_sii/models/res_company.py
+++ l10n_es_aeat_sii/models/res_company.py
@@ -11,20 +11,17 @@ from openerp.exceptions import Warning
 
 class ResCompany(models.Model):
     _inherit = 'res.company'
-    
-    @api.onchange('use_connector')
-    def _check_connector_installed(self):
-        module = self.env['ir.module.module'].search(
-            [('name', '=', 'connector'), ('state', '=', 'installed')])
-        if not module:
-            raise Warning(
-                _('The module "Connector" is not installed. You have '
-                  'to install it to activate this option'))
 
     sii_enabled = fields.Boolean(string='Enable SII')
     sii_test = fields.Boolean(string='Test Enviroment')
     chart_template_id = fields.Many2one(
         comodel_name='account.chart.template', string='Chart Template')
+    sii_method = fields.Selection(
+        string='Method',
+        selection=[('auto', 'Automatic'),('manual', 'Manual')],
+        default='auto',
+        help='By default the invoice send in validate process, with manual '
+        'method, there a button to send the invoice.')
     use_connector = fields.Boolean(
         string='Use connector',
         help='Check it to use connector instead to send the invoice '
