6d63c24116003e9279282e66795a92e2e2aaec8e
Daniel Rodriguez <drodriguez@praxya.com>
2017-06-15 10:49:35 +0200

    WIP vat_book

--- l10n_es_vat_book/models/__init__.py
+++ l10n_es_vat_book/models/__init__.py
@@ -5,3 +5,5 @@ from . import l10n_es_vat_book
 from . import l10n_es_vat_book_issued_lines
 from . import l10n_es_vat_book_received_lines
 from . import l10n_es_vat_book_rectification_lines
+from . import l10n_es_vat_book_issued_tax_summary
+from . import l10n_es_vat_book_received_tax_summary
