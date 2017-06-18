00649ea0b1e7d0ec896ee90a7a476c45435743a6
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-06-18 13:27:45 +0200

    l10n_es_aeat_sii: General refactoring:
    * Onchanges renamed for avoiding conflicts with other modules
    * Methods renamed for avoiding conflicts with other modules
    * Some method docstrings.
    * Use recordset instead of lists.
    * Remove incorrect mix of @api.multi and @api.model decorators
    * Remove method arguments that are passed when there are other data that are taken from self.
    * Place in the proper model some of the methods for using self reference
    * Variables renamed according their use.
    * POT file removed.
    * Reduce cyclomatic complexity of some methods.
    * Code style more homogeneous.
    * Uniformize file headers.
    * Full Spanish translation.
    * Add icon.

--- l10n_es_aeat_sii_oca/hooks.py
+++ l10n_es_aeat_sii_oca/hooks.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright © 2017 Oihane Crucelaegui - AvanzOSC
+# Copyright 2017 Oihane Crucelaegui - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from openerp import SUPERUSER_ID
