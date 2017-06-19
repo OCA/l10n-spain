4873d47f8778bce6db9525d20d815c8983270573
Daniel Rodriguez <drodriguez@praxya.com>
2017-06-19 12:21:39 +0200

    Traduccion al español y actulizando readme

--- l10n_es_vat_book/models/l10n_es_vat_book.py
+++ l10n_es_vat_book/models/l10n_es_vat_book.py
@@ -1,4 +1,23 @@
 # -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (c) 2017 Praxya (http://praxya.com/)
+#                       Daniel Rodriguez Lijo <drl.9319@gmail.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 from openerp import models, api, fields, _
 from openerp.exceptions import Warning
 
@@ -41,6 +60,9 @@ class L10nEsVatBook(models.Model):
         inverse_name='l10n_es_vat_book_id',
         string=_('Rectification received'),
         readonly="True")
+
+    calculation_date = fields.Date(
+        string=_('Calculation Date'))
 
     amount_without_tax_issued = fields.Float(
         string=_('Total without taxes'),
@@ -561,10 +583,10 @@ class L10nEsVatBook(models.Model):
             self._create_invoice_tax(
                 invoice_vat_book_line_id, invoice_id, tax_code_ids)
         # Write state and date in the report
-        # self.write({
-        #     'state': 'calculated',
-        #     # 'calculation_date': fields.Datetime.now(),
-        # })
+        self.write({
+            'state': 'calculated',
+            'calculation_date': fields.Datetime.now(),
+        })
 
     def __init__(self, pool, cr):
         self._aeat_number = 'vat_book'
