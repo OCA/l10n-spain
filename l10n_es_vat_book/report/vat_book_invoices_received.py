9ce9c992972c282b00b90e524cb8aab421695cde
Daniel Rodriguez <drodriguez@praxya.com>
2017-06-20 15:45:00 +0200

    FIX travis

--- l10n_es_vat_book/report/vat_book_invoices_received.py
+++ l10n_es_vat_book/report/vat_book_invoices_received.py
@@ -1,18 +1,18 @@
-# -*- coding:utf-8 -*-
+# -*- coding: utf-8 -*-
 
-from openerp.osv import osv
+from openerp import models
 from openerp.report import report_sxw
 
 
-class vat_book_invoices_received_report(report_sxw.rml_parse):
+class VatBookInvoicesReceivedReport(report_sxw.rml_parse):
 
     def __init__(self, cr, uid, name, context):
-        super(vat_book_invoices_received_report, self).__init__(
+        super(VatBookInvoicesReceivedReport, self).__init__(
             cr, uid, name, context)
 
 
-class wrapped_vat_book_invoices_received(osv.AbstractModel):
+class WrappedVatBookInvoicesReceived(models.AbstractModel):
     _name = 'report.l10n_es_vat_book.tmp_report_vat_book_invoices_received'
     _inherit = 'report.abstract_report'
     _template = 'l10n_es_vat_book.tmp_report_vat_book_invoices_received'
-    _wrapped_report_class = vat_book_invoices_received_report
+    _wrapped_report_class = VatBookInvoicesReceivedReport
--- l10n_es_vat_book/report/vat_book_rectification_received_invoices.py
+++ l10n_es_vat_book/report/vat_book_rectification_received_invoices.py
@@ -1,20 +1,20 @@
-# -*- coding:utf-8 -*-
+# -*- coding: utf-8 -*-
 
-from openerp.osv import osv
+from openerp import models
 from openerp.report import report_sxw
 
 
-class vat_book_rectification_received_invoices_report(report_sxw.rml_parse):
+class VatBookRectificationReceivedInvoicesReport(report_sxw.rml_parse):
 
     def __init__(self, cr, uid, name, context):
-        super(vat_book_rectification_received_invoices_report, self).__init__(
+        super(VatBookRectificationReceivedInvoicesReport, self).__init__(
             cr, uid, name, context)
 
 
-class wrapped_vat_book_rectification_received_invoices(osv.AbstractModel):
+class WrappedVatBookRectificationReceivedInvoices(models.AbstractModel):
     _name = 'report.l10n_es_vat_book.'\
         'tmp_report_vat_book_rectification_received_invoices'
     _inherit = 'report.abstract_report'
     _template = 'l10n_es_vat_book.'\
         'tmp_report_vat_book_rectification_received_invoices'
-    _wrapped_report_class = vat_book_rectification_received_invoices_report
+    _wrapped_report_class = VatBookRectificationReceivedInvoicesReport
