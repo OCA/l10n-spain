89d3ce7f8f400d4d46d1da0f9c44869021377167
etobella <etobella@creublanca.es>
2017-06-20 09:21:26 +0200

    [MIG] l10n_es_facturae: Migration to 10.0

--- l10n_es_facturae/models/__init__.py
+++ l10n_es_facturae/models/__init__.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from . import res_partner
-from . import res_company
 from . import payment_mode
+from . import res_company
+from . import res_partner
+from . import account_tax_template
+from . import account_tax
+from . import res_currency
+from . import account_invoice_integration
+from . import account_invoice_integration_method
+from . import account_invoice_integration_log
+from . import account_invoice
