aba87c28d6544a6f099063b0bb22c624dc09db17
etobella <etobella@creublanca.es>
2017-06-20 09:21:26 +0200

    [MIG] l10n_es_facturae: Migration to 10.0

---
+++ l10n_es_facturae/models/account_tax.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+# © 2017 Creu Blanca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import models, fields
+
+
+class AccountTax(models.Model):
+    _inherit = "account.tax"
+
+    facturae_code = fields.Selection(
+        selection=[
+            ('01', 'IVA: Impuesto sobre el valor añadido'),
+            ('02', 'IPSI: Impuesto sobre la producción, los servicios y'
+                   ' la importación'),
+            ('03', 'IGIC: Impuesto general indirecto de Canarias'),
+            ('04', 'IRPF: Impuesto sobre la Renta de las personas físicas'),
+            ('05', 'Otro'),
+            ('06', 'ITPAJD: Impuesto sobre transmisiones patrimoniales y'
+                   ' actos jurídicos documentados'),
+            ('07', 'IE: Impuestos especiales'),
+            ('08', 'Ra: Renta aduanas'),
+            ('09', 'IGTECM: Impuesto general sobre el tráfico de empresas que'
+                   ' se aplica en Ceuta y Melilla'),
+            ('10', 'IECDPCAC: Impuesto especial sobre los combustibles'
+                   ' derivados del petróleo en la Comunidad Autonoma Canaria'),
+            ('11', 'IIIMAB: Impuesto sobre las instalaciones que inciden sobre'
+                   ' el medio ambiente en la Baleares'),
+            ('12', 'ICIO: Impuesto sobre las construcciones, instalaciones y'
+                   ' obras'),
+            ('13', 'IMVDN: Impuesto municipal sobre las viviendas desocupadas'
+                   ' en Navarra'),
+            ('14', 'IMSN: Impuesto municipal sobre solares en Navarra'),
+            ('15', 'IMGSN: Impuesto municipal sobre gastos suntuarios en'
+                   ' Navarra'),
+            ('16', 'IMPN: Impuesto municipal sobre publicidad en Navarra'),
+            ('17', 'REIVA: Régimen especial de IVA para agencias de viajes'),
+            ('18', 'REIGIC: Régimen especial de IGIC: para agencias de'
+                   'viajes'),
+            ('19', 'REIPSI: Régimen especial de IPSI para agencias de viajes'),
+        ], string='Facturae code', default='01'
+    )
