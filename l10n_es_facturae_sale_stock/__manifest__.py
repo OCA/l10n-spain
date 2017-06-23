623b18f09a4c424b8011003bf9d6a3e87c9c3053
etobella <etobella@creublanca.es>
2017-06-23 13:54:10 +0200

    [ADD] l10n_es_facturae_sale_stock

---
+++ l10n_es_facturae_sale_stock/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# © 2017 Creu Blanca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Entregas en Factura-e",
+    "version": "10.0.1.0.0",
+    "author": "Creu Blanca, "
+              "Odoo Community Association (OCA)",
+    "category": "Accounting & Finance",
+    "website": "https://github.com/OCA/l10n-spain",
+    "license": "AGPL-3",
+    "depends": [
+        "l10n_es_facturae", "sale_stock"
+    ],
+    "data": [
+        "views/report_facturae.xml",
+    ],
+    "installable": True,
+    "auto_install": False
+}
