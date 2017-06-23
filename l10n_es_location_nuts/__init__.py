eec1e10c7a44eb810787fea8cfac73c4b0a3d4a0
David Vidal <chienandalu@gmail.com>
2017-06-23 18:38:32 +0200

    [MIG] l10n_es_location_nuts: Migration to 10.0

--- l10n_es_location_nuts/__init__.py
+++ l10n_es_location_nuts/__init__.py
@@ -1,7 +1,4 @@
 # -*- coding: utf-8 -*-
-# © 2015 Antiun Ingenieria S.L. - Antonio Espinosa
-# © 2015 Antiun Ingenieria S.L. - Jairo Llopis
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import wizard
 from .hooks import post_init_hook
--- l10n_es_location_nuts/wizard/__init__.py
+++ l10n_es_location_nuts/wizard/__init__.py
@@ -1,6 +1,3 @@
 # -*- coding: utf-8 -*-
-# © 2015 Antiun Ingenieria S.L. - Antonio Espinosa
-# © 2015 Antiun Ingenieria S.L. - Jairo Llopis
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import nuts_import
