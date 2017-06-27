6818c1e58ceacfbc2bb8a066349203ec187509b2
Omar <omarcs7r@gmail.com>
2017-06-27 20:58:18 +0200

    [UPD] l10n_es_dua_sii: Adaptamos el módulo a la última versión del l10n_es_aeat_sii

--- l10n_es_dua_sii/__openerp__.py
+++ l10n_es_dua_sii/__openerp__.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
-# (c) 2017 Consultoría Informática Studio 73 S.L.
+# Copyright 2017 Consultoría Informática Studio 73 S.L.
+# Copyright 2017 Comunitea Servicios Tecnológicos S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Suministro Inmediato de Información de importaciones con DUA",
     "version": "8.0.1.0.0",
     "category": "Accounting & Finance",
     "website": "https://odoo-community.org/",
-    "author": "Consultoría Informática Studio 73 S.L., "
+    "author": "Studio73, "
+              "Comunitea, "
               "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "depends": [
         "l10n_es_aeat_sii",
         "l10n_es_dua",
-    ],
-    "data": [
     ],
     "application": False,
     "installable": True,
