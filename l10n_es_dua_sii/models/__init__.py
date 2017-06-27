8c6c4ae5f6682453e6818e5330ef9a70b296fea0
Omar <omarcs7r@gmail.com>
2017-06-27 20:58:18 +0200

    [UPD] l10n_es_dua_sii: Adaptamos el módulo a la última versión del l10n_es_aeat_sii

--- l10n_es_dua_sii/models/__init__.py
+++ l10n_es_dua_sii/models/__init__.py
@@ -1,7 +1,4 @@
 # -*- coding: utf-8 -*-
-# (c) 2017 Consultoría Informática Studio 73 S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import account_invoice
-
-
--- l10n_es_dua_sii/models/account_invoice.py
+++ l10n_es_dua_sii/models/account_invoice.py
@@ -1,15 +1,40 @@
 # -*- coding: utf-8 -*-
-# (c) 2017 Consultoría Informática Studio 73 S.L.
+# Copyright 2017 Consultoría Informática Studio 73 S.L.
+# Copyright 2017 Comunitea Servicios Tecnológicos S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, api
+from openerp import models, fields, api
 
 
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
     @api.multi
-    def _get_invoices(self):
+    @api.depends('tax_line')
+    def _compute_sii_enabled(self):
+        for invoice in self:
+            if invoice.fiscal_position.name == u'Importación con DUA' and \
+                    not invoice.sii_dua_invoice:
+                invoice.sii_enabled = False
+            else:
+                super(AccountInvoice, invoice)._compute_sii_enabled()
+
+    @api.multi
+    def _compute_dua_invoice(self):
+        for invoice in self:
+            if invoice.fiscal_position.name == u'Importación con DUA' and \
+                    invoice.tax_line.\
+                    filtered(lambda x: x.tax_code_id.code in
+                             ['DIBYSCC21', 'DIBYSCC10', 'DIBYSCC04']):
+                invoice.sii_dua_invoice = True
+            else:
+                invoice.sii_dua_invoice = False
+
+    sii_dua_invoice = fields.Boolean("SII DUA Invoice",
+                                     compute="_compute_dua_invoice")
+
+    @api.multi
+    def _get_sii_invoice_dict_in(self):
         """
         Según la documentación de la AEAT, la operación de importación se
         registra con TipoFactura = F5, sin FechaOperacion y con el NIF de la
@@ -19,45 +44,16 @@ class AccountInvoice(models.Model):
         http://bit.ly/2rGWiAI
 
         """
-        res = super(AccountInvoice, self)._get_invoices()
-        if res.get('FacturaRecibida', False) and self.is_dua_sii_invoice():
-                res['FacturaRecibida']['TipoFactura'] = 'F5'
-                res['FacturaRecibida'].pop('FechaOperacion', None)
-                res['FacturaRecibida']['IDEmisorFactura'] = \
-                    {'NIF': self.company_id.vat[2:]}
-                res['IDFactura']['IDEmisorFactura'] = \
-                    {'NIF': self.company_id.vat[2:]}
-                res['FacturaRecibida']['Contraparte']['NIF'] = \
-                    self.company_id.vat[2:]
-                res['FacturaRecibida']['Contraparte']['NombreRazon'] = \
-                    self.company_id.name
+        res = super(AccountInvoice, self)._get_sii_invoice_dict_in()
+        if res.get('FacturaRecibida') and self.sii_dua_invoice:
+            res['FacturaRecibida']['TipoFactura'] = 'F5'
+            res['FacturaRecibida'].pop('FechaOperacion', None)
+            res['FacturaRecibida']['IDEmisorFactura'] = \
+                {'NIF': self.company_id.vat[2:]}
+            res['IDFactura']['IDEmisorFactura'] = \
+                {'NIF': self.company_id.vat[2:]}
+            res['FacturaRecibida']['Contraparte']['NIF'] = \
+                self.company_id.vat[2:]
+            res['FacturaRecibida']['Contraparte']['NombreRazon'] = \
+                self.company_id.name
         return res
-
-    @api.multi
-    def is_dua_sii_invoice(self):
-        """
-        :return:
-        """
-        self.ensure_one()
-        if self.fiscal_position.name == u'Importación con DUA':
-            if self.tax_line.filtered(
-                    lambda x: x.tax_code_id.code in
-                    ['DIBYSCC21', 'DIBYSCC10', 'DIBYSCC04']
-            ):
-                return True
-        return False
-
-    @api.multi
-    def is_sii_invoice(self):
-        """ is_sii_invoice() -> bool
-            Check if the invoice must be not sended to AEAT because is
-            a supplier DUA invoice, in that case should be sended
-            the freight forwarder invoice instead.
-            :param
-            :return: bool
-        """
-        self.ensure_one()
-        if self.fiscal_position.name == u'Importación con DUA' and \
-                not self.is_dua_sii_invoice():
-            return False
-        return True
