6a53ce0560a84c4692003a953f1958cc95525338
Miguel Paraiso Martin <miguel.paraiso@aselcis.com>
2017-06-28 08:19:36 +0200

    [10.0][MIG] l10n_es_aeat_mod340

--- l10n_es_aeat_mod340/models/account.py
+++ l10n_es_aeat_mod340/models/account.py
@@ -1,62 +1,19 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright (c) 2013 Acysos S.L. (http://acysos.com)
-#                       Ignacio Ibeas Izquierdo <ignacio@acysos.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2012 - Acysos S.L. (http://acysos.com)
+#                - Ignacio Ibeas <ignacio@acysos.com>
+# Copyright 2016 - Tecnativa - Angel Moya <odoo@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp.osv import fields, orm
+from odoo import models, fields
 
 
-class AccountTaxCodeTemplate(orm.Model):
-    _inherit = 'account.tax.code.template'
-
-    _columns = {
-        'mod340': fields.boolean("Include in mod340"),
-        'surcharge_tax_id': fields.many2one(
-            'account.tax.code',
-            'Surcharge tax of'),
-    }
-
-
-class AccountTaxCode(orm.Model):
-    _inherit = 'account.tax.code'
-
-    _columns = {
-        'mod340': fields.boolean("Include in mod340"),
-        'surcharge_tax_id': fields.many2one(
-            'account.tax.code',
-            'Surcharge tax of'),
-    }
-
-
-class AccountTax(orm.Model):
+class AccountTax(models.Model):
     _inherit = 'account.tax'
 
-    _columns = {
-        'is_340_reserve_charge': fields.boolean(
-            "Include in mod340 as reserve charge"),
-    }
+    mod340 = fields.Boolean(string='Include in mod340')
 
 
-class AccountAccounte(orm.Model):
-    _inherit = 'account.account'
+class AccountTaxTemplate(models.Model):
+    _inherit = 'account.tax.template'
 
-    _columns = {
-        'is_340_leasing_account': fields.boolean(
-            "Include in mod340 as leasing account"),
-    }
+    mod340 = fields.Boolean(string='Include in mod340')
