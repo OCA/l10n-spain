7d9d690bdfd425a27fb99de271d88e53bb88a1dc
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-06-29 01:19:55 +0200

    [MIG] l10n_es_irnr: Migration to 10.0

--- l10n_es_irnr/__manifest__.py
+++ l10n_es_irnr/__manifest__.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016 Antonio Espinosa <antonio.espinosa@tecnativa.com>
+# Copyright 2016 Tecnativa - Antonio Espinosa
+# Copyright 2017 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Retenciones IRNR (No residentes)",
     "summary": "Module summary",
-    'version': '9.0.1.0.0',
+    'version': '10.0.1.0.0',
     'category': 'Localization/Account Charts',
     'website': 'https://odoo-community.org',
     'author': 'Tecnativa, '
