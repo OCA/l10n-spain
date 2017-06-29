ee2a2555aa942eee4d22d7a02357308a48faaae7
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-06-30 00:39:32 +0200

    [FIX] account_balance_reporting: Don't fail on initial empty name
    Closes #492

--- account_balance_reporting/__manifest__.py
+++ account_balance_reporting/__manifest__.py
@@ -8,7 +8,7 @@
 
 {
     "name": "Account balance reporting engine",
-    "version": "10.0.1.0.1",
+    "version": "10.0.1.0.2",
     "author": "Pexego, "
               "AvanzOSC, "
               "Tecnativa, "
