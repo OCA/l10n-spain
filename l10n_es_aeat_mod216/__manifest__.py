534034873cadeed4994b47fe0db7f7825a19a962
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-07-01 19:56:08 +0200

    [MIG] l10n_es_aeat_mod216: Migration to 10.0
    * Standard migration procedure

--- l10n_es_aeat_mod216/__manifest__.py
+++ l10n_es_aeat_mod216/__manifest__.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 # Copyright 2015 AvanzOSC - Ainara Galdona
-# Copyright 2015 Pedro M. Baeza
-# Copyright 2016 Antonio Espinosa <antonio.espinosa@tecnativa.com>
+# Copyright 2016 Tecnativa - Antonio Espinosa
+# Copyright 2015-2017 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'AEAT modelo 216',
-    'version': '9.0.1.0.0',
+    'version': '10.0.1.0.0',
     'category': "Localisation/Accounting",
     'author': "AvanzOSC,"
               "Tecnativa,"
