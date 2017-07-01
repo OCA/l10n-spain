35d43f4cedc7e030269a5e1aad41c8e612f4813b
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-07-01 19:56:08 +0200

    [MIG] l10n_es_aeat_mod216: Migration to 10.0
    * Standard migration procedure

--- l10n_es_aeat_mod216/models/mod111.py
+++ l10n_es_aeat_mod216/models/mod111.py
@@ -4,7 +4,7 @@
 # Copyright 2016 Antonio Espinosa <antonio.espinosa@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, api
+from odoo import models, api
 
 
 class L10nEsAeatMod111Report(models.Model):
