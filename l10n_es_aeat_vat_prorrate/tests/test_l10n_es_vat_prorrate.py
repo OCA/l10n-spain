bedc9c631e5ca7a358450e74aae89dbe09c912f8
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-07-02 12:15:09 +0200

    [MIG] l10n_es_aeat_vat_prorrate: Migration to 10.0
    OCA Transbot updated translations from Transifex

--- l10n_es_aeat_vat_prorrate/tests/test_l10n_es_vat_prorrate.py
+++ l10n_es_aeat_vat_prorrate/tests/test_l10n_es_vat_prorrate.py
@@ -1,59 +1,80 @@
 # -*- coding: utf-8 -*-
-# © 2016 Pedro M. Baeza
+# Copyright 2016-2017 Tecnativa - Pedro M. Baeza
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0
 
-from openerp.tests import common
-from openerp.addons.l10n_es_aeat_mod303.tests.test_l10n_es_aeat_mod303 \
+from odoo.addons.l10n_es_aeat_mod303.tests.test_l10n_es_aeat_mod303 \
     import TestL10nEsAeatMod303Base
 
 
-class TestL10nEsAeatVatProrrate(TestL10nEsAeatMod303Base,
-                                common.TransactionCase):
+class TestL10nEsAeatVatProrrate(TestL10nEsAeatMod303Base):
     def setUp(self):
+        self.taxes_sale['S_IVA0'] = (2000, 0)  # Add exempt amount
         super(TestL10nEsAeatVatProrrate, self).setUp()
-        self.model303.vat_prorrate_type = 'general'
-        self.model303.vat_prorrate_percent = 80
-        self.model303.button_calculate()
-
-    def test_deductable_part(self):
-        self.assertEqual(self.model303.total_deducir, 32)
-
-    def test_regularization_move(self):
-        analytic_journal = self.env['account.analytic.journal'].create({
-            'name': 'Test analytic journal',
-            'type': 'purchase',
-        })
-        journal = self.env['account.journal'].create({
+        self.journal = self.env['account.journal'].create({
             'name': 'Test journal',
             'code': 'TEST',
             'type': 'general',
-            'analytic_journal_id': analytic_journal.id,
         })
-        self.model303.journal_id = journal.id
-        counterpart_account = self.env['account.account'].create({
+        self.account_type = self.env['account.account.type'].create({
+            'name': 'Test account type',
+            'type': 'other',
+        })
+        self.counterpart_account = self.env['account.account'].create({
             'name': 'Test counterpart account',
             'code': 'COUNTERPART',
-            'type': 'other',
-            'user_type': self.account_type.id,
+            'user_type_id': self.account_type.id,
         })
-        self.model303.counterpart_account = counterpart_account.id
+        self.prorrate_regul_account = self.env['account.account'].search([
+            ('code', 'like', '6391%'),
+            ('company_id', '=', self.model303.company_id.id),
+        ], limit=1)
+        if not self.prorrate_regul_account:
+            self.prorrate_regul_account = self.env['account.account'].create({
+                'name': 'Test prorrate regularization account',
+                'code': '6391000',
+                'user_type_id': self.account_type.id,
+            })
+        self.model303.write({
+            'vat_prorrate_type': 'general',
+            'vat_prorrate_percent': 80,
+            'journal_id': self.journal.id,
+            'counterpart_account_id': self.counterpart_account.id,
+        })
+        self.model303_4t = self.model303.copy({
+            'name': '9994000000303',
+            'period_type': '4T',
+            'date_start': '2017-09-01',
+            'date_end': '2017-12-31',
+        })
+
+    def test_vat_prorrate(self):
+        # First semester
+        self.model303.button_calculate()
+        self.assertAlmostEqual(self.model303.total_deducir, 2043.82, 2)
         self.model303.create_regularization_move()
-        self.assertEqual(len(self.model303.move_id.line_id), 5)
-        lines = self.model303.move_id.line_id
-        line_tax = lines.filtered(lambda x: x.account_id == self.account_tax)
-        self.assertEqual(line_tax.credit, 40)
+        self.assertAlmostEqual(len(self.model303.move_id.line_ids), 82, 2)
+        lines = self.model303.move_id.line_ids
         line_counterpart = lines.filtered(
-            lambda x: x.account_id == counterpart_account)
-        self.assertEqual(line_counterpart.debit, 32)
-        line_vat_prorrate_1 = lines.filtered(
-            lambda x: (x.account_id == self.account_expense and
-                       x.analytic_account_id == self.analytic_account_1))
-        self.assertEqual(line_vat_prorrate_1.debit, 2)
-        line_vat_prorrate_2 = lines.filtered(
-            lambda x: (x.account_id == self.account_expense and
-                       x.analytic_account_id == self.analytic_account_2))
-        self.assertEqual(line_vat_prorrate_2.debit, 4)
-        line_vat_prorrate_3 = lines.filtered(
-            lambda x: (x.account_id == self.account_expense and
-                       not x.analytic_account_id))
-        self.assertEqual(line_vat_prorrate_3.debit, 2)
+            lambda x: x.account_id == self.counterpart_account
+        )
+        self.assertAlmostEqual(line_counterpart.credit, 2126.98, 2)
+        # Last semester
+        wizard = self.env['l10n.es.aeat.compute.vat.prorrate'].with_context(
+            active_id=self.model303_4t.id,
+        ).create({
+            'year': 2017,
+        })
+        wizard.button_compute()
+        self.assertAlmostEqual(self.model303_4t.vat_prorrate_percent, 82, 2)
+        self.model303_4t.button_calculate()
+        self.assertAlmostEqual(self.model303_4t.casilla_44, 51.10, 2)
+        self.assertEqual(
+            self.model303_4t.prorrate_regularization_account_id,
+            self.prorrate_regul_account,
+        )
+        self.model303_4t.create_regularization_move()
+        lines = self.model303_4t.move_id.line_ids
+        line_prorrate_regularization = lines.filtered(
+            lambda x: x.account_id == self.prorrate_regul_account
+        )
+        self.assertAlmostEqual(line_prorrate_regularization.credit, 51.10, 2)
--- l10n_es_aeat_vat_prorrate/wizard/l10n_es_aeat_compute_vat_prorrate.py
+++ l10n_es_aeat_vat_prorrate/wizard/l10n_es_aeat_compute_vat_prorrate.py
@@ -1,27 +1,52 @@
 # -*- coding: utf-8 -*-
-# © 2015 Antiun Ingeniería S.L. - Pedro M. Baeza
+# Copyright 2015-2017 Tecnativa - Pedro M. Baeza
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import models, fields, api
+import math
+from odoo import api, fields, models
 
 
 class ComputeVatProrrate(models.TransientModel):
     _name = "l10n.es.aeat.compute.vat.prorrate"
 
-    fiscalyear_id = fields.Many2one(
-        comodel_name="account.fiscalyear", string="Fiscal year", required=True)
+    def _default_year(self):
+        return int(fields.Date.today()[:4])
+
+    year = fields.Integer(
+        required=True,
+        default=_default_year,
+    )
 
     @api.multi
     def button_compute(self):
         self.ensure_one()
+        date_from = '%s-01-01' % self.year
+        date_to = '%s-12-31' % self.year
         mod303 = self.env['l10n.es.aeat.mod303.report'].browse(
-            self.env.context['active_id'])
-        codes = ['RGIDBI10', 'RGIDBI21', 'RGIDBI4', 'MBYCRBI']
-        move_lines = mod303._get_tax_code_lines(
-            codes, periods=self.fiscalyear_id.period_ids)
-        taxed = sum(move_lines.mapped('tax_amount'))
-        move_lines = mod303._get_tax_code_lines(
-            ['OESDAD'], periods=self.fiscalyear_id.period_ids)
-        exempt = sum(move_lines.mapped('tax_amount'))
-        mod303.vat_prorrate_percent = taxed / (taxed + exempt) * 100
+            self.env.context['active_id']
+        )
+        # Get base amount for taxed operations
+        taxed_taxes_codes = [
+            'S_IVA4B', 'S_IVA4S',
+            'S_IVA10B', 'S_IVA10S',
+            'S_IVA21B', 'S_IVA21S', 'S_IVA21ISP',
+        ]
+        move_lines = mod303._get_tax_lines(
+            taxed_taxes_codes, date_from, date_to, 'regular', 'base', 'both',
+        )
+        taxed = (sum(move_lines.mapped('credit')) -
+                 sum(move_lines.mapped('debit')))
+        move_lines = mod303._get_tax_lines(
+            taxed_taxes_codes, date_from, date_to, 'refund', 'base', 'both',
+        )
+        taxed += (sum(move_lines.mapped('credit')) -
+                  sum(move_lines.mapped('debit')))
+        # Get base amount of exempt operations
+        move_lines = mod303._get_tax_lines(
+            ['S_IVA0'], date_from, date_to, 'all', 'base', 'both',
+        )
+        exempt = (sum(move_lines.mapped('credit')) -
+                  sum(move_lines.mapped('debit')))
+        # compute prorrate percentage performing ceiling operation
+        mod303.vat_prorrate_percent = math.ceil(taxed / (taxed + exempt) * 100)
         return True
