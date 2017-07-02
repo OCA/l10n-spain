5ba1fb098205635a356e88415df890399d4bcc40
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-07-02 12:38:04 +0200

    [MIG] l10n_es_aeat_vat_prorrate_asset: Migration to 10.0

--- l10n_es_aeat_vat_prorrate_asset/__manifest__.py
+++ l10n_es_aeat_vat_prorrate_asset/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "AEAT - Prorrata de IVA - Extensión para los activos",
-    "version": "8.0.1.0.0",
+    "version": "10.0.1.0.0",
     "license": "AGPL-3",
     "author": "Tecnativa, "
               "Odoo Community Association (OCA)",
--- l10n_es_aeat_vat_prorrate_asset/models/mod303.py
+++ l10n_es_aeat_vat_prorrate_asset/models/mod303.py
@@ -2,7 +2,7 @@
 # Copyright 2015-2017 Tecnativa - Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import fields, models
+from odoo import models
 
 
 class L10nEsAeatMod303Report(models.Model):
@@ -10,10 +10,10 @@ class L10nEsAeatMod303Report(models.Model):
 
     def _calculate_casilla_44(self):
         super(L10nEsAeatMod303Report, self)._calculate_casilla_44()
-        year = fields.Date.from_string(self.fiscalyear_id.date_start).year
         assets = self.env['account.asset.asset'].search(
-            [('purchase_date', 'like', '{}%'.format(year))]
+            [('date', 'like', '{}%'.format(self.year))]
         )
+        result = self.casilla_44
         for asset in assets.filtered('vat_prorrate_percent'):
             original_taxes = (
                 asset.vat_prorrate_increment * 100 /
@@ -22,4 +22,5 @@ class L10nEsAeatMod303Report(models.Model):
             real_increment = (
                 original_taxes * (100 - self.vat_prorrate_percent) / 100
             )
-            self.casilla_44 += asset.vat_prorrate_increment - real_increment
+            result += asset.vat_prorrate_increment - real_increment
+        self.casilla_44 = round(result, 2)
