d8f4c4a25ce400a526d68a62720d9e220b8d8f23
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-07-02 12:38:04 +0200

    [MIG] l10n_es_aeat_vat_prorrate_asset: Migration to 10.0

--- l10n_es_aeat_vat_prorrate_asset/models/account_asset_asset.py
+++ l10n_es_aeat_vat_prorrate_asset/models/account_asset_asset.py
@@ -2,7 +2,7 @@
 # Copyright 2015-2017 Tecnativa - Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import fields, models
+from odoo import fields, models
 
 
 class AccountAssetAsset(models.Model):
