339839b8db44cfbd2b2454675aa3616627361d67
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-07-02 12:15:09 +0200

    [MIG] l10n_es_aeat_vat_prorrate: Migration to 10.0
    OCA Transbot updated translations from Transifex

--- l10n_es_aeat_vat_prorrate/__manifest__.py
+++ l10n_es_aeat_vat_prorrate/__manifest__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
-# (c) 2015 Antiun Ingeniería S.L. - Pedro M. Baeza
-# (c) 2015 AvanzOSC - Ainara Galdona
+# Copyright 2015 AvanzOSC - Ainara Galdona
+# Copyright 2015-2017 Tecnativa - Pedro M. Baeza
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0
 
 {
     "name": "AEAT - Prorrata de IVA",
-    "version": "8.0.2.0.0",
+    "version": "10.0.1.0.0",
     "license": "AGPL-3",
     "author": "AvanzOSC, "
               "Tecnativa, "
