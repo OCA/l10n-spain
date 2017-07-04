3dfddc0dcf5e2b9e69ac2b99ea9e19dbf63c0387
Roodin <santi@comunitea.com>
2017-07-04 12:43:26 +0200

    [FIX+IMP] l10n_es_aeat_sii:
    * Corrige errores y pylint
    * post_init_hook
    * Corregida referencia a conector
    * Permisos y cmapo de posicion fiscal erróneo

--- l10n_es_aeat_sii_oca/hooks.py
+++ l10n_es_aeat_sii_oca/hooks.py
@@ -2,34 +2,36 @@
 # Copyright 2017 Oihane Crucelaegui - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import SUPERUSER_ID
+from odoo import api, SUPERUSER_ID
 
 
 def add_key_to_existing_invoices(cr, registry):
     """This post-init-hook will update all existing invoices"""
-    invoice_obj = registry['account.invoice']
-    invoices = invoice_obj.search(cr, SUPERUSER_ID, [])
-    if invoices:
-        sii_key_obj = registry['aeat.sii.mapping.registration.keys']
-        sale_key = sii_key_obj.search(
-            cr, SUPERUSER_ID, [('code', '=', '01'), ('type', '=', 'sale')],
-            limit=1)
-        purchase_key = sii_key_obj.search(
-            cr, SUPERUSER_ID, [('code', '=', '01'), ('type', '=', 'purchase')],
-            limit=1)
-        if purchase_key:
+    with api.Environment.manage():
+        env = api.Environment(cr, SUPERUSER_ID, {})
+        invoice_obj = env['account.invoice']
+        invoices = invoice_obj.search([])
+        if invoices:
+            sii_key_obj = env['aeat.sii.mapping.registration.keys']
+            sale_key = sii_key_obj.search(
+                [('code', '=', '01'), ('type', '=', 'sale')],
+                limit=1)
+            purchase_key = sii_key_obj.search(
+                [('code', '=', '01'), ('type', '=', 'purchase')],
+                limit=1)
+            if purchase_key:
+                cr.execute("""
+                    UPDATE account_invoice
+                    SET sii_registration_key = %s
+                    WHERE type IN ('in_invoice', 'in_refund');""",
+                           (purchase_key[0].id,))
+            if sale_key:
+                cr.execute("""
+                    UPDATE account_invoice
+                    SET sii_registration_key = %s
+                    WHERE type NOT IN ('in_invoice', 'in_refund');""",
+                           (sale_key[0].id,))
             cr.execute("""
-                UPDATE account_invoice
-                SET sii_registration_key = %s
-                WHERE type IN ('in_invoice', 'in_refund');""",
-                       (purchase_key[0],))
-        if sale_key:
-            cr.execute("""
-                UPDATE account_invoice
-                SET sii_registration_key = %s
-                WHERE type NOT IN ('in_invoice', 'in_refund');""",
-                       (sale_key[0],))
-        cr.execute("""
-            ALTER TABLE account_invoice
-            ALTER COLUMN sii_registration_key SET NOT NULL;
-            """)
+                ALTER TABLE account_invoice
+                ALTER COLUMN sii_registration_key SET NOT NULL;
+                """)
