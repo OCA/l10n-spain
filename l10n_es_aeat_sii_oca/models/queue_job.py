dec74393b0c3b490b4c66382ac91e74639258645
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-07-07 01:59:15 +0200

    [IMP] l10n_es_aeat_sii: Seguridad + usabilidad
    * Filtro de no enviados SII que sólo incluya 2017
    * Los trabajos no realizados se eliminan en lugar de pasarlos a estado "Hecho"
      al reenviar, cancelar, etc.
    * Permiso de lectura a queue.job para los gestores de la AEAT.
    * Regla de seguridad para que los gestores de la AEAT sólo vean los trabajos
      del SII.
    * Los trabajos se encolan como admin para evitar errores al validar por gente
      sin permisos del conector. Desafortunadamente, no se puede mantener el
      usuario original en el trabajo.
    
    [IMP] l10n_es_aeat_sii: Share queue.job view for both types of invoices
    
    [FIX] l10n_es_aeat_sii: Add jobs with sudo for avoiding permission errors
    
    [FIX] l10n_es_aeat_sii: Permissions problem
    
    No more need for connector or AEAT permissions
    
    [IMP] l10n_es_aeat_sii: Track content sent + better layout for return

---
+++ l10n_es_aeat_sii_oca/models/queue_job.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Tecnativa - Pedro M. Baeza
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+from openerp import api, models
+
+
+class QueueJob(models.Model):
+    _inherit = 'queue.job'
+
+    @api.multi
+    def do_now(self):
+        self.sudo().write({'eta': 0})
+
+    @api.multi
+    def cancel_now(self):
+        self.sudo().filtered(
+            lambda x: x.state in ['pending', 'enqueued']
+        ).unlink()
