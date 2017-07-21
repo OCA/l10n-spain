73869b5dfb7c97478450a60a5da0ffe41f7905ad
Luis M. Ontalba <luismaront@gmail.com>
2017-07-21 20:39:47 +0200

    [MIG] l10n_es_account_banking_sepa_fsdd: Migrated to 10.0

--- l10n_es_account_banking_sepa_fsdd/__init__.py
+++ l10n_es_account_banking_sepa_fsdd/__init__.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-# © 2016 Comunitea Servicios Tecnológicos <omar@comunitea.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import models
-from . import wizard
