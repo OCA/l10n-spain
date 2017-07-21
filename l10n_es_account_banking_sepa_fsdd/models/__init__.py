5dd66094fccea5f111c5d5aa0a69fa5271f64db0
Luis M. Ontalba <luismaront@gmail.com>
2017-07-21 20:39:47 +0200

    [MIG] l10n_es_account_banking_sepa_fsdd: Migrated to 10.0

--- l10n_es_account_banking_sepa_fsdd/models/__init__.py
+++ l10n_es_account_banking_sepa_fsdd/models/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# © 2016 Comunitea Servicios Tecnológicos <omar@comunitea.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from . import payment_mode
+from . import account_payment_mode
+from . import account_payment_order
--- l10n_es_account_banking_sepa_fsdd/models/account_payment_mode.py
+++ l10n_es_account_banking_sepa_fsdd/models/account_payment_mode.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
-# © 2016 Comunitea Servicios Tecnológicos <omar@comunitea.com>
+# Copyright 2016 Comunitea Servicios Tecnológicos <omar@comunitea.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields
+from odoo import fields, models
 
 
 class PaymentMode(models.Model):
 
-    _inherit = "payment.mode"
+    _inherit = "account.payment.mode"
 
     charge_financed = fields.Boolean(
         'Financed Charge', help="Adds FSDD prefix to sepa file id")
