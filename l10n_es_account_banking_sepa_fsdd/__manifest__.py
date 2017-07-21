e9dadec716994e0afbf336a2d3d13521b8bed3ad
Luis M. Ontalba <luismaront@gmail.com>
2017-07-21 20:39:47 +0200

    [MIG] l10n_es_account_banking_sepa_fsdd: Migrated to 10.0

--- l10n_es_account_banking_sepa_fsdd/__manifest__.py
+++ l10n_es_account_banking_sepa_fsdd/__manifest__.py
@@ -1,19 +1,21 @@
 # -*- coding: utf-8 -*-
-# © 2016 Comunitea Servicios Tecnológicos <omar@comunitea.com>
+# Copyright 2016 Comunitea Servicios Tecnológicos <omar@comunitea.com>
+# Copyright 2017 Tecnativa - Luis M. Ontalba <luis.martinez@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Account Banking Sepa - FSDD (Anticipos de crédito)",
-    "version": "8.0.1.0.0",
-    "author": "Comunitea,Odoo Community Association (OCA)",
+    "version": "10.0.1.0.0",
+    "author": "Tecnativa, "
+              "Odoo Community Association (OCA)",
+    'license': 'AGPL-3',
     "website": "http://www.comunitea.com",
     "category": "Banking addons",
     "depends": [
-        "account_banking_sepa_direct_debit"
+        "account_banking_sepa_direct_debit",
     ],
-    "demo": [],
     "data": [
         "views/payment_mode_view.xml",
     ],
-    'installable': False,
+    'installable': True,
 }
