b8ef65933e7cfb4e2a2ed49f1eaf4b721e727bdb
Enric Tobella <etobella@creublanca.es>
2017-07-24 13:36:04 +0200

    [ADD] l10n_es_facturae_efact
    (cherry picked from commit 10604ac)

---
+++ l10n_es_facturae_efact/models/account_invoice_integration_method.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# © 2017 Creu Blanca
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+
+from odoo import models, exceptions, _
+import base64
+
+
+class AccountInvoiceIntegrationMethod(models.Model):
+    _inherit = "account.invoice.integration.method"
+
+    # Default values for integration. It could be extended
+    def integration_values(self, invoice):
+        res = super(AccountInvoiceIntegrationMethod, self).integration_values(
+            invoice
+        )
+        if self.code == 'eFACT':
+            if not invoice.company_id.facturae_cert:
+                raise exceptions.UserError(
+                    _('Certificate must be added for company'))
+            if not invoice.company_id.facturae_cert_password:
+                raise exceptions.UserError(
+                    _('Certificate password must be added for company'))
+            invoice_file, file_name = invoice.get_facturae(True)
+            attachment = self.env['ir.attachment'].create({
+                'name': file_name,
+                'datas': base64.b64encode(invoice_file),
+                'datas_fname': file_name,
+                'res_model': 'account.invoice',
+                'res_id': invoice.id,
+                'mimetype': 'application/xml'
+            })
+            res['attachment_id'] = attachment.id
+        return res
