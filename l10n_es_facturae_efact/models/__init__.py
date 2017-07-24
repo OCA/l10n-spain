05d7bae7c76bd28e07c79da14ed929e6af711bd8
Enric Tobella <etobella@creublanca.es>
2017-07-24 13:36:04 +0200

    [ADD] l10n_es_facturae_efact
    (cherry picked from commit 10604ac)

---
+++ l10n_es_facturae_efact/models/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import res_partner
+from . import config
+from . import account_invoice_integration_method
+from . import account_invoice_integration
+from . import account_invoice_integration_log
