5c6e8fabc1cc23757bd93d444eb07a03c20d64cd
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-08-18 12:58:59 +0200

    [FIX] l10n_es_aeat_sii: Look for user current company to see if SII is enabled
    In partners, the check is only visible if the company has SII enabled. Before this,
    the check was done looking to the partner company, but you can leave this field
    empty (specially in a multi-company environment), and then the result is not correct.
    
    Looking to the user company gives us the proper result.

--- l10n_es_aeat_sii_oca/models/res_partner.py
+++ l10n_es_aeat_sii_oca/models/res_partner.py
@@ -2,17 +2,23 @@
 # Copyright 2017 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import fields, models
+from openerp import api, fields, models
 
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     sii_enabled = fields.Boolean(
-        related="company_id.sii_enabled", readonly=True,
+        compute="_compute_sii_enabled",
     )
     sii_simplified_invoice = fields.Boolean(
         string="Simplified invoices in SII?",
         help="Checking this mark, invoices done to this partner will be "
              "sent to SII as simplified invoices."
     )
+
+    @api.multi
+    def _compute_sii_enabled(self):
+        sii_enabled = self.env.user.company_id.sii_enabled
+        for partner in self:
+            partner.sii_enabled = sii_enabled
