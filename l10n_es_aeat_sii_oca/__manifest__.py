bc6ed7788dbef02a7d02b04f68401eba1580d872
Pedro Rodríguez Gil <pedro@otherway.es>
2017-08-28 12:22:22 +0200

    [IMP] l10n_es_aeat_sii: Añadido mapeo para servicios extracomunitarios

--- l10n_es_aeat_sii_oca/__manifest__.py
+++ l10n_es_aeat_sii_oca/__manifest__.py
@@ -11,7 +11,7 @@
 
 {
     "name": "Suministro Inmediato de Información en el IVA",
-    "version": "9.0.1.7.1",
+    "version": "9.0.1.8.0",
     "category": "Accounting & Finance",
     "website": "https://odoospain.odoo.com",
     "author": "Acysos S.L.,"
