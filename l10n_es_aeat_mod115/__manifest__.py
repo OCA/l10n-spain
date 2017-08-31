04a116251346a0b9c7ed9947bb6c5618b969e21f
Marçal <marsal.isern@qubiq.es>
2017-08-31 12:11:13 +0200

    [MIG] l10n_es_aeat_mod115: Migration  to 10.0

--- l10n_es_aeat_mod115/__manifest__.py
+++ l10n_es_aeat_mod115/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     'name': 'AEAT modelo 115',
-    'version': '9.0.1.0.0',
+    'version': '10.0.1.0.0',
     'category': "Localisation/Accounting",
     'author': "AvanzOSC,"
               "Tecnativa,"
