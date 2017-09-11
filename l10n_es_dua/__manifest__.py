31016ba921df2174a5c90d3f058dc9bc8b56c2e1
Luis M. Ontalba <luis.martinez@tecnativa.com>
2017-09-11 16:11:56 +0200

    [MIG] l10n_es_dua: Migrationt to 10.0 (#633)

--- l10n_es_dua/__manifest__.py
+++ l10n_es_dua/__manifest__.py
@@ -1,27 +1,25 @@
 # -*- coding: utf-8 -*-
-# License AGPL-3: Antiun Ingenieria S.L. - Antonio Espinosa
+# Copyright 2017 Tecnativa - Antonio Espinosa
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 # See README.rst file on addon root folder for more details
 
 {
     'name': "Importaciones con DUA",
     'category': 'Localization/Account Charts',
-    'version': '8.0.1.0.0',
+    'version': '10.0.1.0.0',
     'depends': [
         'product',
         'l10n_es',
     ],
-    'external_dependencies': {},
     'data': [
         'data/taxes_dua.xml',
         'data/fiscal_positions_dua.xml',
         'data/fiscal_position_taxes_dua.xml',
         'data/products_dua.xml',
     ],
-    'author': 'Antiun Ingeniería S.L., '
+    'author': 'Tecnativa, '
               'Odoo Community Association (OCA)',
     'website': 'http://www.antiun.com',
     'license': 'AGPL-3',
-    'demo': [],
-    'test': [],
-    'installable': False,
+    'installable': True,
 }
