b05cd621f3f7af3ee8b37fb515407ed2d0c18d7e
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-09-15 13:39:35 +0200

    Update pre-migration.py

--- l10n_es_account_invoice_sequence/migrations/9.0.1.0.0/pre-migration.py
+++ l10n_es_account_invoice_sequence/migrations/9.0.1.0.0/pre-migration.py
@@ -12,13 +12,13 @@ def migrate(env, version):
     """
     column_name = openupgrade.get_legacy_name('merged_journal_id')
     if not openupgrade.column_exists(env.cr, 'account_journal', column_name):
-        # Avoid to crash if the column doesn't exists
+        # Avoid to crash if the column doesn't exist
         return
     openupgrade.logged_query(
         env.cr,
         """UPDATE account_journal aj
         SET refund_inv_sequence_id = aj2.invoice_sequence_id
         FROM account_journal aj2
-        WHERE aj2.merged_journal_id = aj.id
-        """
+        WHERE aj2.%s = aj.id
+        """ % column_name
     )
