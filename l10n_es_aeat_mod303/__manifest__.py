5022c3f12ceeeef63d7c232f6479282a16d4723f
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-09-26 17:49:33 +0200

    [FIX] l10_es_aeat_mod303: Import 2017 data file

--- l10n_es_aeat_mod303/__manifest__.py
+++ l10n_es_aeat_mod303/__manifest__.py
@@ -7,7 +7,7 @@
 
 {
     "name": "AEAT modelo 303",
-    "version": "10.0.1.0.0",
+    "version": "10.0.1.1.0",
     'category': "Accounting & Finance",
     'author': "Guadaltech,"
               "AvanzOSC,"
@@ -22,6 +22,7 @@
     "data": [
         "data/tax_code_map_mod303_data.xml",
         "data/aeat_export_mod303_data.xml",
+        "data/aeat_export_mod303_2017_data.xml",
         "views/mod303_view.xml",
         "security/l10n_es_aeat_mod303_security.xml",
         "security/ir.model.access.csv",
