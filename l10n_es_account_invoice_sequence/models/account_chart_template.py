02d5107366ab79637d1ce4319bf0d00695a1753d
Enric Tobella <etobella@creublanca.es>
2017-10-10 13:26:54 +0200

    [MIG] l10n_es_account_invoice_sequence

--- l10n_es_account_invoice_sequence/models/account_chart_template.py
+++ l10n_es_account_invoice_sequence/models/account_chart_template.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # Copyright 2015-2017 Pedro M. Baeza
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+# License AGPL-3 - See https://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import _, api, models, tools
+from odoo import _, api, models, tools
 
 
 class AccountChartTemplate(models.Model):
