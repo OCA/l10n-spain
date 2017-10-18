db95228b3f9326089e8955999cfa41f77c4bd813
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-10-18 17:53:40 +0200

    [FIX] l10n_es_aeat_mod303: Set date_end on 2014 export config

--- l10n_es_aeat_mod303/__manifest__.py
+++ l10n_es_aeat_mod303/__manifest__.py
@@ -7,7 +7,7 @@
 
 {
     "name": "AEAT modelo 303",
-    "version": "10.0.1.1.0",
+    "version": "10.0.1.2.0",
     'category': "Accounting & Finance",
     'author': "Guadaltech,"
               "AvanzOSC,"
