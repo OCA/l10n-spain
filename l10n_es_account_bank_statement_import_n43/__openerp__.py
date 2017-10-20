7bfb38816a33265b021b26e40ba6e2e418a08843
Roberto Lizana (mi) <robertolizana@trey.es>
2017-10-20 09:38:34 +0200

    [FIX] l10n_es_account_bank_statement_import_n43: al parsear los registros 22, nº documento incorrecto

--- l10n_es_account_bank_statement_import_n43/__openerp__.py
+++ l10n_es_account_bank_statement_import_n43/__openerp__.py
@@ -6,7 +6,7 @@
 {
     'name': 'Importación de extractos bancarios españoles (Norma 43)',
     'category': 'Accounting & Finance',
-    'version': '8.0.4.0.2',
+    'version': '8.0.4.0.3',
     'license': 'AGPL-3',
     'author': 'Spanish Localization Team,'
               'Tecnativa,'
