6d096b7fd83432a1b0dfcd2c6eafa5633e5e940d
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-10-23 09:55:23 +0200

    [FIX] l10n_es_aeat_sii: tax instead of tax_line
    Cherry-picking of v8 commit doesn't serve as is as the structure is
    different in >=v9.
    
    [FIX] l10n_es_aeat_sii: Improve exemption causes in products
    
    * Don't default to 'none'. As field is not required, there's no need for this.
    * Filter out when looking for product exemption causes those equal to 'none'.

--- l10n_es_aeat_sii_oca/__manifest__.py
+++ l10n_es_aeat_sii_oca/__manifest__.py
@@ -11,7 +11,7 @@
 
 {
     "name": "Suministro Inmediato de Información en el IVA",
-    "version": "10.0.1.0.1",
+    "version": "10.0.1.0.2",
     "category": "Accounting & Finance",
     "website": "https://odoospain.odoo.com",
     "author": "Acysos S.L.,"
