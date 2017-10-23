2108ad4f25937f1cdbe68848c6b27ccba8231a9e
Enric Tobella <etobella@creublanca.es>
2017-10-23 11:36:10 +0200

    [MIG] l10n_es_toponyms

--- l10n_es_toponyms/gen_src/gen_toponyms_geonames.py
+++ l10n_es_toponyms/gen_src/gen_toponyms_geonames.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 # Copyright 2013-2017 Pedro M. Baeza <pedro.baeza@tecnativa.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 import os.path
 import requests
 import tempfile
-import StringIO
-import unicodecsv
+from io import BytesIO
+import csv
 import zipfile
 
 
@@ -21,23 +21,25 @@ def filter_city(originalName):
 
 if __name__ == "__main__":
     url = "http://download.geonames.org/export/zip/ES.zip"
-    print 'Comenzando descarga de %s' % url
+    print('Comenzando descarga de %s' % url)
     res_request = requests.get(url)
     if res_request.status_code != requests.codes.ok:
-        print 'Error descargando archivo: %s.' % res_request.status_code
+        print('Error descargando archivo: %s.' % res_request.status_code)
         exit(res_request.status_code)
-    f_geonames = zipfile.ZipFile(StringIO.StringIO(res_request.content))
-    tempdir = tempfile.mkdtemp(prefix='openerp')
+    f_geonames = zipfile.ZipFile(BytesIO(res_request.content))
+    tempdir = tempfile.mkdtemp(prefix='odoo')
     f_geonames.extract('ES.txt', tempdir)
-    print 'Archivo descomprimido'
+    print('Archivo descomprimido')
     data_file = open(os.path.join(tempdir, 'ES.txt'), 'r')
     data_file.seek(0)
-    print 'Generando XML...'
+    print('Generando XML...')
     output = open("l10n_es_toponyms_zipcodes.xml", 'w')
     output.write('<?xml version="1.0" encoding="UTF-8"?>\n')
     output.write('<odoo noupdate="1">\n')
     cont = 0
-    reader = unicodecsv.reader(data_file, encoding='utf-8', delimiter='	')
+    dialect = csv.excel
+    dialect.delimiter = '	'
+    reader = csv.reader(data_file, dialect=dialect)
     for row in reader:
         cont += 1
         output.write(' ' * 8 +
@@ -51,11 +53,11 @@ if __name__ == "__main__":
             '<field name="state_id" ref="base.state_es_{}"/>\n'.format(state)
         )
         output.write(' ' * 12 + '<field name="city">%s</field>\n' %
-                     filter_city(row[2]).encode('utf-8'))
+                     filter_city(row[2]))
         output.write(' ' * 12 + '<field name="name">%s</field>\n' % row[1])
         output.write(' ' * 12 + '<field name="country_id" ref="base.es"/>\n')
         output.write(' ' * 8 + '</record>\n')
     data_file.close()
     output.write('</odoo>\n')
     output.close()
-    print "Proceso terminado"
+    print("Proceso terminado")
