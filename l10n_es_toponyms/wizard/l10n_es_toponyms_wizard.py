dfb7b50ea7a2ebbf1b189d9b0f59d297c4d96cb8
Enric Tobella <etobella@creublanca.es>
2017-10-23 11:36:10 +0200

    [MIG] l10n_es_toponyms

--- l10n_es_toponyms/wizard/l10n_es_toponyms_wizard.py
+++ l10n_es_toponyms/wizard/l10n_es_toponyms_wizard.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Copyright 2009 Jordi Esteve <jesteve@zikzakmedia.com>
 # Copyright 2013-2017 Pedro M. Baeza <pedro.baeza@tecnativa.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import models, fields, api, tools
 import os
