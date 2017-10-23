1d2fcd94d6d6c96927cf97ab5e9fdeca6e19586c
Jordi Ballester <jordi.ballester@eficent.com>
2017-10-23 20:31:37 +0200

    [ADD] l10n_es_mis_reports: MIS reports for PL / BS

---
+++ l10n_es_mis_report/__manifest__.py
@@ -0,0 +1,29 @@
+# Copyright 2018 Eficent Business and IT Consulting Services, S.L.
+#                <contact@eficent.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl
+
+{
+    'name': 'Plantillas MIS Builder para informes contables españoles',
+    'summary': """
+        Plantillas MIS Builder para informes contables españoles""",
+    'author': 'Eficent,'
+              'Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/l10n-spain',
+    'category': 'Reporting',
+    'version': '11.0.1.0.0',
+    'license': 'AGPL-3',
+    'depends': [
+        'mis_builder',  # OCA/mis-builder
+    ],
+    'data': [
+        'data/mis_report_styles.xml',
+        'data/mis_report_balance_abreviated.xml',
+        'data/mis_report_balance_normal.xml',
+        'data/mis_report_balance_sme.xml',
+        'data/mis_report_pyg_abreviated.xml',
+        'data/mis_report_pyg_normal.xml',
+        'data/mis_report_pyg_sme.xml',
+        'data/mis_report_revenues_expenses_normal.xml',
+    ],
+    'installable': True,
+}
