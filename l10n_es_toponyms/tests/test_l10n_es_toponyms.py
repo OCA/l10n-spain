478a2f2d929e54b3703ec8e475424a2be538df99
Enric Tobella <etobella@creublanca.es>
2017-10-23 11:36:10 +0200

    [MIG] l10n_es_toponyms

--- l10n_es_toponyms/tests/test_l10n_es_toponyms.py
+++ l10n_es_toponyms/tests/test_l10n_es_toponyms.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright 2013-2017 Pedro M. Baeza <pedro.baeza@tecnativa.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo.tests import common
 
