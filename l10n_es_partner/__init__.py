f9f014cdf4277ddcd580a7aa121a44e188f94f05
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-10-26 13:16:48 +0200

    [MIG] l10n_es_partner: Migration to 11.0
    * Updated banks local file.
    * Updated import script
    * Adapted res.partner.bank logic and views
    * Adapted account.journal logic

--- l10n_es_partner/__init__.py
+++ l10n_es_partner/__init__.py
@@ -1,27 +1,4 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (c) 2008 Spanish Localization Team
-#    Copyright (c) 2009 Zikzakmedia S.L. (http://zikzakmedia.com)
-#                       Jordi Esteve <jesteve@zikzakmedia.com>
-#    Copyright (c) 2013 Serv. Tecnol. Avanzados (http://www.serviciosbaeza.com)
-#                       Pedro Manuel Baeza <pedro.baeza@serviciosbaeza.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# License AGPL-3 - See https://www.gnu.org/licenses/agpl-3.0.html
 
 from . import models
 from . import wizard
