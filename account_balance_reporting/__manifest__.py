6dbc98803e9d99d8c2ce0484713203cf04d1449d
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-11-03 09:37:46 +0100

    [FIX] account_balance_reporting: Switch closing condition

--- account_balance_reporting/__manifest__.py
+++ account_balance_reporting/__manifest__.py
@@ -8,7 +8,7 @@
 
 {
     "name": "Account balance reporting engine",
-    "version": "10.0.1.3.0",
+    "version": "10.0.1.3.1",
     "author": "Pexego, "
               "AvanzOSC, "
               "Tecnativa, "
--- account_balance_reporting/models/account_balance_reporting_report.py
+++ account_balance_reporting/models/account_balance_reporting_report.py
@@ -371,8 +371,8 @@ class AccountBalanceReportingLine(models.Model):
             # Exclude closing/opening/PL moves (if account_fiscal_year_closing
             # is installed)
             if 'closing_type' in self.env['account.move']._fields:
-                domain_current.append(('move_id.closing_type', '!=', 'none'))
-                domain_previous.append(('move_id.closing_type', '!=', 'none'))
+                domain_current.append(('move_id.closing_type', '=', 'none'))
+                domain_previous.append(('move_id.closing_type', '=', 'none'))
             for line in self.filtered(lambda l: l.report_id == report):
                 if (line.calc_date and
                         line.calc_date == line.report_id.calc_date):
