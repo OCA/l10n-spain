0f5fd2a4f71ee9b0eff40fdef5f583e7159238a7
oihane <oihanecruce@gmail.com>
2017-11-13 16:17:53 +0100

    [MIG] l10n_es_aeat_mod303: Migration to 11.0

--- l10n_es_aeat_mod303/models/mod303.py
+++ l10n_es_aeat_mod303/models/mod303.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
-# © 2013 - Guadaltech - Alberto Martín Cortada
-# © 2015 - AvanzOSC - Ainara Galdona
+# Copyright 2013 - Guadaltech - Alberto Martín Cortada
+# Copyright 2015 - AvanzOSC - Ainara Galdona
 # Copyright 2016 Tecnativa - Antonio Espinosa
 # Copyright 2014-2017 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
