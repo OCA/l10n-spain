d6b14ee154e24ce78b4b43e2bb36fa5245f4644e
javierjcf <javierjcf@gmail.com>
2017-11-14 17:47:10 +0100

    [MIG] l10n_es_aeat_sii: Migration to 11.0

--- l10n_es_aeat_sii_oca/models/product_product.py
+++ l10n_es_aeat_sii_oca/models/product_product.py
@@ -1,8 +1,7 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 MINORISA (http://www.minorisa.net)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import fields, models
+from odoo import fields, models
 
 
 class ProductTemplate(models.Model):
