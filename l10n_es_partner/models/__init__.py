15d02d2402b27e71e43262229252f18a78a0ef87
Carlos Dauden <carlos.dauden@tecnativa.com>
2017-11-15 03:39:20 +0100

    [11.0][IMP] l10n_es_partner: Move validation IBAN to base_bank_from_iban
    [UPD] Update l10n_es_partner.pot

--- l10n_es_partner/models/__init__.py
+++ l10n_es_partner/models/__init__.py
@@ -1,6 +1,4 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl-3).
 
-from . import account_journal
 from . import res_bank
 from . import res_partner
-from . import res_partner_bank
