6ec2ac159ae8acb8930206274ea9401555032bc7
Luis M. Ontalba <luismaront@gmail.com>
2017-11-15 17:42:00 +0100

    [IMP] l10n_es_aeat_mod349: Based on account moves

---
+++ l10n_es_aeat_mod349/models/aeat_349_map_line.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Luis M. Ontalba <luis.martinez@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import api, models, fields
+
+
+class Aeat349MapLines(models.Model):
+    _name = 'aeat.349.map.line'
+    _description = 'Aeat 349 Map Line'
+
+    code = fields.Char(string='Code', required=True)
+    name = fields.Char(string='Name')
+    taxes = fields.One2many(
+        comodel_name='account.tax.template',
+        inverse_name='aeat_349_operation_key',
+        string="Taxes")
+
+    @api.multi
+    @api.depends('name', 'code')
+    def name_get(self):
+        result = []
+        for line in self:
+            name = line.code + ' ' + line.name
+            result.append((line.id, name))
+        return result
