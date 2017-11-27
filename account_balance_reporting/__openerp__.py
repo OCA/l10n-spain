cd78f92a534717619336cfcd460dd3c8f1c937bf
Pedro M. Baeza <pedro.baeza@gmail.com>
2017-11-27 10:38:58 +0100

    [FIX] account_balance_reporting: Final adjustment for closing

--- account_balance_reporting/__openerp__.py
+++ account_balance_reporting/__openerp__.py
@@ -8,7 +8,7 @@
 
 {
     "name": "Account balance reporting engine",
-    "version": "9.0.1.0.3",
+    "version": "9.0.1.0.4",
     "author": "Pexego, "
               "AvanzOSC, "
               "Tecnativa, "
