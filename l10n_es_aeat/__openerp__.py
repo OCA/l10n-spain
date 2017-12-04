0544026111947986b188b77adda2b1c5cbdabfa1
cubells <vicent@vcubells.net>
2017-12-04 19:57:30 +0100

    [FIX] l10n_es_aeat: onchange_chart_template_id is missing

--- l10n_es_aeat/__openerp__.py
+++ l10n_es_aeat/__openerp__.py
@@ -9,7 +9,7 @@
 {
     'name': "AEAT Base",
     'summary': "Modulo base para declaraciones de la AEAT",
-    'version': "9.0.1.0.5",
+    'version': "9.0.1.0.6",
     'author': "Pexego,"
               "Acysos,"
               "AvanzOSC,"
