0033ac3ea43ed5f5a96eb1c7d99a4491f5ae32cf
Jordi Ballester <jordi.ballester@eficent.com>
2017-12-14 18:39:27 +0100

    fixes export to boe

--- l10n_es_aeat/__manifest__.py
+++ l10n_es_aeat/__manifest__.py
@@ -9,7 +9,7 @@
 {
     'name': "AEAT Base",
     'summary': "Modulo base para declaraciones de la AEAT",
-    'version': "11.0.1.0.0",
+    'version': "11.0.1.0.1",
     'author': "Pexego,"
               "Acysos,"
               "AvanzOSC,"
