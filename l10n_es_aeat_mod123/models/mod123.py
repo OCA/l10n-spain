f296632ab558b3c950e22a3a4106da226f15b3d3
Enric Tobella <etobella@creublanca.es>
2017-12-19 13:44:20 +0100

    [MIG] Migrate l10n_es_aeat_mod123 to 11.0

--- l10n_es_aeat_mod123/models/mod123.py
+++ l10n_es_aeat_mod123/models/mod123.py
@@ -1,14 +1,14 @@
-# -*- coding: utf-8 -*-
 # 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from openerp import fields, models, api, _
+from odoo import api, fields, models
 
 
 class L10nEsAeatMod123Report(models.Model):
     _description = 'AEAT 123 report'
     _inherit = 'l10n.es.aeat.report.tax.mapping'
     _name = 'l10n.es.aeat.mod123.report'
+    _aeat_number = '123'
 
     number = fields.Char(default='123')
     casilla_01 = fields.Integer(
@@ -19,18 +19,10 @@ class L10nEsAeatMod123Report(models.Model):
         string='[02] Base retenciones', readonly=True,
         states={'calculated': [('readonly', False)]},
         help="Casilla [02] Base de la retención y/o del ingreso a cuenta")
-    move_lines_02 = fields.Many2many(
-        comodel_name='account.move.line',
-        relation='mod123_account_move_line02_rel',
-        column1='mod123', column2='account_move_line')
     casilla_03 = fields.Float(
         string='[03] Retenciones', readonly=True,
         states={'calculated': [('readonly', False)]},
         help="Casilla [03] Retenciones e ingresos a cuenta")
-    move_lines_03 = fields.Many2many(
-        comodel_name='account.move.line',
-        relation='mod123_account_move_line03_rel',
-        column1='mod123', column2='account_move_line')
     casilla_04 = fields.Float(
         string='[04] Ingresos ejercicios anteriores', readonly=True,
         states={'calculated': [('readonly', False)]},
@@ -67,43 +59,23 @@ class L10nEsAeatMod123Report(models.Model):
     @api.depends('casilla_03', 'casilla_05')
     def _compute_casilla06(self):
         for report in self:
-            report.casilla_06 = self.casilla_03 + self.casilla_05
+            report.casilla_06 = report.casilla_03 + report.casilla_05
 
-    @api.depends('casilla_03', 'casilla_05')
+    @api.depends('casilla_06', 'casilla_07')
     def _compute_casilla08(self):
         for report in self:
-            report.casilla_08 = self.casilla_06 + self.casilla_07
-
-    def __init__(self, pool, cr):
-        self._aeat_number = '123'
-        super(L10nEsAeatMod123Report, self).__init__(pool, cr)
+            report.casilla_08 = report.casilla_06 + report.casilla_07
 
     @api.multi
     def calculate(self):
-        self.ensure_one()
-        move_lines02 = self._get_tax_code_lines(
-            ['RBI123'], periods=self.periods)
-        move_lines03 = self._get_tax_code_lines(
-            ['RICC123'], periods=self.periods)
-        self.move_lines_02 = move_lines02.ids
-        self.move_lines_03 = move_lines03.ids
-        self.casilla_02 = sum(move_lines02.mapped('tax_amount'))
-        self.casilla_03 = sum(move_lines03.mapped('tax_amount'))
-        partners = (move_lines02 + move_lines03).mapped('partner_id')
+        super(L10nEsAeatMod123Report, self).calculate()
+        move_lines02 = self.tax_line_ids.filtered(
+            lambda r: r.field_number == 2)
+        move_lines03 = self.tax_line_ids.filtered(
+            lambda r: r.field_number == 3)
+        self.casilla_02 = move_lines02.amount
+        self.casilla_03 = move_lines03.amount
+        partners = (
+            move_lines02.move_line_ids + move_lines03.move_line_ids
+        ).mapped('partner_id')
         self.casilla_01 = len(set(partners.ids))
-
-    @api.multi
-    def show_move_lines(self):
-        move_lines = []
-        if self.env.context.get('move_lines02', False):
-            move_lines = self.move_lines_02.ids
-        elif self.env.context.get('move_lines03', False):
-            move_lines = self.move_lines_03.ids
-        return {
-            'type': 'ir.actions.act_window',
-            'name': _('Account Move Lines'),
-            'view_mode': 'tree,form',
-            'view_type': 'form',
-            'res_model': 'account.move.line',
-            'domain': [('id', 'in', move_lines)]
-        }
