4513df1b40d5f6325593d30aa1d9237016bcf633
Enric Tobella <etobella@creublanca.es>
2017-12-29 11:46:14 +0100

    [MIG] l10n_es_facturae_face: Migration to 11.0

--- l10n_es_facturae_face/__manifest__.py
+++ l10n_es_facturae_face/__manifest__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 # © 2017 Creu Blanca
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Envío de Factura-e a FACe",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "author": "Creu Blanca, "
               "Odoo Community Association (OCA)",
     "category": "Accounting & Finance",
