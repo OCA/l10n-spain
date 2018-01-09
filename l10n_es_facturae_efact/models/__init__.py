d1e5fafdfdb3774f06710392535415667e52e29e
Enric Tobella <etobella@creublanca.es>
2018-01-09 16:01:23 +0100

    [MIG] Migration to 11.0

--- l10n_es_facturae_efact/models/__init__.py
+++ l10n_es_facturae_efact/models/__init__.py
@@ -1,7 +1,7 @@
-# -*- coding: utf-8 -*-
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from . import res_partner
+from . import res_company
 from . import config
 from . import account_invoice_integration_method
 from . import account_invoice_integration
