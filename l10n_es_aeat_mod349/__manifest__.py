60079ed483424bc3e92a6effd2bb100ddeebd7a2
Jordi Ballester <jordi.ballester@eficent.com>
2018-01-12 17:50:09 +0100

    refactoring de l10n_es_aeat_mod349. Mejoras incluídas: * Mejora del rendimiento. * Apuntes contables como fuente de datos. El motor del informe se construye a partir de apuntes   contables, y la información de facturas se consulta cuando se necesita. De este modo aseguramos que   los resultados serán consistentes con los otros modelos de AEAT, que también toman como fuente de   datos los apuntes contables. Así evitamos posibles inconsistencias. * Nuevo informe Qweb, que sustituye al antiguo RML * Correcciones para exporación a formato BOE. Permite importar el modelo a la web de hacienda. * Permite borrar operaciones que no se quiere declarar. El usuario puede eliminar las operaciones que   no desee incluir en la presentación. Esto será especialmente útil en el caso de presentaciones   complementarias. * En declaraciones normales y complementarias se excluyen automáticamente operaciones ya   presentadas anteriormente. * Script de migración para tener en cuenta los cambios en el modelo de datos.

--- l10n_es_aeat_mod349/__manifest__.py
+++ l10n_es_aeat_mod349/__manifest__.py
@@ -6,32 +6,39 @@
 #                     - Pedro M. Baeza (http://www.serviciosbaeza.com)
 # Copyright 2016 - Tecnativa - Angel Moya <odoo@tecnativa.com>
 # Copyright 2017 - Tecnativa - Luis M. Ontalba <luis.martinez@tecnativa.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# Copyright 2017 - Eficent Business and IT Consulting Services, S.L.
+#                  <contact@eficent.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
     "name": "Modelo 349 AEAT",
-    "version": "9.0.1.1.0",
+    "version": "9.0.2.0.0",
     "author": "Pexego, "
               "Top Consultant, "
-              "Tecnativa,"
+              "Tecnativa, "
+              "Eficent, "
               "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": 'Localisation/Accounting',
     'website': 'https://github.com/OCA/l10n-spain',
     "depends": [
-        "account_invoice_refund_link",
         "l10n_es_aeat",
         "l10n_es",
+        'account_invoice_refund_link',
     ],
     'data': [
         "data/aeat_349_map_data.xml",
         "data/aeat_export_mod349_partner_refund_data.xml",
         "data/aeat_export_mod349_partner_data.xml",
         "data/aeat_export_mod349_data.xml",
+        "views/account_invoice_view.xml",
+        "views/account_move_view.xml",
         "views/account_tax_view.xml",
         "views/aeat_349_map_view.xml",
         "views/mod349_view.xml",
-        "report/mod349_report.xml",
+        'report/common_templates.xml',
+        "report/aeat_mod349.xml",
+        "report/report_views.xml",
         "security/ir.model.access.csv",
         "security/mod_349_security.xml",
     ],
