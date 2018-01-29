c9f3b8a098085809f552d7bc10837d56fe0fe116
Pedro M. Baeza <pedro.baeza@gmail.com>
2018-01-29 17:38:44 +0100

    [FIX+IMP] l10n_es_aeat_mod349: Several fixes + imps to new approach
    * Save in account.move.line directly the 349 export key via a selection field, so you are not bound to a map line record instead of mapping changes.
    * Field renamings for better understanding.
    * Map data with command `4` instead of `6` for allowing custom mappings.
    * Added constraint for avoiding duplicated mapping lines with the same operation key.
    * Fix tests asserts and code
    * Fix refund records computation
    * Complete behavior for imported move lines declarations
    * Switch certain fields to computed ones, for avoiding extra code that can be handled easily with ORM, but without performance penalty thanks to norecompute ORM option.
    * Remove useless check for invalid characters, as export now removes them

--- l10n_es_aeat_mod349/models/account_invoice.py
+++ l10n_es_aeat_mod349/models/account_invoice.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017 Luis M. Ontalba <luis.martinez@tecnativa.com>
-# Copyright 2017 - Eficent Business and IT Consulting Services, S.L.
-#                  <contact@eficent.com>
+# Copyright 2017 Tecnativa - Luis M. Ontalba
+# Copyright 2017 Eficent Business & IT Consult. Services <contact@eficent.com>
+# Copyright 2018 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from openerp import api, fields, models
@@ -15,14 +15,17 @@ class AccountInvoice(models.Model):
         string='EU Triangular deal',
         help='This invoice constitutes a triangular operation for the '
              'purposes of intra-community operations.',
-        readonly=True, states={'draft': [('readonly', False)]})
+        readonly=True,
+        states={'draft': [('readonly', False)]},
+    )
 
     @api.model
     def line_get_convert(self, line, part):
         """Copy from invoice to move lines"""
         res = super(AccountInvoice, self).line_get_convert(line, part)
-        res['aeat_349_operation_key'] = line.get(
-            'aeat_349_operation_key', False)
+        res['l10n_es_aeat_349_operation_key'] = line.get(
+            'l10n_es_aeat_349_operation_key', False,
+        )
         return res
 
     @api.model
@@ -30,7 +33,38 @@ class AccountInvoice(models.Model):
         """We pass on the operation key from invoice line to the move line"""
         ml_dicts = super(AccountInvoice, self).invoice_line_move_line_get()
         for ml_dict in ml_dicts:
-            invl_id = ml_dict['invl_id']
-            invl = self.env['account.invoice.line'].browse(invl_id)
-            ml_dict['aeat_349_operation_key'] = invl.aeat_349_operation_key.id
+            invl = self.env['account.invoice.line'].browse(ml_dict['invl_id'])
+            ml_dict['l10n_es_aeat_349_operation_key'] = (
+                invl.l10n_es_aeat_349_operation_key
+            )
         return ml_dicts
+
+
+class AccountInvoiceLine(models.Model):
+    """Inheritance of account invoice line to add some fields:
+    - AEAT_349_operation_key
+    """
+    _inherit = 'account.invoice.line'
+
+    def _selection_operation_key(self):
+        return self.env['account.move.line'].fields_get(
+            allfields=['l10n_es_aeat_349_operation_key'],
+        )['l10n_es_aeat_349_operation_key']['selection']
+
+    l10n_es_aeat_349_operation_key = fields.Selection(
+        selection=_selection_operation_key,
+        string='AEAT 349 Operation key',
+        compute='_compute_l10n_es_aeat_349_operation_key',
+    )
+
+    @api.depends('invoice_line_tax_ids', 'invoice_id.eu_triangular_deal')
+    def _compute_l10n_es_aeat_349_operation_key(self):
+        for rec in self:
+            if rec.invoice_id.eu_triangular_deal:
+                rec.l10n_es_aeat_349_operation_key = 'T'
+            else:
+                rec.l10n_es_aeat_349_operation_key = (
+                    rec.invoice_line_tax_ids.filtered(
+                        lambda x: x.l10n_es_aeat_349_operation_key
+                    )[:1].l10n_es_aeat_349_operation_key
+                )
