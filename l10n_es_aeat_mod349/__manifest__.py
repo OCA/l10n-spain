c4656117d82883d8f793230b48aa1e35c6a4880b
Pedro M. Baeza <pedro.baeza@gmail.com>
2018-03-19 17:19:47 +0100

    [FIX] l10n_es_aeat_mod349: Refunds are created when outside period
    Change <= / >= by < / > for avoiding including refunds that belongs
    to the period to the refund records.

--- l10n_es_aeat_mod349/__manifest__.py
+++ l10n_es_aeat_mod349/__manifest__.py
@@ -9,7 +9,7 @@
 
 {
     "name": "Modelo 349 AEAT",
-    "version": "10.0.2.1.1",
+    "version": "10.0.2.1.2",
     "author": "Pexego, "
               "Top Consultant, "
               "Tecnativa, "
