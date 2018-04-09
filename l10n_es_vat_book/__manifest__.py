4132476b50fe2cc93b7da362ae245a865be42d86
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2018-04-09 19:18:37 +0200

    corrige problema por el cual en el libro de iva se reflejan facturas "normales" como rectificativas.

--- l10n_es_vat_book/__manifest__.py
+++ l10n_es_vat_book/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Libro de IVA",
-    "version": "10.0.1.0.0",
+    "version": "10.0.1.0.1",
     "author": "PRAXYA, "
               "Eficent, "
               "Odoo Community Association (OCA)",
