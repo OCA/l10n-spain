cb8fd4d9ae1e5e0b177b2c2c4f4b1b3c5a1bfd1d
David <david.vidal@tecnativa.com>
2018-04-10 20:27:30 +0200

    [MIG] l10n_es_pos: Migration to 11.0

--- l10n_es_pos/static/src/js/screens.js
+++ l10n_es_pos/static/src/js/screens.js
@@ -1,39 +1,28 @@
-/****************************************************************************
- *
- *    OpenERP, Open Source Management Solution
- *    Copyright (C) 2016 Aselcis Consulting (http://www.aselcis.com). All Rights Reserved
- *    Copyright (C) 2016 David Gómez Quilón (http://www.aselcis.com). All Rights Reserved
- *
- *    This program is free software: you can redistribute it and/or modify
- *    it under the terms of the GNU Affero General Public License as
- *    published by the Free Software Foundation, either version 3 of the
- *    License, or (at your option) any later version.
- *
- *    This program is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU Affero General Public License for more details.
- *
- *    You should have received a copy of the GNU Affero General Public License
- *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- ******************************************************************************/
+/* Copyright 2016 David Gómez Quilón <david.gomez@aselcis.com>
+   Copyright 2018 David Vidal <david.vidal@tecnativa.com>
+   License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+*/
 
 odoo.define('l10n_es_pos.screens', function (require) {
     "use strict";
 
-    var core = require('web.core');
     var screens = require('point_of_sale.screens');
-    var QWeb = core.qweb;
-    var _t = core._t;
-    var exports = {};
+
 
     screens.PaymentScreenWidget.include({
         validate_order: function (force_validate) {
-            this.pos.get_order().set_simple_inv_number();
+            var below_limit = this.pos.get_order().get_total_with_tax() <= this.pos.config.l10n_es_simplified_invoice_limit;
+            if (this.pos.config.iface_l10n_es_simplified_invoice) {
+                var order = this.pos.get_order();
+                if (below_limit) {
+                    order.set_simple_inv_number();
+                } else {
+                    // Force invoice above limit. Online is needed.
+                    order.to_invoice = true;
+                }
+            }
             this._super(force_validate);
         }
     });
 
-    return exports;
 });
