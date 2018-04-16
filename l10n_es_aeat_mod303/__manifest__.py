da7a824bd73e02c772be6a6cd32dab6fdc7b73be
Pedro M. Baeza <pedro.baeza@gmail.com>
2018-04-16 15:57:50 +0200

    [FIX] l10n_es_aeat_mod303: Perform strict search of counterpart account
    Odoo's operator "like" encloses the text to search into "%s", making
    the search to match with accounts like "157000" and others as addition
    to the expected "570000".

--- l10n_es_aeat_mod303/__manifest__.py
+++ l10n_es_aeat_mod303/__manifest__.py
@@ -8,7 +8,7 @@
 
 {
     "name": "AEAT modelo 303",
-    "version": "10.0.2.0.0",
+    "version": "10.0.2.0.1",
     'category': "Accounting & Finance",
     'author': "Guadaltech,"
               "AvanzOSC,"
