fef98ed378315a5b878295568be303b69cd0a0ec
Pedro M. Baeza <pedro.baeza@gmail.com>
2018-04-19 08:58:10 +0200

    [FIX] Version numbers in manifest

--- l10n_es_aeat_mod123/__manifest__.py
+++ l10n_es_aeat_mod123/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'AEAT modelo 123',
-    'version': '8.0.1.0.0',
+    'version': '11.0.1.0.0',
     'category': "Localisation/Accounting",
     'author': "Tecnativa, "
               "Spanish Localization Team, "
