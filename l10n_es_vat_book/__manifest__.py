43fd577af3dbcfbb99bc24df0474756b2fa5199a
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2018-04-20 14:54:44 +0200

    corrige error en la lectura de asientos no asociados a facturas.

--- l10n_es_vat_book/__manifest__.py
+++ l10n_es_vat_book/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     "name": "Libro de IVA",
-    "version": "11.0.1.0.0",
+    "version": "11.0.1.0.1",
     "author": "PRAXYA, "
               "Eficent, "
               "Odoo Community Association (OCA)",
