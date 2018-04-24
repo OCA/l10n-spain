550a431e5f3c7bb1cdf01eebd8d54860e008e5ed
Pedro M. Baeza <pedro.baeza@gmail.com>
2018-04-24 19:45:43 +0200

    [IMP] l10n_es_aeat: Add tax_exigibility option to tax mapping
    With this, cash basis can be done in an easy way, and it's a base field
    in account module.

--- l10n_es_aeat/models/l10n_es_aeat_map_tax_line.py
+++ l10n_es_aeat/models/l10n_es_aeat_map_tax_line.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2013-2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# Copyright 2013-2018 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl
 
 from odoo import fields, models
@@ -12,7 +12,7 @@ class L10nEsAeatMapTaxLine(models.Model):
     field_number = fields.Integer(string="Field number", required=True)
     tax_ids = fields.Many2many(
         comodel_name='account.tax.template', string="Taxes templates",
-        required=True)
+    )
     name = fields.Char(string="Name", required=True)
     map_parent_id = fields.Many2one(
         comodel_name='l10n.es.aeat.map.tax', required=True)
@@ -21,17 +21,39 @@ class L10nEsAeatMapTaxLine(models.Model):
             ('all', 'All'),
             ('regular', 'Regular'),
             ('refund', 'Refund'),
-        ], string="Operation type", default='all')
+        ],
+        string="Operation type",
+        default='all',
+        required=True,
+    )
     field_type = fields.Selection(
         selection=[
             ('base', 'Base'),
             ('amount', 'Amount'),
-        ], string="Field type", default='amount')
+        ],
+        string="Field type",
+        default='amount',
+        required=True,
+    )
     sum_type = fields.Selection(
         selection=[
             ('credit', 'Credit'),
             ('debit', 'Debit'),
             ('both', 'Both (Credit - Debit)'),
-        ], string="Summarize type", default='both')
+        ],
+        string="Summarize type",
+        default='both',
+        required=True,
+    )
+    exigible_type = fields.Selection(
+        selection=[
+            ('yes', 'Only exigible amounts'),
+            ('no', 'Only non-exigible amounts'),
+            ('both', 'Both exigible and non-exigible amounts'),
+        ],
+        required=True,
+        string="Exigibility",
+        default='yes',
+    )
     inverse = fields.Boolean(string="Inverse summarize sign", default=False)
     to_regularize = fields.Boolean(string="To regularize")
