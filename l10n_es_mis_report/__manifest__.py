e21e5e2f0a3805b4ede32695a7e4b18523fc621e
Pedro M. Baeza <pedro.baeza@gmail.com>
2018-05-02 10:03:23 +0200

    [IMP] l10n_es_mis_report: Use pbale/nbale
    For the balance sections where the sum of debtor/creditor balances is needed,
    with this new expression we can get the interpreted result.

--- l10n_es_mis_report/__manifest__.py
+++ l10n_es_mis_report/__manifest__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 Eficent Business and IT Consulting Services, S.L.
 #                <contact@eficent.com>
+# Copyright 2018 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl
 
 {
@@ -8,10 +9,11 @@
     'summary': """
         Plantillas MIS Builder para informes contables españoles""",
     'author': 'Eficent,'
+              'Tecnativa,'
               'Odoo Community Association (OCA)',
     'website': 'https://github.com/OCA/l10n-spain',
     'category': 'Reporting',
-    'version': '10.0.2.0.2',
+    'version': '10.0.2.1.0',
     'license': 'AGPL-3',
     'depends': [
         'mis_builder',  # OCA/account-financial-reporting
