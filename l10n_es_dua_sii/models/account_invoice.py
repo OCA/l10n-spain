d550977ef71a3e85800130501970af8251d57d71
Nacho <nacmuro@gmail.com>
2018-05-08 13:20:24 +0200

    [9.0][MIG] l10n_es_dua_sii: Migration to 9.0

--- l10n_es_dua_sii/models/account_invoice.py
+++ l10n_es_dua_sii/models/account_invoice.py
@@ -3,7 +3,7 @@
 # Copyright 2017 Comunitea Servicios Tecnológicos S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields, api
+from openerp import models, fields, api
 
 
 class AccountInvoice(models.Model):
