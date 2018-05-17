8a881bc3ed0a3e0fa2fb6299e3474dfd1dcfcac8
Carlos Dauden <carlos.dauden@tecnativa.com>
2018-05-17 17:29:15 +0200

    [FIX] l10n_es_aeat_mod349: Raise error with amount zero records (invoices+refunds=0.0)

--- l10n_es_aeat_mod349/__manifest__.py
+++ l10n_es_aeat_mod349/__manifest__.py
@@ -5,11 +5,12 @@
 # Copyright 2017 - Tecnativa - Luis M. Ontalba <luis.martinez@tecnativa.com>
 # Copyright 2017 Eficent Business & IT Consult. Services <contact@eficent.com>
 # Copyright 2014-2018 Tecnativa - Pedro M. Baeza
+# Copyright 2018 Tecnativa - Carlos Dauden
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
     "name": "Modelo 349 AEAT",
-    "version": "10.0.2.2.0",
+    "version": "10.0.2.2.1",
     "author": "Pexego, "
               "Top Consultant, "
               "Tecnativa, "
