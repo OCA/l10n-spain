2320028fc2894ba2ee3ef6244f61a5f004e4e8ae
Carlos Dauden <carlos.dauden@tecnativa.com>
2018-05-21 12:22:13 +0200

    [FIX] l10n_es_aeat_mod349: Missed by name param assignation precision_rounding

--- l10n_es_aeat_mod349/__manifest__.py
+++ l10n_es_aeat_mod349/__manifest__.py
@@ -9,7 +9,7 @@
 
 {
     "name": "Modelo 349 AEAT",
-    "version": "11.0.1.0.4",
+    "version": "11.0.1.0.5",
     "author": "Pexego, "
               "Top Consultant, "
               "Tecnativa, "
