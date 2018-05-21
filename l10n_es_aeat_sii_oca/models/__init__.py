10deacc13fab3e6ce3d1dbbc945a2d76bbbc0638
Javi Melendez <javimelex@gmail.com>
2018-05-21 20:16:31 +0200

    [IMP] l10n_es_aeat_sii: multiple Tax Agencies (multicompany)

--- l10n_es_aeat_sii_oca/models/__init__.py
+++ l10n_es_aeat_sii_oca/models/__init__.py
@@ -1,6 +1,8 @@
 # Copyright 2017 Ignacio Ibeas <ignacio@acysos.com>
+# Copyright 2018 Javi Melendez <javimelex@gmail.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+from . import aeat_sii_tax_agency
 from . import res_company
 from . import aeat_sii
 from . import aeat_sii_mapping_registration_keys
