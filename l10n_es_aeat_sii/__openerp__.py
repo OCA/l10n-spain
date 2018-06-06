a92730dda40e719a66d4a46c6c89f521fb9e9f28
Pedro M. Baeza <pedro.baeza@gmail.com>
2018-06-06 14:01:54 +0200

    [FIX] l10n_es_aeat_sii: Constraint not suitable for multi-company
    This constraint leads to integrity errors due to missing values in companies that
    doesn't have SII enabled. As the required attribute is set in view, this should be removed.
    
    There's still the chance of writing an invoice without this key by code, but putting
    a constraint seems excessive as this key is only needed when sending the invoice. If
    not present, there will be an SII error that will help you to identify the problem.

--- l10n_es_aeat_sii/__openerp__.py
+++ l10n_es_aeat_sii/__openerp__.py
@@ -11,7 +11,7 @@
 
 {
     "name": "Suministro Inmediato de Información en el IVA",
-    "version": "9.0.2.0.6",
+    "version": "9.0.2.0.7",
     "category": "Accounting & Finance",
     "website": "https://odoospain.odoo.com",
     "author": "Acysos S.L.,"
