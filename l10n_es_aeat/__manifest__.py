d47192a895609e19f9bf3f56dcbde6338352bc45
Abranes <abrahamanes@gmail.com>
2018-06-07 16:11:39 +0200

    [IMP] l10n_es_aeat: New export_type alphabetic

--- l10n_es_aeat/__manifest__.py
+++ l10n_es_aeat/__manifest__.py
@@ -10,7 +10,7 @@
 {
     'name': "AEAT Base",
     'summary': "Modulo base para declaraciones de la AEAT",
-    'version': "11.0.1.1.0",
+    'version': "11.0.1.1.1",
     'author': "Pexego,"
               "Acysos,"
               "AvanzOSC,"
