4f82597f1d39e36d3029edacf7735491b83b8841
Abranes <abrahamanes@gmail.com>
2018-06-07 16:12:43 +0200

    [FIX] l10n_es_aeat_mod303: Change export_type of Nombre to alphabetic

--- l10n_es_aeat_mod303/__manifest__.py
+++ l10n_es_aeat_mod303/__manifest__.py
@@ -7,7 +7,7 @@
 
 {
     "name": "AEAT modelo 303",
-    "version": "11.0.2.0.2",
+    "version": "11.0.2.1.0",
     "category": "Accounting",
     "author": "Guadaltech,"
               "AvanzOSC,"
