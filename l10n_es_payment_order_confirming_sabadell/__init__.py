8ca8fb5ad5cd99f42f8d2797bce449ea1a06d536
Soluntec <info@soluntec.es>
2018-07-03 00:17:31 +0200

    [ADD] l10n_es_payment_order_confirming_XXXX: Modulos exportación de fichero confirming para bancos sabadell, popular y bankia. (#393)
    * Exportación de fichero confirming para banco sabadell.
    * Exportación de ficheros bancarios según el formato Confirming para Banco Popular.
    * Exportación de ficheros bancarios según el formato Confirming para Bankia.
    
    [UPD] Update l10n_es_payment_order_confirming_sabadell.pot

---
+++ l10n_es_payment_order_confirming_sabadell/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# (c) 2016 Soluntec Proyectos y Soluciones TIC. - Rubén Francés , Nacho Torró
+# (c) 2015 Serv. Tecnol. Avanzados - Pedro M. Baeza
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from . import models
+from . import wizard
