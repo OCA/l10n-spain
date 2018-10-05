919f733707566769981cc17c711f469b2be898bc
Pedro M. Baeza <pedro.baeza@gmail.com>
2018-10-05 08:05:05 +0200

    [MIG] l10n_es_aeat_mod303: Migration to 12.0
    * Standard procedure
    * Adapted tests
    * README by fragments

--- l10n_es_aeat_mod303/__manifest__.py
+++ l10n_es_aeat_mod303/__manifest__.py
@@ -7,7 +7,7 @@
 
 {
     "name": "AEAT modelo 303",
-    "version": "11.0.2.1.0",
+    "version": "12.0.1.0.0",
     "category": "Accounting",
     "author": "Guadaltech,"
               "AvanzOSC,"
