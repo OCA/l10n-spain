fa232cf51e59dce0693a2c0935c41b6508f0a0f4
Sergio Teruel <sergio.teruel@tecnativa.com>
2018-11-26 10:07:07 +0100

    [MIG] l10n_es_toponyms: Migration to 12.0

--- l10n_es_toponyms/__manifest__.py
+++ l10n_es_toponyms/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # Copyright 2009 Jordi Esteve <jesteve@zikzakmedia.com>
 # Copyright 2013-2017 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Topónimos españoles",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "author": "Spanish Localization Team, "
               "ZikZakMedia, "
               "Tecnativa, "
