5d713cf200f1b477f36f2c95626ccff09ca4006f
Sergio Teruel <sergio.teruel@tecnativa.com>
2018-11-26 10:07:07 +0100

    [MIG] l10n_es_toponyms: Migration to 12.0

--- l10n_es_toponyms/wizard/l10n_es_toponyms_wizard.py
+++ l10n_es_toponyms/wizard/l10n_es_toponyms_wizard.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 # Copyright 2009 Jordi Esteve <jesteve@zikzakmedia.com>
-# Copyright 2013-2017 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# Copyright 2013-2017 Tecnativa - Pedro M. Baeza
+# Copyright 2018 Tecnativa - Sergio Teruel
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields, api, tools
-import os
+from odoo import models, fields, api
 
 
 class ConfigEsToponyms(models.TransientModel):
@@ -13,25 +12,11 @@ class ConfigEsToponyms(models.TransientModel):
 
     name = fields.Char('Name', size=64)
 
-    @api.model
-    def create_zipcodes(self):
-        """Import spanish zipcodes information through an XML file."""
-        file_name = 'l10n_es_toponyms_zipcodes.xml'
-        path = os.path.join('l10n_es_toponyms', 'wizard', file_name)
-        with tools.file_open(path) as fp:
-            tools.convert_xml_import(self.env.cr, 'l10n_es_toponyms', fp, {},
-                                     'init', noupdate=True)
-        return True
-
     @api.multi
     def execute(self):
         res = super(ConfigEsToponyms, self).execute()
-        wizard_obj = self.env['better.zip.geonames.import']
+        wizard_obj = self.env['city.zip.geonames.import']
         country_es = self.env['res.country'].search([('code', '=', 'ES')])
         wizard = wizard_obj.create({'country_id': country_es.id})
         wizard.run_import()
         return res
-
-    @api.multi
-    def execute_local(self):  # pragma: no cover
-        self.create_zipcodes()
