09a06720eb04c883fa09d0a0d82609b6b0dbfd7a
Enric Tobella <etobella@creublanca.es>
2018-12-11 11:02:47 +0100

    [IMP] l10n_es_facturae: Añadir name a la página

--- l10n_es_facturae/__manifest__.py
+++ l10n_es_facturae/__manifest__.py
@@ -7,7 +7,7 @@
 
 {
     "name": "Creación de Factura-e",
-    "version": "11.0.1.0.4",
+    "version": "11.0.1.0.5",
     "author": "ASR-OSS, "
               "FactorLibre, "
               "Tecon, "
