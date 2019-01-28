9fadf1ee1ae27e649ddbc441caf7d968623f639d
Jairo Llopis <yajo.sk8@gmail.com>
2019-01-28 13:48:17 +0000

    [FIX] l10n_es_partner: Hide only HTTP errors
    Any problems besides HTTP errors are hidden here, and they shouldn't, because there's no way to debug then.

--- l10n_es_partner/wizard/l10n_es_partner_wizard.py
+++ l10n_es_partner/wizard/l10n_es_partner_wizard.py
@@ -2,11 +2,14 @@
 # © 2013-2016 Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl-3).
 
+import logging
 from openerp import models, fields, api, _
 from openerp import tools
 from ..gen_src.gen_data_banks import gen_bank_data_xml
 import tempfile
 import os
+
+_logger = logging.getLogger(__name__)
 
 
 class L10nEsPartnerImportWizard(models.TransientModel):
@@ -33,8 +36,7 @@ class L10nEsPartnerImportWizard(models.TransientModel):
             response = requests.get(
                 'http://www.bde.es/f/webbde/IFI/servicio/regis/ficheros/es/'
                 'REGBANESP_CONESTAB_A.XLS')
-            if not response.ok:
-                raise Exception()
+            response.raise_for_status()
             src_file.write(response.content)
             src_file.close()
             # Generate XML and reopen it
@@ -42,7 +44,8 @@ class L10nEsPartnerImportWizard(models.TransientModel):
             tools.convert_xml_import(
                 self._cr, 'l10n_es_partner', dest_file.name, {}, 'init',
                 noupdate=True)
-        except:
+        except requests.exceptions.HTTPError:
+            _logger.exception()
             self.import_fail = True
             return {
                 'name': _('Import spanish bank data'),
