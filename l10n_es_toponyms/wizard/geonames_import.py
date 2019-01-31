044e1a74ece30940ff8ec460f82d917064d7907d
Pedro M. Baeza <pedro.baeza@gmail.com>
2019-01-31 17:51:04 +0100

    [MIG+FIX] l10n_es_toponyms: Adapt tests+code to 12.0

--- l10n_es_toponyms/wizard/geonames_import.py
+++ l10n_es_toponyms/wizard/geonames_import.py
@@ -1,4 +1,4 @@
-# Copyright 2013-2017 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# Copyright 2013-2019 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import models, api
@@ -10,20 +10,9 @@ class BetterZipGeonamesImport(models.TransientModel):
     @api.model
     def transform_city_name(self, city, country):
         """Change determinants casing."""
+        res = super().transform_city_name(city, country)
         if country.code == 'ES':
-            return city.replace(' De ', ' de ').replace(' Del ', ' del ').\
+            res = res.replace(' De ', ' de ').replace(' Del ', ' del ').\
                 replace(' La ', ' la ').replace(' Las ', ' las ').\
                 replace(' El ', ' el ').replace(' Los ', ' los ')
-        else:
-            return super(BetterZipGeonamesImport, self).transform_city_name(
-                city, country)
-
-    @api.model
-    def select_or_create_state(self, row, country, code_row_index=4,
-                               name_row_index=3):
-        if country.code == 'ES':
-            code_row_index = 6
-            name_row_index = 5
-        return super(BetterZipGeonamesImport, self).select_or_create_state(
-            row, country, code_row_index=code_row_index,
-            name_row_index=name_row_index)
+        return res
