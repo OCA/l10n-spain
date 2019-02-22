ef5ad62adc4ef3d5eba44990fd7a8c0a2515f50a
Enric Tobella <etobella@creublanca.es>
2019-02-22 12:25:44 +0100

    [FIX] facturae_face: Certificado en formato DER

--- l10n_es_facturae_face/__manifest__.py
+++ l10n_es_facturae_face/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Envío de Factura-e a FACe",
-    "version": "11.0.1.0.0",
+    "version": "11.0.1.0.1",
     "author": "Creu Blanca, "
               "Odoo Community Association (OCA)",
     "category": "Accounting & Finance",
