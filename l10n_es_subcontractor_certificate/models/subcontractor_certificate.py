911d085c734a1e67082613949987c6542374abc3
Pablo Fuentes <pablo@studio73.es>
2019-04-05 15:58:37 +0200

    [MIG] l10n_es_subcontractor_certificate: Migration to 12.0

--- l10n_es_subcontractor_certificate/models/subcontractor_certificate.py
+++ l10n_es_subcontractor_certificate/models/subcontractor_certificate.py
@@ -1,27 +1,7 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (c) 2014 Domatix Technologies  S.L. (http://www.domatix.com)
-#                       info <info@domatix.com>
-#                        Angel Moya <angel.moya@domatix.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2014 Domatix Technologies S.L. - Angel Moya
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl
 
-from openerp import models, fields, api, _
+from odoo import _, api, fields, models
 
 EXPIRED_WARNING_AEAT = {
     'title': _('Warning!'),
@@ -56,29 +36,33 @@ class ResPartner(models.Model):
         string='AEAT Certificate Expiration')
     certificate_expired_aeat = fields.Boolean(
         string='AEAT Certificate Expirated',
-        compute='_certificate_expired_aeat')
+        compute='_compute_certificate_expired_aeat')
     certificate_expiration_ss = fields.Date(string='SS Certificate Expiration')
     certificate_expired_ss = fields.Boolean(
         string='SS Certificate Expirated',
-        compute='_certificate_expired_ss')
+        compute='_compute_certificate_expired_ss')
 
-    def _certificate_expired_aeat(self):
-        self.certificate_expired_aeat = self.certificate_expiration_aeat and \
-            (self.certificate_expiration_aeat < fields.Date.today())
+    def _compute_certificate_expired_aeat(self):
+        for partner in self:
+            partner.certificate_expired_aeat = (
+                partner.certificate_expiration_aeat and
+                (partner.certificate_expiration_aeat < fields.Date.today()))
 
-    def _certificate_expired_ss(self):
-        self.certificate_expired_ss = self.certificate_expiration_ss and \
-            (self.certificate_expiration_ss < fields.Date.today())
+    def _compute_certificate_expired_ss(self):
+        for partner in self:
+            partner.certificate_expired_ss = (
+                partner.certificate_expiration_ss and
+                (partner.certificate_expiration_ss < fields.Date.today()))
 
 
 class PurchaseOrder(models.Model):
     _inherit = ['purchase.order']
 
-    @api.multi
-    def onchange_partner_id(self, partner_id):
-        res = super(PurchaseOrder, self).onchange_partner_id(partner_id)
-        if partner_id:
-            partner = self.env['res.partner'].browse(partner_id)[0]
+    @api.onchange('partner_id', 'company_id')
+    def onchange_partner_id(self):
+        res = super(PurchaseOrder, self).onchange_partner_id() or {}
+        partner = self.partner_id
+        if partner:
             if partner.certificate_required:
                 if not partner.certificate_expiration_aeat:
                     res['warning'] = REQUIRED_WARNING_AEAT
@@ -94,16 +78,11 @@ class PurchaseOrder(models.Model):
 class AccountInvoice(models.Model):
     _inherit = ['account.invoice']
 
-    @api.multi
-    def onchange_partner_id(self, type, partner_id, date_invoice=False,
-                            payment_term=False, partner_bank_id=False,
-                            company_id=False):
-        res = super(AccountInvoice, self).onchange_partner_id(
-            type, partner_id, date_invoice=False,
-            payment_term=False, partner_bank_id=False,
-            company_id=False)
-        if type == 'in_invoice' and partner_id:
-            partner = self.env['res.partner'].browse(partner_id)
+    @api.onchange('partner_id', 'company_id')
+    def _onchange_partner_id(self):
+        res = super(AccountInvoice, self)._onchange_partner_id() or {}
+        if self.type == 'in_invoice' and self.partner_id:
+            partner = self.partner_id
             if partner.certificate_required:
                 if not partner.certificate_expiration_aeat:
                     res['warning'] = REQUIRED_WARNING_AEAT
