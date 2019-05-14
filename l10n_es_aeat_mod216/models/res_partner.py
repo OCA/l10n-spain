61b8f945298e5e9d5a2d57dd045a61ce1c1ee3fa
Victor Martin <victor.martin@tecnativa.com>
2019-05-14 12:03:13 +0200

    [MIG] l10n_es_aeat_mod216: Migration to 11.0

--- l10n_es_aeat_mod216/models/res_partner.py
+++ l10n_es_aeat_mod216/models/res_partner.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
-# Copyright 2015 AvanzOSC - Ainara Galdona
-# Copyright 2016 Antonio Espinosa <antonio.espinosa@tecnativa.com>
+# Copyright 2015-2019 AvanzOSC - Ainara Galdona
+# Copyright 2016-2019 Antonio Espinosa <antonio.espinosa@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import models, fields
