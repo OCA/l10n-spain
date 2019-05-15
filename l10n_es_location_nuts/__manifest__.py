79096138552d1ee109b59c1f65b0ce27f38b3f8f
Alexandre Díaz <alexandre.diaz@tecnativa.com>
2019-05-15 17:17:57 +0200

    [MIG] l10n_es_location_nuts: Migration to 12.0

--- l10n_es_location_nuts/__manifest__.py
+++ l10n_es_location_nuts/__manifest__.py
@@ -5,7 +5,7 @@
 {
     'name': 'NUTS Regions for Spain',
     'summary': 'NUTS specific options for Spain',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': 'Localisation/Europe',
     'website': 'https://github.com/OCA/l10n-spain',
     'author': 'Tecnativa, '
