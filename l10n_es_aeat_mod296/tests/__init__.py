5d463a94e8e14f367eab39734e5de0af888c41b4
Victor Martin <victor.martin@tecnativa.com>
2019-05-20 18:44:43 +0200

    [MIG] l10n_es_aeat_mod296: Migration to 11.0

--- l10n_es_aeat_mod296/tests/__init__.py
+++ l10n_es_aeat_mod296/tests/__init__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 Valentin Vinagre <valentin.vinagre@qubiq.es>
 # Copyright Odoo Community Association (OCA)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
