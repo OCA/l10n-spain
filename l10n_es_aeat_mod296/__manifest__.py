0abb108d06d545a1da9f3c406956e55742ba5b3c
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2019-05-23 23:16:24 +0200

    [MIG] l10n_es_aeat_mod296: Migration to 12.0
    Standar procedure.

--- l10n_es_aeat_mod296/__manifest__.py
+++ l10n_es_aeat_mod296/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     'name': 'AEAT modelo 296',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'category': "Localisation/Accounting",
     'author': "Tecnativa,"
               "AvanzOSC,"
