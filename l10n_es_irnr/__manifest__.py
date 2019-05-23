6b85bad68452d1c26916b954cb4b175eceb1033d
ernesto <ernesto.tejeda87@gmail.com>
2019-05-23 11:14:14 -0400

    [MIG] l10n_es_irnr: Migration to 12.0

--- l10n_es_irnr/__manifest__.py
+++ l10n_es_irnr/__manifest__.py
@@ -1,15 +1,16 @@
-# -*- coding: utf-8 -*-
-# License AGPL-3: Antiun Ingenieria S.L. - Antonio Espinosa
-# See README.rst file on addon root folder for more details
+# Copyright 2016 Tecnativa - Antonio Espinosa
+# Copyright 2017 Tecnativa - Pedro M. Baeza
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': "Retenciones IRNR (No residentes)",
-    'category': 'Localization/Account Charts',
+    "name": "Retenciones IRNR (No residentes)",
+    "summary": "Module summary",
+    'version': '12.0.1.0.0',
+    'category': 'Localization',
     'depends': [
         'l10n_es',
     ],
     'data': [
-        'data/tax_codes_irnr.xml',
         'data/taxes_irnr.xml',
         'data/fiscal_positions_irnr.xml',
         'data/fiscal_position_taxes_irnr.xml',
