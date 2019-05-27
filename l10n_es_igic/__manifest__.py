0938f2f5a086dc235926a55af6a5019b7f3b6a2d
Omar (Comunitea) <omar@comunitea.com>
2019-05-27 12:22:08 +0200

    [MERGE] l10n_es_igic: Fusinamos repositorios para subir a OCA.

--- l10n_es_igic/__manifest__.py
+++ l10n_es_igic/__manifest__.py
@@ -2,21 +2,28 @@
 ##############################################################################
 #
 #    Sistemas de Datos, Open Source Management Solution
-#    Copyright (C) 2004-2011 Pexego Sistemas Informáticos. All Rights Reserved
-#    Copyright (C) 2014 Arturo Esparragón Goncalves (http://sdatos.com). All Rights Reserved
-#    Copyright (C) 2016-2018 Rodrigo Colombo Vlaeminch (http://sdatos.com). All Rights Reserved
+#    Copyright (C) 2004-2011 Pexego Sistemas Informáticos.
+#    Copyright (C) 2014 Arturo Esparragón Goncalves (http://sdatos.com).
+#    Copyright (C) 2016-2018 Rodrigo Colombo Vlaeminch (http://sdatos.com).
+#    Copyright (C) 2019 Comunitea (https://comunitea.com).
 #    License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0
 #
 ##############################################################################
 
 {
-    'name' : 'Planes de cuentas españoles (según PGCE 2008) ampliado para IGIC',
-    'version' : '10.0.1.0.0',
-    'author' : 'Sistemas de Datos',
-    'category' : 'Localisation/Account Charts',
-    'website': 'http://www.sdatos.com',
-    'depends' : ['l10n_es'],
-    'data': ['data/account_account_common_igic.xml',
+    'name': 'IGIC (Impuesto General Indirecto Canario',
+    'version': '10.0.1.0.0',
+    'author': "David Diz Martínez <daviddiz@gmail.com>,"
+               "Atlantux Consultores - Enrique Zanardi,"
+               "Sistemas de Datos,"
+               "Comunitea,"
+               "Odoo Community Association (OCA)",
+    'category': 'Localisation/Account Charts',
+    "website": "https://github.com/OCA/l10n-spain",
+    "depends": ['l10n_es'],
+    "license": "AGPL-3",
+    'data': ['data/account_chart_template_igic.xml',
+             'data/account_account_common_igic.xml',
              'data/account_tax_group_data.xml',
              'data/taxes_common_igic.xml',
              'data/fiscal_position_common_igic.xml'],
