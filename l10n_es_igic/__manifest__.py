d1295d3b14413123abad09b389d2fa98bdc312aa
Rodrigo Colombo <rodrigo.covl@gmail.com>
2019-05-28 11:28:32 +0100

    [MIG] l10n_es_igic: Migration to 12.0

--- l10n_es_igic/__manifest__.py
+++ l10n_es_igic/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- encoding: utf-8 -*-
 ##############################################################################
 #
 #    Sistemas de Datos, Open Source Management Solution
@@ -6,13 +5,14 @@
 #    Copyright (C) 2014 Arturo Esparragón Goncalves (http://sdatos.com).
 #    Copyright (C) 2016-2018 Rodrigo Colombo Vlaeminch (http://sdatos.com).
 #    Copyright (C) 2019 Comunitea (https://comunitea.com).
+#    Copyright (C) 2019 Héctor J. Ravelo (http://sdatos.com)
 #    License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0
 #
 ##############################################################################
 
 {
     'name': 'IGIC (Impuesto General Indirecto Canario',
-    'version': '10.0.1.0.0',
+    'version': '12.0.1.0.0',
     'author': "David Diz Martínez <daviddiz@gmail.com>,"
               "Atlantux Consultores - Enrique Zanardi,"
               "Sistemas de Datos,"
@@ -23,6 +23,7 @@
     "depends": ['l10n_es'],
     "license": "AGPL-3",
     'data': ['data/account_chart_template_igic.xml',
+             'data/account_group.xml',
              'data/account_account_common_igic.xml',
              'data/account_tax_group_data.xml',
              'data/taxes_common_igic.xml',
