cc31a8f56ae2d9451e3d259740fa2afaa6854717
Daniel Duque <daniel.duque@factorlibre.com>
2019-05-28 17:50:42 +0200

    [11.0][ADD] l10n_es_intrastat_product_declaration

--- l10n_es_intrastat_report/models/account_invoice.py
+++ l10n_es_intrastat_report/models/account_invoice.py
@@ -1,8 +1,7 @@
-# -*- coding: utf-8 -*-
 # Copyright 2009-2017 Noviat.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import api, fields, models
+from odoo import api, fields, models
 
 
 class AccountInvoice(models.Model):
@@ -25,19 +24,17 @@ class AccountInvoice(models.Model):
     def _default_src_dest_state_id(self):
         rco = self.env['res.company']
         company_id = rco._company_default_get('account.invoice')
-        company = rco.browse(company_id)
-        return company.intrastat_state_id
+        return company_id.intrastat_state_id
 
     @api.model
     def _default_intrastat_transaction_id(self):
         transaction = super(
             AccountInvoice, self)._default_intrastat_transaction_id()
         if not transaction:
-            cpy_id = self.env[
+            cpy = self.env[
                 'res.company']._company_default_get('account.invoice')
-            cpy = self.env['res.company'].browse(cpy_id)
             if cpy.country_id.code.lower() == 'es':
                 module = __name__.split('addons.')[1].split('.')[0]
                 transaction = self.env.ref(
-                    '%s.intrastat_transaction_1' % module)
+                    '%s.intrastat_transaction_11' % module)
         return transaction
