775db4d3ac560f4ccb6b10b085fbfa3d4a3eb652
Ignacio Ibeas - Acysos S.L <ignacio@acysos.com>
2019-05-28 23:46:15 +0200

    [ADD] l10n_es_aeat_partner_check

---
+++ l10n_es_aeat_partner_check/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright 2019 Acysos S.L.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    'name': 'AEAT - Comprobación de Calidad de datos identificativos',
+    'author': 'Acysos S.L.,'
+              'Odoo Community Association (OCA)',
+    'website': 'https://www.aeodoo.org',
+    'category': 'Accounting',
+    'version': '11.0.0.1.0',
+    'license': 'AGPL-3',
+    'depends': [
+        'base',
+        'base_vat',
+        'l10n_es_aeat_soap'
+    ],
+    'data': [
+        'views/res_partner_view.xml',
+        'views/res_company_view.xml',
+        'views/res_config_settings_view.xml'
+    ],
+    'installable': True,
+}
