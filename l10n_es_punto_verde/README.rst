6237bfcbc186347f388cef67ce444beaced0b85c
Lopsanz <slop@sdi.es>
2019-05-29 10:56:48 +0200

    [ADD] Tasa Punto Verde

---
+++ l10n_es_punto_verde/README.rst
@@ -0,0 +1,13 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.png
+   :target: http://www.gnu.org/licenses/agpl
+   :alt: License: AGPL-3
+
+====================
+Punto verde (Basic)
+====================
+
+This module allows to companies that need to show Punto verde tax amount in their sale invoices.
+
+To do this, you simply put Punto verde amout per unit in product template, and add Punto Verde tax.
+
+
---
+++ l10n_es_punto_verde/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2018 Creu Blanca
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Punto Verde',
+    'version': '11.0.0.0.0',
+    'license': 'AGPL-3',
+    'author': "SDi",
+    'category': 'Generic Modules/Accounting',
+    'website': "https://github.com/OCA/l10n-spain",
+    'depends': ['product.template','account.tax','account.tax.python'],
+    'data': [
+        'data/account_tax_group.xml',
+        'views/product_view.xml',
+    ],
+    'installable': True,
+}
