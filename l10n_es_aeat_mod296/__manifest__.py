8f7c5108f39ccd0eb0006cb0d03fb339c62b6b9c
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2019-06-10 19:07:31 +0200

    [FIX] l10n_es_aeat_mod296: Adapt to l10n_es_irnr
    Same change as done in model 216

--- l10n_es_aeat_mod296/__manifest__.py
+++ l10n_es_aeat_mod296/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     'name': 'AEAT modelo 296',
-    'version': '12.0.1.0.0',
+    'version': '12.0.2.0.0',
     'category': "Localisation/Accounting",
     'author': "Tecnativa,"
               "AvanzOSC,"
