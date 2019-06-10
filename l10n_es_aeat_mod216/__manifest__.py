8d20c339752ac437393bbab391f5637cad7b71f5
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2019-06-10 18:09:13 +0200

    [FIX] l10n_es_aeat_mod216: Use l10n_es_irnr
    Previous approach was not correct, as the used taxes don't apply to non resident.
    
    We use now l10n_es_irnr module for having proper taxes, map them in the model, and
    adapt the rest of the needed things.

--- l10n_es_aeat_mod216/__manifest__.py
+++ l10n_es_aeat_mod216/__manifest__.py
@@ -1,11 +1,11 @@
-# Copyright 2015-2019 AvanzOSC - Ainara Galdona
-# Copyright 2016-2019 Tecnativa - Antonio Espinosa
+# Copyright 2015 AvanzOSC - Ainara Galdona
+# Copyright 2016 Tecnativa - Antonio Espinosa
 # Copyright 2015-2019 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'AEAT modelo 216',
-    'version': '11.0.1.0.0',
+    'version': '11.0.2.0.0',
     'category': "Localisation/Accounting",
     'author': "AvanzOSC,"
               "Tecnativa,"
@@ -13,14 +13,17 @@
     'website': "https://github.com/OCA/l10n-spain",
     'license': 'AGPL-3',
     'depends': [
-        'l10n_es_aeat_mod111',
+        'l10n_es_aeat',
+        'l10n_es_irnr',
     ],
     'data': [
         'security/ir.model.access.csv',
         'data/tax_code_map_mod216_data.xml',
         'data/aeat_export_mod216_data.xml',
         'views/mod216_view.xml',
-        'views/res_partner_view.xml',
+    ],
+    'maintainers': [
+        'pedrobaeza',
     ],
     'development_status': 'Mature',
     'installable': True,
