d97cd2193b10edb6ab0e4b0044bf5b29a69da41f
angelmoya <amoyapardo@gmail.com>
2019-06-28 15:41:05 +0200

    MIG l10n_es_dua_sii

--- l10n_es_dua_sii/__manifest__.py
+++ l10n_es_dua_sii/__manifest__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Consultoría Informática Studio 73 S.L.
 # Copyright 2017 Comunitea Servicios Tecnológicos S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Suministro Inmediato de Información de importaciones con DUA",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "category": "Accounting & Finance",
     "website": "https://odoo-community.org/",
     "author": "Studio73, "
@@ -14,6 +13,9 @@
     "depends": [
         "l10n_es_aeat_sii",
         "l10n_es_dua",
+    ],
+    "data": [
+        "data/tax_code_map_dua_sii_data.xml",
     ],
     "application": False,
     "installable": True,
