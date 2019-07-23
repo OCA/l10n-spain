2edf85af4017c9fa0a04d30c6eafe3f61d22d756
Enric Tobella <etobella@creublanca.es>
2019-07-23 16:55:46 +0200

    [MIG] l10n_es_facturae: Migration to 12.0

--- l10n_es_facturae/__manifest__.py
+++ l10n_es_facturae/__manifest__.py
@@ -7,7 +7,7 @@
 
 {
     "name": "Creación de Factura-e",
-    "version": "11.0.1.1.0",
+    "version": "12.0.1.0.0",
     "author": "ASR-OSS, "
               "FactorLibre, "
               "Tecon, "
