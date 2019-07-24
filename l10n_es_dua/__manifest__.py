c07c2a322b5918202b75d7d32a1404ff7a46e68e
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2019-07-24 18:43:18 +0200

    [FIX] l10n_es_dua: Add tax group to new tax

--- l10n_es_dua/__manifest__.py
+++ l10n_es_dua/__manifest__.py
@@ -1,5 +1,5 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Tecnativa - Antonio Espinosa
+# Copyright 2019 Tecnativa - Pedro M. Baeza
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 # See README.rst file on addon root folder for more details
 
@@ -12,6 +12,7 @@
         'l10n_es',
     ],
     'data': [
+        'data/tax_group_data.xml',
         'data/taxes_dua.xml',
         'data/fiscal_positions_dua.xml',
         'data/fiscal_position_taxes_dua.xml',
