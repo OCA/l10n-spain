1c2024c68701faec615005d756aa15f47c939df5
maurocebria <maurocebria.a4b@gmail.com>
2019-07-26 14:11:05 +0200

    [FIX] l10n_es_aeat_mod349: refund groups

--- l10n_es_aeat_mod349/__manifest__.py
+++ l10n_es_aeat_mod349/__manifest__.py
@@ -9,7 +9,7 @@
 
 {
     "name": "Modelo 349 AEAT",
-    "version": "12.0.1.1.0",
+    "version": "12.0.1.1.1",
     "author": "Tecnativa, "
               "Eficent, "
               "Odoo Community Association (OCA)",
