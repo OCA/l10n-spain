bf1bbfc65d12177dd94941b85fdd5abf76cbc060
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2019-08-08 17:05:27 +0200

    [FIX] l10n_es_aeat_sii: Apply dict rounding at other level
    Doing it at taxes dictionary level, there are some values left that are not
    rounded, like 'CuotaDeducible'. We apply the rounding over the top dictionary,
    also avoiding to duplicate some code.

--- l10n_es_aeat_sii_oca/__manifest__.py
+++ l10n_es_aeat_sii_oca/__manifest__.py
@@ -12,7 +12,7 @@
 
 {
     "name": "Suministro Inmediato de Información en el IVA",
-    "version": "12.0.1.0.2",
+    "version": "12.0.1.0.3",
     "category": "Accounting & Finance",
     "website": "https://odoospain.odoo.com",
     "author": "Acysos S.L.,"
