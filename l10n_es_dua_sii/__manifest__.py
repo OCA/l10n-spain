28f95f1568f2d2cc25f5f7cf0bbbe6787f8b37c6
Alexandre Díaz <alexandre.diaz@tecnativa.com>
2019-08-13 00:29:42 +0200

    [MIG] l10n_es_dua_sii: Migration to 12.0

--- l10n_es_dua_sii/__manifest__.py
+++ l10n_es_dua_sii/__manifest__.py
@@ -3,9 +3,9 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Suministro Inmediato de Información de importaciones con DUA",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "category": "Accounting & Finance",
-    "website": "https://odoo-community.org/",
+    "website": "https://github.com/OCA/l10n-spain",
     "author": "Studio73, "
               "Comunitea, "
               "Odoo Community Association (OCA)",
