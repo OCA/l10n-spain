613a81eef10c77544f63ee40df4c52d53ba32355
Lopsanz <slop@sdi.es>
2019-09-03 12:31:42 +0200

    [HOTFIX] UOM is not in Sale order summary report

--- l10n_es_greenpoint/models/sale.py
+++ l10n_es_greenpoint/models/sale.py
@@ -23,3 +23,27 @@ class SaleOrderLine(models.Model):
                 'price_total': taxes['total_included'],
                 'price_subtotal': taxes['total_excluded'],
             })
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+
+    @api.multi
+    def _get_tax_amount_by_group(self):
+        self.ensure_one()
+        res = {}
+        for line in self.order_line:
+            price_reduce = line.price_unit * (1.0 - line.discount / 100.0)
+            context = self.env.context.copy()
+            context.update({"uom": line.product_uom})
+            taxes = line.tax_id.with_context(context).compute_all(price_reduce, quantity=line.product_uom_qty, product=line.product_id, partner=self.partner_shipping_id)['taxes']
+            for tax in line.tax_id:
+                group = tax.tax_group_id
+                res.setdefault(group, {'amount': 0.0, 'base': 0.0})
+                for t in taxes:
+                    if t['id'] == tax.id or t['id'] in tax.children_tax_ids.ids:
+                        res[group]['amount'] += t['amount']
+                        res[group]['base'] += t['base']
+        res = sorted(res.items(), key=lambda l: l[0].sequence)
+        res = [(l[0].name, l[1]['amount'], l[1]['base'], len(res)) for l in res]
+        return res
