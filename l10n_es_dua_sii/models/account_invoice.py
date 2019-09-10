02bd13283d3e0aad3ad81930fd8a1677d9859f1c
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2019-09-10 10:45:27 +0200

    [FIX+IMP] l10n_es_aeat_sii: Fix logic + improve tests + contributors

--- l10n_es_dua_sii/models/account_invoice.py
+++ l10n_es_dua_sii/models/account_invoice.py
@@ -1,5 +1,7 @@
 # Copyright 2017 Consultoría Informática Studio 73 S.L.
 # Copyright 2017 Comunitea Servicios Tecnológicos S.L.
+# Copyright 2019 Tecnativa - Alexandre Díaz
+# Copyright 2019 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import models, fields, api, tools
@@ -7,6 +9,9 @@ from odoo import models, fields, api, tools
 
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
+
+    sii_dua_invoice = fields.Boolean("SII DUA Invoice",
+                                     compute="_compute_dua_invoice")
 
     @api.model
     @tools.ormcache('company')
@@ -18,40 +23,33 @@ class AccountInvoice(models.Model):
             ('company_id', '=', company.id),
         ])
 
-    @api.multi
-    @api.depends('sii_dua_invoice')
-    def _compute_sii_enabled(self):
-        super()._compute_sii_enabled()
-        self.filtered(lambda x: x.sii_enabled
-                      and x.fiscal_position_id.name == 'Importación con DUA'
-                      and not x.sii_dua_invoice).update({'sii_enabled': False})
-
-    @api.multi
     @api.depends('company_id', 'fiscal_position_id', 'tax_line_ids')
     def _compute_dua_invoice(self):
         for invoice in self:
             taxes = invoice._get_sii_taxes_map(['DUA'])
+            invoice.sii_dua_invoice = (
+                invoice.tax_line_ids.filtered(lambda x: x.tax_id in taxes))
+
+    @api.depends('sii_dua_invoice', 'fiscal_position_id')
+    def _compute_sii_enabled(self):
+        """Don't sent secondary DUA invoices to SII."""
+        super()._compute_sii_enabled()
+        for invoice in self.filtered('sii_enabled'):
             dua_fiscal_position = self._get_dua_fiscal_position(
                 invoice.company_id)
-            invoice.sii_dua_invoice = (
-                invoice.fiscal_position_id == dua_fiscal_position
-                and invoice.tax_line_ids.filtered(lambda x: x.tax_id in taxes))
+            if (invoice.fiscal_position_id == dua_fiscal_position and
+                    not invoice.sii_dua_invoice):
+                invoice.sii_enabled = False
 
-    sii_dua_invoice = fields.Boolean("SII DUA Invoice",
-                                     compute="_compute_dua_invoice")
-
-    @api.multi
     def _get_sii_invoice_dict_in(self, cancel=False):
-        """
-        Según la documentación de la AEAT, la operación de importación se
+        """Según la documentación de la AEAT, la operación de importación se
         registra con TipoFactura = F5, sin FechaOperacion y con el NIF de la
         propia compañia en IDEmisorFactura y Contraparte
         Más información en: 8.1.2.2.Ejemplo mensaje XML de alta de importación
         en el documento de descripción de los servicios web:
         http://bit.ly/2rGWiAI
-
         """
-        res = super()._get_sii_invoice_dict_in(cancel)
+        res = super()._get_sii_invoice_dict_in(cancel=cancel)
         if res.get('FacturaRecibida') and self.sii_dua_invoice:
             res['FacturaRecibida']['TipoFactura'] = 'F5'
             res['FacturaRecibida'].pop('FechaOperacion', None)
