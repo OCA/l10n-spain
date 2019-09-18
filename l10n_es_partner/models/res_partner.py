54b5e2127ea1d94236cf0af503e3d9e16d84c35f
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2019-09-18 13:54:37 +0200

    [FIX] l10n_es_partner: pass message to logger.exception
    Otherwise it doesn't work
    
    [IMP] l10n_es_partner: black, isort

--- l10n_es_partner/models/res_partner.py
+++ l10n_es_partner/models/res_partner.py
@@ -9,12 +9,12 @@ from odoo.osv import expression
 
 
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
-    comercial = fields.Char('Trade name', size=128, index=True)
-    display_name = fields.Char(compute='_compute_display_name')
+    comercial = fields.Char("Trade name", size=128, index=True)
+    display_name = fields.Char(compute="_compute_display_name")
 
-    @api.depends('comercial')
+    @api.depends("comercial")
     def _compute_display_name(self):
         super(ResPartner, self)._compute_display_name()
 
@@ -24,27 +24,26 @@ class ResPartner(models.Model):
         args = expression.normalize_domain(args)
         for arg in args:
             if isinstance(arg, (list, tuple)):
-                if arg[0] == 'name' or arg[0] == 'display_name':
+                if arg[0] == "name" or arg[0] == "display_name":
                     index = args.index(arg)
                     args = (
-                        args[:index] + ['|', ('comercial', arg[1], arg[2])] +
-                        args[index:]
+                        args[:index]
+                        + ["|", ("comercial", arg[1], arg[2])]
+                        + args[index:]
                     )
                     break
         return super(ResPartner, self).search(
-            args, offset=offset, limit=limit, order=order, count=count,
+            args, offset=offset, limit=limit, order=order, count=count
         )
 
     @api.model
-    def name_search(self, name, args=None, operator='ilike', limit=100):
+    def name_search(self, name, args=None, operator="ilike", limit=100):
         """Give preference to commercial names on name search, appending
         the rest of the results after. This has to be done this way, as
         Odoo overwrites name_search on res.partner in a non inheritable way."""
         if not args:
             args = []
-        partners = self.search(
-            [('comercial', operator, name)] + args, limit=limit,
-        )
+        partners = self.search([("comercial", operator, name)] + args, limit=limit)
         res = partners.name_get()
         if limit:
             limit_rest = limit - len(partners)
@@ -52,32 +51,34 @@ class ResPartner(models.Model):
             # limit can be 0 or None representing infinite
             limit_rest = limit
         if limit_rest or not limit:
-            args += [('id', 'not in', partners.ids)]
+            args += [("id", "not in", partners.ids)]
             res += super(ResPartner, self).name_search(
-                name, args=args, operator=operator, limit=limit_rest,
+                name, args=args, operator=operator, limit=limit_rest
             )
         return res
 
     @api.multi
     def name_get(self):
         result = []
-        name_pattern = self.env['ir.config_parameter'].sudo().get_param(
-            'l10n_es_partner.name_pattern', default='')
+        name_pattern = (
+            self.env["ir.config_parameter"]
+            .sudo()
+            .get_param("l10n_es_partner.name_pattern", default="")
+        )
         origin = super(ResPartner, self).name_get()
-        if self.env.context.get('no_display_commercial', False):
+        if self.env.context.get("no_display_commercial", False):
             return origin
         orig_name = dict(origin)
         for partner in self:
             name = orig_name[partner.id]
             comercial = partner.comercial
             if comercial and name_pattern:
-                name = name_pattern % {'name': name,
-                                       'comercial_name': comercial}
+                name = name_pattern % {"name": name, "comercial_name": comercial}
             result.append((partner.id, name))
         return result
 
     @api.model
     def _commercial_fields(self):
         res = super(ResPartner, self)._commercial_fields()
-        res += ['comercial']
+        res += ["comercial"]
         return res
