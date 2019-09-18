882b6266a7ef00b729d757fe6a19ffe994005dd1
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2019-09-18 13:54:37 +0200

    [FIX] l10n_es_partner: pass message to logger.exception
    Otherwise it doesn't work
    
    [IMP] l10n_es_partner: black, isort

--- l10n_es_partner/models/res_bank.py
+++ l10n_es_partner/models/res_bank.py
@@ -4,12 +4,12 @@
 # Copyright 2017 Tecnativa - Carlos Dauden <carlos.dauden@tecnativa.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl-3).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class ResBank(models.Model):
-    _inherit = 'res.bank'
+    _inherit = "res.bank"
 
-    lname = fields.Char('Long name', size=128)
-    vat = fields.Char('VAT code', size=32, help='Value Added Tax number')
-    website = fields.Char('Website', size=64)
+    lname = fields.Char("Long name", size=128)
+    vat = fields.Char("VAT code", size=32, help="Value Added Tax number")
+    website = fields.Char("Website", size=64)
