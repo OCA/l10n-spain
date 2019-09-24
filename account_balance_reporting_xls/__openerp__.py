8b54ca0e2bc532a906a093d507c7d92480707375
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2019-09-24 12:14:19 +0200

    [FIX] account_balance_reporting_xls: Proper module category

--- account_balance_reporting_xls/__openerp__.py
+++ account_balance_reporting_xls/__openerp__.py
@@ -28,7 +28,7 @@
     "version": "8.0.1.0.0",
     "author": "Spanish Localization Team,Odoo Community Association (OCA)",
     'website': 'http://odoo-spain.org',
-    "category": "Accounting / Reports",
+    "category": "Accounting & Finance",
     'depends': [
         'account_balance_reporting',
         'report_xls',
