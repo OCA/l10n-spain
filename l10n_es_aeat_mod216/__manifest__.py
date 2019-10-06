fbaf4fc31fa6ecbaa1296a330ca101f33ac00b90
David Gómez Quilón <david.gomez.quilon@gmail.com>
2019-10-06 15:40:18 +0200

    [FIX] module version

--- l10n_es_aeat_mod216/__manifest__.py
+++ l10n_es_aeat_mod216/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     'name': 'AEAT modelo 216',
-    'version': '13.0.1.1.0',
+    'version': '13.0.1.0.0',
     'category': "Localisation/Accounting",
     'author': "AvanzOSC,"
               "Tecnativa,"
