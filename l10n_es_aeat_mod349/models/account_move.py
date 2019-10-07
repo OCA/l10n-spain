402a687525ea1da52739690d73e7181a82851b90
David Gómez Quilón <david.gomez@aselcis.com>
2019-10-07 08:45:53 +0200

    [FIX] lint

--- l10n_es_aeat_mod349/models/account_move.py
+++ l10n_es_aeat_mod349/models/account_move.py
@@ -4,7 +4,7 @@
 # Copyright 2019 David Gómez <david.gomez@aselcis.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class AccountMove(models.Model):
