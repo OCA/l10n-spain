2479769cfc15e085f39590f6e04a9be8e3a15e80
Joan Sisquella <joan.sisquella@eficent.com>
2019-10-28 11:23:04 +0100

    update arguments

--- l10n_es_account_asset/models/account_asset.py
+++ l10n_es_account_asset/models/account_asset.py
@@ -139,8 +139,8 @@ class AccountAssetAsset(models.Model):
             self.method_time = 'percentage'
 
     def _get_amount_linear(
-            self, depreciation_start_date, depreciation_stop_date):
+            self, depreciation_start_date, depreciation_stop_date, entry):
         if self.env.context.get('use_percentage', False):
             return self.depreciation_base * self.annual_percentage / 100
         return super(AccountAssetAsset, self)._get_amount_linear(
-            depreciation_start_date, depreciation_stop_date)
+            depreciation_start_date, depreciation_stop_date, entry)
