b68d2f7384753102f3f3939c5d960148931e9ffc
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-01-09 12:18:59 +0100

    [FIX] l10n_es_aeat_sii: Set refund type on direct refund creation/write
    When no context is set and a refund is created, the refund type was not set. Example:
    
    * Create a sales order with stockable product.
    * Deliver it and invoice the sales order.
    * Return the delivery with refund option checked.
    * Invoice the refund.
    * No refund type set.
    
    With this patch, we assure that the type is set no matter the context.

--- l10n_es_aeat_sii_oca/tests/test_l10n_es_aeat_sii.py
+++ l10n_es_aeat_sii_oca/tests/test_l10n_es_aeat_sii.py
@@ -329,3 +329,23 @@ class TestL10nEsAeatSii(TestL10nEsAeatSiiBase):
         for inv_type in ['out_invoice', 'in_invoice']:
             self.invoice.type = inv_type
             self._test_tax_agencies(self.invoice)
+
+    def test_refund_sii_refund_type(self):
+        invoice = self.env['account.invoice'].create({
+            'partner_id': self.partner.id,
+            'date_invoice': '2018-02-01',
+            'type': 'out_refund',
+            'account_id': self.partner.property_account_payable_id.id,
+        })
+        self.assertEqual(invoice.sii_refund_type, 'I')
+
+    def test_refund_sii_refund_type_write(self):
+        invoice = self.env['account.invoice'].create({
+            'partner_id': self.partner.id,
+            'date_invoice': '2018-02-01',
+            'type': 'out_invoice',
+            'account_id': self.partner.property_account_payable_id.id,
+        })
+        self.assertFalse(invoice.sii_refund_type)
+        invoice.type = 'out_refund'
+        self.assertEqual(invoice.sii_refund_type, 'I')
