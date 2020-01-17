f868d4c18881795a46925616bd7df0e658d4beb5
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2020-01-17 16:13:14 +0100

    l10n_es_aeat_mod390: - Añadido Diseño de registro dr390e2019v100 para declaración de 2019

--- l10n_es_aeat_mod390/tests/test_l10n_es_aeat_mod390.py
+++ l10n_es_aeat_mod390/tests/test_l10n_es_aeat_mod390.py
@@ -281,6 +281,7 @@ class TestL10nEsAeatMod390(TestL10nEsAeatMod390Base):
         export_config_xml_ids = [
             'l10n_es_aeat_mod390.aeat_mod390_main_export_config',
             'l10n_es_aeat_mod390.aeat_mod390_2018_main_export_config',
+            'l10n_es_aeat_mod390.aeat_mod390_2019_main_export_config',
         ]
         for xml_id in export_config_xml_ids:
             export_config = self.env.ref(xml_id)
