3152042372cbe796591d0a6b0b7877f7fdb48365
Ignacio Ibeas - Acysos S.L <ignacio@acysos.com>
2020-01-20 20:32:58 +0100

    [ADD] l10n_es_aeat: certificates and soap. AEAT Partner check
    [ADD] l10n_es_aeat: certificates and soap. AEAT Partner check
    
    [ADD] l10n_es_aeat: certificates and soap. AEAT Partner check
    
    [ADD] l10n_es_aeat: certificates and soap. AEAT Partner check
    
    [ADD] l10n_es_aeat: certificates and soap. AEAT Partner check

---
+++ l10n_es_aeat_partner_check/models/__init__.py
@@ -0,0 +1,5 @@
+# Copyright 2019 Acysos S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from . import res_partner
+from . import res_company
+from . import res_config_settings
