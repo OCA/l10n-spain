c51c5f26dcfbafc8393f7d3c7836a0f122019f9d
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-01-22 22:26:34 +0100

    [FIX] l10n_es_aeat_mod390: Make 1st representative date and notary editable
    They weren't by mistake.
    
    Fixes #1256

--- l10n_es_aeat_mod390/__manifest__.py
+++ l10n_es_aeat_mod390/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'AEAT modelo 390',
-    'version': '12.0.2.0.0',
+    'version': '12.0.2.1.0',
     'category': "Localisation/Accounting",
     'author': "Tecnativa, "
               "Odoo Community Association (OCA)",
