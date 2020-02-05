034673be85b1bc13107cae82f0e285c96071cd91
Sergio Teruel <sergio.teruel@tecnativa.com>
2020-02-05 19:27:18 +0100

    [FIX] payment_redsys: Process order after gateway payment successful

--- payment_redsys/__manifest__.py
+++ payment_redsys/__manifest__.py
@@ -4,7 +4,7 @@
     'name': 'Pasarela de pago Redsys',
     'category': 'Payment Acquirer',
     'summary': 'Payment Acquirer: Redsys Implementation',
-    'version': '12.0.1.0.0',
+    'version': '12.0.1.1.0',
     'author': "Tecnativa,"
               "Odoo Community Association (OCA)",
     'depends': [
