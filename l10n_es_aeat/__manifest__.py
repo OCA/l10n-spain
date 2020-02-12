e9357f524cef4434d4fd3fe317ec48bad159a69b
mreficent <miquel.raich@eficent.com>
2020-02-12 13:18:38 +0100

    [IMP] Get taxes from templates instead of code description

--- l10n_es_aeat/__manifest__.py
+++ l10n_es_aeat/__manifest__.py
@@ -10,7 +10,7 @@
 {
     'name': "AEAT Base",
     'summary': "Modulo base para declaraciones de la AEAT",
-    'version': "12.0.2.1.0",
+    'version': "12.0.2.1.1",
     'author': "Pexego,"
               "Acysos S.L.,"
               "AvanzOSC,"
