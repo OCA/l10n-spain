20e5d2765f4176e429372cd81228e935a1c65011
mreficent <miquel.raich@eficent.com>
2020-02-12 12:02:20 +0100

    [UPD] Remove deprecated from_string()

--- l10n_es_aeat_mod303/__manifest__.py
+++ l10n_es_aeat_mod303/__manifest__.py
@@ -7,7 +7,7 @@
 
 {
     "name": "AEAT modelo 303",
-    "version": "12.0.1.3.0",
+    "version": "12.0.1.3.1",
     "category": "Accounting",
     "author": "Guadaltech,"
               "AvanzOSC,"
