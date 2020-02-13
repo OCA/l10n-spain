90d5646602745808e87820ae5f9446e2c50a1354
mreficent <miquel.raich@eficent.com>
2020-02-13 12:23:05 +0100

    [IMP] l10n_es_mis_report: black, isort

--- l10n_es_mis_report/__manifest__.py
+++ l10n_es_mis_report/__manifest__.py
@@ -4,31 +4,25 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl
 
 {
-    'name': 'Plantillas MIS Builder para informes contables españoles',
-    'summary': """
-        Plantillas MIS Builder para informes contables españoles""",
-    'author': 'Eficent,'
-              'Tecnativa,'
-              'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/l10n-spain',
-    'category': 'Reporting',
-    'version': '12.0.1.0.0',
-    'license': 'AGPL-3',
-    'depends': [
-        'l10n_es',
-        'mis_builder',  # OCA/mis-builder
+    "name": "Plantillas MIS Builder para informes contables españoles",
+    "summary": "Plantillas MIS Builder para informes contables españoles",
+    "author": "Eficent," "Tecnativa," "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-spain",
+    "category": "Reporting",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["l10n_es", "mis_builder"],  # OCA/mis-builder
+    "data": [
+        "data/mis_report_styles.xml",
+        "data/mis_report_balance_abreviated.xml",
+        "data/mis_report_balance_normal.xml",
+        "data/mis_report_balance_sme.xml",
+        "data/mis_report_balance_sme_sfl.xml",
+        "data/mis_report_pyg_abreviated.xml",
+        "data/mis_report_pyg_normal.xml",
+        "data/mis_report_pyg_sme.xml",
+        "data/mis_report_pyg_sme_sfl.xml",
+        "data/mis_report_revenues_expenses_normal.xml",
     ],
-    'data': [
-        'data/mis_report_styles.xml',
-        'data/mis_report_balance_abreviated.xml',
-        'data/mis_report_balance_normal.xml',
-        'data/mis_report_balance_sme.xml',
-        'data/mis_report_balance_sme_sfl.xml',
-        'data/mis_report_pyg_abreviated.xml',
-        'data/mis_report_pyg_normal.xml',
-        'data/mis_report_pyg_sme.xml',
-        'data/mis_report_pyg_sme_sfl.xml',
-        'data/mis_report_revenues_expenses_normal.xml',
-    ],
-    'installable': True,
+    "installable": True,
 }
