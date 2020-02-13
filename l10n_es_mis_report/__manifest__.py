d4fe6147eb0106f021eb1db5b1f8bc9f5b767487
mreficent <miquel.raich@eficent.com>
2020-02-13 12:30:17 +0100

    [MIG] l10n_es_mis_report: Migration to 13.0

--- l10n_es_mis_report/__manifest__.py
+++ l10n_es_mis_report/__manifest__.py
@@ -1,15 +1,14 @@
-# Copyright 2018 Eficent Business and IT Consulting Services, S.L.
-#                <contact@eficent.com>
+# Copyright 2018 ForgeFlow, S.L. <contact@forgeflow.com>
 # Copyright 2018-2019 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl
 
 {
     "name": "Plantillas MIS Builder para informes contables españoles",
     "summary": "Plantillas MIS Builder para informes contables españoles",
-    "author": "Eficent," "Tecnativa," "Odoo Community Association (OCA)",
+    "author": "ForgeFlow, Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-spain",
     "category": "Reporting",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["l10n_es", "mis_builder"],  # OCA/mis-builder
     "data": [
