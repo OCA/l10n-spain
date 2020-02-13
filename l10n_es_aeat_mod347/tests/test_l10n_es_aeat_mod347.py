425ccc6a857e716b72f0aadd96febbce770433e2
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-02-13 10:04:46 +0100

    [FIX] l10n_es_aeat_mod347: Take into account proper sign depending on operation
    We can't remove as is the `abs` if we don't take into account possible sign
    depending on the operation. Complement of 3927e92699dcec1db26c483d7946ba549c7b8ea8
    
    Included tests for these cases.

--- l10n_es_aeat_mod347/tests/test_l10n_es_aeat_mod347.py
+++ l10n_es_aeat_mod347/tests/test_l10n_es_aeat_mod347.py
@@ -29,6 +29,8 @@ class TestL10nEsAeatMod347(TestL10nEsAeatModBase):
             # For testing spanish states mapping
             'country_id': self.env.ref('base.es').id,
         })
+        self.customer_6 = self.customer.copy({'name': 'Test customer 6'})
+        self.supplier_2 = self.supplier.copy({'name': 'Test supplier 2'})
         # Invoice lower than the limit
         self.taxes_sale = {
             'S_IVA10B': (2000, 200),
@@ -59,11 +61,27 @@ class TestL10nEsAeatMod347(TestL10nEsAeatModBase):
             'partner_id': self.customer_5.id,
         })
         self.invoice_5.pay_and_reconcile(self.journal_cash, date='2019-01-01')
+        # Customer refund higher than limit
+        self.taxes_sale = {
+            'S_IVA10S': (5000, 500),
+        }
+        self.invoice_5 = self._invoice_sale_create('2019-01-01', {
+            'partner_id': self.customer_6.id,
+            'type': 'out_refund',
+        })
         # Purchase invoice higher than the limit
         self.taxes_purchase = {
             'P_IVA10_SC': (3000, 300),
         }
         self.invoice_suppler_1 = self._invoice_purchase_create('2019-01-01')
+        # Supplier refund higher than limit
+        self.taxes_purchase = {
+            'P_IVA10_SC': (4000, 400),
+        }
+        self.invoice_suppler_2 = self._invoice_purchase_create('2019-01-01', {
+            'partner_id': self.supplier_2.id,
+            'type': 'in_refund',
+        })
 
     def test_model_347(self):
         # Check flag propagation
@@ -71,20 +89,27 @@ class TestL10nEsAeatMod347(TestL10nEsAeatModBase):
         self.assertTrue(self.invoice_3.move_id.not_in_mod347)
         # Check model
         self.model347.button_calculate()
-        self.assertEqual(self.model347.total_partner_records, 4)
-        self.assertAlmostEqual(self.model347.total_amount, 14300)
-        self.assertAlmostEqual(self.model347.total_cash_amount, 13200)
         partner_record_vals = [
             # key, partner, amount, cash_amount, 1T, 2T, 3T, 4T
             ('A', self.supplier, 3300, 0, 3300, 0, 0, 0),
+            ('A', self.supplier_2, -4400, 0, -4400, 0, 0, 0),
             ('B', self.customer_2, 4400, 0, 0, 4400, 0, 0),
             ('B', self.customer_4, 6600, 6600, 0, 0, 6600, 0),
+            ('B', self.customer_6, -5500, 0, -5500, 0, 0, 0),
             ('B', self.customer_5, 0, 6600, 0, 0, 0, 0),
         ]
-        for i, partner_record in enumerate(self.model347.partner_record_ids):
-            vals = partner_record_vals[i]
+        self.assertEqual(
+            self.model347.total_partner_records, len(partner_record_vals))
+        self.assertAlmostEqual(
+            self.model347.total_amount,
+            sum([x[2] for x in partner_record_vals]))
+        self.assertAlmostEqual(
+            self.model347.total_cash_amount,
+            sum([x[3] for x in partner_record_vals]))
+        for vals in partner_record_vals:
+            partner_record = self.model347.partner_record_ids.filtered(
+                lambda x: x.partner_id ==vals[1])
             self.assertEqual(partner_record.operation_key, vals[0])
-            self.assertEqual(partner_record.partner_id, vals[1])
             self.assertAlmostEqual(partner_record.amount, vals[2])
             self.assertAlmostEqual(partner_record.cash_amount, vals[3])
             self.assertAlmostEqual(partner_record.first_quarter, vals[4])
