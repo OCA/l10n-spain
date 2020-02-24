1b84d54d6e1595ce417160a048f4a59697728d83
Francisco Ivan Anton Prieto <ozono@ozonomultimedia.com>
2020-02-24 22:00:53 +0100

    [FIX] l10n_es_aeat_mod347: real state fixes
    Fixed real state onchange bug and some BOE format fields bugs. Tested exporting and uploading sample BOE file to AEAT.

--- l10n_es_aeat_mod347/models/mod347.py
+++ l10n_es_aeat_mod347/models/mod347.py
@@ -702,7 +702,7 @@ class L10nEsAeatMod347RealStateRecord(models.Model):
             vals = self.report_id._get_partner_347_identification(
                 self.partner_id,
             )
-            del vals['community_vat']
+            vals.pop('community_vat', None)
             del vals['partner_country_code']
             self.update(vals)
 
