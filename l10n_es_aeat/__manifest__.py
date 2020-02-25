9a3983fd72eae67bc804979a04fed241ed638338
mreficent <miquel.raich@eficent.com>
2020-02-25 14:37:04 +0100

    [IMP] l10n_es_aeat: black, isort

--- l10n_es_aeat/__manifest__.py
+++ l10n_es_aeat/__manifest__.py
@@ -8,43 +8,36 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl
 
 {
-    'name': "AEAT Base",
-    'summary': "Modulo base para declaraciones de la AEAT",
-    'version': "12.0.2.2.0",
-    'author': "Pexego,"
-              "Acysos S.L.,"
-              "AvanzOSC,"
-              "Tecnativa,"
-              "Odoo Community Association (OCA)",
-    'license': "AGPL-3",
-    'website': "https://odoo-community.org/",
-    'category': "Accounting & Finance",
-    'depends': [
-        'account',
-        'base_iban',
-        'account_tax_balance',
-        'l10n_es',
+    "name": "AEAT Base",
+    "summary": "Modulo base para declaraciones de la AEAT",
+    "version": "12.0.2.2.0",
+    "author": "Pexego, "
+    "Acysos S.L., "
+    "AvanzOSC, "
+    "Tecnativa, "
+    "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "website": "https://odoo-community.org/",
+    "category": "Accounting & Finance",
+    "depends": ["account", "base_iban", "account_tax_balance", "l10n_es"],
+    "external_dependencies": {"python": ["unidecode", "OpenSSL", "zeep", "requests"]},
+    "data": [
+        "security/aeat_security.xml",
+        "security/ir.model.access.csv",
+        "data/aeat_partner.xml",
+        "data/ir_config_parameter.xml",
+        "wizard/export_to_boe_wizard.xml",
+        "wizard/compare_boe_file_views.xml",
+        "wizard/aeat_certificate_password_view.xml",
+        "views/aeat_menuitem.xml",
+        "views/aeat_report_view.xml",
+        "views/aeat_tax_line_view.xml",
+        "views/aeat_export_configuration_view.xml",
+        "views/aeat_tax_code_mapping_view.xml",
+        "views/account_move_line_view.xml",
+        "views/report_template.xml",
+        "views/res_partner_view.xml",
+        "views/aeat_certificate_view.xml",
     ],
-    'external_dependencies': {
-        'python': ['unidecode', 'OpenSSL', "zeep", "requests"],
-    },
-    'data': [
-        'security/aeat_security.xml',
-        'security/ir.model.access.csv',
-        'data/aeat_partner.xml',
-        'data/ir_config_parameter.xml',
-        'wizard/export_to_boe_wizard.xml',
-        'wizard/compare_boe_file_views.xml',
-        'wizard/aeat_certificate_password_view.xml',
-        'views/aeat_menuitem.xml',
-        'views/aeat_report_view.xml',
-        'views/aeat_tax_line_view.xml',
-        'views/aeat_export_configuration_view.xml',
-        'views/aeat_tax_code_mapping_view.xml',
-        'views/account_move_line_view.xml',
-        'views/report_template.xml',
-        'views/res_partner_view.xml',
-        'views/aeat_certificate_view.xml'
-    ],
-    'installable': True,
+    "installable": True,
 }
