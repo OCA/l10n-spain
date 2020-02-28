bf86f66bee70e66ae1299c174137239d2fa02e0c
mreficent <miquel.raich@eficent.com>
2020-02-28 11:46:17 +0100

    [IMP] l10n_es_aeat_mod111: black, isort

--- l10n_es_aeat_mod111/__manifest__.py
+++ l10n_es_aeat_mod111/__manifest__.py
@@ -3,25 +3,22 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl
 
 {
-    'name': 'AEAT modelo 111',
-    'version': '12.0.1.3.0',
-    'category': "Localisation/Accounting",
-    'author': "AvanzOSC,"
-              "RGB Consulting SL,"
-              "Tecnativa,"
-              "Odoo Community Association (OCA)",
-    'website': "https://github.com/OCA/l10n-spain",
-    'license': 'AGPL-3',
-    'depends': [
-        'l10n_es',
-        'l10n_es_aeat',
+    "name": "AEAT modelo 111",
+    "version": "12.0.1.3.0",
+    "category": "Localisation/Accounting",
+    "author": "AvanzOSC, "
+    "RGB Consulting SL, "
+    "Tecnativa, "
+    "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-spain",
+    "license": "AGPL-3",
+    "depends": ["l10n_es", "l10n_es_aeat"],
+    "data": [
+        "data/aeat_export_mod111_data.xml",
+        "data/tax_code_map_mod111_data.xml",
+        "views/mod111_view.xml",
+        "security/ir.model.access.csv",
+        "security/l10n_es_aeat_mod111_security.xml",
     ],
-    'data': [
-        'data/aeat_export_mod111_data.xml',
-        'data/tax_code_map_mod111_data.xml',
-        'views/mod111_view.xml',
-        'security/ir.model.access.csv',
-        'security/l10n_es_aeat_mod111_security.xml',
-    ],
-    'installable': True,
+    "installable": True,
 }
