b5f7705ed72d9d048fe57a0231efac324bd64fe9
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-03-12 13:04:08 +0100

    [IMP] l10n_es_aeat_mod347: Don't depend on tax description
    Continuation of 611c24bc and later commits. This way, we can decouple tax label from
    the matching, thanks to the XML-ID that Odoo gives to created taxes. We also
    simplifies the code thanks to this.

--- l10n_es_aeat_mod347/models/mod347.py
+++ l10n_es_aeat_mod347/models/mod347.py
@@ -176,16 +176,12 @@ class L10nEsAeatMod347Report(models.Model):
 
     @api.model
     def _get_taxes(self, map):
-        tax_obj = self.env['account.tax']
-        # Obtain all the taxes to be considered
-        tax_templates = map.mapped('tax_ids').mapped('description')
+        """Obtain all the taxes to be considered for 347."""
+        self.ensure_one()
+        tax_templates = map.mapped('tax_ids')
         if not tax_templates:
             raise exceptions.Warning(_('No Tax Mapping was found'))
-        # search the account.tax referred to by the template
-        taxes = tax_obj.search(
-            [('description', 'in', tax_templates),
-             ('company_id', 'child_of', self.company_id.id)])
-        return taxes
+        return self.get_taxes_from_templates(tax_templates)
 
     @api.model
     def _get_partner_347_identification(self, partner):
