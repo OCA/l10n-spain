dd97913c342da9fa9d6b0acdb35d729ef1750743
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-03-12 11:05:35 +0100

    [IMP+FIX] l10n_es_aeat: Move get taxes method to company + cleaning in tests
    This way, we can reuse it in other modules that don't inherit AEAT report like SII.
    
    We also clean a non needed reading of the tax description field.
    
    Tests were still using description method for populating data, so we switch to new
    system and perform needed changes in dependent tests.
    
    s

--- l10n_es_aeat/models/res_company.py
+++ l10n_es_aeat/models/res_company.py
@@ -1,7 +1,8 @@
-# Copyright 2019 Tecnativa - Pedro M. Baeza
+# Copyright 2019-2020 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import api, models
+from odoo.tools import ormcache
 
 
 class ResCompany(models.Model):
@@ -20,3 +21,28 @@ class ResCompany(models.Model):
             except Exception:
                 pass
         return company
+
+    @ormcache('tax_template', 'company')
+    def _get_tax_id_from_tax_template(self, tax_template, company):
+        """Low level cached search for a tax given its tax template and
+        company.
+        """
+        xmlids = self.env['ir.model.data'].search_read([
+            ('model', '=', 'account.tax.template'),
+            ('res_id', '=', tax_template.id)
+        ], ['name', 'module'])
+        return xmlids and self.env['ir.model.data'].search([
+            ('model', '=', 'account.tax'),
+            ('module', '=', xmlids[0]['module']),
+            ('name', '=', '{}_{}'.format(company.id, xmlids[0]['name']))
+        ]).res_id or False
+
+    def get_taxes_from_templates(self, tax_templates):
+        """Return company taxes that match the given tax templates."""
+        self.ensure_one()
+        tax_ids = []
+        for tmpl in tax_templates:
+            tax_id = self._get_tax_id_from_tax_template(tmpl, self)
+            if tax_id:
+                tax_ids.append(tax_id)
+        return self.env['account.tax'].browse(tax_ids)
