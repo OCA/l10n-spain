143f2954853c01c306c7d1d6f7077269f78ed2e1
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-03-12 11:05:35 +0100

    [IMP+FIX] l10n_es_aeat: Move get taxes method to company + cleaning in tests
    This way, we can reuse it in other modules that don't inherit AEAT report like SII.
    
    We also clean a non needed reading of the tax description field.
    
    Tests were still using description method for populating data, so we switch to new
    system and perform needed changes in dependent tests.
    
    s

--- l10n_es_aeat_mod303/__manifest__.py
+++ l10n_es_aeat_mod303/__manifest__.py
@@ -7,7 +7,7 @@
 
 {
     "name": "AEAT modelo 303",
-    "version": "12.0.1.3.1",
+    "version": "12.0.1.4.0",
     "category": "Accounting",
     "author": "Guadaltech,"
               "AvanzOSC,"
