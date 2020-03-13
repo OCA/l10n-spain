79e3b4b5b0fb9408e259ec5fe90c1743275b2595
Marçal Isern <marsal.isern@qubiq.es>
2020-03-13 20:49:11 +0100

    [IMP] l10n_es_account_banking_sepa_fsdd: black, isort

--- l10n_es_account_banking_sepa_fsdd/models/account_payment_order.py
+++ l10n_es_account_banking_sepa_fsdd/models/account_payment_order.py
@@ -6,13 +6,14 @@ from odoo import api, models
 
 
 class AccountPaymentOrder(models.Model):
-    _inherit = 'account.payment.order'
+    _inherit = "account.payment.order"
 
     @api.model
     def generate_group_header_block(self, parent_node, gen_args):
-        res = super(AccountPaymentOrder, self).\
-            generate_group_header_block(parent_node, gen_args)
+        res = super(AccountPaymentOrder, self).generate_group_header_block(
+            parent_node, gen_args
+        )
         if self.payment_mode_id.charge_financed:
-            reference = parent_node.xpath('//GrpHdr/MsgId')
+            reference = parent_node.xpath("//GrpHdr/MsgId")
             reference[0].text = u"FSDD " + reference[0].text
         return res
