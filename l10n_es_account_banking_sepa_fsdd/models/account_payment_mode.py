c713d1044f12e69e84e4046453bd01730a243f81
Marçal Isern <marsal.isern@qubiq.es>
2020-03-13 20:49:11 +0100

    [IMP] l10n_es_account_banking_sepa_fsdd: black, isort

--- l10n_es_account_banking_sepa_fsdd/models/account_payment_mode.py
+++ l10n_es_account_banking_sepa_fsdd/models/account_payment_mode.py
@@ -9,4 +9,5 @@ class PaymentMode(models.Model):
     _inherit = "account.payment.mode"
 
     charge_financed = fields.Boolean(
-        'Financed Charge', help="Adds FSDD prefix to sepa file id")
+        "Financed Charge", help="Adds FSDD prefix to sepa file id"
+    )
