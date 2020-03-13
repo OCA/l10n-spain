e5d523b248bf3e7fac257860207807042f2eff84
Marçal Isern <marsal.isern@qubiq.es>
2020-03-13 22:59:37 +0100

    [MIG] l10n_es_account_banking_sepa_fsdd: Migration to 13.0

--- l10n_es_account_banking_sepa_fsdd/models/account_payment_order.py
+++ l10n_es_account_banking_sepa_fsdd/models/account_payment_order.py
@@ -2,17 +2,14 @@
 # Copyright 2017 Tecnativa - Luis M. Ontalba <luis.martinez@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, models
+from odoo import models
 
 
 class AccountPaymentOrder(models.Model):
     _inherit = "account.payment.order"
 
-    @api.model
     def generate_group_header_block(self, parent_node, gen_args):
-        res = super(AccountPaymentOrder, self).generate_group_header_block(
-            parent_node, gen_args
-        )
+        res = super().generate_group_header_block(parent_node, gen_args)
         if self.payment_mode_id.charge_financed:
             reference = parent_node.xpath("//GrpHdr/MsgId")
             reference[0].text = u"FSDD " + reference[0].text
