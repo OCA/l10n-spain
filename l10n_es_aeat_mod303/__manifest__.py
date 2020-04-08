d523e3e9687611d714f3ce23dc4eb302c6ded83b
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-04-08 17:27:14 +0200

    [FIX] l10n_es_aeat_mod303: Avoid error importing BOE due to developer VAT
    AEAT now forces to be a proper VAT format. We adjust at the same time the version
    and some old year references.

--- l10n_es_aeat_mod303/__manifest__.py
+++ l10n_es_aeat_mod303/__manifest__.py
@@ -8,7 +8,7 @@
 
 {
     "name": "AEAT modelo 303",
-    "version": "10.0.2.3.0",
+    "version": "10.0.2.4.0",
     'category': "Accounting & Finance",
     'author': "Guadaltech,"
               "AvanzOSC,"
