bcd0127f8c4c580e0457f3c3397343bdf72f5f77
angelmoya <amoyapardo@gmail.com>
2020-04-15 15:01:22 +0200

    [FIX] 303,facturas rectivicativas en 93 y 94

--- l10n_es_aeat_mod303/__manifest__.py
+++ l10n_es_aeat_mod303/__manifest__.py
@@ -7,7 +7,7 @@
 
 {
     "name": "AEAT modelo 303",
-    "version": "12.0.1.6.0",
+    "version": "12.0.1.7.0",
     "category": "Accounting",
     "author": "Guadaltech,"
               "AvanzOSC,"
