a606a3a428cbda265400bbddaca74ea474bbeb9f
Roberto Lizana <roberto@trey.es>
2020-04-23 08:40:49 +0200

    [12.0] l10n_es_aeat_*: [FIX] Caracteres no válidos 'NIF Empresa Desarrollo'

--- l10n_es_aeat_mod111/__manifest__.py
+++ l10n_es_aeat_mod111/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': 'AEAT modelo 111',
-    'version': '12.0.1.2.0',
+    'version': '12.0.1.3.0',
     'category': "Localisation/Accounting",
     'author': "AvanzOSC,"
               "RGB Consulting SL,"
