ed4036f7333f0deda76c005d93dc02ed1cb8319d
Roberto Lizana <roberto@trey.es>
2020-04-23 08:40:49 +0200

    [12.0] l10n_es_aeat_*: [FIX] Caracteres no válidos 'NIF Empresa Desarrollo'

--- l10n_es_aeat_mod115/__manifest__.py
+++ l10n_es_aeat_mod115/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     'name': 'AEAT modelo 115',
-    'version': '12.0.1.3.0',
+    'version': '12.0.1.4.0',
     'development_status': 'Mature',
     'category': "Localisation/Accounting",
     'author': "AvanzOSC,"
