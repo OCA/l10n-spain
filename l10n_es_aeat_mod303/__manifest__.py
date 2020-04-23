2bbddb5b97929a4cd53bfe62246f2c73c8655c2b
Roberto Lizana <roberto@trey.es>
2020-04-23 08:40:49 +0200

    [12.0] l10n_es_aeat_*: [FIX] Caracteres no válidos 'NIF Empresa Desarrollo'

--- l10n_es_aeat_mod303/__manifest__.py
+++ l10n_es_aeat_mod303/__manifest__.py
@@ -7,7 +7,7 @@
 
 {
     "name": "AEAT modelo 303",
-    "version": "12.0.1.7.0",
+    "version": "12.0.1.8.0",
     "category": "Accounting",
     "author": "Guadaltech,"
               "AvanzOSC,"
