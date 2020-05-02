b1a1afab1b3e341a52b0bdf25a5affbb87d0f4bb
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-05-02 15:23:39 +0200

    [MIG] l10n_es_aeat_vat_prorrate_asset: Migration to 12.0
    * Standard procedure.
    * README by fragments.
    * Adapted to OCA's account_asset_management.
    * Added tests.

--- l10n_es_aeat_vat_prorrate_asset/models/mod303.py
+++ l10n_es_aeat_vat_prorrate_asset/models/mod303.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015-2017 Tecnativa - Pedro M. Baeza <pedro.baeza@tecnativa.com>
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+# License AGPL-3 - See https://www.gnu.org/licenses/agpl-3.0.html
 
 from odoo import models
 
@@ -10,17 +9,21 @@ class L10nEsAeatMod303Report(models.Model):
 
     def _calculate_casilla_44(self):
         super(L10nEsAeatMod303Report, self)._calculate_casilla_44()
-        assets = self.env['account.asset.asset'].search(
-            [('date', 'like', '{}%'.format(self.year))]
-        )
-        result = self.casilla_44
-        for asset in assets.filtered('vat_prorrate_percent'):
-            original_taxes = (
-                asset.vat_prorrate_increment * 100 /
-                (100 - asset.vat_prorrate_percent)
+        for report in self:
+            if (report.vat_prorrate_type != 'general' or
+                    report.period_type not in ('4T', '12')):
+                continue
+            assets = self.env['account.asset'].search(
+                [('date_start', 'like', '{}%'.format(report.year))]
             )
-            real_increment = (
-                original_taxes * (100 - self.vat_prorrate_percent) / 100
-            )
-            result += asset.vat_prorrate_increment - real_increment
-        self.casilla_44 = round(result, 2)
+            result = report.casilla_44
+            for asset in assets.filtered('vat_prorrate_percent'):
+                original_taxes = (
+                    asset.vat_prorrate_increment * 100 /
+                    (100 - asset.vat_prorrate_percent)
+                )
+                real_increment = (
+                    original_taxes * (100 - report.vat_prorrate_percent) / 100
+                )
+                result += asset.vat_prorrate_increment - real_increment
+            report.casilla_44 = round(result, 2)
