9bb108891ca4b1e44d378f7b7a30145549a7f661
Harald Panten <harald.panten@sygel.es>
2020-05-05 14:04:15 +0200

    [IMP] l10n_es_partner_mercantil: black, isort, prettier

--- l10n_es_partner_mercantil/models/partner_es.py
+++ l10n_es_partner_mercantil/models/partner_es.py
@@ -2,15 +2,15 @@
 # Copyright 2016 Antonio Espinosa <antonio.espinosa@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
-    es_libro = fields.Char(string='Book')
-    es_registro_mercantil = fields.Char(string='Commercial Registry')
-    es_hoja = fields.Char(string='Sheet')
-    es_folio = fields.Char(string='Page')
-    es_seccion = fields.Char(string='Section')
-    es_tomo = fields.Char(string='Volume')
+    es_libro = fields.Char(string="Book")
+    es_registro_mercantil = fields.Char(string="Commercial Registry")
+    es_hoja = fields.Char(string="Sheet")
+    es_folio = fields.Char(string="Page")
+    es_seccion = fields.Char(string="Section")
+    es_tomo = fields.Char(string="Volume")
