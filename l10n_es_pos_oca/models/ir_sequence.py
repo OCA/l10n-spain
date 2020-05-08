9fcefe3f51ec0a05f44b8a63796f566e35442646
Carlos Roca <carlos.roca@tecnativa.com>
2020-05-08 13:34:05 +0200

    [IMP] l10n_es_pos: black, isort, prettier

--- l10n_es_pos_oca/models/ir_sequence.py
+++ l10n_es_pos_oca/models/ir_sequence.py
@@ -6,17 +6,28 @@ from odoo.exceptions import UserError
 
 
 class IrSequence(models.Model):
-    _inherit = 'ir.sequence'
+    _inherit = "ir.sequence"
 
-    @api.constrains('prefix', 'code')
+    @api.constrains("prefix", "code")
     def check_simplified_invoice_unique_prefix(self):
-        if self._context.get('copy_pos_config'):
+        if self._context.get("copy_pos_config"):
             return
         for sequence in self.filtered(
-                lambda x: x.code == 'pos.config.simplified_invoice'):
-            if self.search_count([
-                    ('code', '=', 'pos.config.simplified_invoice'),
-                    ('prefix', '=', sequence.prefix)]) > 1:
-                raise UserError(_('There is already a simplified invoice '
-                                  'sequence with that prefix and it should be '
-                                  'unique.'))
+            lambda x: x.code == "pos.config.simplified_invoice"
+        ):
+            if (
+                self.search_count(
+                    [
+                        ("code", "=", "pos.config.simplified_invoice"),
+                        ("prefix", "=", sequence.prefix),
+                    ]
+                )
+                > 1
+            ):
+                raise UserError(
+                    _(
+                        "There is already a simplified invoice "
+                        "sequence with that prefix and it should be "
+                        "unique."
+                    )
+                )
