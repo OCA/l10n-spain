14589ad0accfe60d2f4ad5e1d546395c950708fd
Carlos Roca <carlos.roca@tecnativa.com>
2020-05-08 13:34:05 +0200

    [IMP] l10n_es_pos: black, isort, prettier

--- l10n_es_pos/static/src/js/models.js
+++ l10n_es_pos/static/src/js/models.js
@@ -3,47 +3,61 @@
    License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 */
 
-odoo.define('l10n_es_pos.models', function (require) {
+odoo.define("l10n_es_pos.models", function(require) {
     "use strict";
 
-    var models = require('point_of_sale.models');
+    var models = require("point_of_sale.models");
 
     var pos_super = models.PosModel.prototype;
     models.PosModel = models.PosModel.extend({
-        initialize: function (attributes, options) {
+        initialize: function(attributes, options) {
             pos_super.initialize.apply(this, arguments);
             this.pushed_simple_invoices = [];
             return this;
         },
-        get_simple_inv_next_number: function () {
-            if (this.pushed_simple_invoices.indexOf(this.config.l10n_es_simplified_invoice_number) > -1) {
+        get_simple_inv_next_number: function() {
+            if (
+                this.pushed_simple_invoices.indexOf(
+                    this.config.l10n_es_simplified_invoice_number
+                ) > -1
+            ) {
                 ++this.config.l10n_es_simplified_invoice_number;
             }
-            return this.config.l10n_es_simplified_invoice_prefix+this.get_padding_simple_inv(this.config.l10n_es_simplified_invoice_number);
+            return (
+                this.config.l10n_es_simplified_invoice_prefix +
+                this.get_padding_simple_inv(
+                    this.config.l10n_es_simplified_invoice_number
+                )
+            );
         },
-        get_padding_simple_inv: function (number) {
-            var diff = this.config.l10n_es_simplified_invoice_padding - number.toString().length;
-            var result = '';
+        get_padding_simple_inv: function(number) {
+            var diff =
+                this.config.l10n_es_simplified_invoice_padding -
+                number.toString().length;
+            var result = "";
             if (diff <= 0) {
                 result = number;
             } else {
                 for (var i = 0; i < diff; i++) {
-                    result += '0';
+                    result += "0";
                 }
                 result += number;
             }
             return result;
         },
-        push_simple_invoice: function (order) {
-            if (this.pushed_simple_invoices.indexOf(order.data.simplified_invoice) === -1) {
+        push_simple_invoice: function(order) {
+            if (
+                this.pushed_simple_invoices.indexOf(order.data.simplified_invoice) ===
+                -1
+            ) {
                 this.pushed_simple_invoices.push(order.data.simplified_invoice);
                 ++this.config.l10n_es_simplified_invoice_number;
             }
         },
-        _flush_orders: function (orders, options) {
+        _flush_orders: function(orders, options) {
             var self = this;
             // Save pushed orders numbers
-            _.each(orders, function (order) {
+            _.each(orders, function(order) {
                 if (!order.data.to_invoice) {
                     self.push_simple_invoice(order);
                 }
@@ -54,26 +68,25 @@ odoo.define('l10n_es_pos.models', function (require) {
 
     var order_super = models.Order.prototype;
     models.Order = models.Order.extend({
-        get_total_with_tax: function () {
+        get_total_with_tax: function() {
             var total = order_super.get_total_with_tax.apply(this, arguments);
-            var below_limit = total <=
-                this.pos.config.l10n_es_simplified_invoice_limit;
+            var below_limit = total <= this.pos.config.l10n_es_simplified_invoice_limit;
             this.is_simplified_invoice =
                 below_limit && this.pos.config.iface_l10n_es_simplified_invoice;
             return total;
         },
-        set_simple_inv_number: function () {
+        set_simple_inv_number: function() {
             this.simplified_invoice = this.pos.get_simple_inv_next_number();
             this.name = this.simplified_invoice;
             this.is_simplified_invoice = true;
         },
-        get_base_by_tax: function () {
+        get_base_by_tax: function() {
             var base_by_tax = {};
-            this.get_orderlines().forEach(function (line) {
+            this.get_orderlines().forEach(function(line) {
                 var tax_detail = line.get_tax_details();
                 var base_price = line.get_price_without_tax();
                 if (tax_detail) {
-                    Object.keys(tax_detail).forEach(function (tax) {
+                    Object.keys(tax_detail).forEach(function(tax) {
                         if (Object.keys(base_by_tax).includes(tax)) {
                             base_by_tax[tax] += base_price;
                         } else {
@@ -84,12 +97,12 @@ odoo.define('l10n_es_pos.models', function (require) {
             });
             return base_by_tax;
         },
-        init_from_JSON: function (json) {
+        init_from_JSON: function(json) {
             order_super.init_from_JSON.apply(this, arguments);
             this.to_invoice = json.to_invoice;
             this.simplified_invoice = json.simplified_invoice;
         },
-        export_as_JSON: function () {
+        export_as_JSON: function() {
             var res = order_super.export_as_JSON.apply(this, arguments);
             res.to_invoice = this.is_to_invoice();
             if (!res.to_invoice) {
@@ -99,6 +112,5 @@ odoo.define('l10n_es_pos.models', function (require) {
         },
     });
 
-    models.load_fields('res.company', ['street', 'city', 'state_id', 'zip']);
-
+    models.load_fields("res.company", ["street", "city", "state_id", "zip"]);
 });
