a49a93d5ee3e6c959fd00770922f0d2397ece59f
Carlos Roca <carlos.roca@tecnativa.com>
2020-05-13 09:32:37 +0200

    [IMP] account_multicompany_easy_creation: black, isort, prettier

--- payment_redsys/controllers/main.py
+++ payment_redsys/controllers/main.py
@@ -4,6 +4,7 @@
 
 import logging
 import pprint
+
 import werkzeug
 
 from odoo import http
@@ -13,31 +14,40 @@ _logger = logging.getLogger(__name__)
 
 
 class RedsysController(http.Controller):
-    _return_url = '/payment/redsys/return'
-    _cancel_url = '/payment/redsys/cancel'
-    _exception_url = '/payment/redsys/error'
-    _reject_url = '/payment/redsys/reject'
+    _return_url = "/payment/redsys/return"
+    _cancel_url = "/payment/redsys/cancel"
+    _exception_url = "/payment/redsys/error"
+    _reject_url = "/payment/redsys/reject"
 
-    @http.route([
-        '/payment/redsys/return',
-        '/payment/redsys/cancel',
-        '/payment/redsys/error',
-        '/payment/redsys/reject',
-    ], type='http', auth='none', csrf=False)
+    @http.route(
+        [
+            "/payment/redsys/return",
+            "/payment/redsys/cancel",
+            "/payment/redsys/error",
+            "/payment/redsys/reject",
+        ],
+        type="http",
+        auth="none",
+        csrf=False,
+    )
     def redsys_return(self, **post):
         """ Redsys."""
-        _logger.info('Redsys: entering form_feedback with post data %s',
-                     pprint.pformat(post))
+        _logger.info(
+            "Redsys: entering form_feedback with post data %s", pprint.pformat(post)
+        )
         if post:
-            request.env['payment.transaction'].sudo().form_feedback(
-                post, 'redsys')
-        return_url = post.pop('return_url', '')
+            request.env["payment.transaction"].sudo().form_feedback(post, "redsys")
+        return_url = post.pop("return_url", "")
         if not return_url:
-            return_url = '/shop'
+            return_url = "/shop"
         return werkzeug.utils.redirect(return_url)
 
     @http.route(
-        ['/payment/redsys/result/<page>'], type='http', auth='public',
-        methods=['GET'], website=True)
+        ["/payment/redsys/result/<page>"],
+        type="http",
+        auth="public",
+        methods=["GET"],
+        website=True,
+    )
     def redsys_result(self, page, **vals):
-        return werkzeug.utils.redirect('/payment/process')
+        return werkzeug.utils.redirect("/payment/process")
