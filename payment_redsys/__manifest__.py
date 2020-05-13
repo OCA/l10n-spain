add0097ea93a6af8f03f81b1d7584b81478948cb
Carlos Roca <carlos.roca@tecnativa.com>
2020-05-13 09:32:37 +0200

    [IMP] account_multicompany_easy_creation: black, isort, prettier

--- payment_redsys/__manifest__.py
+++ payment_redsys/__manifest__.py
@@ -1,27 +1,19 @@
 # Copyright 2017 Tecnativa - Sergio Teruel
 
 {
-    'name': 'Pasarela de pago Redsys',
-    'category': 'Payment Acquirer',
-    'summary': 'Payment Acquirer: Redsys Implementation',
-    'version': '12.0.1.1.0',
-    'author': "Tecnativa,"
-              "Odoo Community Association (OCA)",
-    'depends': [
-        'payment',
-        'website_sale',
+    "name": "Pasarela de pago Redsys",
+    "category": "Payment Acquirer",
+    "summary": "Payment Acquirer: Redsys Implementation",
+    "version": "12.0.1.1.0",
+    "author": "Tecnativa," "Odoo Community Association (OCA)",
+    "depends": ["payment", "website_sale"],
+    "external_dependencies": {"python": ["Crypto.Cipher.DES3"]},
+    "data": [
+        "views/redsys.xml",
+        "views/payment_acquirer.xml",
+        "views/payment_redsys_templates.xml",
+        "data/payment_redsys.xml",
     ],
-    "external_dependencies": {
-        "python": [
-            "Crypto.Cipher.DES3",
-        ],
-    },
-    'data': [
-        'views/redsys.xml',
-        'views/payment_acquirer.xml',
-        'views/payment_redsys_templates.xml',
-        'data/payment_redsys.xml'
-    ],
-    'license': 'AGPL-3',
-    'installable': True,
+    "license": "AGPL-3",
+    "installable": True,
 }
