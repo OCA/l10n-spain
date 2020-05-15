cd07309b69020e952eb86904303c5dc80025ecb7
Ignacio Ibeas - Acysos S.L <ignacio@acysos.com>
2020-05-15 23:18:51 +0200

    [IMP] l10n_es_aeat_mod216: black, isort, prettier

--- l10n_es_aeat_mod216/__manifest__.py
+++ l10n_es_aeat_mod216/__manifest__.py
@@ -4,28 +4,21 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'AEAT modelo 216',
-    'version': '12.0.2.4.0',
-    'category': "Localisation/Accounting",
-    'author': "AvanzOSC,"
-              "Tecnativa,"
-              "Odoo Community Association (OCA)",
-    'website': "https://github.com/OCA/l10n-spain",
-    'license': 'AGPL-3',
-    'depends': [
-        'l10n_es_aeat',
-        'l10n_es_irnr',
+    "name": "AEAT modelo 216",
+    "version": "12.0.2.4.0",
+    "category": "Localisation/Accounting",
+    "author": "AvanzOSC," "Tecnativa," "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-spain",
+    "license": "AGPL-3",
+    "depends": ["l10n_es_aeat", "l10n_es_irnr",],
+    "data": [
+        "security/ir.model.access.csv",
+        "security/ir_rule.xml",
+        "data/tax_code_map_mod216_data.xml",
+        "data/aeat_export_mod216_data.xml",
+        "views/mod216_view.xml",
     ],
-    'data': [
-        'security/ir.model.access.csv',
-        'security/ir_rule.xml',
-        'data/tax_code_map_mod216_data.xml',
-        'data/aeat_export_mod216_data.xml',
-        'views/mod216_view.xml',
-    ],
-    'maintainers': [
-        'pedrobaeza',
-    ],
-    'development_status': 'Mature',
-    'installable': True,
+    "maintainers": ["pedrobaeza",],
+    "development_status": "Mature",
+    "installable": True,
 }
