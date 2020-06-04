4d3a64b43d693fcf0af1a4ba93580c40e9f18ae1
Valentin Vinagre Urteaga <valentin.vinagre@sygel.es>
2020-06-04 10:38:31 +0200

    [IMP] l10n_es_account_asset: black, isort, prettier

--- l10n_es_account_asset/models/account_asset_profile.py
+++ l10n_es_account_asset/models/account_asset_profile.py
@@ -6,56 +6,56 @@ from odoo.tools import float_compare
 
 
 class AccountAssetProfile(models.Model):
-    _inherit = 'account.asset.profile'
+    _inherit = "account.asset.profile"
 
     METHOD_PERIOD_MAPPING = {
-        'month': 1,
-        'quarter': 4,
-        'year': 12,
-
+        "month": 1,
+        "quarter": 4,
+        "year": 12,
     }
 
     @api.model
     def _selection_method_time(self):
         res = super()._selection_method_time()
-        res.append(('percentage', _('Fixed percentage')))
+        res.append(("percentage", _("Fixed percentage")))
         return res
 
     annual_percentage = fields.Float(
-        string='Annual depreciation percentage',
-        digits=(3, 8),
-        default=100.0,
+        string="Annual depreciation percentage", digits=(3, 8), default=100.0,
     )
     method_percentage = fields.Float(
-        string='Depreciation percentage',
+        string="Depreciation percentage",
         digits=(3, 8),
         compute="_compute_method_percentage",
         inverse="_inverse_method_percentage",
         store=True,
     )
 
-    @api.depends('annual_percentage', 'method_period')
+    @api.depends("annual_percentage", "method_period")
     def _compute_method_percentage(self):
         for profile in self:
             profile.method_percentage = (
-                profile.annual_percentage * profile.METHOD_PERIOD_MAPPING.get(
-                    profile.method_period, 12,
-                ) / 12
+                profile.annual_percentage
+                * profile.METHOD_PERIOD_MAPPING.get(profile.method_period, 12,)
+                / 12
             )
 
-    @api.onchange('method_percentage')
+    @api.onchange("method_percentage")
     def _inverse_method_percentage(self):
         for profile in self:
             new_percentage = (
-                profile.method_percentage * 12 /
-                profile.METHOD_PERIOD_MAPPING.get(profile.method_period, 12)
+                profile.method_percentage
+                * 12
+                / profile.METHOD_PERIOD_MAPPING.get(profile.method_period, 12)
             )
             # Only change amount when significant delta
             if float_compare(new_percentage, self.annual_percentage, 2) != 0:
                 self.annual_percentage = new_percentage
 
     _sql_constraints = [
-        ('annual_percentage',
-         'CHECK (annual_percentage > 0 and annual_percentage <= 100)',
-         'Wrong percentage!'),
+        (
+            "annual_percentage",
+            "CHECK (annual_percentage > 0 and annual_percentage <= 100)",
+            "Wrong percentage!",
+        ),
     ]
