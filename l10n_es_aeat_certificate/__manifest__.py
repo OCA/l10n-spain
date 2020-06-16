d09cb0ea79f1884e24dc7a1f5d4701c48a96cf83
Ignacio Ibeas - Acysos S.L <ignacio@acysos.com>
2020-06-16 09:35:46 +0200

    [ADD] l10n_es_aeat_certificate

---
+++ l10n_es_aeat_certificate/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright 2019 Acysos S.L.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    'name': 'AEAT Certificados',
+    'author': 'Acysos S.L.,'
+              'Odoo Community Association (OCA)',
+    'website': 'https://odoo-community.org/',
+    'category': 'Accounting',
+    'version': '11.0.0.1.0',
+    'license': 'AGPL-3',
+    'depends': [
+        'base',
+        'l10n_es_aeat'
+    ],
+    'data': [
+        'views/aeat_certificate_view.xml',
+        'wizards/aeat_certificate_password_view.xml',
+        'security/aeat_certificate_security.xml',
+        'security/ir.model.access.csv'
+    ],
+    'installable': True,
+}
