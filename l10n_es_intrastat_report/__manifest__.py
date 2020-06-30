d8fe3ec6c1fa3e49465617225e3328db8d881963
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-06-30 14:15:58 +0200

    [FIX] l10n_es_intrastat_report: Allow invoices with no linked purchases
    Previous code was forcing the existence of linked purchase lines which may be not
    true for all. As we already have a fallback in declaration code, let's not force
    this at location level.

--- l10n_es_intrastat_report/__manifest__.py
+++ l10n_es_intrastat_report/__manifest__.py
@@ -7,7 +7,7 @@
 
 {
     'name': 'Intrastat Product Declaration for Spain',
-    'version': '12.0.1.0.0',
+    'version': '12.0.1.0.1',
     'category': 'Intrastat',
     'license': 'AGPL-3',
     'summary': 'Spanish Intrastat Product Declaration',
