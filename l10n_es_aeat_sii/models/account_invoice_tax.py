8d840531dba2c0a747044022ec99003a6690cdfc
Valentin Vinagre Urteaga <valentin.vinagre@sygel.es>
2020-07-10 10:22:14 +0200

    [IMP] l10n_es_aeat_sii: black, isort, prettier

--- l10n_es_aeat_sii/models/account_invoice_tax.py
+++ l10n_es_aeat_sii/models/account_invoice_tax.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 Comunitea - Omar Castiñeira <omar@comunitea.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -10,26 +9,26 @@ class AccountInvoiceTax(models.Model):
     _inherit = "account.invoice.tax"
 
     base_company = fields.Monetary(
-        string='Base in company currency',
-        compute="_compute_base_amount_company",
+        string="Base in company currency", compute="_compute_base_amount_company",
     )
     amount_company = fields.Monetary(
-        string='Amount in company currency',
-        compute="_compute_base_amount_company",
+        string="Amount in company currency", compute="_compute_base_amount_company",
     )
 
     @api.multi
     def _compute_base_amount_company(self):
         for tax in self:
-            if (tax.invoice_id.currency_id !=
-                    tax.invoice_id.company_id.currency_id):
+            if tax.invoice_id.currency_id != tax.invoice_id.company_id.currency_id:
                 currency = tax.invoice_id.currency_id.with_context(
                     date=tax.invoice_id.date_invoice,
-                    company_id=tax.invoice_id.company_id.id)
+                    company_id=tax.invoice_id.company_id.id,
+                )
                 tax.base_company = currency.compute(
-                    tax.base, tax.invoice_id.company_id.currency_id)
+                    tax.base, tax.invoice_id.company_id.currency_id
+                )
                 tax.amount_company = currency.compute(
-                    tax.amount, tax.invoice_id.company_id.currency_id)
+                    tax.amount, tax.invoice_id.company_id.currency_id
+                )
             else:
                 tax.base_company = tax.base
                 tax.amount_company = tax.amount
