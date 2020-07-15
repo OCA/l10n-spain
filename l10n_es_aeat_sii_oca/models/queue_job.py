2313a34728a7863ae2aa0f051f7def0dd53b7b63
Valentin Vinagre Urteaga <valentin.vinagre@sygel.es>
2020-07-15 16:52:59 +0200

    [MIG] l10n_es_aeat_sii: Migration to 13.0

--- l10n_es_aeat_sii_oca/models/queue_job.py
+++ l10n_es_aeat_sii_oca/models/queue_job.py
@@ -2,20 +2,17 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 
-from odoo import api, models
+from odoo import models
 
 
 class QueueJob(models.Model):
     _inherit = "queue.job"
 
-    @api.multi
     def do_now(self):
         self.sudo().write({"eta": False})
 
-    @api.multi
     def cancel_now(self):
         self.sudo().filtered(lambda x: x.state in ["pending", "enqueued"]).unlink()
 
-    @api.multi
     def requeue_sudo(self):
         self.sudo().requeue()
