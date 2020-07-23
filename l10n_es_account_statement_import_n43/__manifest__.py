235483f820aa5af2a91d467c85ee74e3b1c4577f
Valentin Vinagre Urteaga <valentin.vinagre@sygel.es>
2020-07-23 17:10:44 +0200

    [IMP] l10n_es_account_bank_statement_import_n43: black, isort, prettier

--- l10n_es_account_statement_import_n43/__manifest__.py
+++ l10n_es_account_statement_import_n43/__manifest__.py
@@ -3,27 +3,21 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Importación de extractos bancarios españoles (Norma 43)',
-    'category': 'Accounting & Finance',
-    'version': '12.0.1.0.5',
-    'license': 'AGPL-3',
-    'development_status': 'Mature',
-    'maintainers': ['pedrobaeza'],
-    'author': 'Spanish Localization Team,'
-              'Tecnativa,'
-              'Odoo Community Association (OCA)',
+    "name": "Importación de extractos bancarios españoles (Norma 43)",
+    "category": "Accounting & Finance",
+    "version": "12.0.1.0.5",
+    "license": "AGPL-3",
+    "development_status": "Mature",
+    "maintainers": ["pedrobaeza"],
+    "author": "Spanish Localization Team,"
+    "Tecnativa,"
+    "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-spain",
-    'depends': [
-        'account_bank_statement_import',
+    "depends": ["account_bank_statement_import"],
+    "external_dependencies": {"python": ["chardet"]},
+    "data": [
+        "views/account_journal_views.xml",
+        "wizards/account_bank_statement_import_view.xml",
     ],
-    'external_dependencies': {
-        'python': [
-            'chardet',
-        ],
-    },
-    'data': [
-        'views/account_journal_views.xml',
-        'wizards/account_bank_statement_import_view.xml',
-    ],
-    'installable': True,
+    "installable": True,
 }
