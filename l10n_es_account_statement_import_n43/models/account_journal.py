e47c18b0c7864370a059f72c88ad3bdbaf333b6b
Valentin Vinagre Urteaga <valentin.vinagre@sygel.es>
2020-07-23 17:10:44 +0200

    [IMP] l10n_es_account_bank_statement_import_n43: black, isort, prettier

--- l10n_es_account_statement_import_n43/models/account_journal.py
+++ l10n_es_account_statement_import_n43/models/account_journal.py
@@ -7,13 +7,12 @@ class AccountJournal(models.Model):
     _inherit = "account.journal"
 
     n43_date_type = fields.Selection(
-        string='Date type for N43 Import',
-        selection=[('fecha_valor', 'Value Date'),
-                   ('fecha_oper', 'Operation Date')],
-        default='fecha_valor',
+        string="Date type for N43 Import",
+        selection=[("fecha_valor", "Value Date"), ("fecha_oper", "Operation Date")],
+        default="fecha_valor",
     )
 
     def _get_bank_statements_available_import_formats(self):
         res = super()._get_bank_statements_available_import_formats()
-        res.append('N43')
+        res.append("N43")
         return res
