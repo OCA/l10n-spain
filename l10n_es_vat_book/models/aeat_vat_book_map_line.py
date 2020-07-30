61164605c2cabb8844b17c9efae90b95d05fb29d
AaronHForgeFlow <ahenriquez@eficent.com>
2020-07-30 17:56:54 +0200

    [IMP] l10n_es_vat_book: black, isort, prettier

--- l10n_es_vat_book/models/aeat_vat_book_map_line.py
+++ l10n_es_vat_book/models/aeat_vat_book_map_line.py
@@ -5,35 +5,25 @@ from odoo import fields, models
 
 
 class AeatVatBookMapLines(models.Model):
-    _name = 'aeat.vat.book.map.line'
-    _description = 'AEAT Vat Book Map Line'
+    _name = "aeat.vat.book.map.line"
+    _description = "AEAT Vat Book Map Line"
 
     def _selection_special_tax_group(self):
-        return self.env['l10n.es.vat.book.line.tax'].fields_get(
-            allfields=['special_tax_group'])['special_tax_group']['selection']
+        return self.env["l10n.es.vat.book.line.tax"].fields_get(
+            allfields=["special_tax_group"]
+        )["special_tax_group"]["selection"]
 
-    name = fields.Char(
-        string='Name',
-    )
+    name = fields.Char(string="Name",)
     book_type = fields.Selection(
-        selection=[
-            ('issued', 'Issued'),
-            ('received', 'Received'),
-        ],
-        string='Book type',
+        selection=[("issued", "Issued"), ("received", "Received"),], string="Book type",
     )
     special_tax_group = fields.Selection(
         selection=_selection_special_tax_group,
-        string='Special group',
-        help='Special tax group as R.Eq, IRPF, etc',
+        string="Special group",
+        help="Special tax group as R.Eq, IRPF, etc",
     )
-    fee_type_xlsx_column = fields.Char(
-        string='Type xlsx column'
-    )
-    fee_amount_xlsx_column = fields.Char(
-        string='Base xlsx column'
-    )
+    fee_type_xlsx_column = fields.Char(string="Type xlsx column")
+    fee_amount_xlsx_column = fields.Char(string="Base xlsx column")
     tax_tmpl_ids = fields.Many2many(
-        comodel_name='account.tax.template',
-        string="Taxes",
+        comodel_name="account.tax.template", string="Taxes",
     )
