420469bae777b65823250902a6442f1ca7dd1688
AaronHForgeFlow <ahenriquez@eficent.com>
2020-07-30 15:40:32 +0200

    [IMP] : black, isort, prettier

--- l10n_es_account_bank_statement_import_credit_card_caixabank/__manifest__.py
+++ l10n_es_account_bank_statement_import_credit_card_caixabank/__manifest__.py
@@ -3,22 +3,17 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Importación de extractos bancarios tarjetas crédito CaixaBank',
-    'category': 'Accounting & Finance',
-    'version': '11.0.1.0.1',
-    'license': 'AGPL-3',
-    'author': 'Eficent,'
-              'Tecnativa,'
-              'Comunitea,'
-              'Odoo Community Association (OCA)',
+    "name": "Importación de extractos bancarios tarjetas crédito CaixaBank",
+    "category": "Accounting & Finance",
+    "version": "11.0.1.0.1",
+    "license": "AGPL-3",
+    "author": "Eficent," "Tecnativa," "Comunitea," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-spain",
-    'depends': [
-        'account_bank_statement_import',
+    "depends": [
+        "account_bank_statement_import",
         # https://github.com/OCA/community-data-files
-        'base_currency_iso_4217',
+        "base_currency_iso_4217",
     ],
-    'data': [
-        'wizards/account_bank_statement_import_view.xml',
-    ],
-    'installable': True,
+    "data": ["wizards/account_bank_statement_import_view.xml",],
+    "installable": True,
 }
