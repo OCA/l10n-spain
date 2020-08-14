9c2008f84c330ed87fe3318bd27d5bb8df90a664
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-08-14 20:20:13 +0200

    [FIX] l10n_es_aeat_sii: Compute currency taxes amounts with proper date
    If there's an accounting date, taxes amounts in company currency are not properly
    computed.
    
    The method has been refactored as well for using the modern `_convert` method.

--- l10n_es_aeat_sii_oca/models/account_invoice_tax.py
+++ l10n_es_aeat_sii_oca/models/account_invoice_tax.py
@@ -1,12 +1,11 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 Comunitea - Omar Castiñeira <omar@comunitea.com>
+# Copyright 2020 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class AccountInvoiceTax(models.Model):
-
     _inherit = "account.invoice.tax"
 
     base_company = fields.Monetary(
@@ -18,18 +17,20 @@ class AccountInvoiceTax(models.Model):
         compute="_compute_base_amount_company",
     )
 
-    @api.multi
     def _compute_base_amount_company(self):
         for tax in self:
             if (tax.invoice_id.currency_id !=
                     tax.invoice_id.company_id.currency_id):
-                currency = tax.invoice_id.currency_id.with_context(
-                    date=tax.invoice_id.date_invoice,
-                    company_id=tax.invoice_id.company_id.id)
-                tax.base_company = currency.compute(
-                    tax.base, tax.invoice_id.company_id.currency_id)
-                tax.amount_company = currency.compute(
-                    tax.amount, tax.invoice_id.company_id.currency_id)
+                rate_date = (
+                    tax.invoice_id._get_currency_rate_date() or
+                    fields.Date.today()
+                )
+                currency = tax.invoice_id.currency_id
+                company = tax.invoice_id.company_id
+                tax.base_company = currency._convert(
+                    tax.base, company.currency_id, company, rate_date)
+                tax.amount_company = currency._convert(
+                    tax.amount, company.currency_id, company, rate_date)
             else:
                 tax.base_company = tax.base
                 tax.amount_company = tax.amount
