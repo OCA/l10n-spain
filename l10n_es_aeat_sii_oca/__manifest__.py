21411e0c5b2da77bd175161a0b7e1d6a5a9e0fb9
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-08-14 16:53:21 +0200

    [FIX] l10n_es_aeat_sii: VAT not subject for goods
    Since odoo/odoo#34044, VAT not subject for goods was not included in any SII taxes
    map, which provokes that any vendor bill of this type is not correctly sent.

--- l10n_es_aeat_sii_oca/__manifest__.py
+++ l10n_es_aeat_sii_oca/__manifest__.py
@@ -12,7 +12,7 @@
 
 {
     "name": "Suministro Inmediato de Información en el IVA",
-    "version": "12.0.1.4.5",
+    "version": "12.0.1.5.0",
     "category": "Accounting & Finance",
     "website": "https://odoospain.odoo.com",
     "author": "Acysos S.L.,"
