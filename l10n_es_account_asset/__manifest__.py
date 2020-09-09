9d93aa6bb330c3eba833db448e1933e1efc28617
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-09-09 20:42:58 +0200

    [FIX] l10n_es_account_asset: Don't inject context if no method percentage
    As it alters computations for other methods. TT25453

--- l10n_es_account_asset/__manifest__.py
+++ l10n_es_account_asset/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "Gestión de activos fijos para España",
-    "version": "13.0.1.0.0",
+    "version": "13.0.1.0.1",
     "depends": ["account_asset_management"],
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "license": "AGPL-3",
