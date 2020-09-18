a0168876269efb48387ee6f24851981187661424
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-09-18 10:48:07 +0200

    [FIX] l10n_es_aeat_vat_prorrate: Proper default year
    In v12, date fields are now Python objets

--- l10n_es_aeat_vat_prorrate/__manifest__.py
+++ l10n_es_aeat_vat_prorrate/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "AEAT - Prorrata de IVA",
-    "version": "12.0.1.0.0",
+    "version": "12.0.1.0.1",
     "license": "AGPL-3",
     "author": "AvanzOSC, "
               "Tecnativa, "
