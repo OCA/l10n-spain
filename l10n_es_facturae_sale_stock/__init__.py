913d5c9dc74403e83704ef55049e590f59e91bbb
Eric Antones <eantones@nuobit.com>
2020-09-19 16:28:09 +0200

    [MIG] l10n_es_facturae_sale_stock: Migration to 11.0

--- l10n_es_facturae_sale_stock/__init__.py
+++ l10n_es_facturae_sale_stock/__init__.py
@@ -1,4 +1,5 @@
-# -*- coding: utf-8 -*-
+# Copyright 2017 Creu Blanca
+# Copyright 2020 NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import models
---
+++ l10n_es_facturae_sale_stock/readme/INSTALL.rst
@@ -0,0 +1,2 @@
+Este módulo depende del módulo *l10n_es_facturae*, *stock_picking_invoice_link*
+y sus dependencias.
