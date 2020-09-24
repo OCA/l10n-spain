bdcfe44483f37ee413c0af2d3216bdd3b8ae923f
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-09-24 13:20:49 +0200

    [FIX] l10n_es_aeat_mod303: Adapt odoo/odoo#58062
    Now "IVA Exento No Sujeto" is not included in field 60, but it should be included
    in field 83 for period 12 (December) in 303 for those exempted of the 390.

--- l10n_es_aeat_mod303/__manifest__.py
+++ l10n_es_aeat_mod303/__manifest__.py
@@ -2,12 +2,12 @@
 # Copyright 2015 AvanzOSC - Ainara Galdona
 # Copyright 2016 Tecnativa - Antonio Espinosa
 # Copyright 2017 Eficent Business and IT Consulting Services, S.L.
-# Copyright 2014-2018 Tecnativa - Pedro M. Baeza
+# Copyright 2014-2020 Tecnativa - Pedro M. Baeza
 # License AGPL-3 - See https://www.gnu.org/licenses/agpl-3.0
 
 {
     "name": "AEAT modelo 303",
-    "version": "13.0.1.1.0",
+    "version": "13.0.1.2.0",
     "category": "Accounting",
     "author": "Guadaltech,"
     "AvanzOSC,"
