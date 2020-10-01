39682a578a54ad3343fa42c9994acdf709a8cfe7
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-10-02 01:16:42 +0200

    [MIG+IMP] l10n_es_aeat_sii: Complete migration
    - Redo the tax computation for efficiency and reliability
    - Add test suite for checking obtained JSON for different tax configurations
    - SII description computation reworked for new computed writable
    - Added migration scripts

---
+++ l10n_es_aeat/models/account_tax.py
@@ -0,0 +1,29 @@
+# Copyright 2020 Tecnativa - Pedro M. Baeza
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from odoo import api, models
+
+
+class AccountTax(models.Model):
+    _inherit = "account.tax"
+
+    def _clear_tax_id_from_tax_template_cache(self):
+        Company = self.env["res.company"]
+        Company._get_tax_id_from_tax_template.clear_cache(Company)
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        """Invalidate company cache that links templates with taxes as
+        there are potential new taxes that can match.
+        """
+        res = super().create(vals_list)
+        self._clear_tax_id_from_tax_template_cache()
+        return res
+
+    def unlink(self):
+        """Invalidate company cache that links templates with taxes for not
+        returning a potential invalid tax that has been unlinked.
+        """
+        res = super().unlink()
+        self._clear_tax_id_from_tax_template_cache()
+        return res
