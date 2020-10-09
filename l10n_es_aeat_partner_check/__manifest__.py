bc5c4596ea932220cec70e3f31a51cfd883cc352
Ignacio Ibeas - Acysos S.L <ignacio@acysos.com>
2020-10-09 16:33:56 +0200

    [MIG] l10n_es_aeat_partner_check: Migration to 13.0
    [MIG] l10n_es_aeat_partner_check: Migration to 13.0

--- l10n_es_aeat_partner_check/__manifest__.py
+++ l10n_es_aeat_partner_check/__manifest__.py
@@ -2,17 +2,22 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
-    "name": "AEAT - Comprobación de Calidad de datos identificativos",
-    "author": "Acysos S.L.," "Odoo Community Association (OCA)",
-    "website": "https://github.com/OCA/l10n-spain",
-    "category": "Accounting",
-    "version": "12.0.0.1.1",
-    "license": "AGPL-3",
-    "depends": ["base", "base_vat", "l10n_es_aeat"],
-    "data": [
-        "views/res_partner_view.xml",
-        "views/res_company_view.xml",
-        "views/res_config_settings_view.xml",
+    'name': 'AEAT - Comprobación de Calidad de datos identificativos',
+    'author': 'Acysos S.L.,'
+              'Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/l10n-spain',
+    'category': 'Accounting',
+    'version': '13.0.0.1.0',
+    'license': 'AGPL-3',
+    'depends': [
+        'base',
+        'base_vat',
+        'l10n_es_aeat'
     ],
-    "installable": True,
+    'data': [
+        'views/res_partner_view.xml',
+        'views/res_company_view.xml',
+        'views/res_config_settings_view.xml'
+    ],
+    'installable': True,
 }
