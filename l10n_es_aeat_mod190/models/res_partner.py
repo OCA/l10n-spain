a3b14438039a485a1044988a1b836757bb7311bb
root <ntorres@vunkers.com>
2020-10-15 09:21:33 +0200

    Nuevos modulos

---
+++ l10n_es_aeat_mod190/models/res_partner.py
@@ -0,0 +1,12 @@
+
+from odoo import models, fields
+
+
+class ResPartner(models.Model):
+    _name = 'res.partner'
+    _inherit = ['res.partner', 'l10n.es.mod190.additional.data.mixin']
+
+    include_on_aeat_mod190 = fields.Boolean(
+        string='Included in mod190',
+        default=False
+    )
---
+++ l10n_es_aeat_mod390/readme/CONTRIBUTORS.rst
@@ -0,0 +1,5 @@
+* `Tecnativa <https://www.tecnativa.com>`__:
+
+  * Pedro M. Baeza <pedro.baeza@tecnativa.com>
+
+* Albert Cabedo
