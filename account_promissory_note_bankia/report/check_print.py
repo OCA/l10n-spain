ad20d768e4dc491afec9054d3aaecb0c1f1c72a0
Carlos Roca <carlos.roca@tecnativa.com>
2020-10-30 10:33:19 +0100

    [MIG] account_promissory_note_bankia: Migration to v13

--- account_promissory_note_bankia/report/check_print.py
+++ account_promissory_note_bankia/report/check_print.py
@@ -6,3 +6,4 @@ class ReportPromissoryNotePrintBankia(models.AbstractModel):
     # account_check_printing_report_base for the report to work.
     _name = "report.account_promissory_note_bankia.promissory_footer_bkia"
     _inherit = "report.account_check_printing_report_base.promissory_footer_a4"
+    _description = "Promissory Note Bankia"
