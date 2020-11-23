1ee68472528b9856fbebcac853f2e587bc79321a
sergio-teruel <sergio.teruel@tecnativa.com>
2020-11-23 16:05:57 +0100

    [FIX] payment_redsys: Avoid execute method por non Redsys acquirer

--- payment_redsys/models/redsys.py
+++ payment_redsys/models/redsys.py
@@ -361,6 +361,8 @@ class TxRedsys(models.Model):
     @api.model
     def form_feedback(self, data, acquirer_name):
         res = super(TxRedsys, self).form_feedback(data, acquirer_name)
+        if acquirer_name != 'redsys':
+            return res
         try:
             tx_find_method_name = '_%s_form_get_tx_from_data' % acquirer_name
             if hasattr(self, tx_find_method_name):
