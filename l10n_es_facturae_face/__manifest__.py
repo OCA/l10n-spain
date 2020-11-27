1e0e0e363be195d8998b6b1902f9d78c08036710
Enric Tobella <etobella@creublanca.es>
2020-11-27 13:53:04 +0100

    [REF] l10n_es_facturae_face: Black python code

--- l10n_es_facturae_face/__manifest__.py
+++ l10n_es_facturae_face/__manifest__.py
@@ -4,8 +4,7 @@
 {
     "name": "Envío de Factura-e a FACe",
     "version": "12.0.1.1.0",
-    "author": "Creu Blanca, "
-              "Odoo Community Association (OCA)",
+    "author": "Creu Blanca, " "Odoo Community Association (OCA)",
     "category": "Accounting & Finance",
     "website": "https://github.com/OCA/l10n-spain",
     "license": "AGPL-3",
@@ -17,14 +16,8 @@
         "data/face_data.xml",
         "views/res_company_view.xml",
         "views/res_config_views.xml",
-        "wizard/account_invoice_integration_cancel_view.xml"
+        "wizard/account_invoice_integration_cancel_view.xml",
     ],
-    "external_dependencies": {
-        "python": [
-            "OpenSSL",
-            "zeep",
-            "xmlsec"
-        ]
-    },
+    "external_dependencies": {"python": ["OpenSSL", "zeep", "xmlsec"]},
     "installable": True,
 }
--- l10n_es_facturae_face/models/account_invoice_integration.py
+++ l10n_es_facturae_face/models/account_invoice_integration.py
@@ -18,88 +18,102 @@ except (ImportError, IOError) as err:
 class AccountInvoiceIntegration(models.Model):
     _inherit = "account.invoice.integration"
 
-    integration_status = fields.Selection(selection_add=[
-        ('face-1200', 'Registered on REC'),
-        ('face-1300', 'Registered on RCF'),
-        ('face-2400', 'Accepted'),
-        ('face-2500', 'Payed'),
-        ('face-2600', 'Rejected'),
-        ('face-3100', 'Cancellation approved'),
-    ])
-    cancellation_status = fields.Selection(selection_add=[
-        ('face-4100', 'Not requested'),
-        ('face-4200', 'Cancellation requested'),
-        ('face-4300', 'Cancellation accepted'),
-        ('face-4400', 'Cancellation rejected'),
-    ])
+    integration_status = fields.Selection(
+        selection_add=[
+            ("face-1200", "Registered on REC"),
+            ("face-1300", "Registered on RCF"),
+            ("face-2400", "Accepted"),
+            ("face-2500", "Payed"),
+            ("face-2600", "Rejected"),
+            ("face-3100", "Cancellation approved"),
+        ]
+    )
+    cancellation_status = fields.Selection(
+        selection_add=[
+            ("face-4100", "Not requested"),
+            ("face-4200", "Cancellation requested"),
+            ("face-4300", "Cancellation accepted"),
+            ("face-4400", "Cancellation rejected"),
+        ]
+    )
     company_id = fields.Many2one(
-        "res.company", related="invoice_id.company_id", store=True)
+        "res.company", related="invoice_id.company_id", store=True
+    )
 
     def _cron_face_update_method(self, company_domain=False):
         if not company_domain:
             company_domain = []
-        face = self.env.ref('l10n_es_facturae_face.integration_face')
-        for company in self.env['res.company'].search(company_domain):
-            integrations = self.search([
-                ('company_id', '=', company.id),
-                ('method_id', '=', face.id),
-                ('state', '=', 'sent'),
-                '|',
-                ('integration_status', '=', False),
-                ('integration_status', 'in', [
-                    'face-1200', 'face-1300', 'face-2400'
-                ]),
-            ])
+        face = self.env.ref("l10n_es_facturae_face.integration_face")
+        for company in self.env["res.company"].search(company_domain):
+            integrations = self.search(
+                [
+                    ("company_id", "=", company.id),
+                    ("method_id", "=", face.id),
+                    ("state", "=", "sent"),
+                    "|",
+                    ("integration_status", "=", False),
+                    (
+                        "integration_status",
+                        "in",
+                        ["face-1200", "face-1300", "face-2400"],
+                    ),
+                ]
+            )
             if not integrations:
                 continue
             cert = crypto.load_pkcs12(
                 base64.b64decode(company.facturae_cert),
-                company.facturae_cert_password
+                company.facturae_cert_password,
             )
             cert.set_ca_certificates(None)
             client = Client(
-                wsdl=self.env["ir.config_parameter"].sudo().get_param(
-                    "account.invoice.face.server", default=None),
+                wsdl=self.env["ir.config_parameter"]
+                .sudo()
+                .get_param("account.invoice.face.server", default=None),
                 wsse=MemorySignature(
                     cert.export(),
                     base64.b64decode(
                         self.env.ref(
-                            'l10n_es_facturae_face.face_certificate').datas
+                            "l10n_es_facturae_face.face_certificate"
+                        ).datas
                     ),
-                )
+                ),
             )
             integration_dict = {}
             integration_list = []
             for integration in integrations:
                 integration_list.append(integration.register_number)
                 integration_dict[integration.register_number] = integration
-            request = client.get_type('ns0:ConsultarListadoFacturaRequest')(
+            request = client.get_type("ns0:ConsultarListadoFacturaRequest")(
                 integration_list
             )
             response = client.service.consultarListadoFacturas(request)
-            if response.resultado.codigo != '0':
-                _logger.info(_('Company %s cannot be processed') % company.display_name)
+            if response.resultado.codigo != "0":
+                _logger.info(
+                    _("Company %s cannot be processed") % company.display_name
+                )
                 continue
             for invoice in response.facturas.consultarListadoFactura:
                 integ = integration_dict[invoice.factura.numeroRegistro]
                 factura = invoice.factura
-                if invoice.codigo != '0':
+                if invoice.codigo != "0":
                     # Probably processed from another system
                     continue
-                process_code = 'face-' + factura.tramitacion.codigo
-                revocation_code = 'face-' + factura.anulacion.codigo
+                process_code = "face-" + factura.tramitacion.codigo
+                revocation_code = "face-" + factura.anulacion.codigo
                 if (
-                    integ.integration_status == process_code and
-                    integ.cancellation_status == revocation_code
+                    integ.integration_status == process_code
+                    and integ.cancellation_status == revocation_code
                 ):
                     # Nothing has changed on the invoice
                     continue
-                log = self.env['account.invoice.integration.log'].create(
-                    integ.update_values())
-                log.state = 'sent'
+                log = self.env["account.invoice.integration.log"].create(
+                    integ.update_values()
+                )
+                log.state = "sent"
                 integ.integration_status = process_code
                 integ.integration_description = factura.tramitacion.motivo
                 integ.cancellation_status = revocation_code
                 integ.cancellation_description = factura.anulacion.motivo
-                if integ.cancellation_status != 'face-4100':
+                if integ.cancellation_status != "face-4100":
                     integ.can_cancel = False
