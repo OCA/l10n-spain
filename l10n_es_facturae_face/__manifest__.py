4707c070a5cd5fea14346fcff7720cf0192b834e
Enric Tobella <etobella@creublanca.es>
2020-12-02 09:28:01 +0100

    [MIG] l10n_es_facturae_face

--- l10n_es_facturae_face/__manifest__.py
+++ l10n_es_facturae_face/__manifest__.py
@@ -3,20 +3,18 @@
 
 {
     "name": "Envío de Factura-e a FACe",
-    "version": "12.0.1.1.0",
+    "version": "13.0.1.0.1",
     "author": "Creu Blanca, " "Odoo Community Association (OCA)",
     "category": "Accounting & Finance",
     "website": "https://github.com/OCA/l10n-spain",
     "license": "AGPL-3",
-    "depends": [
-        "l10n_es_facturae",
-    ],
+    "depends": ["l10n_es_facturae", "edi_webservice"],
     "data": [
-        "data/cron_data.xml",
         "data/face_data.xml",
+        "wizards/edi_l10n_es_facturae_face_cancel.xml",
+        "views/account_move.xml",
         "views/res_company_view.xml",
         "views/res_config_views.xml",
-        "wizard/account_invoice_integration_cancel_view.xml",
     ],
     "external_dependencies": {"python": ["OpenSSL", "zeep", "xmlsec"]},
     "installable": True,
---
+++ l10n_es_facturae_face/components/edi_webservice_receive_face_l10n_es_facturae_face_update.py
@@ -0,0 +1,36 @@
+# Copyright 2020 Creu Blanca
+# @author: Enric Tobella
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+import json
+
+from zeep import helpers
+
+from odoo import _
+from odoo.exceptions import UserError
+
+from odoo.addons.component.core import Component
+
+
+class EdiWebServiceReceiveFaceL10nEsFacturaeFaceUpdate(Component):
+    _name = "edi.webservice.receive.face.l10n_es_facturae_face_update"
+    _usage = "webservice.receive"
+    _backend_type = "l10n_es_facturae"
+    _exchange_type = "l10n_es_facturae_face_update"
+    _webservice_protocol = "face"
+    _inherit = "edi.component.receive.mixin"
+
+    def receive(self):
+        invoice = self.exchange_record.record
+        response = self.backend.webservice_backend_id.call(
+            "consult_invoice",
+            invoice.company_id.facturae_cert,
+            invoice.company_id.facturae_cert_password,
+            self.exchange_record.parent_id.external_identifier,
+        )
+        if response.resultado.codigo != "0":
+            raise UserError(
+                _("Connection with FACe returned error %s - %s")
+                % (response.resultado.codigo, response.resultado.descripcion)
+            )
+        return json.dumps(helpers.serialize_object(response.factura))
