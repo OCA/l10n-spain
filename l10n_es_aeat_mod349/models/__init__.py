35f5c0b54e801e2692f4a4ce8bda705225e1675c
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-12-04 11:48:41 +0100

    [MIG] l10n_es_aeat_mod349: Migration to 13.0
    - General procedure
    - In report change type > statement_type + migration script
    - Computation adapted
    - Adapt account.invoice > account.move in logic + migration script
    - Adapt tests

--- l10n_es_aeat_mod349/models/__init__.py
+++ l10n_es_aeat_mod349/models/__init__.py
@@ -1,7 +1,6 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from . import account_invoice
-from . import account_move_line
+from . import account_move
 from . import account_tax
 from . import account_tax_template
 from . import aeat_349_map_line
