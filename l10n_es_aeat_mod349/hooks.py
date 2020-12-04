b91ce29c6957ba3f4c84d221a338cb03edbd866f
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-12-04 11:11:19 +0100

    [IMP] l10n_es_aeat_mod349: black, isort, prettier

--- l10n_es_aeat_mod349/hooks.py
+++ l10n_es_aeat_mod349/hooks.py
@@ -3,6 +3,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 import logging
+
 from odoo import SUPERUSER_ID
 from odoo.api import Environment
 
@@ -12,18 +13,23 @@ logger = logging.getLogger(__name__)
 def update_account_move_line(cr, op_key_taxes):
     for op_key_tax in op_key_taxes:
         aeat_349_operation_key = op_key_tax.l10n_es_aeat_349_operation_key
-        cr.execute("""
+        cr.execute(
+            """
             UPDATE account_move_line aml
             SET l10n_es_aeat_349_operation_key = %s
             FROM account_move_line_account_tax_rel AS rel
             WHERE rel.account_tax_id = %s and aml.id = rel.account_move_line_id
-        """, (aeat_349_operation_key, op_key_tax.id))
-        logger.info('Updated account move lines for operation key %s and tax '
-                    '%s' % (aeat_349_operation_key, op_key_tax.name))
+        """,
+            (aeat_349_operation_key, op_key_tax.id),
+        )
+        logger.info(
+            "Updated account move lines for operation key %s and tax "
+            "%s" % (aeat_349_operation_key, op_key_tax.name)
+        )
 
 
 def post_init_hook(cr, registry):
     env = Environment(cr, SUPERUSER_ID, {})
-    taxes = env['account.tax'].search([])
+    taxes = env["account.tax"].search([])
     op_key_taxes = taxes.filtered(lambda x: x.l10n_es_aeat_349_operation_key)
     update_account_move_line(cr, op_key_taxes)
