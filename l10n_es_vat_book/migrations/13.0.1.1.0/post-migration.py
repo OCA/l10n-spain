4de74c274ff9ae0e016ae8a957d1f10378245561
Ernesto Tejeda <ernesto.tejeda@tecnativa.com>
2021-01-20 23:19:57 -0500

    [IMP] l10n_es_vat_book: black, isort, prettier

--- l10n_es_vat_book/migrations/13.0.1.1.0/post-migration.py
+++ l10n_es_vat_book/migrations/13.0.1.1.0/post-migration.py
@@ -8,7 +8,8 @@ def migrate(cr, version):
         env = api.Environment(cr, SUPERUSER_ID, {})
         domain = "['|',('company_id','=',False),('company_id','in',company_ids)]"
         rule = env.ref(
-            "l10n_es_vat_book.ir_rule_vat_book_company", raise_if_not_found=False,
+            "l10n_es_vat_book.ir_rule_vat_book_company",
+            raise_if_not_found=False,
         )
         if rule:
             rule.write({"domain_force": domain})
