c673fd86827a71dc345833cf7769b9c4457169dd
angelmoya <amoyapardo@gmail.com>
2021-01-21 15:24:03 +0100

    [FIX] l10n_es_aeat_mod190: use float_compare to check amounts to validate report
    [UPD] Update l10n_es_aeat_mod190.pot
    
    l10n_es_aeat_mod190 12.0.1.0.1

--- l10n_es_aeat_mod190/__manifest__.py
+++ l10n_es_aeat_mod190/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': 'AEAT modelo 190',
-    'version': '12.0.1.0.0',
+    'version': '12.0.1.0.1',
     'category': "Localization/Accounting",
     'author': "Punt Sistemes SLU,"
               "Odoo Community Association (OCA)",
