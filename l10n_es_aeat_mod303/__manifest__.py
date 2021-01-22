41ac8dab906b474ac9af4f37479a0f861e060c47
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2021-01-22 15:47:23 +0100

    [IMP] l10n_es_aeat_mod303: Readonly for exonerated/volume on computed state
    This way, we avoid the misunderstanding of changing that but not
    computing fields 76 to 99.
    
    Relates to #1553

--- l10n_es_aeat_mod303/__manifest__.py
+++ l10n_es_aeat_mod303/__manifest__.py
@@ -2,12 +2,12 @@
 # Copyright 2015 AvanzOSC - Ainara Galdona
 # Copyright 2016 Tecnativa - Antonio Espinosa
 # Copyright 2017 ForgeFlow, S.L.
-# Copyright 2014-2020 Tecnativa - Pedro M. Baeza
+# Copyright 2014-2021 Tecnativa - Pedro M. Baeza
 # License AGPL-3 - See https://www.gnu.org/licenses/agpl-3.0
 
 {
     "name": "AEAT modelo 303",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.1.0",
     "category": "Accounting",
     "author": "Guadaltech,"
     "AvanzOSC,"
