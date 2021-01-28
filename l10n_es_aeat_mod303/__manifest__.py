dd708a4cec06b51e7d9849b3612cf6a127b60abc
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2021-01-28 19:53:10 +0100

    [FIX] l10n_es_aeat_mod303: Fix fields 84 and 94
    - Added missing tax "No sujeto (Bienes)" in field 84.
    - Removed tax "Exportación (Bienes)" from field 94 (already on 84).

--- l10n_es_aeat_mod303/__manifest__.py
+++ l10n_es_aeat_mod303/__manifest__.py
@@ -7,7 +7,7 @@
 
 {
     "name": "AEAT modelo 303",
-    "version": "12.0.1.11.0",
+    "version": "12.0.1.12.0",
     "category": "Accounting",
     "author": "Guadaltech,"
               "AvanzOSC,"
