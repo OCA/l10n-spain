b020f71864d23434f1a0c9c3e04b5157917c172d
Ioan Galan <ioan@studio73.es>
2021-01-28 16:45:39 +0100

    [MIG] l10n_es_aeat_vat_prorrate_asset: Migration to 13.0
    [UPD] Update l10n_es_aeat_vat_prorrate_asset.pot
    
    [UPD] README.rst

--- l10n_es_aeat_vat_prorrate_asset/__manifest__.py
+++ l10n_es_aeat_vat_prorrate_asset/__manifest__.py
@@ -3,12 +3,12 @@
 
 {
     "name": "AEAT - Prorrata de IVA - Extensión para los activos",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "license": "AGPL-3",
     "author": "Tecnativa, " "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-spain",
     "category": "Accounting",
-    "depends": ["l10n_es_aeat_vat_prorrate", "account_asset_management",],
-    "data": ["views/account_asset_views.xml", "views/account_invoice_views.xml",],
+    "depends": ["l10n_es_aeat_vat_prorrate", "account_asset_management"],
+    "data": ["views/account_asset_views.xml", "views/account_move_views.xml"],
     "installable": True,
 }
--- l10n_es_aeat_vat_prorrate_asset/readme/CONTRIBUTORS.rst
+++ l10n_es_aeat_vat_prorrate_asset/readme/CONTRIBUTORS.rst
@@ -1,3 +1,6 @@
 * `Tecnativa <https://www.tecnativa.com>`_:
 
   * Pedro M. Baeza
+* `Studio73 <https://www.studio73.es>`_:
+
+  * Ioan Galan
