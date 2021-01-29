479cb866e883e2f785f19c49acb1fb26ca5d1ed8
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2021-01-29 20:52:05 +0100

    [IMP] l10n_es_aeat_mod390: Clean obsolete BOE export formats

--- l10n_es_aeat_mod390/__manifest__.py
+++ l10n_es_aeat_mod390/__manifest__.py
@@ -3,34 +3,13 @@
 
 {
     "name": "AEAT modelo 390",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.1.0",
     "category": "Localisation/Accounting",
     "author": "Tecnativa, " "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-spain",
     "license": "AGPL-3",
     "depends": ["l10n_es", "l10n_es_aeat", "account_tax_balance"],
     "data": [
-        # 2017
-        "data/aeat_export_mod390_sub01_data.xml",
-        "data/aeat_export_mod390_sub02_data.xml",
-        "data/aeat_export_mod390_sub03_data.xml",
-        "data/aeat_export_mod390_sub04_data.xml",
-        "data/aeat_export_mod390_sub05_data.xml",
-        "data/aeat_export_mod390_sub06_data.xml",
-        "data/aeat_export_mod390_sub07_data.xml",
-        "data/aeat_export_mod390_sub08_data.xml",
-        "data/aeat_export_mod390_main_data.xml",
-        # 2018
-        "data/aeat_export_mod390_2018_sub01_data.xml",
-        "data/aeat_export_mod390_2018_sub02_data.xml",
-        "data/aeat_export_mod390_2018_sub03_data.xml",
-        "data/aeat_export_mod390_2018_sub04_data.xml",
-        "data/aeat_export_mod390_2018_sub05_data.xml",
-        "data/aeat_export_mod390_2018_sub06_data.xml",
-        "data/aeat_export_mod390_2018_sub07_data.xml",
-        "data/aeat_export_mod390_2018_sub08_data.xml",
-        "data/aeat_export_mod390_2018_main_data.xml",
-        # 2019
         "data/aeat_export_mod390_2019_sub01_data.xml",
         "data/aeat_export_mod390_2019_sub02_data.xml",
         "data/aeat_export_mod390_2019_sub03_data.xml",
