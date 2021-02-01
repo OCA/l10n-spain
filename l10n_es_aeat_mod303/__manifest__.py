7d1638d632e129df0a0a54c3cf3c6f2d2a6db340
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2021-02-01 20:43:51 +0100

    [FIX] l10n_es_aeat_mod303: Change remaining taxes with only one sign
    Remaining pieces of 6c3a6f2.
    
    At the end, the refund part is controlled by move_type, not by balance
    sign.

--- l10n_es_aeat_mod303/__manifest__.py
+++ l10n_es_aeat_mod303/__manifest__.py
@@ -7,7 +7,7 @@
 
 {
     "name": "AEAT modelo 303",
-    "version": "11.0.2.8.1",
+    "version": "11.0.2.9.0",
     "category": "Accounting",
     "author": "Guadaltech,"
               "AvanzOSC,"
