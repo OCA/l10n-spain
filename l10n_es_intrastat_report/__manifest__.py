888eccaa25e2efdf8516273e2f2565865db04d3b
João Marques <joao.marques@tecnativa.com>
2021-02-04 09:21:38 +0000

    [MIG] l10n_es_intrastat_report: Migration to 13.0
    TT27865

--- l10n_es_intrastat_report/__manifest__.py
+++ l10n_es_intrastat_report/__manifest__.py
@@ -3,17 +3,18 @@
 # Copyright 2019 - FactorLibre - Daniel Duque <daniel.duque@factorlibre.com>
 # Copyright 2020 - Tecnativa - Manuel Calero
 # Copyright 2020 - Tecnativa - Pedro M. Baeza
+# Copyright 2021 Tecnativa - João Marques
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Intrastat Product Declaration for Spain",
-    "version": "12.0.1.0.1",
+    "version": "13.0.1.0.0",
     "category": "Intrastat",
     "license": "AGPL-3",
     "summary": "Spanish Intrastat Product Declaration",
     "author": "FactorLibre,Noviat,Tecnativa,Odoo Community Association (OCA)",
-    "depends": ["intrastat_product", "l10n_es_aeat",],
-    "conflicts": ["report_intrastat",],
+    "depends": ["intrastat_product", "l10n_es_aeat"],
+    "conflicts": ["report_intrastat"],
     "data": [
         "security/l10n_es_intrastat_report_security.xml",
         "security/ir.model.access.csv",
