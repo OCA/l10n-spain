c14221e54fad25627df0e5776ac9e409cde41a40
João Marques <joao.marques@tecnativa.com>
2021-02-09 11:19:32 +0000

    [MIG] l10n_es_dua_sii: Migration to 13.0
    - Refactor and improve tests
    - 10n_es_aeat_sii_oca: adapt helper method
    
      - Allow for defining the base module from which to import the expected
        output json to allow usage downstream.
      - Return created invoice
    
    TT26766
    
    Co-Authored-By: Pedro M. Baeza <pedro.baeza@tecnativa.com>

--- l10n_es_dua_sii/models/account_move.py
+++ l10n_es_dua_sii/models/account_move.py
@@ -2,26 +2,16 @@
 # Copyright 2017 Comunitea Servicios Tecnológicos S.L.
 # Copyright 2019 Tecnativa - Alexandre Díaz
 # Copyright 2019 Tecnativa - Pedro M. Baeza
+# Copyright 2021 Tecnativa - João Marques
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models, tools
 
 
-class AccountInvoice(models.Model):
-    _inherit = "account.invoice"
+class AccountMove(models.Model):
+    _inherit = "account.move"
 
     sii_dua_invoice = fields.Boolean("SII DUA Invoice", compute="_compute_dua_invoice")
-
-    @api.model
-    @tools.ormcache("company")
-    def _get_dua_fiscal_position(self, company):
-        """ Deprecated, this method will be removed in a later version,
-            use `_get_dua_fiscal_position_id` instead. """
-        return self.env.ref(
-            "l10n_es_dua.%i_fp_dua" % company.id, raise_if_not_found=False
-        ) or self.env["account.fiscal.position"].search(
-            [("name", "=", "Importación con DUA"), ("company_id", "=", company.id),]
-        )
 
     @api.model
     @tools.ormcache("company")
@@ -43,12 +33,12 @@ class AccountInvoice(models.Model):
             .id
         )
 
-    @api.depends("company_id", "fiscal_position_id", "tax_line_ids")
+    @api.depends("company_id", "fiscal_position_id", "invoice_line_ids.tax_ids")
     def _compute_dua_invoice(self):
         for invoice in self:
             taxes = invoice._get_sii_taxes_map(["DUA"])
-            invoice.sii_dua_invoice = invoice.tax_line_ids.filtered(
-                lambda x: x.tax_id in taxes
+            invoice.sii_dua_invoice = invoice.invoice_line_ids.filtered(
+                lambda x: any([tax in taxes for tax in x.tax_ids])
             )
 
     @api.depends("sii_dua_invoice", "fiscal_position_id")
@@ -70,17 +60,14 @@ class AccountInvoice(models.Model):
         """Según la documentación de la AEAT, la operación de importación se
         registra con TipoFactura = F5, sin FechaOperacion y con el NIF de la
         propia compañia en IDEmisorFactura y Contraparte
-        Más información en: 8.1.2.2.Ejemplo mensaje XML de alta de importación
-        en el documento de descripción de los servicios web:
-        http://bit.ly/2rGWiAI
+        Más información en: 8.1.2.2. Ejemplo mensaje XML de alta de importación
+        en el documento de descripción de los servicios web.
         """
         res = super()._get_sii_invoice_dict_in(cancel=cancel)
         if res.get("FacturaRecibida") and self.sii_dua_invoice:
             res["FacturaRecibida"]["TipoFactura"] = "F5"
             res["FacturaRecibida"].pop("FechaOperacion", None)
-            nif = self.company_id.vat
-            if nif.upper().startswith("ES"):
-                nif = nif[2:]
+            nif = self.company_id.partner_id._parse_aeat_vat_info()[2]
             res["FacturaRecibida"]["IDEmisorFactura"] = {"NIF": nif}
             res["IDFactura"]["IDEmisorFactura"] = {"NIF": nif}
             res["FacturaRecibida"]["Contraparte"]["NIF"] = nif
