2f8ca17d266882cf4f284176a0556f117efd80e0
João Marques <joao.marques@tecnativa.com>
2021-02-10 09:49:14 +0000

    [MIG] l10n_es_aeat_sii_oca: Migration to 14.0
    Also apply the part of https://github.com/OCA/l10n-spain/commit/86ff0f8ce82250618061b6660bc0d394439d0c7d that related to this.
    
    TT26438

---
+++ l10n_es_aeat_sii_oca/migrations/14.0.1.0.0/pre-migrate.py
@@ -0,0 +1,29 @@
+# Copyright 2021 Tecnativa - João Marques
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openupgradelib import openupgrade
+from psycopg2 import sql
+
+
+def migrate_l10n_es_aeat_sii(env):
+    """Migrate l10n.es.aeat.sii to l10n.es.aeat.certificate"""
+    openupgrade.logged_query(
+        env.cr,
+        sql.SQL(
+            """
+        INSERT INTO l10n_es_aeat_certificate
+        (name, state, file, date_start, date_end,
+         public_key, private_key, company_id)
+        SELECT
+        name, state, file, date_start, date_end,
+        public_key, private_key, company_id
+        FROM l10n_es_aeat_sii
+        """
+        ),
+    )
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    if openupgrade.table_exists(env.cr, "l10n_es_aeat_sii"):
+        migrate_l10n_es_aeat_sii(env)
