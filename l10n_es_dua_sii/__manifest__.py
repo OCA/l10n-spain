7a2cdd650431fab4a7e52bab1ffa9a9ad37b876d
João Marques <joao.marques@tecnativa.com>
2021-02-19 09:01:17 +0000

    [MIG] l10n_es_dua_sii: Migration to 14.0
    TT26439

--- l10n_es_dua_sii/__manifest__.py
+++ l10n_es_dua_sii/__manifest__.py
@@ -6,7 +6,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Suministro Inmediato de Información de importaciones con DUA",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Accounting & Finance",
     "website": "https://github.com/OCA/l10n-spain",
     "author": "Studio73, " "Comunitea, " "Odoo Community Association (OCA)",
