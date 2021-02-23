31af1491cffeb510c79a15cf24904265ee7b6fa1
Carlos Roca <carlos.roca@tecnativa.com>
2021-02-23 13:14:50 +0100

    [IMP] account_promissory_note_caixabank: black, isort, prettier

--- account_promissory_note_caixabank/report/check_print.py
+++ account_promissory_note_caixabank/report/check_print.py
@@ -4,5 +4,5 @@ from odoo import models
 class ReportPromissoryNotePrintCB(models.AbstractModel):
     # The definition of this abstract model is done because is needed by
     # account_check_printing_report_base for the report to work.
-    _name = 'report.account_promissory_note_caixabank.promissory_footer_cb'
-    _inherit = 'report.account_check_printing_report_base.promissory_footer_a4'
+    _name = "report.account_promissory_note_caixabank.promissory_footer_cb"
+    _inherit = "report.account_check_printing_report_base.promissory_footer_a4"
