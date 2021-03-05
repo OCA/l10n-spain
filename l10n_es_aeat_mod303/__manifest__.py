2b8de708a35c3cbbf466b00d725c73fd486fb5fb
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2021-03-05 21:31:01 +0100

    [FIX] l10n_es_aeat_mod303: Write fees to compensate together
    If not, the constraint is evaluated each time, and can lead to
    inconsistent states.
    
    Fixes #1624

--- l10n_es_aeat_mod303/__manifest__.py
+++ l10n_es_aeat_mod303/__manifest__.py
@@ -8,7 +8,7 @@
 
 {
     "name": "AEAT modelo 303",
-    "version": "10.0.2.5.0",
+    "version": "10.0.2.5.1",
     'category': "Accounting & Finance",
     'author': "Guadaltech,"
               "AvanzOSC,"
