354c5abd7b424fff78706aa65f13b9faf262102b
João Marques <joao.marques@tecnativa.com>
2021-03-08 14:33:46 +0000

    [MIG] l10n_es_intrastat_report: Migration to 14.0

--- l10n_es_intrastat_report/__manifest__.py
+++ l10n_es_intrastat_report/__manifest__.py
@@ -8,19 +8,22 @@
 
 {
     "name": "Intrastat Product Declaration for Spain",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Intrastat",
     "license": "AGPL-3",
     "summary": "Spanish Intrastat Product Declaration",
     "author": "FactorLibre,Noviat,Tecnativa,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-spain",
     "depends": ["intrastat_product", "l10n_es_aeat"],
     "conflicts": ["report_intrastat"],
     "data": [
         "security/l10n_es_intrastat_report_security.xml",
         "security/ir.model.access.csv",
         "data/intrastat_transaction.xml",
+        "data/account_fiscal_position_template.xml",
         "views/l10n_es_intrastat_product.xml",
         "wizards/l10n_es_intrastat_code_import.xml",
     ],
+    "post_init_hook": "post_init_hook",
     "installable": True,
 }
