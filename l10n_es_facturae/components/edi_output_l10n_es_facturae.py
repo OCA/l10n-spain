3e66d92e2b6fcc9a80c527b1e3c9a28eab727c5f
Enric Tobella <etobella@creublanca.es>
2021-03-10 12:28:59 +0100

    [FIX] l10n_es_facturae: Fix components names using the new logic

--- l10n_es_facturae/components/edi_output_l10n_es_facturae.py
+++ l10n_es_facturae/components/edi_output_l10n_es_facturae.py
@@ -8,7 +8,8 @@ from odoo.addons.component.core import Component
 class EdiOutputL10nEsFacturae(Component):
     _name = "edi.output.generate.l10n_es_facturae"
     _inherit = "edi.component.output.mixin"
-    _usage = "edi.output.generate.l10n_es_facturae"
+    _usage = "output.generate"
+    _backend_type = "l10n_es_facturae"
 
     def generate(self):
         return self.exchange_record.record.get_facturae(True)[0]
