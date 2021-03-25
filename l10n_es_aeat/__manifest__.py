5e2cdfa192c96c3e2edf8cd5464b45f37579c20a
valen <valentin.vinagre@sygel.es>
2021-03-25 19:57:23 +0100

    [FIX]l10n_es_aeat: the ._origin field is added to the search for accounts and taxes templates to make it compatible with objects of type NewID.

--- l10n_es_aeat/__manifest__.py
+++ l10n_es_aeat/__manifest__.py
@@ -10,7 +10,7 @@
 {
     "name": "AEAT Base",
     "summary": "Modulo base para declaraciones de la AEAT",
-    "version": "14.0.1.1.1",
+    "version": "14.0.1.1.2",
     "author": "Pexego, "
     "Acysos S.L., "
     "AvanzOSC, "
