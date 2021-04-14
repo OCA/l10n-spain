fc486ac628260c74623395de771ee8aa65fbcf30
ao-landoo <ao@landoo.es>
2021-04-14 11:44:45 +0200

    [FIX] Remove country and add ES to vat in tests

--- l10n_es_ticketbai_api/tests/common.py
+++ l10n_es_ticketbai_api/tests/common.py
@@ -466,8 +466,7 @@ class TestL10nEsTicketBAIAPI(common.TransactionCase):
                 'l10n_es_ticketbai_api.tbai_tax_agency_gipuzkoa').id,
             'currency_id': self.env.ref('base.EUR').id,
             'tbai_certificate_id': certificate.id,
-            'tbai_installation_id': installation.id,
-            'country_id': self.env.ref('base.es').id
+            'tbai_installation_id': installation.id
         })
         company.write(vals)
 
