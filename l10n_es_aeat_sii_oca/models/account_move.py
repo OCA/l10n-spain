5f463b3c940fc00c3611cbb07b3632efcb59dfd0
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2021-05-04 09:53:58 +0200

    [FIX] l10n_es_aeat_sii_oca: Don't assign specific refund type
    Adaptation of cc0e79d1363e61dc05c3d8959f4aac866d8d6b02

--- l10n_es_aeat_sii_oca/models/account_move.py
+++ l10n_es_aeat_sii_oca/models/account_move.py
@@ -1429,8 +1429,6 @@ class AccountMove(models.Model):
                     extra_dict["sii_refund_type"] = sii_refund_type
                 if supplier_invoice_number_refund:
                     extra_dict["ref"] = supplier_invoice_number_refund
-                if move.move_type == "out_invoice":
-                    extra_dict["sii_refund_specific_invoice_type"] = "R1"
                 if extra_dict:
                     default_values.update(extra_dict)
         res = super()._reverse_moves(
