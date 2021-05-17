267707225f75ae5486d03bee563e9253731c6625
manu <manuel.regidor@sygel.es>
2021-05-17 08:52:19 +0200

    [MIG]l10n_es_aeat_mod123: Migration to 14.0

--- l10n_es_aeat_mod123/__manifest__.py
+++ l10n_es_aeat_mod123/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "AEAT modelo 123",
-    "version": "13.0.1.1.0",
+    "version": "14.0.1.0.0",
     "category": "Localisation/Accounting",
     "author": "Tecnativa, "
     "Spanish Localization Team, "
