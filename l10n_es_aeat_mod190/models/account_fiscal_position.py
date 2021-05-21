6632d9d9b0f27d9a891a67d1f6c9d274824b70d1
manu <manuel.regidor@sygel.es>
2021-05-21 10:31:10 +0200

    [IMP] l10n_es_aeat_mod190: black, isort, prettier

--- l10n_es_aeat_mod190/models/account_fiscal_position.py
+++ l10n_es_aeat_mod190/models/account_fiscal_position.py
@@ -5,20 +5,20 @@ from odoo import api, fields, models
 
 
 class AccountFiscalPosition(models.Model):
-    _inherit = 'account.fiscal.position'
+    _inherit = "account.fiscal.position"
 
     aeat_perception_key_id = fields.Many2one(
-        comodel_name='l10n.es.aeat.report.perception.key',
-        string='Clave percepción',
-        oldname='performance_key',
-        help='Se consignará la clave alfabética que corresponda a las '
-             'percepciones de que se trate.',
+        comodel_name="l10n.es.aeat.report.perception.key",
+        string="Clave percepción",
+        oldname="performance_key",
+        help="Se consignará la clave alfabética que corresponda a las "
+        "percepciones de que se trate.",
     )
     aeat_perception_subkey_id = fields.Many2one(
-        comodel_name='l10n.es.aeat.report.perception.subkey',
-        string='Subclave',
-        oldname='subclave',
-        help='''Tratándose de percepciones correspondientes a las claves
+        comodel_name="l10n.es.aeat.report.perception.subkey",
+        string="Subclave",
+        oldname="subclave",
+        help="""Tratándose de percepciones correspondientes a las claves
                 B, E, F, G, H, I, K y L, deberá consignarse, además, la
                 subclave numérica de dos dígitos que corresponda a las
                 percepciones de que se trate, según la relación de
@@ -33,15 +33,19 @@ class AccountFiscalPosition(models.Model):
                 registros de percepción como sea necesario, de forma que
                 cada uno de ellos refleje exclusivamente los datos de
                 percepciones correspondientes a una misma clave y, en
-                su caso, subclave.''',
+                su caso, subclave.""",
     )
 
-    @api.onchange('aeat_perception_key_id')
+    @api.onchange("aeat_perception_key_id")
     def onchange_aeat_perception_key_id(self):
         if self.aeat_perception_key_id:
             self.aeat_perception_subkey_id = False
-            return {'domain': {'aeat_perception_subkey_id': [
-                ('aeat_perception_key_id', '=', self.aeat_perception_key_id.id)
-            ]}}
+            return {
+                "domain": {
+                    "aeat_perception_subkey_id": [
+                        ("aeat_perception_key_id", "=", self.aeat_perception_key_id.id)
+                    ]
+                }
+            }
         else:
-            return {'domain': {'aeat_perception_subkey_id': []}}
+            return {"domain": {"aeat_perception_subkey_id": []}}
