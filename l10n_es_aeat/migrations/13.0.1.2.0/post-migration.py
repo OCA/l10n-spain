a5c6ac287371874c4ef11118e14e7cfef60211cb
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2021-05-24 17:46:57 +0200

    [OU-FIX] l10n_es_aeat: Proper path to import XML

--- l10n_es_aeat/migrations/13.0.1.2.0/post-migration.py
+++ l10n_es_aeat/migrations/13.0.1.2.0/post-migration.py
@@ -7,5 +7,5 @@ from openupgradelib import openupgrade  # pylint: disable=W7936
 @openupgrade.migrate()
 def migrate(env, version):
     openupgrade.load_data(
-        env.cr, "l10n_es_aeat", "migrations/13.0.1.0.0/noupdate_changes.xml"
+        env.cr, "l10n_es_aeat", "migrations/13.0.1.2.0/noupdate_changes.xml"
     )
