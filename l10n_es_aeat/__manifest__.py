3cde512402f234e47dd07ad6c0b91fe9e7675f93
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2021-05-24 17:40:11 +0200

    [OU-ADD + IMP] l10n_es_aeat: Include record rule migration script + adjust rule
    It includes the migration scripts for the module.
    
    As the field is required, no need to check for empty company in the record rule.

--- l10n_es_aeat/__manifest__.py
+++ l10n_es_aeat/__manifest__.py
@@ -10,7 +10,7 @@
 {
     "name": "AEAT Base",
     "summary": "Modulo base para declaraciones de la AEAT",
-    "version": "13.0.1.1.2",
+    "version": "13.0.1.2.0",
     "author": "Pexego, "
     "Acysos S.L., "
     "AvanzOSC, "
