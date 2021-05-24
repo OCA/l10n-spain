3742631973a5f5f0ee9244fe93eb51be2cfc1db6
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2021-05-24 17:40:11 +0200

    [OU-ADD + IMP] l10n_es_aeat: Include record rule migration script + adjust rule
    It includes the migration scripts for the module.
    
    As the field is required, no need to check for empty company in the record rule.

---
+++ l10n_es_aeat/migrations/14.0.1.2.0/post-migration.py
@@ -0,0 +1,11 @@
+# Copyright 2021 Tecnativa - Pedro M. Baeza
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openupgradelib import openupgrade  # pylint: disable=W7936
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.load_data(
+        env.cr, "l10n_es_aeat", "migrations/14.0.1.2.0/noupdate_changes.xml"
+    )
