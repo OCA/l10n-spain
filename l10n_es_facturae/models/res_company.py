fa20bc41184939b1efdbfceb9bd9449203da8dd3
Enric Tobella <etobella@creublanca.es>
2021-05-26 13:00:43 +0200

    [MIG] l10n_es_facturae: migration to 14.0

--- l10n_es_facturae/models/res_company.py
+++ l10n_es_facturae/models/res_company.py
@@ -8,10 +8,6 @@ from odoo import fields, models
 class ResCompany(models.Model):
     _inherit = "res.company"
 
-    facturae_cert = fields.Binary(
-        string="Certificado firma electrónica", filters="*.pfx", attachment=False
-    )
-    facturae_cert_password = fields.Char(string="Contraseña certificado")
     facturae_version = fields.Selection(
         [("3_2", "3.2"), ("3_2_1", "3.2.1"), ("3_2_2", "3.2.2")]
     )
