f9492781bec3b837fb7e1cd27f6f2e6cc477e7c6
Enric Tobella <etobella@creublanca.es>
2021-05-27 17:09:51 +0200

    [IMP] l10n_es_aeat: Fullfill data from the certificate

--- l10n_es_aeat/wizard/aeat_certificate_password.py
+++ l10n_es_aeat/wizard/aeat_certificate_password.py
@@ -16,6 +16,7 @@ _logger = logging.getLogger(__name__)
 
 try:
     import cryptography
+    from cryptography import x509
     from cryptography.hazmat.primitives.serialization import (
         Encoding,
         NoEncryption,
@@ -32,14 +33,11 @@ if tuple(map(int, cryptography.__version__.split("."))) < (3, 0):
 
 
 @contextlib.contextmanager
-def pfx_to_pem(file, pfx_password, directory=None):
-    if isinstance(pfx_password, str):
-        pfx_password = bytes(pfx_password, "utf-8")
+def pfx_to_pem(p12, directory=None):
     with tempfile.NamedTemporaryFile(
         prefix="private_", suffix=".pem", delete=False, dir=directory
     ) as t_pem:
         with open(t_pem.name, "wb") as f_pem:
-            p12 = pkcs12.load_key_and_certificates(file, pfx_password)
             f_pem.write(
                 p12[0].private_bytes(
                     Encoding.PEM,
@@ -52,14 +50,11 @@ def pfx_to_pem(file, pfx_password, directory=None):
 
 
 @contextlib.contextmanager
-def pfx_to_crt(file, pfx_password, directory=None):
-    if isinstance(pfx_password, str):
-        pfx_password = bytes(pfx_password, "utf-8")
+def pfx_to_crt(p12, directory=None):
     with tempfile.NamedTemporaryFile(
         prefix="public_", suffix=".crt", delete=False, dir=directory
     ) as t_crt:
         with open(t_crt.name, "wb") as f_crt:
-            p12 = pkcs12.load_key_and_certificates(file, pfx_password)
             f_crt.write(p12[1].public_bytes(Encoding.PEM))
             f_crt.close()
         yield t_crt.name
@@ -90,11 +85,28 @@ class L10nEsAeatCertificatePassword(models.TransientModel):
         try:
             if directory and not os.path.exists(directory):
                 os.makedirs(directory)
-            with pfx_to_pem(file, self.password, directory) as private_key:
-                record.private_key = private_key
-            with pfx_to_crt(file, self.password, directory) as public_key:
-                record.public_key = public_key
+            pfx_password = self.password
+            if isinstance(pfx_password, str):
+                pfx_password = bytes(pfx_password, "utf-8")
+            p12 = pkcs12.load_key_and_certificates(file, pfx_password)
+            vals = self._process_certificate_vals(record, p12, directory)
+            record.write(vals)
         except Exception as e:
             if e.args:
                 args = list(e.args)
             raise ValidationError(args[-1])
+
+    def _process_certificate_vals(self, record, p12, directory):
+        vals = {}
+        with pfx_to_pem(p12, directory) as private_key:
+            vals["private_key"] = private_key
+        with pfx_to_crt(p12, directory) as public_key:
+            vals["public_key"] = public_key
+        certificate = p12[1]
+        vals["date_start"] = certificate.not_valid_before
+        vals["date_end"] = certificate.not_valid_after
+        if not record.name:
+            name = certificate.subject.get_attributes_for_oid(x509.NameOID.COMMON_NAME)
+            if name:
+                vals["name"] = name[0].value
+        return vals
