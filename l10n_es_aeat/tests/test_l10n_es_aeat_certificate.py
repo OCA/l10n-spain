4b1983d16c97d50ef7a1726a3d139e4eb36cf410
Enric Tobella <etobella@creublanca.es>
2021-05-27 17:09:51 +0200

    [IMP] l10n_es_aeat: Fullfill data from the certificate

--- l10n_es_aeat/tests/test_l10n_es_aeat_certificate.py
+++ l10n_es_aeat/tests/test_l10n_es_aeat_certificate.py
@@ -2,9 +2,15 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
 import base64
+from datetime import datetime, timedelta
+
+from cryptography import x509
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives.serialization import BestAvailableEncryption, pkcs12
+from cryptography.x509 import oid
 
 from odoo import exceptions
-from odoo.modules.module import get_resource_path
 from odoo.tests import common
 
 
@@ -12,18 +18,40 @@ class TestL10nEsAeatCertificateBase(common.SavepointCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.certificate_path = get_resource_path(
-            "l10n_es_aeat",
-            "tests",
-            "cert",
-            "entidadspj_act.p12",
-        )
         cls.certificate_password = b"794613"
-        with open(cls.certificate_path, "rb") as certificate:
-            content = certificate.read()
+        private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
+        public_key = private_key.public_key()
+        builder = x509.CertificateBuilder()
+        cls.certificate_name = "Test Certificate"
+        one_day = timedelta(1, 0, 0)
+        builder = (
+            builder.subject_name(
+                x509.Name(
+                    [x509.NameAttribute(oid.NameOID.COMMON_NAME, cls.certificate_name)]
+                )
+            )
+            .issuer_name(
+                x509.Name(
+                    [
+                        x509.NameAttribute(oid.NameOID.COMMON_NAME, u"cryptography.io"),
+                    ]
+                )
+            )
+            .not_valid_before(datetime.today() - one_day)
+            .not_valid_after(datetime.today() + (one_day * 30))
+            .serial_number(x509.random_serial_number())
+            .public_key(public_key)
+        )
+        certificate = builder.sign(private_key=private_key, algorithm=hashes.SHA256())
+        content = pkcs12.serialize_key_and_certificates(
+            None,
+            private_key,
+            certificate,
+            None,
+            BestAvailableEncryption(cls.certificate_password),
+        )
         cls.sii_cert = cls.env["l10n.es.aeat.certificate"].create(
             {
-                "name": "Test Certificate",
                 "folder": "Test folder",
                 "file": base64.b64encode(content),
             }
@@ -41,6 +69,7 @@ class TestL10nEsAeatCertificateBase(common.SavepointCase):
         self.sii_cert.company_id.write(
             {"name": "ENTIDAD FICTICIO ACTIVO", "vat": "ESJ7102572J"}
         )
+        self.assertEqual(self.certificate_name, self.sii_cert.name)
 
 
 class TestL10nEsAeatCertificate(TestL10nEsAeatCertificateBase):
