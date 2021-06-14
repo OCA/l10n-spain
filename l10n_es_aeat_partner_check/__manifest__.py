945cf4ae57cc668f6b1b4221fac55fd10b64b3c7
Ethan Hildick <ethan@studio73.es>
2021-06-14 10:29:52 +0200

    [12.0][IMP] l10n_es_aeat_partner_check: black, isort, prettier

--- l10n_es_aeat_partner_check/__manifest__.py
+++ l10n_es_aeat_partner_check/__manifest__.py
@@ -2,22 +2,17 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
-    'name': 'AEAT - Comprobación de Calidad de datos identificativos',
-    'author': 'Acysos S.L.,'
-              'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/l10n-spain',
-    'category': 'Accounting',
-    'version': '12.0.1.0.0',
-    'license': 'AGPL-3',
-    'depends': [
-        'base',
-        'base_vat',
-        'l10n_es_aeat'
+    "name": "AEAT - Comprobación de Calidad de datos identificativos",
+    "author": "Acysos S.L.," "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-spain",
+    "category": "Accounting",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["base", "base_vat", "l10n_es_aeat"],
+    "data": [
+        "views/res_partner_view.xml",
+        "views/res_company_view.xml",
+        "views/res_config_settings_view.xml",
     ],
-    'data': [
-        'views/res_partner_view.xml',
-        'views/res_company_view.xml',
-        'views/res_config_settings_view.xml'
-    ],
-    'installable': True,
+    "installable": True,
 }
