5857600c4f73c6b249e65e41fea48f9102436a47
Enrique Martin Zabalza <enriquemartin@digital5.es>
2021-06-18 11:34:36 +0200

    [12.0][FIX] l10n_es_ticketbai - existen impuestos sin description, se opta por usar método get_taxes_from_templates()

--- l10n_es_ticketbai/models/account_tax.py
+++ l10n_es_ticketbai/models/account_tax.py
@@ -1,4 +1,5 @@
 # Copyright 2020 Binovo IT Human Project SL
+# Copyright 2021 Digital5, S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from odoo import models
 
@@ -7,13 +8,13 @@ class AccountTax(models.Model):
     _inherit = 'account.tax'
 
     def tbai_is_subject_to_tax(self):
-        s_iva_ns_descriptions = \
-            self.env.ref('l10n_es_ticketbai.tbai_tax_map_SNS').tax_template_ids.mapped(
-                'description')
-        s_iva_ns_b_descriptions = \
-            self.env.ref('l10n_es_ticketbai.tbai_tax_map_BNS').tax_template_ids.mapped(
-                'description')
-        return self.description not in s_iva_ns_descriptions + s_iva_ns_b_descriptions
+        s_iva_ns_tbai_maps = self.env["tbai.tax.map"].search(
+            [('code', 'in', ("SNS", "BNS"))]
+        )
+        s_iva_ns_taxes = self.company_id.get_taxes_from_templates(
+            s_iva_ns_tbai_maps.mapped("tax_template_ids")
+        )
+        return self not in s_iva_ns_taxes
 
     def tbai_is_tax_exempted(self):
         return self.tax_group_id.id == self.env.ref('l10n_es.tax_group_iva_0').id
