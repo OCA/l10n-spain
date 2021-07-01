e15d1d976ec45e5b82db80c9c3e2d9c1eacb021c
Aritz Olea <ao@landoo.es>
2021-07-01 14:04:52 +0200

    [IMP] l10n_es_ticketbai: black, isort, prettier

--- l10n_es_ticketbai/models/ticketbai_vat_exemption_key.py
+++ l10n_es_ticketbai/models/ticketbai_vat_exemption_key.py
@@ -1,19 +1,19 @@
 # Copyright 2020 Binovo IT Human Project SL
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from odoo import api, models, fields
+from odoo import api, fields, models
 
 
 class TicketBAIVATExemptionKey(models.Model):
-    _name = 'tbai.vat.exemption.key'
-    _description = 'TicketBAI VAT Exemption mapping keys'
+    _name = "tbai.vat.exemption.key"
+    _description = "TicketBAI VAT Exemption mapping keys"
 
-    code = fields.Char(string='Code', required=True)
-    name = fields.Char('Name', required=True)
+    code = fields.Char(string="Code", required=True)
+    name = fields.Char("Name", required=True)
 
     @api.multi
     def name_get(self):
         vals = []
         for record in self:
-            name = '[{}]-{}'.format(record.code, record.name)
+            name = "[{}]-{}".format(record.code, record.name)
             vals.append(tuple([record.id, name]))
         return vals
