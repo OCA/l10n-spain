28a8cdb658e35aec8f0f1b2506f04ae8ba30cff1
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2021-08-19 13:14:03 +0200

    [FIX] l10n_es_aeat_mod303: 2021/07 format: Don't populate values if no return
    This hasn't been detected before, as testing reports didn't have bank
    account populated, but on a new one, it seems AEAT doesn't allow to have
    any content in these fields if the result is not to return.

--- l10n_es_aeat_mod303/__manifest__.py
+++ l10n_es_aeat_mod303/__manifest__.py
@@ -7,7 +7,7 @@
 
 {
     "name": "AEAT modelo 303",
-    "version": "12.0.2.0.2",
+    "version": "12.0.2.1.0",
     "category": "Accounting",
     "author": "Guadaltech,"
               "AvanzOSC,"
