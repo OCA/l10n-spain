4c8912268e6dc065c945fa26ad8947da36964826
Rodrigo <rodrigo.bonilla@factorlibre.com>
2021-08-23 14:48:00 +0200

    [IMP] l10n_es_aeat_mod303_oss: Add field 124 and 127

--- l10n_es_aeat_mod303_oss/models/mod303.py
+++ l10n_es_aeat_mod303_oss/models/mod303.py
@@ -1,4 +1,5 @@
 # Copyright 2021 PESOL - Angel Moya
+# Copyright 2021 FactorLibre - Rodrigo Bonilla <rodrigo.bonilla@factorlibre.com>
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0
 
 from odoo import models
@@ -20,3 +21,32 @@ class L10nEsAeatMod303Report(models.Model):
                 ]
             )
         return super(L10nEsAeatMod303Report, self).get_taxes_from_map(map_line)
+
+    def _get_tax_lines(self, codes, date_start, date_end, map_line):
+        """Don't populate results for fields 126-127 for reports different from
+        last of the year one or when not exonerated of presenting model 390.
+        """
+        res = super(L10nEsAeatMod303Report, self)._get_tax_lines(
+            codes, date_start, date_end, map_line
+        )
+        if 126 <= map_line.field_number <= 127:
+            if (
+                self.exonerated_390 == "2"
+                or not self.has_operation_volume
+                or self.period_type not in ("4T", "12")
+            ):
+                return self.env["account.move.line"]
+        return res
+
+    def _get_move_line_domain(self, codes, date_start, date_end, map_line):
+        """Changes dates to full year when the summary on last report of the
+        year for the corresponding fields. Only field number is checked as
+        the complete check for not bringing results is done on
+        `_get_tax_lines`.
+        """
+        if 126 <= map_line.field_number <= 127:
+            date_start = date_start[:4] + "-01-01"
+            date_end = date_end[:4] + "-12-31"
+        return super(L10nEsAeatMod303Report, self)._get_move_line_domain(
+            codes, date_start, date_end, map_line,
+        )
