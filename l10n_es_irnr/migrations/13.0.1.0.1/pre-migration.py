c721db5fbf064685dcfec07deb5493734019ec21
Víctor Martínez <victor.martinez@tecnativa.com>
2021-09-24 16:11:18 +0200

    [IMP] l10n_es_irnr: black, isort, prettier

--- l10n_es_irnr/migrations/13.0.1.0.1/pre-migration.py
+++ l10n_es_irnr/migrations/13.0.1.0.1/pre-migration.py
@@ -4,7 +4,10 @@ from openupgradelib import openupgrade
 
 xmlid_renames = [
     ("l10n_es_irnr.tax_group_retenciones_24", "l10n_es_irnr.tax_group_irnr_no_ue"),
-    ("l10n_es_irnr.tax_group_retenciones_19_irnr", "l10n_es_irnr.tax_group_irnr_ue",),
+    (
+        "l10n_es_irnr.tax_group_retenciones_19_irnr",
+        "l10n_es_irnr.tax_group_irnr_ue",
+    ),
 ]
 
 
