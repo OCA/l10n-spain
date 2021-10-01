9aaf5070aa454a6fdb8c5105c5de440ad9d96493
Víctor Martínez <victor.martinez@tecnativa.com>
2021-10-01 16:55:24 +0200

    [MIG] l10n_es_aeat_vat_prorrate: Migration to 14.0

--- l10n_es_aeat_vat_prorrate/__manifest__.py
+++ l10n_es_aeat_vat_prorrate/__manifest__.py
@@ -4,13 +4,14 @@
 
 {
     "name": "AEAT - Prorrata de IVA",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
-    "author": "AvanzOSC, " "Tecnativa, " "Odoo Community Association (OCA)",
+    "author": "AvanzOSC, Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-spain",
     "category": "Invoices & Payments",
     "depends": ["l10n_es_aeat_mod303"],
     "data": [
+        "security/ir.model.access.csv",
         "data/tax_code_map_mod303_data.xml",
         "data/aeat_export_mod303_data.xml",
         "wizard/l10n_es_aeat_compute_vat_prorrate_view.xml",
