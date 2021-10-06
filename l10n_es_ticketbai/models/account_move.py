17668f41cd66997cfb947412cf896977b1555018
Aritz Olea <ao@landoo.es>
2021-10-06 17:18:14 +0200

    [IMP] journals can be configured so that they don't send invoices to tax agency

--- l10n_es_ticketbai/models/account_move.py
+++ l10n_es_ticketbai/models/account_move.py
@@ -17,6 +17,7 @@ class AccountMove(models.Model):
     _inherit = "account.move"
 
     tbai_enabled = fields.Boolean(related="company_id.tbai_enabled", readonly=True)
+    tbai_send_invoice = fields.Boolean(related="journal_id.tbai_send_invoice")
     tbai_substitution_invoice_id = fields.Many2one(
         comodel_name="account.move",
         copy=False,
@@ -419,12 +420,13 @@ class AccountMove(models.Model):
         res = super().post()
         tbai_invoices = self.sudo().env["account.move"]
         tbai_invoices |= self.sudo().filtered(
-            lambda x: x.tbai_enabled and "out_invoice" == x.type
+            lambda x: x.tbai_enabled and "out_invoice" == x.type and x.tbai_send_invoice
         )
         refund_invoices = self.sudo().filtered(
             lambda x: x.tbai_enabled
             and "out_refund" == x.type
             and x.tbai_refund_type == RefundType.differences.value
+            and x.tbai_send_invoice
         )
 
         validate_refund_invoices()
