cf8f79f765cb16c7d43575f0de7b90f0c90c5311
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2021-10-31 16:42:30 +0100

    [MIG] l10n_es_aeat_mod303_oss: Migration to 14.0
    - Standard procedure

--- l10n_es_aeat_mod303_oss/__manifest__.py
+++ l10n_es_aeat_mod303_oss/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "AEAT modelo 303 - OSS",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Accounting",
     "author": "PESOL, Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-spain",
