99c55f6af50ad709b29ae85aab26cd66cd43d990
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2021-10-31 11:30:07 +0100

    [MIG+IMP] l10n_es_aeat_mod303_oss: Migration to 13.0 + tests
    - Standard procedure.
    - Add tests for full coverage.
    - Fix compatibility with `l10n_es_aeat_vat_prorrate` due to a problem
      with ormcache and virtual records.

--- l10n_es_aeat_mod303_oss/__manifest__.py
+++ l10n_es_aeat_mod303_oss/__manifest__.py
@@ -1,11 +1,12 @@
 # Copyright 2021 PESOL - Angel Moya
+# Copyright 2021 Tecnativa - Pedro M. Baeza
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0
 
 {
     "name": "AEAT modelo 303 - OSS",
-    "version": "11.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Accounting",
-    "author": "PESOL, Odoo Community Association (OCA)",
+    "author": "PESOL, Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-spain",
     "license": "AGPL-3",
     "depends": ["l10n_es_aeat_mod303", "l10n_eu_oss"],
--- l10n_es_aeat_mod303_oss/models/mod303.py
+++ l10n_es_aeat_mod303_oss/models/mod303.py
@@ -1,5 +1,6 @@
 # Copyright 2021 PESOL - Angel Moya
 # Copyright 2021 FactorLibre - Rodrigo Bonilla <rodrigo.bonilla@factorlibre.com>
+# Copyright 2021 Tecnativa - Pedro M. Baeza
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0
 
 from odoo import models
@@ -20,16 +21,14 @@ class L10nEsAeatMod303Report(models.Model):
                     ("company_id", "=", self.company_id.id),
                 ]
             )
-        return super(L10nEsAeatMod303Report, self).get_taxes_from_map(map_line)
+        return super().get_taxes_from_map(map_line)
 
-    def _get_tax_lines(self, codes, date_start, date_end, map_line):
+    def _get_tax_lines(self, date_start, date_end, map_line):
         """Don't populate results for fields 126-127 for reports different from
         last of the year one or when not exonerated of presenting model 390.
         """
-        res = super(L10nEsAeatMod303Report, self)._get_tax_lines(
-            codes, date_start, date_end, map_line
-        )
-        if 126 <= map_line.field_number <= 127:
+        res = super()._get_tax_lines(date_start, date_end, map_line)
+        if map_line.field_number in (126, 127):
             if (
                 self.exonerated_390 == "2"
                 or not self.has_operation_volume
@@ -38,15 +37,12 @@ class L10nEsAeatMod303Report(models.Model):
                 return self.env["account.move.line"]
         return res
 
-    def _get_move_line_domain(self, codes, date_start, date_end, map_line):
-        """Changes dates to full year when the summary on last report of the
-        year for the corresponding fields. Only field number is checked as
-        the complete check for not bringing results is done on
-        `_get_tax_lines`.
+    def _get_move_line_domain(self, date_start, date_end, map_line):
+        """Change dates to full year when the summary on last report of the year for the
+        corresponding fields. Only field number is checked as the complete check for not
+        bringing results is done on `_get_tax_lines`.
         """
-        if 126 <= map_line.field_number <= 127:
-            date_start = date_start[:4] + "-01-01"
-            date_end = date_end[:4] + "-12-31"
-        return super(L10nEsAeatMod303Report, self)._get_move_line_domain(
-            codes, date_start, date_end, map_line,
-        )
+        if map_line.field_number in (126, 127):
+            date_start = date_start.replace(day=1, month=1)
+            date_end = date_end.replace(day=31, month=12)
+        return super()._get_move_line_domain(date_start, date_end, map_line)
