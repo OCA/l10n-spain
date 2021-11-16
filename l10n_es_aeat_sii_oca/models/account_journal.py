14d3496bbcbba9c7bca54df163bc3fe0da10c41d
Víctor Martínez <victor.martinez@tecnativa.com>
2021-11-16 16:43:30 +0100

    [IMP] l10n_es_aeat_sii_oca: Rename thirdparty fields to allow inheritance in other addons. Hide thirdparty fields in some cases: - Hide thirdparty fields in moves when it is not an invoice. - Hide thirdparty field in journals when it is not sale/purchase type. - Avoid modifying third-party number when an invoice has already been sent to the SII. - Avoid modifying customer invoice name when an invoice has already been sent to the SII. TT32612

---
+++ l10n_es_aeat_sii_oca/models/account_journal.py
@@ -0,0 +1,48 @@
+# Copyright 2021 Tecnativa - Víctor Martínez
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from lxml import etree
+
+from odoo import api, fields, models
+
+
+class AccountFiscalPosition(models.Model):
+    _inherit = "account.journal"
+
+    thirdparty_invoice = fields.Boolean(string="Third-party invoice", copy=False)
+
+    @api.model
+    def fields_view_get(
+        self, view_id=None, view_type="form", toolbar=False, submenu=False
+    ):
+        """Thirdparty fields are added to the form view only if they don't exist
+        previously (l10n_es_facturae addon also has the same field names).
+        """
+        res = super().fields_view_get(
+            view_id=view_id,
+            view_type=view_type,
+            toolbar=toolbar,
+            submenu=submenu,
+        )
+        if view_type == "form":
+            doc = etree.XML(res["arch"])
+            node = doc.xpath("//field[@name='thirdparty_invoice']")
+            if node:
+                return res
+            target = doc.xpath("//field[@name='type'][last()]")
+            if target:
+                node = target[0]
+                attrs = {
+                    "invisible": [("type", "not in", ("sale", "purchase"))],
+                }
+                elem = etree.Element(
+                    "field", {"name": "thirdparty_invoice", "attrs": str(attrs)}
+                )
+                node.addnext(elem)
+            res["arch"] = etree.tostring(doc)
+            xarch, xfields = self.env["ir.ui.view"].postprocess_and_fields(
+                etree.fromstring(res["arch"]), self._name
+            )
+            res["arch"] = xarch
+            res["fields"] = xfields
+        return res
