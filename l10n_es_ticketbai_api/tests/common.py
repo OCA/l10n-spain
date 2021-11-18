032bd41f72939285286425dc29524003c5cb210c
Victor Laskurain <blaskurain@binovo.es>
2021-11-18 10:03:08 +0100

    [REF] l10n_es_ticketbai_api Move XML schema dir to tests and make usage more flexible.
    Move the schema directory into the tests directory because after all
    that is where it is used and also travis prefers it this way.
    
    Make the usage of the catalog a little more flexible as stepping stone
    for related addons.

--- l10n_es_ticketbai_api/tests/common.py
+++ l10n_es_ticketbai_api/tests/common.py
@@ -7,7 +7,10 @@ import json
 import base64
 from random import randrange
 from lxml import etree
+<<<<<<< HEAD
 from urllib import pathname2url
+=======
+>>>>>>> [REF] l10n_es_ticketbai_api Move XML schema dir to tests and make usage more flexible.
 from ..models.ticketbai_invoice import RefundCode, RefundType
 from ..models.ticketbai_invoice_tax import ExemptedCause, NotExemptedType, \
     NotSubjectToCause, VATRegimeKey, SurchargeOrSimplifiedRegimeType
@@ -19,6 +22,12 @@ from odoo.tests import common
 @common.at_install(False)
 @common.post_install(True)
 class TestL10nEsTicketBAIAPI(common.TransactionCase):
+
+    catalogs = [
+        'file:' + (os.path.join(
+            os.path.abspath(
+                os.path.dirname(__file__)), 'schemas/catalog.xml'))
+    ]
 
     def _send_to_tax_agency(self, invoice):
         pending_invoices = self.env['tbai.invoice'].get_next_pending_invoice(
@@ -493,6 +502,11 @@ class TestL10nEsTicketBAIAPI(common.TransactionCase):
 
     def setUp(self):
         super(TestL10nEsTicketBAIAPI, self).setUp()
+        # can only set this environment variable once because lxml
+        # loads it only at startup. Luckily having several catalogs is
+        # supported so we provide the catalogs variable for related
+        # addons to plug any required additional catalog.
+        os.environ['XML_CATALOG_FILES'] = ' '.join(self.catalogs)
         test_dir_path = os.path.abspath(os.path.dirname(__file__))
         self.company_values_json_filepath = os.path.join(test_dir_path, 'company.json')
         # Disabled by default for automatic tests
@@ -501,10 +515,7 @@ class TestL10nEsTicketBAIAPI(common.TransactionCase):
         self.refund_number_prefix = '%d/' % randrange(1, 10 ** 19)
         schemas_version_dirname = XMLSchema.schemas_version_dirname
         script_dirpath = os.path.abspath(os.path.dirname(__file__))
-        schemas_dirpath = os.path.join(script_dirpath, '../ticketbai/schemas')
-        url = pathname2url(os.path.join(schemas_dirpath, 'catalog.xml'))
-        catalog_path = "file:%s" % url
-        os.environ['XML_CATALOG_FILES'] = catalog_path
+        schemas_dirpath = os.path.join(script_dirpath, 'schemas')
         # Load XSD file with XADES imports
         test_xml_invoice_filepath = os.path.abspath(
             os.path.join(schemas_dirpath,
