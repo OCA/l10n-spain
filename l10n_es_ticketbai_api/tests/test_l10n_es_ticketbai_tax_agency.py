ea27cc08ad3137ac522480b3219113ba6f9eb4fd
Victor Laskurain <blaskurain@binovo.es>
2021-11-18 12:03:44 +0100

    [IMP] l10n_es_ticketbai and l10n_es_ticketbai_api: Add missing tests

--- l10n_es_ticketbai_api/tests/test_l10n_es_ticketbai_tax_agency.py
+++ l10n_es_ticketbai_api/tests/test_l10n_es_ticketbai_tax_agency.py
@@ -22,4 +22,11 @@ class TestL10nEsTicketBAITaxAgency(TestL10nEsTicketBAIAPI):
             "l10n_es_ticketbai_api.tbai_tax_agency_araba"
         )
         self.main_company.onchange_tbai_tax_agency()
+        self.assertTrue(self.main_company.tbai_test_available)
+        self.assertTrue(self.main_company.tbai_pro_available)
+        self.main_company.tbai_tax_agency_id.test_qr_base_url = False
+        self.main_company.tbai_tax_agency_id.qr_base_url = ""
+        self.main_company.tbai_tax_agency_id.tbai_pro_available = False
+        self.main_company.onchange_tbai_tax_agency()
+        self.assertFalse(self.main_company.tbai_test_available)
         self.assertFalse(self.main_company.tbai_pro_available)
