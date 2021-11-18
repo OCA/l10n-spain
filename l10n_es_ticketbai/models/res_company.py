74c06751089bd8a7449e25ae8372641ee562427d
Victor Laskurain <blaskurain@binovo.es>
2021-11-18 11:40:17 +0100

    [IMP] l10n_es_ticketbai Allow the user to say that the line detail is confidential
    Gipuzkoa requires that invoices include line details but at the same
    time requires that this details should be obfuscated in case the are
    to be considered confidential for any reason (medical data for
    example).

--- l10n_es_ticketbai/models/res_company.py
+++ l10n_es_ticketbai/models/res_company.py
@@ -12,6 +12,11 @@ class ResCompany(models.Model):
     tbai_aeat_certificate_id = fields.Many2one(
         comodel_name='l10n.es.aeat.certificate', string='AEAT Certificate',
         domain="[('state', '=', 'active'), ('company_id', '=', id)]", copy=False)
+    tbai_protected_data = fields.Boolean('Protected Data', default=False)
+    tbai_protected_data_txt = fields.Text(
+        "Substitution Text",
+        translate=True,
+        default='Information protected by Article 9 Regulation 679/2016')
 
     @api.onchange('tbai_enabled')
     def onchange_tbai_enabled_unset_tbai_aeat_certificate_id(self):
