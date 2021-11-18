96fde7677151a98ea1a3a3d16f651ee6d64ca6e1
Victor Laskurain <blaskurain@binovo.es>
2021-11-18 10:35:25 +0100

    [FIX] l10n_es_ticketbai_api If an invoice substitutes simplified invoice, it is not a refund

--- l10n_es_ticketbai_api/models/ticketbai_invoice.py
+++ l10n_es_ticketbai_api/models/ticketbai_invoice.py
@@ -1013,7 +1013,9 @@ class TicketBAIInvoice(models.Model):
         return res
 
     def build_facturas_rectificadas_sustituidas(self):
-        if self.is_invoice_refund:
+        res = {}
+        if self.is_invoice_refund or \
+                SiNoType.S.value == self.substitutes_simplified_invoice:
             refunds_values = []
             for refund in self.tbai_invoice_refund_ids:
                 vals = OrderedDict()
@@ -1024,8 +1026,6 @@ class TicketBAIInvoice(models.Model):
                 vals["FechaExpedicionFactura"] = refund.expedition_date
                 refunds_values.append(vals)
             res = {"IDFacturaRectificadaSustituida": refunds_values}
-        else:
-            res = {}
         return res
 
     def build_huella_tbai(self):
