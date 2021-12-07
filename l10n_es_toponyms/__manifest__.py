cda0452bb9eefdd105cc6ed5da363ee47008799b
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2021-12-07 12:19:03 +0100

    [MIG] l10n_es_toponyms: Migration to 15.0

--- l10n_es_toponyms/__manifest__.py
+++ l10n_es_toponyms/__manifest__.py
@@ -1,13 +1,13 @@
 # Copyright 2009 Jordi Esteve <jesteve@zikzakmedia.com>
-# Copyright 2013-2017 Pedro M. Baeza
 # Copyright 2019 David Gómez <david.gomez@aselcis.com>
 # Copyright 2020 Jose Luis Algara <osotranquilo@gmail.com>
 # Copyright 2021 Tecnativa - João Marques
+# Copyright 2013-2021 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Topónimos españoles",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "author": "Spanish Localization Team, "
     "ZikZakMedia, "
     "Tecnativa, "
