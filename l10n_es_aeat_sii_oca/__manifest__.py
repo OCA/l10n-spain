c96d1fbd004f6dfac7e22be98f01a778cc0cf258
Valentin Vinagre Urteaga <valentin.vinagre@sygel.es>
2021-12-30 09:13:55 +0100

    [MIG] l10n_es_aeat_sii_oca: Migration to 15.0

--- l10n_es_aeat_sii_oca/__manifest__.py
+++ l10n_es_aeat_sii_oca/__manifest__.py
@@ -14,7 +14,7 @@
 
 {
     "name": "Suministro Inmediato de Información en el IVA",
-    "version": "14.0.1.8.3",
+    "version": "15.0.1.0.0",
     "category": "Accounting & Finance",
     "website": "https://github.com/OCA/l10n-spain",
     "author": "Acysos S.L.,"
@@ -30,7 +30,7 @@
     "license": "AGPL-3",
     "application": False,
     "installable": True,
-    "development_status": "Production/Stable",
+    "development_status": "Mature",
     "maintainers": ["pedrobaeza"],
     "external_dependencies": {"python": ["zeep", "requests", "cryptography"]},
     "depends": ["account_invoice_refund_link", "l10n_es", "l10n_es_aeat", "queue_job"],
