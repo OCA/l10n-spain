22f6c12ba19f4d5e1207ba74e044caccf9497ef9
Valentin Vinagre Urteaga <valentin.vinagre@sygel.es>
2021-12-30 09:22:26 +0100

    [MIG] l10n_es_aeat_mod349: Migration to 15.0

--- l10n_es_aeat_mod349/__manifest__.py
+++ l10n_es_aeat_mod349/__manifest__.py
@@ -9,7 +9,7 @@
 
 {
     "name": "Modelo 349 AEAT",
-    "version": "14.0.1.1.0",
+    "version": "15.0.1.0.0",
     "author": "Tecnativa, Eficent, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Localisation/Accounting",
