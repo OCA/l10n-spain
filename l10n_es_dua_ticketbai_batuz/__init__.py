33814e5d9b0a263782541666bab160c9c82701cb
Aritz Olea <ao@landoo.es>
2022-01-13 14:33:23 +0100

    [ADD] l10n_es_dua_ticketbai_batuz module

---
+++ l10n_es_dua_ticketbai_batuz/__init__.py
@@ -0,0 +1,4 @@
+# Copyright 2022 Landoo Sistemas de Informacion SL
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import models
---
+++ l10n_es_dua_ticketbai_batuz/readme/USAGE.rst
@@ -0,0 +1,2 @@
+Cuando se valida una factura de proveedor se comprueba si se trata de una factura DUA
+y se envía con los datos requeridos por BATUZ.
