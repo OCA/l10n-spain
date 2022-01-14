688b8032f38c6c69142297930647a16622f0eb4a
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2022-01-14 09:11:02 +0100

    [FIX] l10n_es_aeat_mod347: Don't include cancel and draft moves
    Previously, if there are cancel or draft moves with the corresponding
    taxes, they were included in the report.
    
    We should filter them out.
    
    TT33910

--- l10n_es_aeat_mod347/__manifest__.py
+++ l10n_es_aeat_mod347/__manifest__.py
@@ -10,7 +10,7 @@
 
 {
     "name": "AEAT modelo 347",
-    "version": "14.0.1.1.0",
+    "version": "14.0.1.1.1",
     "author": "Tecnativa,PESOL,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-spain",
     "category": "Accounting",
