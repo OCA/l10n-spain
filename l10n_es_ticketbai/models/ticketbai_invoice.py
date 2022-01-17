979dab0281d1c7cadb4a2673bc352c96bf2e73a8
Aritz Olea <ao@landoo.es>
2022-01-17 16:00:48 +0100

    [IMP] l10n_es_ticketbai: refund invoice without having a tbai.invoice assigned

--- l10n_es_ticketbai/models/ticketbai_invoice.py
+++ l10n_es_ticketbai/models/ticketbai_invoice.py
@@ -1,12 +1,15 @@
 # Copyright 2021 Binovo IT Human Project SL
 # Copyright 2021 Landoo Sistemas de Informacion SL
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from odoo import _, exceptions, fields, models
+from datetime import datetime
+
+from odoo import _, api, exceptions, fields, models
 
 from odoo.addons.l10n_es_ticketbai_api.models.ticketbai_invoice import (
     TicketBaiInvoiceState,
 )
 from odoo.addons.l10n_es_ticketbai_api.ticketbai.xml_schema import TicketBaiSchema
+from odoo.addons.l10n_es_ticketbai_api.utils import utils as tbai_utils
 
 
 class TicketBAIInvoice(models.Model):
@@ -52,3 +55,84 @@ class TicketBAIInvoice(models.Model):
                 and record.cancelled_invoice_id
             ):
                 record.cancelled_invoice_id._tbai_invoice_cancel()
+
+
+class TicketBAIInvoiceRefundOrigin(models.Model):
+    _name = "tbai.invoice.refund.origin"
+    _description = "TicketBAI Refunded Invoices Origin Invoice Data"
+
+    account_refund_invoice_id = fields.Many2one(
+        comodel_name="account.move", domain=[("type", "=", "out_refund")], required=True
+    )
+    number = fields.Char(
+        required=True,
+        help="The number of the invoice name e.g. if the invoice name is "
+        "INV/2021/00001 then the number is 00001",
+    )
+    number_prefix = fields.Char(
+        default="",
+        help="Number prefix of this invoice name e.g. if the invoice name is"
+        " INV/2021/00001 then the prefix is INV/2021/, "
+        "ending back slash included",
+    )
+    expedition_date = fields.Char(
+        required=True, help="Expected string date format: %dd-%mm-%yyyy"
+    )
+
+    @api.constrains("number")
+    def _check_number(self):
+        for record in self:
+            if 20 < len(record.number):
+                raise exceptions.ValidationError(
+                    _(
+                        "Refunded Invoice Number %s longer than expected. "
+                        "Should be 20 characters max.!"
+                    )
+                    % record.number
+                )
+
+    @api.constrains("number_prefix")
+    def _check_number_prefix(self):
+        for record in self:
+            if record.number_prefix and 20 < len(record.number_prefix):
+                raise exceptions.ValidationError(
+                    _(
+                        "Refunded Invoice %s Number Prefix %s longer than expected. "
+                        "Should be 20 characters max.!"
+                    )
+                    % (record.number, record.number_prefix)
+                )
+
+    @api.constrains("expedition_date")
+    def _check_expedition_date(self):
+        for record in self:
+            tbai_utils.check_date(
+                _("Refunded Invoice %s Expedition Date") % record.number,
+                record.expedition_date,
+            )
+
+    @api.constrains("number", "number_prefix", "expedition_date")
+    def _check_account_invoice_exists(self):
+        for record in self:
+            invoice_number = ""
+            if record.number_prefix:
+                invoice_number = record.number_prefix
+            if record.number:
+                invoice_number += record.number
+            if invoice_number and record.expedition_date:
+                record._check_expedition_date()
+                invoice_date = datetime.strptime(record.expedition_date, "%d-%m-%Y")
+                date_invoice = invoice_date.date().strftime("%Y-%m-%d")
+                domain_invoice = [
+                    ("name", "=", invoice_number),
+                    ("invoice_date", "=", date_invoice),
+                ]
+                account_invoice = self.sudo().env["account.move"].search(domain_invoice)
+                if account_invoice:
+                    raise exceptions.ValidationError(
+                        _(
+                            "Invoice: number %s prefix %s invoice_date %s exists. "
+                            "Create a credit note from this invoice."
+                        )
+                        % (record.number, record.number_prefix, record.expedition_date)
+                    )
