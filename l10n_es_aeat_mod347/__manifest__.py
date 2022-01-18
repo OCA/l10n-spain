c4461d46d271388c7b98054d599f3008aed79f86
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2022-01-18 20:04:40 +0100

    [FIX] l10n_es_aeat_mod347: Save proper real state fields
    The provided field name was not correct.
    
    Fixes #2001

--- l10n_es_aeat_mod347/__manifest__.py
+++ l10n_es_aeat_mod347/__manifest__.py
@@ -5,12 +5,12 @@
 # Copyright 2016 Tecnativa - Antonio Espinosa
 # Copyright 2016 Tecnativa - Angel Moya <odoo@tecnativa.com>
 # Copyright 2018 PESOL - Angel Moya <info@pesol.es>
-# Copyright 2014-2020 Tecnativa - Pedro M. Baeza
+# Copyright 2014-2022 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "AEAT modelo 347",
-    "version": "13.0.1.3.1",
+    "version": "13.0.1.3.2",
     "author": "Tecnativa,PESOL,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-spain",
     "category": "Accounting",
