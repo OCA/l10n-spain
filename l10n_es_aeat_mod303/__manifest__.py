a7f5a6931021bf4fdecdff70257a846417b5efc0
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2022-01-18 14:51:44 +0100

    [FIX] l10n_es_aeat_mod303: Last BOE export inaccuracies
    - Don't apply sign to "Volumen operaciones"
    - Don't export SWIFT if Spanish or European bank

--- l10n_es_aeat_mod303/__manifest__.py
+++ l10n_es_aeat_mod303/__manifest__.py
@@ -7,7 +7,7 @@
 
 {
     "name": "AEAT modelo 303",
-    "version": "12.0.2.1.0",
+    "version": "12.0.2.2.0",
     "category": "Accounting",
     "author": "Guadaltech,"
               "AvanzOSC,"
