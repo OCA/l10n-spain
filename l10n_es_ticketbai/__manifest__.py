51cecca11ea71097f188cefe6a6e080859349cdb
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2022-01-21 07:47:56 +0100

    [IMP] l10n_es_ticketbai: README by fragments

--- l10n_es_ticketbai/__manifest__.py
+++ l10n_es_ticketbai/__manifest__.py
@@ -2,19 +2,19 @@
 # Copyright 2021 Landoo Sistemas de Informacion SL
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    "name": "TicketBAI - "
-    "declaración de todas las operaciones de venta realizadas por las personas "
-    "y entidades que desarrollan actividades económicas",
+    "name": "TicketBAI",
+    "summary": "Declaración de todas las operaciones de venta realizadas por las "
+    "personas y entidades que desarrollan actividades económicas",
     "version": "13.0.1.6.0",
     "category": "Accounting & Finance",
     "website": "https://github.com/OCA/l10n-spain",
-    "author": "Binovo," "Odoo Community Association (OCA)",
+    "author": "Binovo, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
     "auto_install": False,
     "development_status": "Beta",
-    "maintainers": ["ljsalvatierra-binovo", "ao-landoo"],
+    "maintainers": ["ao-landoo"],
     "depends": [
         "base_vat",
         "l10n_es",
