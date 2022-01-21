31556f47a138d43a43cd7e39156388f8d6b333aa
Enric Tobella <etobella@creublanca.es>
2022-01-21 12:32:25 +0100

    [IMP] l10n_es_aeat_sii_oca: Check if the sii is enabled checking all the companies

--- l10n_es_aeat_sii_oca/models/res_partner.py
+++ l10n_es_aeat_sii_oca/models/res_partner.py
@@ -1,7 +1,7 @@
 # Copyright 2017 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class ResPartner(models.Model):
@@ -16,7 +16,10 @@ class ResPartner(models.Model):
         "sent to SII as simplified invoices.",
     )
 
+    @api.depends("company_id")
     def _compute_sii_enabled(self):
-        sii_enabled = self.env.user.company_id.sii_enabled
+        sii_enabled = any(self.env.companies.mapped("sii_enabled"))
         for partner in self:
-            partner.sii_enabled = sii_enabled
+            partner.sii_enabled = (
+                partner.company_id.sii_enabled if partner.company_id else sii_enabled
+            )
