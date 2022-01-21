390d5aceb73144cf8b9edbbf71a574f838cb122b
Ernesto Tejeda <ernesto.tejeda@tecnativa.com>
2022-01-20 23:03:16 -0500

    [IMP] l10n_es_location_nuts: black, isort, prettier

--- l10n_es_location_nuts/hooks.py
+++ l10n_es_location_nuts/hooks.py
@@ -2,8 +2,9 @@
 # © 2015 Antiun Ingenieria S.L. - Jairo Llopis
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, SUPERUSER_ID
 import logging
+
+from odoo import SUPERUSER_ID, api
 
 _logger = logging.getLogger(__name__)
 
@@ -12,8 +13,10 @@ def post_init_hook(cr, registry):
     """Define Spanish specific configuration in res.country."""
     with api.Environment.manage():
         env = api.Environment(cr, SUPERUSER_ID, {})
-        spain = env.ref('base.es')
-        _logger.info('Setting Spain NUTS configuration')
-        spain.write({
-            'state_level': 4,
-        })
+        spain = env.ref("base.es")
+        _logger.info("Setting Spain NUTS configuration")
+        spain.write(
+            {
+                "state_level": 4,
+            }
+        )
