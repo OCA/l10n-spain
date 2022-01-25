e792368a9d4d366dcf0edaeea29f6025a86e6287
Jordi Ballester <jordi.ballester@forgeflow.com>
2022-01-25 13:55:42 +0100

    [MIG] l10n_es_aeat_mod190: Migration to 14.0

--- l10n_es_aeat_mod190/models/l10n_es_aeat_perception.py
+++ l10n_es_aeat_mod190/models/l10n_es_aeat_perception.py
@@ -25,7 +25,8 @@ class L10nEsAeatReportPerceptionSubkey(models.Model):
     _description = "Perception Subkey"
 
     aeat_perception_key_id = fields.Many2one(
-        comodel_name="l10n.es.aeat.report.perception.key", string="Perception ID",
+        comodel_name="l10n.es.aeat.report.perception.key",
+        string="Perception ID",
     )
     name = fields.Char(string="Name", size=2, required=True)
     aeat_number = fields.Char(string="Model number", size=3, required=True)
