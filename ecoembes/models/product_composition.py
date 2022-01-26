345a226125a883fadf64ddf77b30a89f942472c9
Víctor Martínez <victor.martinez@tecnativa.com>
2022-01-26 14:35:49 +0100

    [IMP] ecoembes: Some changes: - Add ecoembes fields to product template model. - Add product template to compositions. - Create ecoembes menu in General > Settings related to master data. TT34138

--- ecoembes/models/product_composition.py
+++ ecoembes/models/product_composition.py
@@ -1,7 +1,7 @@
 # Copyright 2017 FactorLibre - Janire Olagibel
 # Copyright 2021 Tecnativa - Víctor Martínez
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class ProductComposition(models.Model):
@@ -10,8 +10,9 @@ class ProductComposition(models.Model):
     _order = "default_code asc, order asc"
     _rec_name = "submaterial_id"
 
-    product_id = fields.Many2one(
-        comodel_name="product.product", string="Product", required=True
+    product_id = fields.Many2one(comodel_name="product.product", string="Product",)
+    product_tmpl_id = fields.Many2one(
+        comodel_name="product.template", string="Template",
     )
     default_code = fields.Char(
         related="product_id.default_code", readonly=True, store=True
@@ -38,7 +39,22 @@ class ProductComposition(models.Model):
         res = []
         for item in self:
             item_name = "[{}]{}".format(
-                item.product_id.default_code, item.submaterial_id.name,
+                item.product_id.default_code, item.submaterial_id.name
             )
             res.append((item.id, item_name))
         return res
+
+    def _update_vals_list(self, vals_list):
+        product_model = self.env["product.product"]
+        for vals in vals_list:
+            if vals.get("product_id") and not vals.get("product_tmpl_id"):
+                product = product_model.browse(vals.get("product_id"))
+                vals["product_tmpl_id"] = product.product_tmpl_id.id
+        return vals_list
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        return super().create(self._update_vals_list(vals_list))
+
+    def write(self, vals):
+        return super().write(self._update_vals_list([vals])[0])
