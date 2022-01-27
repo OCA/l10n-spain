71dc8e118d751959cdd2efd9ea9914294fc95f7c
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2022-01-27 18:28:08 +0100

    [FIX] l10n_es_aeat_mod390: Add field 110
    The tax "Extracomunitario (servicios)" should go into this field instead
    of in field 104, as these are the cases for the 104 field:
    
    ```
    - La suma total de las contraprestaciones correspondientes a
    exportaciones y operaciones asimiladas a la exportación o, en su
    defecto, los valores en el interior de las operaciones reseñadas
    (artículos 21 y 22 de la Ley del IVA.).
    - El importe de las bases imponibles correspondientes a las devoluciones
    efectuadas durante el año como consecuencia de las exportaciones
    realizadas en régimen de viajeros.
    - El importe de las operaciones exentas del Impuesto en virtud de lo
    dispuesto en los artículos 23 y 24 de la Ley del IVA.
    - El importe de las prestaciones de servicios relacionadas con la
    importación y cuya contraprestación esté incluida en la base imponible
    de las importaciones.
    - El importe de las prestaciones de servicios exentas realizadas en
    aplicación del régimen especial de las agencias de viajes (artículo 143
    de la Ley del IVA).
    ```
    
    which neither of them fits with this case.
    
    On contrary, field 110 serves for this purpose:
    
    ```
    Se hará constar el importe de las entregas de bienes y prestaciones de
    servicios no sujetas por aplicación de las reglas de localización...
    ```
    
    TT34160

--- l10n_es_aeat_mod390/__manifest__.py
+++ l10n_es_aeat_mod390/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "AEAT modelo 390",
-    "version": "14.0.2.0.0",
+    "version": "14.0.2.1.0",
     "category": "Localisation/Accounting",
     "author": "Tecnativa, " "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-spain",
