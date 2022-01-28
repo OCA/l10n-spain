5fe104571b39da8a2fc3827bbaa73421e9d98c96
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2022-01-28 11:27:48 +0100

    [IMP] l10n_es_aeat_mod390: Remove old BOE export formats
    They are not useful now that has passed a lot of time. If you need to
    repeat a declaration from that years, something worst than the export
    format is happening...
    
    With this, we lighten the module size and speed up the update process.

--- l10n_es_aeat_mod390/__manifest__.py
+++ l10n_es_aeat_mod390/__manifest__.py
@@ -10,26 +10,6 @@
     "license": "AGPL-3",
     "depends": ["l10n_es", "l10n_es_aeat", "account_tax_balance"],
     "data": [
-        # 2017
-        "data/aeat_export_mod390_sub01_data.xml",
-        "data/aeat_export_mod390_sub02_data.xml",
-        "data/aeat_export_mod390_sub03_data.xml",
-        "data/aeat_export_mod390_sub04_data.xml",
-        "data/aeat_export_mod390_sub05_data.xml",
-        "data/aeat_export_mod390_sub06_data.xml",
-        "data/aeat_export_mod390_sub07_data.xml",
-        "data/aeat_export_mod390_sub08_data.xml",
-        "data/aeat_export_mod390_main_data.xml",
-        # 2018
-        "data/aeat_export_mod390_2018_sub01_data.xml",
-        "data/aeat_export_mod390_2018_sub02_data.xml",
-        "data/aeat_export_mod390_2018_sub03_data.xml",
-        "data/aeat_export_mod390_2018_sub04_data.xml",
-        "data/aeat_export_mod390_2018_sub05_data.xml",
-        "data/aeat_export_mod390_2018_sub06_data.xml",
-        "data/aeat_export_mod390_2018_sub07_data.xml",
-        "data/aeat_export_mod390_2018_sub08_data.xml",
-        "data/aeat_export_mod390_2018_main_data.xml",
         # 2019
         "data/aeat_export_mod390_2019_sub01_data.xml",
         "data/aeat_export_mod390_2019_sub02_data.xml",
