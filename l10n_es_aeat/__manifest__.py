bd6a65fda91423f23c79c311447c8ace0ed5586c
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2022-02-04 17:12:54 +0100

    [IMP] l10n_es_aeat: Use dynamic parent for AEAT reports
    As enterprise module `account_accountant` is using another root menu,
    and other kind of hacks are not desirable, making a dynamic search over
    the parent of another well know menu (Customers - menu_finance_receivables),
    makes the trick for having the menu correctly placed in both editions.
    
    Thanks to Martin Trigaux for suggesting this solution.

--- l10n_es_aeat/__manifest__.py
+++ l10n_es_aeat/__manifest__.py
@@ -10,7 +10,7 @@
 {
     "name": "AEAT Base",
     "summary": "Modulo base para declaraciones de la AEAT",
-    "version": "15.0.1.1.0",
+    "version": "15.0.1.2.0",
     "author": "Pexego, "
     "Acysos S.L., "
     "AvanzOSC, "
