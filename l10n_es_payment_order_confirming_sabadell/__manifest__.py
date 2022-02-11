3c484e9c3d2a5b9baca687296fa34bb59fe51137
Víctor Martínez <victor.martinez@tecnativa.com>
2022-02-11 13:25:26 +0100

    [MIG] l10n_es_payment_order_confirming_sabadell: Migration to 14.0 TT33376

---
+++ l10n_es_payment_order_confirming_sabadell/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2016 Soluntec Proyectos y Soluciones TIC. - Rubén Francés
+# Copyright 2016 Soluntec Proyectos y Soluciones TIC. - Nacho Torró
+# Copyright 2015 Tecnativa - Pedro M. Baeza
+# Copyright 2021 Tecnativa - Víctor Martínez
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Exportación de fichero bancario Confirming para Banco Sabadell",
+    "version": "14.0.1.0.0",
+    "author": "Soluntec, Odoo Community Association (OCA), Tecnativa",
+    "category": "Accounting & Finance",
+    "website": "https://github.com/OCA/l10n-spain",
+    "license": "AGPL-3",
+    "depends": ["account_payment_order"],
+    "data": [
+        "data/account_payment_method.xml",
+        "views/account_payment_mode_view.xml",
+    ],
+    "installable": True,
+}
