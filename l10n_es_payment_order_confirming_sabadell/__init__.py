1ea65db60fc418474a18fa065e24b3518576f3a9
Víctor Martínez <victor.martinez@tecnativa.com>
2022-02-11 12:32:53 +0100

    [IMP] l10n_es_payment_order_confirming_sabadell: black, isort, prettier

--- l10n_es_payment_order_confirming_sabadell/__init__.py
+++ l10n_es_payment_order_confirming_sabadell/__init__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # (c) 2016 Soluntec Proyectos y Soluciones TIC. - Rubén Francés , Nacho Torró
 # (c) 2015 Serv. Tecnol. Avanzados - Pedro M. Baeza
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
