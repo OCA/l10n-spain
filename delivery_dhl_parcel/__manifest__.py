abeff0f4e478d601f70e13f8dd29c5ee15066200
Ethan Hildick <ethan@studio73.es>
2022-02-15 16:33:08 +0100

    [14.0][IMP] delivery_dhl_parcel: Minimum 1 kg en envíos DHL
    400 Client Error: Bad Request for url: https://external.dhl.es/cimapi/api/v1/customer/shipment
    El peso del envío tiene que ser como mínimo 1 kilo o como máximo 99999 kilos
    
    delivery_dhl_parcel 14.0.1.2.2

--- delivery_dhl_parcel/__manifest__.py
+++ delivery_dhl_parcel/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Delivery DHL Parcel",
     "summary": "Delivery Carrier implementation for DHL Parcel using their API",
-    "version": "14.0.1.2.1",
+    "version": "14.0.1.2.2",
     "category": "Stock",
     "website": "https://github.com/OCA/l10n-spain",
     "author": "Studio73, Odoo Community Association (OCA)",
