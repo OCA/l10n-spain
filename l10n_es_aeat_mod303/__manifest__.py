5e5b665a9781447e2badd501127941eda38fbb03
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2022-02-17 11:15:08 +0100

    [IMP] l10n_es_aeat_mod303: New BOE format 2022

--- l10n_es_aeat_mod303/__manifest__.py
+++ l10n_es_aeat_mod303/__manifest__.py
@@ -7,7 +7,7 @@
 
 {
     "name": "AEAT modelo 303",
-    "version": "12.0.2.2.0",
+    "version": "12.0.3.0.0",
     "category": "Accounting",
     "author": "Guadaltech,"
               "AvanzOSC,"
@@ -25,6 +25,8 @@
         "data/2021-07/aeat.model.export.config.line.csv",
         "data/2021-07/l10n.es.aeat.map.tax.csv",
         "data/2021-07/l10n.es.aeat.map.tax.line.csv",
+        "data/2022/aeat.model.export.config.csv",
+        "data/2022/aeat.model.export.config.line.csv",
         "data/tax_code_map_mod303_data.xml",
         "data/aeat_export_mod303_data.xml",
         "data/aeat_export_mod303_2017_data.xml",
