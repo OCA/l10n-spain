2e5b9bcecea6a6911658d56273d20a4e7752a6af
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2022-02-17 11:15:08 +0100

    [IMP] l10n_es_aeat_mod303: New BOE format 2022

--- l10n_es_aeat_mod303/__manifest__.py
+++ l10n_es_aeat_mod303/__manifest__.py
@@ -22,6 +22,8 @@
         "data/2021-07/aeat.model.export.config.line.csv",
         "data/2021-07/l10n.es.aeat.map.tax.csv",
         "data/2021-07/l10n.es.aeat.map.tax.line.csv",
+        "data/2022/aeat.model.export.config.csv",
+        "data/2022/aeat.model.export.config.line.csv",
         "data/tax_code_map_mod303_data.xml",
         "data/aeat_export_mod303_data.xml",
         "data/aeat_export_mod303_2017_data.xml",
