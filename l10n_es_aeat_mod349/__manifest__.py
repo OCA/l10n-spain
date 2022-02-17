8b8ca42b1e84d2b77f009ffb653728b8aaae2f05
Rafael Blasco <rafaelbn@users.noreply.github.com>
2022-02-17 23:25:06 +0100

    Set the standar name for all AEAT modules
    Hello,
    
    All the AEAT modules have a name like:
    
    AEAT modelo 111
    AEAT modelo 115
    AEAT modelo 123
    AEAT modelo 303
    AEAT modelo 347
    Modelo 349 AEAT
    AEAT modelo 390
    
    This change just estandarize the name of this module.
    
    Thanks

--- l10n_es_aeat_mod349/__manifest__.py
+++ l10n_es_aeat_mod349/__manifest__.py
@@ -8,7 +8,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    "name": "Modelo 349 AEAT",
+    "name": "AEAT modelo 349",
     "version": "14.0.1.1.1",
     "author": "Tecnativa, Eficent, Odoo Community Association (OCA)",
     "license": "AGPL-3",
