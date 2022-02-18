104339824c6e54a26d11a281f27c6643e8a8bb24
Rafael Blasco <rafaelbn@users.noreply.github.com>
2022-02-18 02:47:24 +0100

    [15.0] Set the standar name for all AEAT modules
    The same detail from here: https://github.com/OCA/l10n-spain/pull/2129#issue-1141983559
    
    Just same name as other AEAT modules

--- l10n_es_aeat_mod349/__manifest__.py
+++ l10n_es_aeat_mod349/__manifest__.py
@@ -8,7 +8,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    "name": "Modelo 349 AEAT",
+    "name": "AEAT modelo 349",
     "version": "15.0.1.0.1",
     "author": "Tecnativa, Eficent, Odoo Community Association (OCA)",
     "license": "AGPL-3",
