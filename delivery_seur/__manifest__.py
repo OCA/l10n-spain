a0bc82dbdb9dc0faf0aef66c7e770515a29ae237
david <david.vidal@tecnativa.com>
2022-02-18 10:33:37 +0100

    [FIX] delivery_seur: label filename
    In v12, with the parameter datas_fname we could force the file to be
    downloaded as such filename. Now that parameter doesn't exist and the
    filename format is lost from the previous version.
    
    odoo/odoo@c212cfe8992292f57acacd809de26e1817533095
    
    This is specilly problematic with txt format and some printer which
    doesnt support joliet filenames or even files without extension.
    
    The mimetype for text files was also wrong, as 'application/txt' doesn't
    exist. See Mozilla Dev docs for more info on this:
    
    https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Common_types
    
    TT34599
    
    delivery_seur 13.0.1.1.2

--- delivery_seur/__manifest__.py
+++ delivery_seur/__manifest__.py
@@ -8,7 +8,7 @@
     "website": "https://github.com/OCA/l10n-spain",
     "license": "AGPL-3",
     "category": "Delivery",
-    "version": "13.0.1.1.1",
+    "version": "13.0.1.1.2",
     "depends": [
         "delivery",
         "delivery_package_number",
