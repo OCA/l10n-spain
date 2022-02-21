b73b3d4061fbaff9d7596903d10b1cd2a19e128e
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2022-02-21 08:46:34 +0100

    [FIX] l10n_es_aeat_mod303: Marca SEPA flag should be w/o sign
    TT34690

--- l10n_es_aeat_mod303/__openerp__.py
+++ l10n_es_aeat_mod303/__openerp__.py
@@ -6,7 +6,7 @@
 
 {
     "name": "AEAT modelo 303",
-    "version": "8.0.3.0.0",
+    "version": "8.0.3.1.0",
     'category': "Accounting & Finance",
     'author': "Guadaltech,"
               "AvanzOSC,"
