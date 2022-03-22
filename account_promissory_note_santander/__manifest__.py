5b030d95ab8f55055cc2cf6f66a48c6164b8e260
ChristianSantamaria <christiancameselle@hotmail.com>
2022-03-22 08:45:05 +0100

    [MIG] account_promissory_note_santander: Migration to 13.0

--- account_promissory_note_santander/__manifest__.py
+++ account_promissory_note_santander/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Impresión de pagaré del Santander en A4",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "license": "AGPL-3",
     "author": "Tecnativa," "Obertix," "Odoo Community Association (OCA)",
     "maintainers": ["cubells"],
