d157f6c67a719ad0dc86895b1f52a6f7322fd4ff
Enric Tobella <etobella@creublanca.es>
2022-03-23 13:41:16 +0100

    [IMP] l10n_es_facturae_face: Remove unnecessary listener, passed as template

--- l10n_es_facturae_face/components/__init__.py
+++ l10n_es_facturae_face/components/__init__.py
@@ -5,4 +5,3 @@ from . import account_move_l10n_es_facturae_face_listener
 from . import edi_webservice_receive_face_l10n_es_facturae_face_update
 from . import edi_input_process_l10n_es_facturae_l10n_es_facturae_face_update
 from . import edi_send_l10n_es_facturae_face_cancel
-from . import edi_output_l10n_es_facturae
