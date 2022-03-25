dd5b4a9d39236a8bda5c77f1780523a9efdf3a5c
Miquel Raïch <miquel.raich@forgeflow.com>
2022-03-25 11:09:46 +0100

    [IMP] l10n_es_facturae_sale_stock: black, isort, prettier

--- l10n_es_facturae_sale_stock/__manifest__.py
+++ l10n_es_facturae_sale_stock/__manifest__.py
@@ -5,9 +5,7 @@
 {
     "name": "Entregas en Factura-e",
     "version": "11.0.1.0.0",
-    "author": "Creu Blanca, "
-              "NuoBiT Solutions, "
-              "Odoo Community Association (OCA)",
+    "author": "Creu Blanca, " "NuoBiT Solutions, " "Odoo Community Association (OCA)",
     "category": "Accounting & Finance",
     "website": "https://github.com/OCA/l10n-spain",
     "license": "AGPL-3",
@@ -19,5 +17,5 @@
         "views/report_facturae.xml",
     ],
     "installable": True,
-    "auto_install": False
+    "auto_install": False,
 }
