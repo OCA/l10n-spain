c7125d41677596cf4503b11c44474293273e2cb8
Miquel Raïch <miquel.raich@forgeflow.com>
2022-03-25 11:09:46 +0100

    [IMP] l10n_es_facturae_sale_stock: black, isort, prettier

--- l10n_es_facturae_sale_stock/models/account_invoice_line.py
+++ l10n_es_facturae_sale_stock/models/account_invoice_line.py
@@ -2,12 +2,11 @@
 # Copyright 2020 NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class AccountInvoiceLine(models.Model):
     _inherit = "account.invoice.line"
 
     def get_datetime(self, dt):
-        return fields.Datetime.context_timestamp(
-            self, fields.Datetime.from_string(dt))
+        return fields.Datetime.context_timestamp(self, fields.Datetime.from_string(dt))
