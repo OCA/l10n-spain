09731b69a51dbc3dbf32c1c905462b54d64d1689
Miquel Raïch <miquel.raich@forgeflow.com>
2022-03-25 11:31:20 +0100

    [MIG] l10n_es_facturae_sale_stock: Migration to 14.0

--- l10n_es_facturae_sale_stock/models/__init__.py
+++ l10n_es_facturae_sale_stock/models/__init__.py
@@ -1,5 +1,3 @@
-# Copyright 2017 Creu Blanca
-# Copyright 2020 NuoBiT Solutions - Eric Antones <eantones@nuobit.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from . import account_invoice_line
+from . import account_move_line
