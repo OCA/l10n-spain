382cccfde99582a61936422baa19938a2c28680b
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2022-04-02 00:37:06 +0200

    [FIX] l10n_es_aeat_mod303: Avoid config name clash

--- l10n_es_aeat_mod303/__manifest__.py
+++ l10n_es_aeat_mod303/__manifest__.py
@@ -7,7 +7,7 @@
 
 {
     "name": "AEAT modelo 303",
-    "version": "12.0.3.1.0",
+    "version": "12.0.3.2.0",
     "category": "Accounting",
     "author": "Guadaltech,"
               "AvanzOSC,"
