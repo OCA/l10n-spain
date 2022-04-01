0f895f90bd80377548ccb89f76c45d35567a2daa
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2022-04-02 00:37:06 +0200

    [FIX] l10n_es_aeat_mod303: Avoid config name clash

--- l10n_es_aeat_mod303/__openerp__.py
+++ l10n_es_aeat_mod303/__openerp__.py
@@ -6,7 +6,7 @@
 
 {
     "name": "AEAT modelo 303",
-    "version": "8.0.3.2.0",
+    "version": "8.0.3.3.0",
     'category': "Accounting & Finance",
     'author': "Guadaltech,"
               "AvanzOSC,"
