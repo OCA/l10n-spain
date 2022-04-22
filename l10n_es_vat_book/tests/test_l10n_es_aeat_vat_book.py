OCA/l10n-spain

commit 784b25992c54f1d4f0941cb353ea9de66a5ce97c

[FIX] l10n_es_vat_book: Filter properly accounts
No se estaba filtrando por cuenta correctamente y las cuotas de los
impuestos de adquisiciones intracomunitarias salían a 0.

l10n_es_vat_book/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     "name": "Libro de IVA",
-    "version": "13.0.1.4.4",
+    "version": "13.0.1.4.5",
     "author": "PRAXYA, ForgeFlow, Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-spain",
     "license": "AGPL-3",

l10n_es_vat_book/models/l10n_es_vat_book.py
@@ -415,12 +415,14 @@ class L10nEsVatBook(models.Model):
                     taxes |= map_line.get_taxes(rec)
                 # Searches for all possible usable lines to report
                 moves = self._get_account_move_lines()
-                account = rec.get_account_from_template(map_line.tax_account_id)
-                # Filters in all possible data using, sets for improving performance
-                if account:
+                accounts = self.env["account.account"]
+                for account in map_lines.mapped("tax_account_id"):
+                    accounts |= rec.get_account_from_template(account)
+                # Filter in all possible data using sets for improving performance
+                if accounts:
                     lines = moves.filtered(
                         lambda line: line.tax_ids & taxes
-                        or (line.tax_line_id in taxes and line.account_id == account)
+                        or (line.tax_line_id in taxes and line.account_id in accounts)
                     )
                 else:
                     lines = moves.filtered(

commit aa9bdfbf2934bbca4afc6da052445890486434a4

[IMP] l10n_es_vat_book: Include test for P_IVA21_IC_BC
For avoiding regressions after 784b25992c54f1d4f0941cb353ea9de66a5ce97c

l10n_es_vat_book/tests/test_l10n_es_aeat_vat_book.py
@@ -23,6 +23,7 @@ class TestL10nEsAeatVatBook(TestL10nEsAeatModBase):
         # tax code: (base, tax_amount)
         "P_IVA21_SC": (230, 48.3),
         "P_IVA0_ND": (100, 21),
+        "P_IVA21_IC_BC": (200, 42),
     }
 
     def test_model_vat_book(self):
@@ -69,13 +70,17 @@ class TestL10nEsAeatVatBook(TestL10nEsAeatModBase):
             self.assertEqual(line.base_amount, 0.0)
             self.assertEqual(line.tax_amount, 0.0)
         # Check tax summary for received invoices
-        self.assertEqual(len(vat_book.received_tax_summary_ids), 2)
+        self.assertEqual(len(vat_book.received_tax_summary_ids), 3)
         # P_IVA21_SC - 21% IVA soportado (servicios corrientes)
         line = vat_book.received_tax_summary_ids[0]
         self.assertAlmostEqual(line.base_amount, 230)
         self.assertAlmostEqual(line.tax_amount, 48.3)
-        # P_IVA0_ND - 21% IVA Soportado no deducible
+        # P_IVA21_IC_BC - IVA 21% Adquisición Intracomunitaria. Bienes corrientes
         line = vat_book.received_tax_summary_ids[1]
+        self.assertAlmostEqual(line.base_amount, 200)
+        self.assertAlmostEqual(line.tax_amount, 42)
+        # P_IVA0_ND - 21% IVA Soportado no deducible
+        line = vat_book.received_tax_summary_ids[2]
         self.assertAlmostEqual(line.base_amount, 100)
         self.assertAlmostEqual(line.tax_amount, 21)
         # Print to PDF