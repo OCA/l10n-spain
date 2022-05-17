1fc2a6aaf4ef6c067a788d4b9c36c17532108e77
JesusVMayor <jesus@comunitea.com>
2022-05-17 12:18:10 +0200

    [FIX]l10n_es_dua_ticketbai_batuz: Se buscan los impuestos de la compañía de la factura en vez de los del usuario.

--- l10n_es_dua_ticketbai_batuz/models/account_invoice.py
+++ l10n_es_dua_ticketbai_batuz/models/account_invoice.py
@@ -26,7 +26,7 @@ class AccountInvoice(models.Model):
             tbai_dua_map = self.env['tbai.tax.map'].search(
                 [('code', '=', 'DUA')]
             )
-            dua_taxes = self.env['l10n.es.aeat.report'].get_taxes_from_templates(
+            dua_taxes = invoice.company_id.get_taxes_from_templates(
                 tbai_dua_map.mapped("tax_template_ids")
             )
             invoice.tbai_dua_invoice = (
