7c26b5451bb768f2ab85de4654a006cac07bd681
Aritz Olea <ao@landoo.es>
2022-05-17 10:39:28 +0200

    [MIG] l10n_es_ticketbai_pos: Migration to 12.0

--- l10n_es_ticketbai_pos/static/src/js/widgets.js
+++ l10n_es_ticketbai_pos/static/src/js/widgets.js
@@ -1,4 +1,5 @@
 /* Copyright 2021 Binovo IT Human Project SL
+   Copyright 2022 Landoo Sistemas de Informacion SL
    License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 */
 
@@ -9,8 +10,8 @@ odoo.define('l10n_es_ticketbai_pos.widgets', function (require) {
     var tbai_models = require('l10n_es_ticketbai_pos.tbai_models');
 
     widgets.OrderListScreenWidget.include({
-        _prepare_order_from_order_data: function (order_data) {
-            var order = this._super(order_data);
+        _prepare_order_from_order_data: function (order_data, action) {
+            var order = this._super(order_data, action);
             if (this.pos.company.tbai_enabled) {
                 order.tbai_simplified_invoice = new tbai_models.TicketBAISimplifiedInvoice({}, {
                     pos: this.pos,
