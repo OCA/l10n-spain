a30597366def385ae492ba65fb1705ffd46dce5e
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2022-05-29 14:13:52 +0200

    [OU-IMP] l10n_es_aeat_sii_oca: Pre-create thirdparty_invoice column

--- l10n_es_aeat_sii_oca/migrations/13.0.1.0.0/pre-migration.py
+++ l10n_es_aeat_sii_oca/migrations/13.0.1.0.0/pre-migration.py
@@ -5,7 +5,17 @@ from openupgradelib import openupgrade
 
 @openupgrade.migrate()
 def migrate(env, version):
-    # Pre-create column for avoiding triggering the compute method
+    # Pre-create columns for avoiding triggering the compute method
     openupgrade.logged_query(
         env.cr, "ALTER TABLE account_move ADD sii_description text"
+    )
+    openupgrade.add_fields(
+        env,
+        "thirdparty_invoice",
+        "account.move",
+        "account_move",
+        "boolean",
+        False,
+        "l10n_es_aeat_sii_oca",
+        False,
     )
