9940bdc9b33eaa645076706db22b51f8895dd2cd
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2022-06-02 18:06:09 +0200

    [IMP] l10n_es_aeat: Avoid deprecated class in tests

--- l10n_es_aeat/__manifest__.py
+++ l10n_es_aeat/__manifest__.py
@@ -10,7 +10,7 @@
 {
     "name": "AEAT Base",
     "summary": "Modulo base para declaraciones de la AEAT",
-    "version": "15.0.2.0.2",
+    "version": "15.0.2.0.3",
     "author": "Pexego, "
     "Acysos S.L., "
     "AvanzOSC, "
