66cb45f1a2fa5d4f61243c78372e29a219cb55b4
Víctor Martínez <victor.martinez@tecnativa.com>
2022-06-07 10:25:16 +0200

    [IMP] delivery_dhl_parcel: Add all customer accounts check option to end day wizard. TT37046

--- delivery_dhl_parcel/wizard/dhl_parcel_end_day_wizard.py
+++ delivery_dhl_parcel/wizard/dhl_parcel_end_day_wizard.py
@@ -1,4 +1,5 @@
 # Copyright 2021 Studio73 - Ethan Hildick <ethan@studio73.es>
+# Copyright 2022 Tecnativa - Víctor Martínez
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 from odoo import fields, models
 
@@ -17,6 +18,7 @@ class DhlParcelEndDayWizard(models.TransientModel):
             "You can also use 'ALL' to end all of them"
         ),
     )
+    all_customer_accounts = fields.Boolean(string="All customer accounts")
     carrier_id = fields.Many2one(
         string="DHL Parcel Service",
         comodel_name="delivery.carrier",
@@ -26,7 +28,10 @@ class DhlParcelEndDayWizard(models.TransientModel):
 
     def button_end_day(self):
         dhl_parcel_request = DhlParcelRequest(self.carrier_id)
-        res = dhl_parcel_request.end_day(self.customer_accounts, "PDF")
+        customer_accounts = (
+            "ALL" if self.all_customer_accounts else self.customer_accounts
+        )
+        res = dhl_parcel_request.end_day(customer_accounts, "PDF")
         self.carrier_id.write(
             {
                 "dhl_parcel_last_end_day_report": (res.get("Report", False)),
