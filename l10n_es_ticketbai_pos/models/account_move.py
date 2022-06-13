8cc5121dc7b979d2e70a54d45b51ccd23d7293c9
Aritz Olea <ao@landoo.es>
2022-06-13 09:18:00 +0200

    [MIG] l10n_es_ticketbai_pos: Migration to 13.0

--- l10n_es_ticketbai_pos/models/account_move.py
+++ l10n_es_ticketbai_pos/models/account_move.py
@@ -1,10 +1,11 @@
 # Copyright 2021 Binovo IT Human Project SL
+# Copyright 2022 Landoo Sistemas de Informacion SL
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from odoo import fields, models
 
 
-class AccountInvoice(models.Model):
-    _inherit = "account.invoice"
+class AccountMove(models.Model):
+    _inherit = "account.move"
 
     tbai_substitution_pos_order_id = fields.Many2one(
         comodel_name="pos.order",
@@ -14,6 +15,12 @@ class AccountInvoice(models.Model):
 
     def tbai_prepare_invoice_values(self):
         res = super().tbai_prepare_invoice_values()
+        if not res.get("tbai.vat.regime.key", False):
+            res["vat_regime_key"] = (
+                self.env["tbai.vat.regime.key"]
+                .search([("code", "=", "01")], limit=1)
+                .code
+            )
         if self.tbai_substitute_simplified_invoice:
             refunded_pos_order = self.tbai_substitution_pos_order_id
             res.update(
@@ -24,9 +31,15 @@ class AccountInvoice(models.Model):
                             0,
                             0,
                             {
-                                "number_prefix": refunded_pos_order.tbai_get_value_serie_factura(),
-                                "number": refunded_pos_order.tbai_get_value_num_factura(),
-                                "expedition_date": refunded_pos_order.tbai_get_value_fecha_expedicion_factura(),
+                                "number_prefix": (
+                                    refunded_pos_order.tbai_get_value_serie_factura()
+                                ),
+                                "number": (
+                                    refunded_pos_order.tbai_get_value_num_factura()
+                                ),
+                                "expedition_date": (
+                                    refunded_pos_order.tbai_get_value_fecha_expedicion_factura()
+                                ),
                             },
                         )
                     ],
