6f005c0ac63f984d2c846b026042bb79f167f534
manu <manuel.regidor@sygel.es>
2022-06-17 14:40:38 +0200

    [MIG]l10n_es_aeat_mod390_oss: Migration to 15.0

--- l10n_es_aeat_mod390_oss/__manifest__.py
+++ l10n_es_aeat_mod390_oss/__manifest__.py
@@ -3,12 +3,12 @@
 
 {
     "name": "AEAT modelo 390 - OSS",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "Accounting",
     "author": "Sygel, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-spain",
     "license": "AGPL-3",
-    "depends": ["l10n_es_aeat_mod390", "l10n_eu_oss"],
+    "depends": ["l10n_es_aeat_mod390", "l10n_eu_oss_oca"],
     "data": ["data/tax_code_map_mod390_data.xml"],
     "installable": True,
     "autoinstall": True,
