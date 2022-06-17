3252fb03dead5122919b490528f433b32f354009
manu <manuel.regidor@sygel.es>
2022-06-17 11:59:13 +0200

    [MIG]l10n_es_aeat_mod390_oss: Migration to 14.0

--- l10n_es_aeat_mod390_oss/__manifest__.py
+++ l10n_es_aeat_mod390_oss/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "AEAT modelo 390 - OSS",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Accounting",
     "author": "Sygel, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-spain",
