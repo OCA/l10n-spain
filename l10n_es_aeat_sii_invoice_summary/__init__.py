18a8227dcdb8f5c3edfca75ee20aec90b0eeacab
David Ramia <david.ramia@processcontrol.es>
2022-07-05 19:52:34 +0200

    [IMP] l10n_es_aeat_sii_invoice_summary: añadir rectificativas y tests

--- l10n_es_aeat_sii_invoice_summary/__init__.py
+++ l10n_es_aeat_sii_invoice_summary/__init__.py
@@ -1,4 +1,3 @@
 # © 2017 FactorLibre - Hugo Santos <hugo.santos@factorlibre.com>
-# © 2022 ProcessControl - David Ramia <david.ramia@processcontrol.es>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from . import models
