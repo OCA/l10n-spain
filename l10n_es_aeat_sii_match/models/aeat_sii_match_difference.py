c0b19134b958b6f429ad07431a70da8733b66307
Javier Iniesta <javier.iniesta@factorlibre.com>
2022-07-18 09:26:05 +0200

    [MIG][11.0] l10n_es_aeat_sii_match

---
+++ l10n_es_aeat_sii_match/models/aeat_sii_match_difference.py
@@ -0,0 +1,28 @@
+# Copyright 2018 Studio73 - Abraham Anes
+# Copyright 2019 Studio73 - Pablo Fuentes
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class SiiMatchDifferences(models.Model):
+    _name = "l10n.es.aeat.sii.match.difference"
+
+    invoice_id = fields.Many2one(
+        string="Related invoice", comodel_name="account.invoice"
+    )
+    report_id = fields.Many2one(
+        string="Related SII match report", comodel_name="l10n.es.aeat.sii.match.result"
+    )
+    sii_field = fields.Char(
+        string="SII field name",
+        copy=False,
+    )
+    sii_return_field_value = fields.Char(
+        string="SII return field value",
+        copy=False,
+    )
+    sii_sent_field_value = fields.Char(
+        string="SII sent field value",
+        copy=False,
+    )
