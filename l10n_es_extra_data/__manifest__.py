7a8dc25ee25491238a9e104f2c0c65583a19415e
cubells <vicent@vcubells.net>
2022-07-20 00:20:03 +0200

    [ADD][12.0] l10n_es_extra_data: new module

---
+++ l10n_es_extra_data/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright (C) 2022 Trey, Kilobytes de Soluciones - Vicent Cubells
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Datos Extra",
+    "summary": "Datos extra de la localización española",
+    "author": "Trey (www.trey.es), Odoo Community Association (OCA)",
+    "maintainers": [
+        "cubells",
+    ],
+    "website": "https://github.com/OCA/l10n-spain",
+    "license": "AGPL-3",
+    "category": "Accounting & Finance",
+    "version": "12.0.1.0.0",
+    "depends": [
+        "l10n_es",
+    ],
+    "data": [
+        "data/account_data.xml",
+        "data/account_tax_data.xml",
+    ],
+}
