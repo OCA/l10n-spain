e6e7fef3a128bf69779f6a58f1d23ecad631eefa
cubells <vicent@vcubells.net>
2022-07-21 23:50:19 +0200

    [ADD][12.0] l10n_es_aeat_sii_extra_data: new module

---
+++ l10n_es_aeat_sii_extra_data/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright (C) 2022 Trey, Kilobytes de Soluciones - Vicent Cubells
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Datos extra para el SII",
+    "summary": "Módulo para incluir los impuestos extra en el módulo del SII.",
+    "author": "Trey (www.trey.es), Odoo Community Association (OCA)",
+    "maintainers": [
+        "cubells",
+    ],
+    "website": "https://github.com/OCA/l10n-spain",
+    "license": "AGPL-3",
+    "category": "Accounting & Finance",
+    "version": "12.0.1.0.0",
+    "depends": [
+        "l10n_es_aeat_sii",
+        "l10n_es_extra_data",
+    ],
+    "data": [
+        "data/aeat_sii_map_data.xml",
+    ],
+}
