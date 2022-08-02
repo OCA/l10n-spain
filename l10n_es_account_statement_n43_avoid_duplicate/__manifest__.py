bf1993317f6bb1697dd53199c2315bf95f985825
jinye <jinye@domatix.com>
2022-08-02 10:57:01 +0200

    l10n_es_account_statement_n43_avoid_duplicate: new module
    setup missed files
    
    l10n_es_account_statement_n43_avoid_duplicate: new module
    
    setup missed files

---
+++ l10n_es_account_statement_n43_avoid_duplicate/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2016 Comunitea - Omar Castiñeira
+# Copyright 2013-2021 Tecnativa - Pedro M. Baeza
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Account Statement Avoid Duplicated Line",
+    "author": "Domatix," "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-spain",
+    "version": "14.0.1.0.1",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": ["l10n_es_account_statement_import_n43"],
+    "data": [
+        "views/account_bank_statement_views.xml",
+    ],
+}
