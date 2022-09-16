e04965b480eaa740bc3848016be5eb9a2f4467a2
Miquel Alzanillas <malzanillas@apsl.net>
2022-09-16 12:36:16 +0200

    [FIX] l10n_es_ticketbai_pos: Set pos origin invoice in pos invoice refunds

--- l10n_es_ticketbai_pos/tests/test_l10n_es_ticketbai_pos.py
+++ l10n_es_ticketbai_pos/tests/test_l10n_es_ticketbai_pos.py
@@ -72,6 +72,22 @@ class TestL10nEsTicketBAIPoSOrder(TestL10nEsTicketBAIPoSCommon):
         self.assertTrue(pos_order.account_move.tbai_substitute_simplified_invoice)
         self.assertEqual("pending", pos_order.account_move.tbai_invoice_id.state)
 
+    def test_create_refund_invoice_from_pos_order(self):
+        self.pos_config.open_session_cb()
+        pos_order = self.create_pos_order(self.account_billing.id)
+        self.assertFalse(pos_order.tbai_invoice_id)
+        pos_order.account_move._post(True)
+        pos_order.sudo()._tbai_build_invoice()
+        self.assertEqual("paid", pos_order.state)
+        self.assertEqual("pending", pos_order.tbai_invoice_id.state)
+        pos_order.sudo().with_context(
+            with_company=pos_order.company_id.id
+        ).action_pos_order_invoice()
+        self.assertEqual("invoiced", pos_order.state)
+        self.assertEqual("posted", pos_order.account_move.state)
+        self.assertTrue(pos_order.account_move.tbai_substitute_simplified_invoice)
+        self.assertEqual("pending", pos_order.account_move.tbai_invoice_id.state)
+
     def test_open_session_error_seq(self):
         with self.assertRaises(exceptions.ValidationError):
             self.pos_config.iface_l10n_es_simplified_invoice = False
