94fe7e7049e43600a3797c79c158b25ed35c5c81
Jairo Llopis <yajo.sk8@gmail.com>
2022-09-29 09:05:58 +0100

    [FIX] l10n_es_*: remove pyOpenSSL and cryptography dependencies
    - pyOpenSSL was replaced by cryptography and is no longer used.
    - cryptography has a specific version dependency specified in `l10n_es_aeat`, which in turn is dependency of all these modules. So there's no real need to indicate that dependency in these files.
    
    @moduon MT-1067

--- l10n_es_aeat_sii_oca/__manifest__.py
+++ l10n_es_aeat_sii_oca/__manifest__.py
@@ -15,7 +15,7 @@
 
 {
     "name": "Suministro Inmediato de Información en el IVA",
-    "version": "15.0.2.4.0",
+    "version": "15.0.2.4.1",
     "category": "Accounting & Finance",
     "website": "https://github.com/OCA/l10n-spain",
     "author": "Acysos S.L.,"
@@ -33,7 +33,7 @@
     "installable": True,
     "development_status": "Mature",
     "maintainers": ["pedrobaeza"],
-    "external_dependencies": {"python": ["zeep", "requests", "cryptography"]},
+    "external_dependencies": {"python": ["zeep", "requests"]},
     "depends": ["account_invoice_refund_link", "l10n_es", "l10n_es_aeat", "queue_job"],
     "data": [
         "data/aeat_sii_queue_job.xml",
