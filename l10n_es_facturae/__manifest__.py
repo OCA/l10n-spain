d126f164201b3fd2fa9f944c727185a7a2f9e376
Enric Tobella <etobella@creublanca.es>
2022-10-02 22:05:43 +0200

    [IMP] l10n_es_facturae: Fix cryptography dependancy

--- l10n_es_facturae/__manifest__.py
+++ l10n_es_facturae/__manifest__.py
@@ -40,7 +40,7 @@
         "wizard/account_move_reversal_view.xml",
         "views/account_move_view.xml",
     ],
-    "external_dependencies": {"python": ["pycountry", "xmlsig"]},
+    "external_dependencies": {"python": ["pycountry", "xmlsig", "cryptography"]},
     "installable": True,
     "maintainers": ["etobella"],
 }
