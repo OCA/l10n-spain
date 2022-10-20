69cf3670f34925e5c70777b884b615505c57836b
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2022-10-20 12:09:39 +0200

    [MIG] l10n_es_aeat_mod130: Migration to 14.0
    - Standard procedure
    - Switch to computed fields
    - AEAT base adaptation
    - Tests adaptation

--- l10n_es_aeat_mod130/models/mod130.py
+++ l10n_es_aeat_mod130/models/mod130.py
@@ -14,15 +14,6 @@ class L10nEsAeatMod130Report(models.Model):
     _name = "l10n.es.aeat.mod130.report"
     _aeat_number = "130"
 
-    company_partner_id = fields.Many2one(
-        "res.partner",
-        string="Company partner",
-        relation="company_id.partner_id",
-        store=True,
-    )
-    currency_id = fields.Many2one(
-        "res.currency", string="Currency", relation="company_id.currency_id", store=True
-    )
     activity_type = fields.Selection(
         [
             ("primary", "Actividad agrícola, ganadera, forestal o pesquera"),
@@ -84,15 +75,24 @@ class L10nEsAeatMod130Report(models.Model):
         help="Calculado como el 5% del rendimiento del periodo (ingresos - "
         "gastos reales).",
     )
-    casilla_02 = fields.Monetary(string="Casilla [02] - Gastos", readonly=True)
+    casilla_02 = fields.Monetary(
+        string="Casilla [02] - Gastos", store=True, compute="_compute_casilla_02"
+    )
     casilla_03 = fields.Monetary(
         string="Casilla [03] - Rendimiento",
-        readonly=True,
+        store=True,
+        compute="_compute_casilla_03",
     )
-    casilla_04 = fields.Monetary(string="Casilla [04] - IRPF", readonly=True)
+    casilla_04 = fields.Monetary(
+        string="Casilla [04] - IRPF",
+        store=True,
+        compute="_compute_casilla_04",
+    )
     casilla_05 = fields.Monetary(string="Casilla [05]")
     casilla_06 = fields.Monetary(string="Casilla [06]")
-    casilla_07 = fields.Monetary(string="Casilla [07]", readonly=True)
+    casilla_07 = fields.Monetary(
+        string="Casilla [07]", store=True, compute="_compute_casilla_07"
+    )
     casilla_08 = fields.Monetary(
         string="Casilla [08] - Ingresos primario",
         readonly=True,
@@ -103,18 +103,25 @@ class L10nEsAeatMod130Report(models.Model):
     )
     casilla_10 = fields.Monetary(string="Casilla [10]", readonly=True)
     casilla_11 = fields.Monetary(string="Casilla [11]", readonly=True)
-    casilla_12 = fields.Monetary(string="Casilla [12]", readonly=True)
+    casilla_12 = fields.Monetary(
+        string="Casilla [12]", store=True, compute="_compute_casilla_12"
+    )
     casilla_13 = fields.Monetary(
         string="Casilla [13] - Deducción art. 80 bis",
         readonly=True,
     )
-    casilla_14 = fields.Monetary(string="Casilla [14]", readonly=True)
+    casilla_14 = fields.Monetary(
+        string="Casilla [14]", store=True, compute="_compute_casilla_14"
+    )
     casilla_15 = fields.Monetary(string="Casilla [15]", readonly=True)
     casilla_16 = fields.Monetary(
         string="Casilla [16] - Deducción por pago de hipoteca",
-        readonly=True,
+        store=True,
+        compute="_compute_casilla_16",
     )
-    casilla_17 = fields.Monetary(string="Casilla [17]", readonly=True)
+    casilla_17 = fields.Monetary(
+        string="Casilla [17]", store=True, compute="_compute_casilla_17"
+    )
     casilla_18 = fields.Monetary(string="Casilla [18]", readonly=True)
     result = fields.Monetary(
         string="Resultado",
@@ -127,6 +134,72 @@ class L10nEsAeatMod130Report(models.Model):
         compute="_compute_tipo_declaracion",
         store=True,
     )
+
+    @api.depends("real_expenses", "non_justified_expenses")
+    def _compute_casilla_02(self):
+        for report in self:
+            report.casilla_02 = report.real_expenses + report.non_justified_expenses
+
+    @api.depends("casilla_01", "casilla_02")
+    def _compute_casilla_03(self):
+        for report in self:
+            report.casilla_03 = report.casilla_01 - report.casilla_02
+
+    @api.depends("casilla_03")
+    def _compute_casilla_04(self):
+        for report in self:
+            if report.casilla_03 < 0:
+                report.casilla_04 = 0.0
+            else:
+                # IRPF - Truncar resultado, ya que es lo que hace la AEAT
+                report.casilla_04 = trunc(0.20 * report.casilla_03, 2)
+
+    @api.depends("casilla_04", "casilla_05", "casilla_06")
+    def _compute_casilla_07(self):
+        for report in self:
+            report.casilla_07 = (
+                report.casilla_04 - report.casilla_05 - report.casilla_06
+            )
+
+    @api.depends("casilla_07", "casilla_11", "activity_type")
+    def _compute_casilla_12(self):
+        for report in self:
+            if report.activity_type == "other":
+                report.casilla_12 = report.casilla_07 if report.casilla_07 > 0 else 0.0
+            else:
+                # TODO: Modelo 130 para actividades primarias
+                report.casilla_12 = report.casilla_11
+
+    @api.depends("casilla_12", "casilla_13")
+    def _compute_casilla_14(self):
+        for report in self:
+            report.casilla_14 = report.casilla_12 - report.casilla_13
+
+    @api.depends(
+        "has_prestamo", "casilla_03", "casilla_14", "casilla_15", "activity_type"
+    )
+    def _compute_casilla_16(self):
+        # Deducción por hipóteca
+        self.casilla_16 = 0
+        for report in self.filtered(lambda x: x.has_prestamo and x.casilla_14 > 0):
+            # Truncar resultado, ya que es lo que hace la AEAT
+            deduccion = trunc(0.02 * report.casilla_03, 2)
+            if report.activity_type == "other":
+                if deduccion > 660.14:
+                    deduccion = 660.14
+            else:
+                raise exceptions.UserError(_("No implementado"))
+            dif = report.casilla_14 - report.casilla_15
+            if deduccion > dif:
+                deduccion = dif
+            report.casilla_16 = deduccion
+
+    @api.depends("casilla_14", "casilla_15", "casilla_16")
+    def _compute_casilla_17(self):
+        for report in self:
+            report.casilla_17 = (
+                report.casilla_14 - report.casilla_15 - report.casilla_16
+            )
 
     @api.depends("casilla_18", "casilla_17")
     def _compute_result(self):
@@ -141,7 +214,6 @@ class L10nEsAeatMod130Report(models.Model):
             else:
                 report.tipo_declaracion = "I"
 
-    @api.multi
     def _calc_ingresos_gastos_retenciones(self):
         self.ensure_one()
         aml_obj = self.env["account.move.line"]
@@ -180,7 +252,6 @@ class L10nEsAeatMod130Report(models.Model):
         withholdings = groups[0]["balance"] or 0.0
         return (incomes, expenses, withholdings)
 
-    @api.multi
     def _calc_prev_trimesters_data(self):
         self.ensure_one()
         amount = 0
@@ -190,91 +261,51 @@ class L10nEsAeatMod130Report(models.Model):
                 amount += prev.casilla_07 - prev.casilla_16
         return amount
 
-    @api.multi
     def calculate(self):
         for report in self:
             if report.activity_type == "primary":
-                raise exceptions.Warning(
-                    _("Este tipo de actividad no " "está aún soportado por el módulo.")
+                raise exceptions.UserError(
+                    _("Este tipo de actividad no está aún soportado por el módulo.")
                 )
             if report.has_deduccion_80:
-                raise exceptions.Warning(
+                raise exceptions.UserError(
                     _(
-                        "No se pueden calcular por el "
-                        "momento declaraciones que contengan deducciones por el "
-                        "artículo 80 bis."
+                        "No se pueden calcular por el momento declaraciones que "
+                        "contengan deducciones por el artículo 80 bis."
                     )
                 )
-            vals = {}
+            vals = {
+                "casilla_01": 0,
+                "real_expenses": 0,
+                "non_justified_expenses": 0,
+                "casilla_05": 0,
+                "casilla_06": 0,
+                "casilla_13": 0,  # TODO: Deducción artículo 80 bis
+                "casilla_15": 0,  # TODO: Poner resultados negativos ant. trimestres
+            }
             if report.activity_type == "other":
                 (
-                    ingresos,
-                    gastos,
-                    retenciones,
+                    vals["casilla_01"],
+                    vals["real_expenses"],
+                    vals["casilla_06"],
                 ) = report._calc_ingresos_gastos_retenciones()
-                vals["casilla_01"] = ingresos
-                vals["real_expenses"] = gastos
-                rendimiento_bruto = ingresos - gastos
+                rendimiento_bruto = vals["casilla_01"] - vals["real_expenses"]
                 if rendimiento_bruto > 0:
                     vals["non_justified_expenses"] = round(rendimiento_bruto * 0.05, 2)
-                else:
-                    vals["non_justified_expenses"] = 0.0
-                vals["casilla_02"] = gastos + vals["non_justified_expenses"]
-                # Rendimiento
-                vals["casilla_03"] = ingresos - vals["casilla_02"]
-                # IRPF - Truncar resultado, ya que es lo que hace la AEAT
-                if vals["casilla_03"] < 0:
-                    vals["casilla_04"] = 0.0
-                else:
-                    vals["casilla_04"] = trunc(0.20 * vals["casilla_03"], 2)
                 # Pago fraccionado previo del trimestre
                 vals["casilla_05"] = report._calc_prev_trimesters_data()
-                vals["casilla_06"] = retenciones
-                vals["casilla_07"] = (
-                    vals["casilla_04"] - vals["casilla_05"] - vals["casilla_06"]
-                )
-                vals["casilla_12"] = vals["casilla_07"]
-                if vals["casilla_12"] < 0:
-                    vals["casilla_12"] = 0.0
-            else:
-                # TODO: Modelo 130 para actividades primarias
-                vals["casilla_12"] = vals["casilla_11"]
-            # TODO: Deducción artículo 80 bis
-            vals["casilla_13"] = 0.0
-            vals["casilla_14"] = vals["casilla_12"] - vals["casilla_13"]
-            # TODO: Poner los resultados negativos de anteriores trimestres
-            vals["casilla_15"] = 0.0
-            # Deducción por hipóteca
-            if report.has_prestamo and vals["casilla_14"] > 0:
-                # Truncar resultado, ya que es lo que hace la AEAT
-                deduccion = trunc(0.02 * vals["casilla_03"], 2)
-                if report.activity_type == "other":
-                    if deduccion > 660.14:
-                        deduccion = 660.14
-                else:
-                    raise exceptions.Warning(_("No implementado"))
-                dif = vals["casilla_14"] - vals["casilla_15"]
-                if deduccion > dif:
-                    deduccion = dif
-                vals["casilla_16"] = deduccion
-            else:
-                vals["casilla_16"] = 0.0
-            vals["casilla_17"] = (
-                vals["casilla_14"] - vals["casilla_15"] - vals["casilla_16"]
-            )
             report.write(vals)
         return True
 
-    @api.multi
     def button_confirm(self):
         """Check its records"""
         msg = ""
         for report in self:
-            if report.type == "C" and not report.casilla_18:
+            if report.statement_type == "C" and not report.casilla_18:
                 msg = _(
                     "Debe introducir una cantidad en la casilla 18 como "
                     "ha marcado la casilla de declaración complementaria."
                 )
         if msg:
             raise exceptions.ValidationError(msg)
-        return super(L10nEsAeatMod130Report, self).button_confirm()
+        return super().button_confirm()
