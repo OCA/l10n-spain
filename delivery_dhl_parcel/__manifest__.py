8a16bc7f9189c024fdfe9fb88e47be7036578b28
Víctor Martínez <victor.martinez@tecnativa.com>
2022-10-20 15:44:55 +0200

    [FIX] delivery_dhl_parcel: Improve changes
    Create _prepare_dhl_parcel_parcel_address_info() function to reduce code.
    
    Improve receiver + sender information:
    - Remove blank spaces on partner phone
    - Set phone or mobile from partner
    - Concatenate street and street2 field
    - Limit address field to 40 characters
    
    TT40002
    
    delivery_dhl_parcel 14.0.1.4.5

--- delivery_dhl_parcel/__manifest__.py
+++ delivery_dhl_parcel/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Delivery DHL Parcel",
     "summary": "Delivery Carrier implementation for DHL Parcel using their API",
-    "version": "14.0.1.4.4",
+    "version": "14.0.1.4.5",
     "category": "Stock",
     "website": "https://github.com/OCA/l10n-spain",
     "author": "Studio73, Odoo Community Association (OCA)",
