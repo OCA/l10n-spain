b006f220ee9efef8f7c76eaa1e9823f2bed912c8
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2022-10-20 12:03:43 +0200

    [IMP] l10n_es_aeat_mod130: pre-commit stuff

--- l10n_es_aeat_mod130/__manifest__.py
+++ l10n_es_aeat_mod130/__manifest__.py
@@ -3,10 +3,9 @@
 {
     "name": "AEAT modelo 130",
     "version": "12.0.1.3.1",
-    "author": "Tecnativa, "
-              "Odoo Community Association (OCA)",
+    "author": "Tecnativa, " "Odoo Community Association (OCA)",
     "license": "AGPL-3",
-    'website': "https://github.com/OCA/l10n-spain",
+    "website": "https://github.com/OCA/l10n-spain",
     "category": "Localization/Accounting",
     "depends": [
         "l10n_es_aeat",
@@ -15,7 +14,7 @@
         "data/aeat_export_mod130_data.xml",
         "views/mod130_view.xml",
         "security/ir.model.access.csv",
-        "security/ir_rule.xml"
+        "security/ir_rule.xml",
     ],
     "installable": True,
 }
