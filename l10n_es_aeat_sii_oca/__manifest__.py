e8f9d31a71cf24e1752259f1683bf4a8a986c42a
Eduardo De Miguel <edu@moduon.team>
2022-11-22 10:31:10 +0100

    [FIX] l10n_es_aeat_sii_oca: Remove `thirdparty_invoice` field from l10n_es_aeat_sii_oca

--- l10n_es_aeat_sii_oca/__manifest__.py
+++ l10n_es_aeat_sii_oca/__manifest__.py
@@ -9,6 +9,7 @@
 # Copyright 2018 Javi Melendez <javimelex@gmail.com>
 # Copyright 2018 Angel Moya <angel.moya@pesol.es>
 # Copyright 2020 Sygel Technology - Valentín Vinagre <valentin.vinagre@sygel.es>
+# Copyright 2022 Moduon - Eduardo de Miguel
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
@@ -53,6 +54,7 @@
         "views/account_fiscal_position_view.xml",
         "views/res_partner_views.xml",
         "views/aeat_sii_tax_agency_view.xml",
+        "views/account_journal_view.xml",
     ],
     "images": ["static/description/SII_1.jpg"],
     "post_init_hook": "add_key_to_existing_invoices",
