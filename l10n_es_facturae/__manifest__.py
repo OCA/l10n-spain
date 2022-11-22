7f048c7667d22f3299f81797002c0c7f9d678fa7
Eduardo De Miguel <edu@moduon.team>
2022-11-22 10:29:37 +0100

    [FIX] l10n_es_facturae: Remove `thirdparty_invoice` field from l10n_es_facturae

--- l10n_es_facturae/__manifest__.py
+++ l10n_es_facturae/__manifest__.py
@@ -3,6 +3,7 @@
 # Copyright 2015 Tecon
 # Copyright 2015 Omar Castiñeira (Comunitea)
 # Copyright 2016-2020 Tecnativa - Pedro M. Baeza
+# Copyright 2022 Moduon - Eduardo de Miguel
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
@@ -39,6 +40,7 @@
         "wizard/create_facturae_view.xml",
         "wizard/account_move_reversal_view.xml",
         "views/account_move_view.xml",
+        "views/account_journal_view.xml",
     ],
     "external_dependencies": {"python": ["pycountry", "xmlsig", "cryptography"]},
     "installable": True,
