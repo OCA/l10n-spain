d648ed7807cf49e817d58e643cadd94411b2c154
Víctor Martínez <victor.martinez@tecnativa.com>
2022-11-23 09:31:43 +0100

    [FIX] l10n_es_aeat_sii_oca: Remove thirdparty_invoice + thirdparty_number fields from l10n_es_aeat_sii_oca

--- l10n_es_aeat_sii_oca/__manifest__.py
+++ l10n_es_aeat_sii_oca/__manifest__.py
@@ -16,7 +16,7 @@
 
 {
     "name": "Suministro Inmediato de Información en el IVA",
-    "version": "15.0.2.5.1",
+    "version": "15.0.2.6.0",
     "category": "Accounting & Finance",
     "website": "https://github.com/OCA/l10n-spain",
     "author": "Acysos S.L.,"
