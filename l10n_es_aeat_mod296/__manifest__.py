6e705afc6efe62a30f09f8fe0e2a65b2090a9a38
Víctor Martínez <victor.martinez@tecnativa.com>
2022-11-25 08:56:23 +0100

    [MIG] l10n_es_aeat_mod296: Migration to 15.0
    TT36962

--- l10n_es_aeat_mod296/__manifest__.py
+++ l10n_es_aeat_mod296/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     "name": "AEAT modelo 296",
-    "version": "13.0.1.2.0",
+    "version": "15.0.1.0.0",
     "category": "Localisation/Accounting",
     "author": "Tecnativa, AvanzOSC, Qubiq, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-spain",
