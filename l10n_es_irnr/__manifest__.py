0b4e4814e000a27b686ae9fd8b005d4b4e1dd9a9
FernandoRomera <fromera@astrohotel.es>
2022-12-22 11:20:25 +0100

    [16.0][MIG] l10n_es_irnr: Migration to 16.0

--- l10n_es_irnr/__manifest__.py
+++ l10n_es_irnr/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "Retenciones IRNR (No residentes)",
-    "version": "15.0.1.0.1",
+    "version": "16.0.1.0.0",
     "category": "Localization",
     "depends": ["l10n_es"],
     "development_status": "Mature",
