743b400110b77f885edea3b215fb1462b9952339
FernandoRomera <fromera@astrohotel.es>
2022-12-22 11:17:17 +0100

    [16.0][MIG] l10n_es_aeat_mod216: Migration to 16.0

--- l10n_es_aeat_mod216/__manifest__.py
+++ l10n_es_aeat_mod216/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "AEAT modelo 216",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Localisation/Accounting",
     "author": "AvanzOSC, Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-spain",
