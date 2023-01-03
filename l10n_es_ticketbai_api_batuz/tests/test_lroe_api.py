a3737352d587b8b4d4ee24576b3a8e031cbb3aa1
Enrique Martín <enriquemartin@digital5.es>
2023-01-03 12:07:52 +0100

    [FIX] l10n_es_ticketbai_api_batuz - normalizar cálculo de ejercicio

--- l10n_es_ticketbai_api_batuz/tests/test_lroe_api.py
+++ l10n_es_ticketbai_api_batuz/tests/test_lroe_api.py
@@ -1,6 +1,7 @@
 # Copyright (2021) Binovo IT Human Project SL
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 import json
+from datetime import datetime
 
 from requests import exceptions
 
@@ -24,6 +25,9 @@ class TestLroeTicketBaiApi(common.TransactionCase):
             tbai_invoice_ids = False
             company_id = Company()
             model = "MODE"
+
+            def build_cabecera_ejercicio(self):
+                return str(datetime.now().year)
 
         h = api.get_request_headers(Op())
 
