158cb0d2d242b4950f18b9b7e903e8f1736b1de2
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2023-01-27 13:46:00 +0100

    [FIX] l10n_es_aeat_mod390: Adapt to 2022 BOE format + changes
    - New activity codes
    - New fields for 5%
    - New BOE format
    
    TODO: New 2023 taxes (0% food national, intra-community and Req)

--- l10n_es_aeat_mod390/__manifest__.py
+++ l10n_es_aeat_mod390/__manifest__.py
@@ -1,14 +1,14 @@
-# Copyright 2017-2021 Tecnativa - Pedro M. Baeza
+# Copyright 2017-2023 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl
 
 {
     "name": "AEAT modelo 390",
-    "version": "16.0.1.0.0",
+    "version": "16.0.2.0.0",
     "category": "Localisation/Accounting",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-spain",
     "license": "AGPL-3",
-    "depends": ["l10n_es", "l10n_es_aeat", "account_tax_balance"],
+    "depends": ["l10n_es_aeat_mod303"],
     "data": [
         "data/aeat_export_mod390_2019_sub01_data.xml",
         "data/aeat_export_mod390_2019_sub02_data.xml",
@@ -22,6 +22,10 @@
         # 2021
         "data/2021/aeat.model.export.config.csv",
         "data/2021/aeat.model.export.config.line.csv",
+        # 2022
+        "data/2022/aeat.model.export.config.csv",
+        "data/2022/aeat.model.export.config.line.csv",
+        # rest of stuff
         "data/tax_code_map_mod390_data.xml",
         "views/mod390_view.xml",
         "security/ir.model.access.csv",
