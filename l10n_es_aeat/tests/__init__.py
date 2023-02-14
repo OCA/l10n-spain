9088349a42f37f6a509463747f7c1072edf7073b
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2023-02-14 21:01:15 +0100

    [FIX] l10n_es_aeat: Proper interval overlap checking in tax map
    It includes unit tests.

--- l10n_es_aeat/tests/__init__.py
+++ l10n_es_aeat/tests/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from . import test_l10n_es_aeat
+from . import test_l10n_es_aeat_map_tax
 from . import test_l10n_es_aeat_report
 from . import test_l10n_es_aeat_export_config
