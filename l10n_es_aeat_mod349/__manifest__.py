a4e266e6ac23eb8ffc24d6da2ccd25bd7f54f0b4
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2023-02-15 14:04:22 +0100

    [IMP] l10n_es_aeat_mod349: Include food taxes + missing intra-community ones

--- l10n_es_aeat_mod349/__manifest__.py
+++ l10n_es_aeat_mod349/__manifest__.py
@@ -3,13 +3,13 @@
 # Copyright 2016 - Tecnativa - Angel Moya <odoo@tecnativa.com>
 # Copyright 2017 - Tecnativa - Luis M. Ontalba <luis.martinez@tecnativa.com>
 # Copyright 2017 ForgeFlow <contact@forgeflow.com>
-# Copyright 2014-2020 Tecnativa - Pedro M. Baeza
 # Copyright 2018 Tecnativa - Carlos Dauden
+# Copyright 2014-2023 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
     "name": "AEAT modelo 349",
-    "version": "16.0.1.0.0",
+    "version": "16.0.1.1.0",
     "author": "Tecnativa, ForgeFlow, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "category": "Localisation/Accounting",
