81d73bfda766a216d068d0c4177d85bced2c7798
pol <pol.reig@qubiq.es>
2023-02-23 16:14:05 +0100

    [IMP] l10n_es_facturae: improvements wizard facturae

--- l10n_es_facturae/wizard/create_facturae.py
+++ l10n_es_facturae/wizard/create_facturae.py
@@ -5,35 +5,21 @@
 # Copyright 2015 Omar Castiñeira (Comunitea)
 # Copyright 2016 Serv. Tecnol. Avanzados - Pedro M. Baeza
 # Copyright 2017 Creu Blanca
+# Copyright 2023 Jan Tugores (jan.tugores@qubiq.es)
+# Copyright 2023 Pol Reig (pol.reig@qubiq.es)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 import base64
 
-from odoo import _, fields, models
+from odoo import _, api, fields, models
 from odoo.exceptions import UserError
-
-
-class Log(Exception):
-    def __init__(self):
-        self.content = ""
-        self.error = False
-
-    def add(self, s, error=True):
-        self.content = self.content + s
-        if error:
-            self.error = error
-
-    def __call__(self):
-        return self.content
-
-    def __str__(self):
-        return self.content
 
 
 class CreateFacturae(models.TransientModel):
     _name = "create.facturae"
     _description = "Create Facturae Wizard"
 
+    move_id = fields.Many2one("account.move")
     facturae = fields.Binary("Facturae file", readonly=True)
     facturae_fname = fields.Char("File name", size=64)
     note = fields.Text("Log")
@@ -48,14 +34,20 @@ class CreateFacturae(models.TransientModel):
         default=True,
     )
 
-    def create_facturae_file(self):
-        log = Log()
+    @api.model
+    def default_get(self, fields):
+        res = super(CreateFacturae, self).default_get(fields)
+        # obtains the move_id record from context
         move_ids = self.env.context.get("active_ids", [])
-        if not move_ids or len(move_ids) > 1:
-            raise UserError(_("You can only select one move to export"))
-        active_model = self.env.context.get("active_model", False)
-        assert active_model == "account.move", "Bad context propagation"
-        move = self.env["account.move"].browse(move_ids[0]).ensure_one()
+        if not move_ids:
+            raise UserError(_("You must select a move to export"))
+        res.update({"move_id": move_ids[0]})
+        return res
+
+    def create_facturae_file(self):
+        move = self.move_id
+        # Validating mandatory fields
+        move.validate_facturae_fields()
         if self.firmar_facturae:
             move_file = self.env.ref("l10n_es_facturae.report_facturae_signed")._render(
                 move.ids
@@ -67,7 +59,7 @@ class CreateFacturae(models.TransientModel):
             )[0]
             file_name = ("facturae_" + move.name + ".xml").replace("/", "-")
         file = base64.b64encode(move_file)
-        self.env["ir.attachment"].create(
+        attachment = self.env["ir.attachment"].create(
             {
                 "name": file_name,
                 "datas": file,
@@ -76,11 +68,11 @@ class CreateFacturae(models.TransientModel):
                 "mimetype": "application/xml",
             }
         )
-        log.add(_("Export successful\n\nSummary:\nMove number: %s\n") % move.name)
+        note = _("Export successful\n\nSummary:\nMove number: %s\n") % move.name
         self.write(
             {
-                "note": log(),
-                "facturae": file,
+                "note": note,
+                "facturae": attachment.datas,
                 "facturae_fname": file_name,
                 "state": "second",
             }
