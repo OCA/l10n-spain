d454216c767b949670ba550800a689e0227a6195
Javier Iniesta <javier.iniesta@factorlibre.com>
2023-03-17 07:48:59 +0100

    [MIG] l10n_es_aeat_mod369: Migration to 11.0

--- l10n_es_aeat_mod369/tests/test_l10n_es_aeat_mod369.py
+++ l10n_es_aeat_mod369/tests/test_l10n_es_aeat_mod369.py
@@ -15,55 +15,54 @@ class TestL10nEsAeatMod369Base(TestL10nEsAeatModBase):
     # Do not forget to include '--log-handler aeat:DEBUG' in Odoo command line
     debug = False
 
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-        general_tax = cls.env.ref(
-            "l10n_es.%s_account_tax_template_s_iva21b" % cls.company.id
+    def setUp(self):
+        super().setUp()
+        general_tax = self.env.ref(
+            "l10n_es.%s_account_tax_template_s_iva21b" % self.company.id
         )
-        cls.oss_taxes = {}
-        cls.oss_countries = {}
-        cls.sale_invoices = {}
+        self.oss_taxes = {}
+        self.oss_countries = {}
+        self.sale_invoices = {}
         invoice_date = "2017-01-01"
         for country_key in ["FR", "DE"]:
-            country = cls.env.ref("base.%s" % country_key.lower())
-            wizard = cls.env["l10n.eu.oss.wizard"].create(
+            country = self.env.ref("base.%s" % country_key.lower())
+            wizard = self.env["l10n.eu.oss.wizard"].create(
                 {
-                    "company_id": cls.company.id,
+                    "company_id": self.company.id,
                     "general_tax": general_tax.id,
                     "todo_country_ids": [(4, country.id)],
                 }
             )
             wizard.generate_eu_oss_taxes()
-            tax = cls.env["account.tax"].search(
+            tax = self.env["account.tax"].search(
                 [
                     ("oss_country_id", "=", country.id),
-                    ("company_id", "=", cls.company.id),
+                    ("company_id", "=", self.company.id),
                 ]
             )
             tax.service_type = "goods"
             line_data = {
                 "name": "Test for OSS tax",
-                "account_id": cls.accounts["700000"].id,
+                "account_id": self.accounts["700000"].id,
                 "price_unit": 100,
                 "quantity": 1,
-                "tax_ids": [(4, tax.id)],
+                "invoice_line_tax_ids": [(4, tax.id)],
             }
             extra_vals = {"invoice_line_ids": [(0, 0, line_data)]}
-            invoice = cls._invoice_sale_create(invoice_date, extra_vals)
-            cls.sale_invoices[country_key] = invoice
-            cls.oss_taxes[country_key] = tax
-            cls.oss_countries[country_key] = country
+            invoice = self._invoice_sale_create(invoice_date, extra_vals)
+            self.sale_invoices[country_key] = invoice
+            self.oss_taxes[country_key] = tax
+            self.oss_countries[country_key] = country
         # 369
-        model369_model = cls.env["l10n.es.aeat.mod369.report"].with_user(
-            cls.account_manager
+        model369_model = self.env["l10n.es.aeat.mod369.report"].sudo(
+            self.account_manager
         )
-        cls.model369 = model369_model.create(
+        self.model369 = model369_model.create(
             {
-                "company_id": cls.company.id,
+                "company_id": self.company.id,
                 "company_vat": "1234567890",
                 "contact_name": "Test owner",
-                "statement_type": "N",
+                "type": "N",
                 "support_type": "T",
                 "contact_phone": "911234455",
                 "year": 2017,
@@ -81,7 +80,8 @@ class TestL10nEsAeatMod369Base(TestL10nEsAeatModBase):
         for country_code in self.sale_invoices.keys():
             sale_invoice_by_key = self.sale_invoices[country_code]
             spain_goods_line_filter = self.model369.goods_line_ids.filtered(
-                lambda x: x.country_code == country_code and not x.is_page_8_line
+                lambda x, country_code=country_code: x.country_code == country_code
+                and not x.is_page_8_line
             )
             self.assertEqual(
                 self.oss_taxes[country_code].amount, spain_goods_line_filter.vat_type
