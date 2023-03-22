eafe7f7469e230afe2a28ded7954d74242c63e1b
Ethan Hildick <ethan@studio73.es>
2023-03-22 12:26:28 +0100

    [FIX] l10n_es_aeat_mod303: Add new result_types to domain

--- l10n_es_aeat_mod303/__manifest__.py
+++ l10n_es_aeat_mod303/__manifest__.py
@@ -7,7 +7,7 @@
 
 {
     "name": "AEAT modelo 303",
-    "version": "16.0.2.1.0",
+    "version": "16.0.2.2.0",
     "category": "Accounting",
     "development_status": "Mature",
     "author": "Guadaltech,"
