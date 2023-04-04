1bb0b1f28042ee02617fb607b148926bdfcac8cb
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2023-04-04 19:34:33 +0200

    [MIG] l10n_es_mis_report: Migration to 16.0

--- l10n_es_mis_report/__manifest__.py
+++ l10n_es_mis_report/__manifest__.py
@@ -1,5 +1,5 @@
 # Copyright 2018 ForgeFlow, S.L. <contact@forgeflow.com>
-# Copyright 2018-2019 Tecnativa - Pedro M. Baeza
+# Copyright 2018-2023 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl
 
 {
@@ -8,7 +8,7 @@
     "author": "ForgeFlow, Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-spain",
     "category": "Reporting",
-    "version": "15.0.1.0.1",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["l10n_es", "mis_builder"],  # OCA/mis-builder
     "data": [
