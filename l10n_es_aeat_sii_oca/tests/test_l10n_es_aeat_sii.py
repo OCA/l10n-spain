595308fa3987008adc4669474458fbe5ff341ea3
Jairo Llopis <yajo.sk8@gmail.com>
2023-04-12 11:13:24 +0100

    [FIX] l10n_es_aeat_sii_oca: cliente intracomunitario, entrega extracomunitaria
    Una venta a un cliente intracomunitario enviada al extranjero es exportación.
    
    Nunca se puede informar el tipo de identificador 02 para estos envíos porque la AEAT los rechaza siempre. Al poner identificador 06, funciona bien.
    
    Fix https://github.com/OCA/l10n-spain/issues/2969
    
    @moduon MT-2407
    
    Co-authored-by: Rafael Blasco <8736623+rafaelbn@users.noreply.github.com>

--- l10n_es_aeat_sii_oca/tests/test_l10n_es_aeat_sii.py
+++ l10n_es_aeat_sii_oca/tests/test_l10n_es_aeat_sii.py
@@ -174,6 +174,40 @@ class TestL10nEsAeatSii(TestL10nEsAeatSiiBase):
             [("sii_wsdl_out", "!=", False)]
         )
 
+    def test_intracomunitary_customer_extracomunitary_delivery(self):
+        """Comprobar venta a un cliente intracomunitario enviada al extranjero.
+
+        Este caso se puede dar cuando una asesoría contable contabiliza facturas
+        para otro cliente, en caso de que ese cliente le venda a otro cliente
+        intracomunitario pero envíe a una dirección extracomunitaria.
+
+        También se puede dar cuando se instala el módulo `sale` en Odoo. Al instalarlo,
+        se añade el campo `partner_shipping_id`, que permite indicar una dirección
+        de entrega extracomunitaria para clientes intracomunitarios.
+        """
+        self._activate_certificate(self.certificate_password)
+        eu_customer = self.env["res.partner"].create(
+            {
+                "name": "French Customer",
+                "country_id": self.ref("base.fr"),
+                "vat": "FR23334175221",
+            }
+        )
+        fp_extra = self.browse_ref(f"l10n_es.{self.company.id}_fp_extra")
+        fp_extra.sii_partner_identification_type = "3"
+        invoice = self.invoice.copy(
+            {"partner_id": eu_customer.id, "fiscal_position_id": fp_extra.id}
+        )
+        invoice.action_post()
+        sii_info = invoice._get_sii_invoice_dict()
+        self.assertEqual(
+            sii_info["FacturaExpedida"]["Contraparte"],
+            {
+                "NombreRazon": "French Customer",
+                "IDOtro": {"CodigoPais": "FR", "IDType": "06", "ID": "23334175221"},
+            },
+        )
+
     def test_job_creation(self):
         self.assertTrue(self.invoice.invoice_jobs_ids)
 
