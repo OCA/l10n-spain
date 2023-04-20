4d01cd9d571ddf713a883239536d4cb34c1a5f7e
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2023-04-20 12:23:20 +0200

    [FIX] l10n_es_aeat_mod130: Type for 0 result must be N

--- l10n_es_aeat_mod130/__manifest__.py
+++ l10n_es_aeat_mod130/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     "name": "AEAT modelo 130",
-    "version": "14.0.1.0.1",
+    "version": "14.0.1.0.2",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "website": "https://github.com/OCA/l10n-spain",
