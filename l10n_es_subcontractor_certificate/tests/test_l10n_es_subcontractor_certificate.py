c8bac5f5cbe4682c65ddc1b20ecc9deea1a8db11
aiendry-aktivsoftware <aiendry@aktivsoftware.com>
2023-05-01 17:32:47 +0530

    [MIG] l10n_es_subcontractor_certificate: Migration to 15.0

--- l10n_es_subcontractor_certificate/tests/test_l10n_es_subcontractor_certificate.py
+++ l10n_es_subcontractor_certificate/tests/test_l10n_es_subcontractor_certificate.py
@@ -21,37 +21,29 @@ class TestL10nEsSubcontractorCertificate(common.TransactionCase):
             "mail_create_nosubscribe": True,
             "mail_create_nolog": True,
         }
-        Partner = self.env["res.partner"].with_context(self.ctx_no_mail)
-        Purchase = self.env["purchase.order"].with_context(self.ctx_no_mail)
-        Invoice = self.env["account.invoice"].with_context(self.ctx_no_mail)
+        Partner = self.env["res.partner"].with_context(**self.ctx_no_mail)
+        Purchase = self.env["purchase.order"].with_context(**self.ctx_no_mail)
+        Move = self.env["account.move"].with_context(**self.ctx_no_mail)
         self.partner = Partner.create(
-            {
-                "name": "Demo Supplier",
-                "email": "demo@supplier.com",
-                "supplier": True,
-            }
+            {"name": "Demo Supplier", "email": "demo@supplier.com"}
         )
         self.purchase = Purchase.create({"partner_id": self.partner.id})
-        self.invoice = Invoice.create(
-            {"partner_id": self.partner.id, "type": "in_invoice"}
+        self.move = Move.create(
+            {"partner_id": self.partner.id, "move_type": "in_invoice"}
         )
         self.expired_date = fields.Date.today() - timedelta(days=1)
 
     def test_00_onchange_partner_id(self):
-        self.partner.write(
-            {
-                "certificate_required": True,
-            }
-        )
+        self.partner.write({"certificate_required": True})
         res_purchase = self.purchase.onchange_partner_id()
         self.assertEqual(
             res_purchase.get("warning", False),
             REQUIRED_WARNING_AEAT,
             "Purchase should show warning required AEAT" "certificate expiration date",
         )
-        res_invoice = self.invoice._onchange_partner_id()
+        res_move = self.move._onchange_partner_id()
         self.assertEqual(
-            res_invoice.get("warning", False),
+            res_move.get("warning", False),
             REQUIRED_WARNING_AEAT,
             "Invoice should show warning required AEAT" "certificate expiration date",
         )
@@ -69,9 +61,9 @@ class TestL10nEsSubcontractorCertificate(common.TransactionCase):
             REQUIRED_WARNING_SS,
             "Purchase should show warning required SS " "certificate expiration date",
         )
-        res_invoice = self.invoice._onchange_partner_id()
+        res_move = self.move._onchange_partner_id()
         self.assertEqual(
-            res_invoice.get("warning"),
+            res_move.get("warning"),
             REQUIRED_WARNING_SS,
             "Invoice should show warning required SS " "certificate expiration date",
         )
@@ -91,9 +83,9 @@ class TestL10nEsSubcontractorCertificate(common.TransactionCase):
             EXPIRED_WARNING_AEAT,
             "Purchase should show warning expired AEAT certificate",
         )
-        res_invoice = self.invoice._onchange_partner_id()
+        res_move = self.move._onchange_partner_id()
         self.assertEqual(
-            res_invoice.get("warning"),
+            res_move.get("warning"),
             EXPIRED_WARNING_AEAT,
             "Invoice should show warning expired AEAT certificate",
         )
@@ -112,9 +104,9 @@ class TestL10nEsSubcontractorCertificate(common.TransactionCase):
             EXPIRED_WARNING_SS,
             "Purchase should show warning expired SS certificate",
         )
-        res_invoice = self.invoice._onchange_partner_id()
+        res_move = self.move._onchange_partner_id()
         self.assertEqual(
-            res_invoice.get("warning"),
+            res_move.get("warning"),
             EXPIRED_WARNING_SS,
             "Invoice should show warning expired SS certificate",
         )
