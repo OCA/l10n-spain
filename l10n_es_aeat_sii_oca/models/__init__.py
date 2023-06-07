7f752aa3e94bf7c990947ff4f984013da8361a2e
Jose Zambudio <jose@aurestic.es>
2023-06-07 15:57:22 +0200

    [REF] account.move extraemos parte de sii a un mixin. [ADD] módulo l10n_es_pos_sii para permitir enviar pedidos del TPV al sii
    Roadmap:
     - Comprobar que funcionalidad de account.move sigue siendo correcta.
     - Posibilidad de enviar al sii una sesión (tipo summary)
     - Envío devoluciones al SII -> POS
     - Cancelar envío al SII -> POS

--- l10n_es_aeat_sii_oca/models/__init__.py
+++ l10n_es_aeat_sii_oca/models/__init__.py
@@ -5,5 +5,6 @@ from . import aeat_sii_map
 from . import product_product
 from . import queue_job
 from . import account_fiscal_position
+from . import sii_mixin
 from . import account_move
 from . import res_partner
