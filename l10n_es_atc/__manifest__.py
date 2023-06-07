c79e7448a364ea4fbd8288b5c8ca573b51c8851f
Nicolás Ramos <n.ramos@binhex.es>
2023-06-07 15:00:00 +0100

    ADD 420
    ADD l10n_es_atc
    
    RM
    
    Revert "ADD 420"
    
    This reverts commit dcb18e7dba1426511b39fb3b7b11c13f4bf4ebe8.
    
    [16.0][ADD] l10n_es_atc
    
    ADD Readme
    
    Glue l10n_es_aeat and pre-commit
    
    pre-commit
    
    Update icon and readme

---
+++ l10n_es_atc/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2023 Binhex - Nicolás Ramos (http://binhex.es)
+
+{
+    "name": "ATC Menú",
+    "summary": "Modulo 'glue' de la AEAT para el menú de la ATC",
+    "version": "16.0.1.0.0",
+    "author": "Binhex System Solutions," "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/l10n-spain",
+    "category": "Accounting",
+    "depends": ["l10n_es_aeat"],
+    "data": [
+        "security/atc_security.xml",
+        "views/atc_menuitem.xml",
+        "data/atc_partner.xml",
+    ],
+    "installable": True,
+    "auto_install": True,
+}
