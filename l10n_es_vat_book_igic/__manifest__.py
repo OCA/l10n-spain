734ebabfb321d110af7db3e40a2ec6f502cc7297
Nicolás Ramos <n.ramos@binhex.es>
2023-06-07 19:39:17 +0100

    [16.0][ADD] l10n_es_vat_book_igic

---
+++ l10n_es_vat_book_igic/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2014-2023 Binhex - Nicolás Ramos (http://binhex.es)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Libro de IGIC",
+    "version": "16.0.1.0.0",
+    "author": "Binhex System Solutions, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-spain",
+    "license": "AGPL-3",
+    "category": "Accounting",
+    "depends": ["l10n_es_igic", "l10n_es_vat_book"],
+    "data": ["data/atc_vat_book_igic_map_data.xml"],
+    "maintainers": ["nicolasramos"],
+    "installable": True,
+}
