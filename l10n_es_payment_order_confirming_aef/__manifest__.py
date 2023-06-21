4a72292f6a0dfcc01a10c76ae905383daca18ec6
Yadier A. De Quesada <yadier.dequesada@tecnativa.com>
2023-06-21 15:49:16 -0400

    [MIG] l10n_es_payment_order_confirming_aef: Migration to 16.0

--- l10n_es_payment_order_confirming_aef/__manifest__.py
+++ l10n_es_payment_order_confirming_aef/__manifest__.py
@@ -3,12 +3,15 @@
 
 {
     "name": "Exportación de fichero bancario Confirming estándar AEF",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "category": "Localisation/Accounting",
     "website": "https://github.com/OCA/l10n-spain",
     "license": "AGPL-3",
     "depends": ["account_payment_order", "l10n_es"],
-    "data": ["data/account_payment_method.xml", "views/account_payment_mode_view.xml"],
+    "data": [
+        "data/account_payment_method.xml",
+        "views/account_payment_mode_view.xml",
+    ],
     "installable": True,
 }
