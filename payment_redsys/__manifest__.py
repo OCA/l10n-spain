a76198443cf03a72c1d22ec5730e641f9dca3ed2
rafaferri <rafa.ferri@studio73.es>
2023-06-26 13:16:08 +0200

    [16.0][MIG] payment_redsys: Migration to 16.0

---
+++ payment_redsys/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2017 Tecnativa - Sergio Teruel
+# Copyright 2020 Tecnativa - João Marques
+
+{
+    "name": "Pasarela de pago Redsys",
+    "category": "Payment Acquirer",
+    "summary": "Payment Acquirer: Redsys Implementation",
+    "version": "16.0.1.0.0",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-spain",
+    "depends": ["payment", "website_sale"],
+    "external_dependencies": {"python": ["pycryptodome"]},
+    "data": [
+        "views/payment_provider.xml",
+        "views/payment_redsys_templates.xml",
+        "data/payment_redsys.xml",
+    ],
+    "license": "AGPL-3",
+    "installable": True,
+}
