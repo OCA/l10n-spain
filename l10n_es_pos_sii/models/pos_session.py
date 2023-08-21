149ff5cc7eab22b3801fc69da93a6c401c592ea5
Carlos Ramos <cramosh84@gmail.com>
2023-08-21 17:22:34 +0200

    [MIG] l10n_es_pos_sii: Migration to 14.0

--- l10n_es_pos_sii/models/pos_session.py
+++ l10n_es_pos_sii/models/pos_session.py
@@ -6,14 +6,7 @@ class PosSession(models.Model):
 
     def _validate_session(
         self,
-        balancing_account=False,
-        amount_to_balance=0,
-        bank_payment_method_diffs=None,
     ):
-        res = super()._validate_session(
-            balancing_account=balancing_account,
-            amount_to_balance=amount_to_balance,
-            bank_payment_method_diffs=bank_payment_method_diffs,
-        )
+        res = super()._validate_session()
         self.order_ids.send_sii()
         return res
