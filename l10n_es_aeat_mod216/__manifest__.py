ec6e5f309d98232f89a6123997a5c8cece785ec3
Ivorra78 <informatica@totmaterial.es>
2023-08-21 17:08:18 +0000

    Translated using Weblate (Spanish)
    Currently translated at 100.0% (97 of 97 strings)
    
    Translation: l10n-spain-16.0/l10n-spain-16.0-l10n_es_aeat_mod216
    Translate-URL: https://translation.odoo-community.org/projects/l10n-spain-16-0/l10n-spain-16-0-l10n_es_aeat_mod216/es/

--- l10n_es_aeat_mod216/__manifest__.py
+++ l10n_es_aeat_mod216/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "AEAT modelo 216",
-    "version": "16.0.1.0.0",
+    "version": "16.0.1.0.1",
     "category": "Localisation/Accounting",
     "author": "AvanzOSC, Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-spain",
