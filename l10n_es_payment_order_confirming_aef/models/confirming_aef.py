41910a90d1ff3da1d7527993e064b271d3c72dd9
Gil Arasa Verge <gilarasaverge@gmail.com>
2023-11-27 21:32:41 +0100

    [14.0][FIX] l10n_es_payment_order_confirming_aef: Fecha de proceso no es obligatoria pero Bankinter si la pide

--- l10n_es_payment_order_confirming_aef/models/confirming_aef.py
+++ l10n_es_payment_order_confirming_aef/models/confirming_aef.py
@@ -109,7 +109,8 @@ class ConfirmingAEF(object):
             vat = vat.replace(self.partner_bank.country_id.code, "")
         text += self._aef_convert_text(vat, 15, "left")
         # 67 - 74 Fecha proceso
-        text += self._aef_convert_text("", 8)
+        fecha_proceso = fields.first(self.record.payment_line_ids).date
+        text += self._aef_convert_text(str(fecha_proceso).replace("-", ""), 8)
         # 75 - 82 Fecha remesa
         if self.record.date_prefered == "due":
             fecha_planificada = fields.first(
