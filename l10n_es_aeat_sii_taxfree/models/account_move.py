8ea20e28aff57d939c120ecabdea4626eadb1279
Alejandro Ji Cheung <alejandro.jicheung@factorlibre.com>
2024-01-11 12:51:18 +0100

    [ADD] l10n_es_taxfree_sii: module to send a reversal invoice to SII as F1

---
+++ l10n_es_aeat_sii_taxfree/models/account_move.py
@@ -0,0 +1,20 @@
+# © 2019 - FactorLibre - Daniel Duque <daniel.duque@factorlibre.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from odoo import models
+
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    def _get_sii_invoice_dict_out(self, cancel=False):
+        inv_dict = super()._get_sii_invoice_dict_out(cancel=cancel)
+        if self.fiscal_position_id.sii_refund_as_regular:
+            if inv_dict["FacturaExpedida"].get("TipoRectificativa", False):
+                del inv_dict["FacturaExpedida"]["TipoRectificativa"]
+            inv_dict["FacturaExpedida"]["TipoFactura"] = (
+                "F2"
+                if self.partner_id.commercial_partner_id.sii_simplified_invoice
+                else "F1"
+            )
+            del inv_dict["FacturaExpedida"]["ImporteTotal"]
+        return inv_dict
