bde6c53e32d9418a323dbfe3fceacb8c4c847726
Jairo Llopis <jairo@moduon.team>
2024-01-26 08:33:24 +0000

    [FIX] l10n_es_facturae_face: upgrade mocks
    In
    https://github.com/mvantellingen/python-zeep/commit/decbb761a78c4ea06fecdcfb24d90e248cd3a3dd
    zeep moved the `ServiceProxy` class to a different file, and our test
    mocks started to fail. This made those tests flaky, making them randomly
    fail, as you can see in
    https://github.com/OCA/l10n-spain/pull/3394#issuecomment-1908583904 and
    some other comments below.
    
    I'm now setting a lower bound in the zeep requirement and mocking the
    correct path to avoid those problems in the future.
    
    In any case, there are still more possible connection problems that I
    cannot fix at the moment because... well, the server is down and I
    cannot obtain the WSDL file. For those cases, we will just record the
    failure and skip the test.
    
    I took the chance to DRY all those mocks and upgrade some outdated
    import patterns that are not relevant in Odoo 16 anymore.
    
    These tests are now labeled with the correct tags, so they can be
    completely skipped for those of us that don't care about external tests.

--- l10n_es_facturae_face/components/webservice_face.py
+++ l10n_es_facturae_face/components/webservice_face.py
@@ -7,6 +7,7 @@ import logging
 
 from cryptography import x509
 from cryptography.hazmat.primitives import serialization
+from zeep import Client
 
 from odoo.exceptions import UserError, ValidationError
 from odoo.models import _
@@ -16,10 +17,6 @@ from odoo.addons.component.core import Component
 from ..models.wsse_signature import MemorySignature
 
 _logger = logging.getLogger(__name__)
-try:
-    from zeep import Client
-except (ImportError, IOError) as err:
-    _logger.info(err)
 
 
 class WebServiceFace(Component):
