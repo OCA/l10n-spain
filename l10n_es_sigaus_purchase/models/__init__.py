45b97851eacb1ffb15b30d673ccb5ba745e7bd30
manu <manuel.regidor@sygel.es>
2024-01-29 09:01:46 +0100

    [ADD]l10n_es_sigaus_purchase: manage sigaus expenses in purchases

---
+++ l10n_es_sigaus_purchase/models/__init__.py
@@ -0,0 +1,6 @@
+# Copyright 2023 Manuel Regidor <manuel.regidor@sygel.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from . import purchase_order
+from . import purchase_order_line
+from . import account_move
