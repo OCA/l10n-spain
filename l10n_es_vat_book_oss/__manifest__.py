d6241a46c020872f9bb1adc78bce1b4ccbba1f00
manu <manuel.regidor@sygel.es>
2024-02-01 09:35:09 +0100

    [15.0][FIX] l10n_es_vat_book_oss: Wrong dependency

--- l10n_es_vat_book_oss/__manifest__.py
+++ l10n_es_vat_book_oss/__manifest__.py
@@ -9,7 +9,7 @@
     "website": "https://github.com/OCA/l10n-spain",
     "license": "AGPL-3",
     "category": "Accounting",
-    "depends": ["l10n_es_vat_book", "l10n_eu_oss"],
+    "depends": ["l10n_es_vat_book", "l10n_eu_oss_oca"],
     "installable": True,
     "autoinstall": True,
 }
