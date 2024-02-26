0158201a88b8c60a2585b6a44658e64fafcbd4f7
manu <manuel.regidor@sygel.es>
2024-02-26 16:15:05 +0100

    [16.0] [ADD] l10n_es_sigaus_stock_picking_report_valued: Show SIGAUS amounts in valued pickings

---
+++ l10n_es_sigaus_stock_picking_report_valued/models/__init__.py
@@ -0,0 +1,5 @@
+# Copyright 2024 Manuel Regidor <manuel.regidor@sygel.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from . import stock_picking
+from . import stock_move_line
