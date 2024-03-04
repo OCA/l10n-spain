162289a1bab610d7d821d650df8f57befce9f3f4
Christian-RB <c.ramos@binhex.es>
2024-03-04 10:39:31 +0000

    [16.0][ADD] l10n_es_aeat_mod347_igic

---
+++ l10n_es_aeat_mod347_igic/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2014-2023 Binhex - Nicolás Ramos (http://binhex.es)
+
+{
+    "name": "AEAT modelo 347 IGIC",
+    "version": "16.0.1.0.0",
+    "author": "Binhex System Solutions," "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-spain",
+    "category": "Accounting",
+    "license": "AGPL-3",
+    "depends": [
+        "l10n_es_igic",
+        "l10n_es_aeat_mod347",
+    ],
+    "data": [
+        "data/tax_code_map_mod347_igic_data.xml",
+    ],
+    "maintainers": ["nicolasramos"],
+    "installable": True,
+}
