61f6e90b66dd8fd8fdbe2cc213be98cb7c980b78
manu <manuel.regidor@sygel.es>
2024-03-14 16:41:06 +0100

    [16.0] [IMP] l10n_es_sigaus_sale: Make sigaus line non invoicible

--- l10n_es_sigaus_sale/models/sale_order_line.py
+++ l10n_es_sigaus_sale/models/sale_order_line.py
@@ -1,7 +1,8 @@
 # Copyright 2023 Manuel Regidor <manuel.regidor@sygel.es>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import models
+from odoo import api, models
+from odoo.tools import float_compare
 
 
 class SaleOrderLine(models.Model):
@@ -21,3 +22,21 @@ class SaleOrderLine(models.Model):
         res = super()._prepare_invoice_line(**optional_values)
         res["is_sigaus"] = self.is_sigaus
         return res
+
+    @api.depends("is_sigaus")
+    def _compute_invoice_status(self):
+        precision = self.env["decimal.precision"].precision_get(
+            "Product Unit of Measure"
+        )
+        sigaus_lines = self.filtered("is_sigaus")
+        for line in sigaus_lines:
+            if (
+                float_compare(
+                    line.qty_invoiced, line.product_uom_qty, precision_digits=precision
+                )
+                >= 0
+            ):
+                line.invoice_status = "invoiced"
+            else:
+                line.invoice_status = "no"
+        return super(SaleOrderLine, self - sigaus_lines)._compute_invoice_status()
