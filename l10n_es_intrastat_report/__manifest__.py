cf600015ee4b14900826bcda1d1fd121dc964e85
Aritz Olea <aritz.olea@factorlibre.com>
2024-03-18 12:06:58 +0100

    [FIX] l10n_es_intrastat_report: Adapt to changes on confirmation

--- l10n_es_intrastat_report/__manifest__.py
+++ l10n_es_intrastat_report/__manifest__.py
@@ -8,7 +8,7 @@
 
 {
     "name": "Intrastat Product Declaration for Spain",
-    "version": "16.0.1.3.1",
+    "version": "16.0.1.3.2",
     "category": "Intrastat",
     "license": "AGPL-3",
     "summary": "Spanish Intrastat Product Declaration",
