214b19cc7be70b6d674971d295e0593f99f7d446
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2024-03-21 22:07:17 +0100

    [MIG] l10n_es_aeat_mod303: Migration to 17.0
    - Standard procedure.
    - Converted tax map lines to new format.
    - Converted views to new readonly/required/invisible syntax.
    - Adapted tests.
    
    TT48475

--- l10n_es_aeat_mod303/__manifest__.py
+++ l10n_es_aeat_mod303/__manifest__.py
@@ -2,12 +2,12 @@
 # Copyright 2015 AvanzOSC - Ainara Galdona
 # Copyright 2016 Tecnativa - Antonio Espinosa
 # Copyright 2017 ForgeFlow, S.L.
-# Copyright 2014-2021 Tecnativa - Pedro M. Baeza
+# Copyright 2014-2024 Tecnativa - Pedro M. Baeza
 # License AGPL-3 - See https://www.gnu.org/licenses/agpl-3.0
 
 {
     "name": "AEAT modelo 303",
-    "version": "16.0.2.3.2",
+    "version": "17.0.1.0.0",
     "category": "Accounting",
     "development_status": "Mature",
     "author": "Guadaltech,"
@@ -19,19 +19,14 @@
     "license": "AGPL-3",
     "depends": ["l10n_es", "l10n_es_aeat"],
     "data": [
-        "data/2021-07/aeat.model.export.config.csv",
-        "data/2021-07/aeat.model.export.config.line.csv",
-        "data/2021-07/l10n.es.aeat.map.tax.csv",
-        "data/2021-07/l10n.es.aeat.map.tax.line.csv",
         "data/2022/aeat.model.export.config.csv",
         "data/2022/aeat.model.export.config.line.csv",
         "data/2023/aeat.model.export.config.csv",
         "data/2023/aeat.model.export.config.line.csv",
         "data/2023/l10n.es.aeat.map.tax.csv",
+        "data/2023/l10n.es.aeat.map.tax.line.account.csv",
+        "data/2023/l10n.es.aeat.map.tax.line.tax.csv",
         "data/2023/l10n.es.aeat.map.tax.line.csv",
-        "data/tax_code_map_mod303_data.xml",
-        "data/aeat_export_mod303_2018_data.xml",
-        "data/aeat_export_mod303_2021_data.xml",
         "data/l10n.es.aeat.mod303.report.activity.code.csv",
         "views/mod303_view.xml",
         "views/l10n_es_aeat_mod303_report_activity_code_data_views.xml",
