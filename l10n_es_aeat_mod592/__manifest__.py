4e5b077e560dd8d12d4504ac6f45cfc5110da0db
Víctor Martínez <victor.martinez@tecnativa.com>
2024-04-01 09:40:17 +0200

    [IMP] l10n_es_aeat_mod592: New module
    Changes done:
    - Adding specific groups to view acquirer/manufacturers
    - Code refactoring
    - Sequence compatibility
    - Reports
    - Remove report_csv dependency
    - Add search option to template fields
    - Show the fields in product.template if there are no variants.
    - Add tests
    
    TT48472

--- l10n_es_aeat_mod592/__manifest__.py
+++ l10n_es_aeat_mod592/__manifest__.py
@@ -1,32 +1,29 @@
 # Copyright 2014-2022 Nicolás Ramos (http://binhex.es)
 # Copyright 2023 Binhex System Solutions
+# Copyright 2024 Tecnativa - Víctor Martínez
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "AEAT modelo 592",
-    "version": "14.0.1.0.0",
-    "category": "Localisation/Accounting",
-    "author": "Binhex System Solutions, Odoo Community Association (OCA)",
+    "version": "15.0.1.0.0",
+    "category": "Accounting",
+    "author": "Tecnativa, Binhex System Solutions, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-spain",
     "license": "AGPL-3",
-    "depends": [
-        "product", "account", "stock", "mrp", "l10n_es",
-        "l10n_es_aeat", "report_xlsx", "report_csv"],
+    "depends": ["l10n_es_aeat", "report_xlsx", "stock"],
     "data": [
+        "data/ir_sequence_data.xml",
+        "security/aeat_security.xml",
         "security/ir.model.access.csv",
-        "security/ir_rule.xml",
-        "views/product_template.xml",
-        "views/product_template.xml",
-        "views/res_partner.xml",
-        "views/mod592_view.xml",
-        "views/mod592_acquirer_line_view.xml",
-        "views/mod592_manufacturer_line_view.xml",
-        "views/res_company.xml",
+        "views/product_template_views.xml",
+        "views/res_partner_views.xml",
+        "views/mod592_views.xml",
+        "views/mod592_acquirer_line_views.xml",
+        "views/mod592_manufacturer_line_views.xml",
+        "views/res_company_views.xml",
         "report/aeat_mod592.xml",
         "report/common_templates.xml",
         "report/report_views.xml",
-        "report/mod592_csv.xml",
-        "data/ir_sequence_data.xml"
     ],
     "development_status": "Beta",
     "installable": True,
