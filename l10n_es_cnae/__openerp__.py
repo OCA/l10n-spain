b304bb9e8e4231cb9837107930bf47952f76147c
manu <manuel.regidor@sygel.es>
2024-05-29 11:37:58 +0200

    [IMP] l10n_es_cnae: black, isort, prettier

--- l10n_es_cnae/__openerp__.py
+++ l10n_es_cnae/__openerp__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016 Jairo Llopis <jairo.llopis@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
@@ -6,7 +5,7 @@
     "summary": "Extiende los códigos NACE europeos con los CNAE españoles",
     "version": "9.0.1.0.0",
     "category": "Localization",
-    "website": "https://www.tecnativa.com/",
+    "website": "https://github.com/OCA/l10n-spain",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
