1e8d60978adf92f5eeda3cae5dc86de90b09d2ec
Eduardo López Peñalver <elp@moduon.team>
2024-05-31 10:53:22 +0200

    [16.0][MIG] l10n_es_location_nuts
    @moduon MT-1811

--- l10n_es_location_nuts/__manifest__.py
+++ l10n_es_location_nuts/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "NUTS Regions for Spain",
     "summary": "NUTS specific options for Spain",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Localisation/Europe",
     "website": "https://github.com/OCA/l10n-spain",
     "author": "Tecnativa, Odoo Community Association (OCA)",
@@ -16,4 +16,5 @@
         "base_location_nuts",
     ],
     "post_init_hook": "post_init_hook",
+    "maintainers": ["rafaelbn", "edlopen"],
 }
