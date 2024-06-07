f3528e3727b8289bd9320625f81055da8b963f28
manu <manuel.regidor@sygel.es>
2024-06-07 10:00:51 +0200

    [ADD] l10n_ca_cnae: Traducciones de industrias NACE al catalan

---
+++ l10n_ca_cnae/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2024 Manuel Regidor <manuel.regidor@sygel.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl-3).
+{
+    "name": "Categorías de empresa CNAE 2009 (catalán)",
+    "summary": "Genera la traducción al catalán de todos los códigos Nace",
+    "version": "17.0.1.0.0",
+    "category": "Localization",
+    "website": "https://github.com/OCA/l10n-spain",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "l10n_es_cnae",
+    ],
+}
---
+++ l10n_ca_cnae/readme/USAGE.md
@@ -0,0 +1,4 @@
+Para usar este módulo:
+
+1.	Vaya a *Ventas > Ventas > Clientes* y edite uno.
+2.	En el campo *Etiquetas* podrá escoger los CNAE o NACE que quiera aplicarle.
