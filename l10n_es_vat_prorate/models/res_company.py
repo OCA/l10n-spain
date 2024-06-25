7c1be25bc784e42ee3cbc00bbd18bb2186aa983e
Enric Tobella <enric.tobella@dixmit.com>
2024-06-25 11:27:33 +0200

    [IMP] l10n_es_vat_porate: Use a proper account on investments and assets

--- l10n_es_vat_prorate/models/res_company.py
+++ l10n_es_vat_prorate/models/res_company.py
@@ -2,7 +2,7 @@
 # Copyright 2023 Tecnativa Carolina Fernandez
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import _, api, fields, models
+from odoo import _, api, fields, models, tools
 from odoo.exceptions import ValidationError
 from odoo.tools import ormcache
 
@@ -19,6 +19,36 @@ class ResCompany(models.Model):
     vat_prorate_ids = fields.One2many(
         "res.company.vat.prorate", inverse_name="company_id"
     )
+    prorrate_asset_account_id = fields.Many2one(
+        "account.account",
+        domain="[('company_id', '=', id)]",
+        compute="_compute_prorrate_accounts",
+        store=True,
+        readonly=False,
+    )
+    prorrate_investment_account_id = fields.Many2one(
+        "account.account",
+        domain="[('company_id', '=', id)]",
+        compute="_compute_prorrate_accounts",
+        store=True,
+        readonly=False,
+    )
+
+    @api.depends("chart_template_id", "with_vat_prorate")
+    def _compute_prorrate_accounts(self):
+        for record in self:
+            if record.with_vat_prorate and record.chart_template_id:
+                record.prorrate_asset_account_id = self.env.ref(
+                    "l10n_es.%s_account_common_6341" % record.id,
+                    raise_if_not_found=False,
+                )
+                record.prorrate_investment_account_id = self.env.ref(
+                    "l10n_es.%s_account_common_6342" % record.id,
+                    raise_if_not_found=False,
+                )
+            else:
+                record.prorrate_asset_account_id = False
+                record.prorrate_investment_account_id = False
 
     @ormcache("self")
     def _get_prorate_accounts(self):
@@ -49,6 +79,21 @@ class ResCompany(models.Model):
         for rec in self.sudo():
             if rec.with_vat_prorate and not rec.vat_prorate_ids:
                 raise ValidationError(_("You must complete VAT prorate information"))
+
+    @tools.ormcache(
+        "self.id",
+        "self.prorrate_asset_account_id.id",
+        "self.prorrate_investment_account_id.id",
+    )
+    def _get_tax_prorrate_account_map(self):
+        """Get the account mapping according user type"""
+        return {
+            "asset_current": self.prorrate_asset_account_id.id,
+            "asset_non_current": self.prorrate_asset_account_id.id,
+            "asset_fixed": self.prorrate_asset_account_id.id,
+            "liability_current": self.prorrate_investment_account_id.id,
+            "liability_non_current": self.prorrate_investment_account_id.id,
+        }
 
 
 class ResCompanyVatProrate(models.Model):
