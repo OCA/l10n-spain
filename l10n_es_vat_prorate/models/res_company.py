286999c93472f6198a4eb8b5c16a25e8e017935c
Enric Tobella <enric.tobella@dixmit.com>
2024-06-25 11:27:33 +0200

    [IMP] l10n_es_vat_porate: Use a proper account on investments and assets

--- l10n_es_vat_prorate/models/res_company.py
+++ l10n_es_vat_prorate/models/res_company.py
@@ -1,7 +1,7 @@
 # Copyright 2022 Creu Blanca
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import api, fields, models, tools
 
 
 class ResCompany(models.Model):
@@ -12,6 +12,36 @@ class ResCompany(models.Model):
     vat_prorate_ids = fields.One2many(
         "res.company.vat.prorate", inverse_name="company_id"
     )
+    prorrate_asset_account_id = fields.Many2one(
+        "account.account",
+        domain="[('company_id', '=', id)]",
+        compute="_compute_prorrate_accounts",
+        store=True,
+        readonly=False,
+    )
+    prorrate_investment_account_id = fields.Many2one(
+        "account.account",
+        domain="[('company_id', '=', id)]",
+        compute="_compute_prorrate_accounts",
+        store=True,
+        readonly=False,
+    )
+
+    @api.depends("chart_template_id", "with_vat_prorate")
+    def _compute_prorrate_accounts(self):
+        for record in self:
+            if record.with_vat_prorate and record.chart_template_id:
+                record.prorrate_asset_account_id = self.env.ref(
+                    "l10n_es.%s_account_common_6341" % record.id,
+                    raise_if_not_found=False,
+                )
+                record.prorrate_investment_account_id = self.env.ref(
+                    "l10n_es.%s_account_common_6342" % record.id,
+                    raise_if_not_found=False,
+                )
+            else:
+                record.prorrate_asset_account_id = False
+                record.prorrate_investment_account_id = False
 
     def get_prorate(self, date):
         self.ensure_one()
@@ -21,6 +51,31 @@ class ResCompany(models.Model):
             limit=1,
         )
         return prorate.vat_prorate
+
+    @tools.ormcache(
+        "self.id",
+        "self.prorrate_asset_account_id.id",
+        "self.prorrate_investment_account_id.id",
+    )
+    def _get_tax_prorrate_account_map(self):
+        """Get the account mapping according user type"""
+        return {
+            self.env.ref(
+                "account.data_account_type_current_assets"
+            ).id: self.prorrate_asset_account_id.id,
+            self.env.ref(
+                "account.data_account_type_non_current_assets"
+            ).id: self.prorrate_asset_account_id.id,
+            self.env.ref(
+                "account.data_account_type_fixed_assets"
+            ).id: self.prorrate_asset_account_id.id,
+            self.env.ref(
+                "account.data_account_type_current_liabilities"
+            ).id: self.prorrate_investment_account_id.id,
+            self.env.ref(
+                "account.data_account_type_non_current_liabilities"
+            ).id: self.prorrate_investment_account_id.id,
+        }
 
 
 class ResCompanyVatProrate(models.Model):
