38f0809df99c5d9b3672de1c1f45d2c8addf2fce
Miquel Alzanillas <malzanillas@apsl.net>
2024-09-04 12:54:03 +0200

    [MIG] l10n_es_aeat_partner_check: Migration to 17.0

--- l10n_es_aeat_partner_check/__manifest__.py
+++ l10n_es_aeat_partner_check/__manifest__.py
@@ -6,7 +6,7 @@
     "author": "Acysos S.L., Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-spain",
     "category": "Accounting",
-    "version": "16.0.1.0.1",
+    "version": "17.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["base", "base_vat", "l10n_es_aeat"],
     "data": [
--- l10n_es_aeat_partner_check/readme/CONTRIBUTORS.md
+++ l10n_es_aeat_partner_check/readme/CONTRIBUTORS.md
@@ -1,3 +1,5 @@
 - Ignacio Ibeas - Acysos S.L. \<<ignacio@acysos.com>\>
 - Ethan Hildick - Studio73 \<<ethan@studio73.es>\>
 - Aritz Olea - Landoo Sistemas de Información S.L. \<<ao@landoo.es>\>
+- Miquel Alzanillas - Advanced Programming Solutions
+  \<<malzanillas@apsl.net>\>
