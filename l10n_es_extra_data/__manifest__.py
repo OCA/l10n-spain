dce275339446f15d2bcc271d01cf593d383310d2
Ignacio Ibeas <ignacio@acysos.com>
2024-09-24 18:37:35 +0200

    [ADD] l10n_es_extra_data: nuevos impuestos no soportados por Odoo Oficial
    [ADD] l10n_es_extra_data: nuevos impuestos no soportados por Odoo Oficial
    
    [ADD] l10n_es_extra_data: nuevos impuestos no soportados por Odoo Oficial
    
    [ADD] l10n_es_extra_data: nuevos impuestos no soportados por Odoo Oficial
    
    [ADD] l10n_es_extra_data: nuevos impuestos no soportados por Odoo Oficial
    
    [ADD] l10n_es_extra_data: nuevos impuestos no soportados por Odoo Oficial
    
    [ADD] l10n_es_extra_data: nuevos impuestos no soportados por Odoo Oficial
    
    [ADD] l10n_es_extra_data: nuevos impuestos no soportados por Odoo Oficial

---
+++ l10n_es_extra_data/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright (C) 2024 Acysos S.L. - Ignacio Ibeas
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Datos Extra",
+    "summary": "Datos extra de la localización española",
+    "author": "Acysos (www.acysos.com), Odoo Community Association (OCA)",
+    "maintainers": ["acysos"],
+    "website": "https://github.com/OCA/l10n-spain",
+    "license": "AGPL-3",
+    "category": "Accounting & Finance",
+    "version": "13.0.1.0.0",
+    "depends": ["l10n_es"],
+    "data": [
+        "data/account_data.xml",
+        "data/account_tax_data.xml",
+        "data/account_fiscal_position_template_data.xml",
+    ],
+}
