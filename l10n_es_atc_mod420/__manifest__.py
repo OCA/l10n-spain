b74b2caac4fd70780e237b92e6bfb253db841dc3
Christian-RB <c.ramos@binhex.es>
2024-12-26 16:19:26 +0000

    [MIG] l10n_es_atc_mod420: Migration to 17.0

--- l10n_es_atc_mod420/__manifest__.py
+++ l10n_es_atc_mod420/__manifest__.py
@@ -4,14 +4,17 @@
 
 {
     "name": "ATC Modelo 420",
-    "version": "16.0.1.1.0",
+    "version": "17.0.1.0.0",
     "author": "Binhex, Odoo Community Association (OCA)",
     "category": "Accounting",
     "website": "https://github.com/OCA/l10n-spain",
     "license": "AGPL-3",
     "depends": ["l10n_es_aeat", "l10n_es_igic", "l10n_es_atc"],
     "data": [
-        "data/tax_code_map_mod420_data.xml",
+        "data/l10n.es.aeat.map.tax.csv",
+        "data/l10n.es.aeat.map.tax.line.tax.csv",
+        "data/l10n.es.aeat.map.tax.line.account.csv",
+        "data/l10n.es.aeat.map.tax.line.csv",
         "views/mod420_view.xml",
         "security/ir.model.access.csv",
     ],
