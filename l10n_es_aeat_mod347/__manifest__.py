a682988c6cf20c7a93760f66256a2191fcce79e0
Sergio Bustamante <sergio.bustamante@factorlibre.com>
2025-01-21 14:12:54 +0100

    [IMP] l10n_es_aeat_mod347: Display move name or move ref depending on its move_type

--- l10n_es_aeat_mod347/__manifest__.py
+++ l10n_es_aeat_mod347/__manifest__.py
@@ -10,7 +10,7 @@
 
 {
     "name": "AEAT modelo 347",
-    "version": "17.0.1.2.2",
+    "version": "17.0.1.3.0",
     "author": "Tecnativa,PESOL,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-spain",
     "category": "Accounting",
