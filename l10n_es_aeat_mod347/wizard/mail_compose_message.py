21cc98efdb6bdc57fda87b5de7357b229bf8bfab
Aritz Olea <aritz.olea@factorlibre.com>
2025-01-21 15:22:27 +0100

    [IMP] l10n_es_aeat_mod347: Refactor mail sending code

--- l10n_es_aeat_mod347/wizard/mail_compose_message.py
+++ l10n_es_aeat_mod347/wizard/mail_compose_message.py
@@ -9,10 +9,9 @@ class MailComposer(models.TransientModel):
     def _action_send_mail(self, auto_commit=False):
         res = super()._action_send_mail(auto_commit=auto_commit)
         default_model = self._context.get("default_model", False)
-        if default_model == "l10n.es.aeat.mod347.partner_record":
-            result_message = res[1]
+        partner_record_model = "l10n.es.aeat.mod347.partner_record"
+        if default_model == partner_record_model:
             active_id = self._context.get("active_id")
-            record = self.env["l10n.es.aeat.mod347.partner_record"].browse(active_id)
-            if result_message.mail_ids:
-                record.write({"state": "sent"})
+            record = self.env[partner_record_model].browse(active_id)
+            record.write({"state": "sent"})
         return res
