4d6188912c04b7b1dd52ef45244addd9d6af5bda
LauraCForgeFlow <laura.cazorla@forgeflow.com>
2025-01-27 08:06:42 +0100

    [IMP] l10n_es_aeat_mod390_oss: Add field 126 to BOE export

--- l10n_es_aeat_mod390_oss/__manifest__.py
+++ l10n_es_aeat_mod390_oss/__manifest__.py
@@ -9,7 +9,10 @@
     "website": "https://github.com/OCA/l10n-spain",
     "license": "AGPL-3",
     "depends": ["l10n_es_aeat_mod390", "l10n_eu_oss_oca"],
-    "data": ["data/tax_code_map_mod390_data.xml"],
+    "data": [
+        "data/tax_code_map_mod390_data.xml",
+        "data/aeat_export_mod390_sub06_data.xml",
+    ],
     "installable": True,
     "autoinstall": True,
 }
