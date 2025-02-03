9993355d25cca4f6cbcab657748f471b3c3c68a7
Luis Rodriguez <luis.rodriguez@dixmit.com>
2025-02-03 13:02:11 +0100

    [IMP] l10n_es_irnr: Add xmlid rename in script migration

--- l10n_es_irnr/migrations/16.0.1.2.1/pre-migration.py
+++ l10n_es_irnr/migrations/16.0.1.2.1/pre-migration.py
@@ -1,10 +1,53 @@
 # Copyright 2024 Aritz Olea <aritz.olea@factorlibre.com>
+# Copyright 2025 Luis Rodríguez <luis.rodriguez@dixmit.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from odoo import SUPERUSER_ID, api
+from openupgradelib import openupgrade
+
+taxes_to_rename = [
+    (
+        "l10n_es_irnr.%s_account_tax_template_s_irpfnrnue24",
+        "l10n_es.%s_account_tax_template_s_irpfnrnue24",
+    ),
+    (
+        "l10n_es_irnr.%s_account_tax_template_p_irpfnrnue24p",
+        "l10n_es.%s_account_tax_template_p_irpfnrnue24p",
+    ),
+    (
+        "l10n_es_irnr.%s_account_tax_template_s_irpfnrue19",
+        "l10n_es.%s_account_tax_template_s_irpfnrue19",
+    ),
+    (
+        "l10n_es_irnr.%s_account_tax_template_p_irpfnrue19p",
+        "l10n_es.%s_account_tax_template_p_irpfnrue19p",
+    ),
+    (
+        "l10n_es_irnr.%s_account_tax_template_s_irpfnrnue0",
+        "l10n_es.%s_account_tax_template_s_irpfnrnue0",
+    ),
+    (
+        "l10n_es_irnr.%s_account_tax_template_p_irpfnrnue0p",
+        "l10n_es.%s_account_tax_template_p_irpfnrnue0p",
+    ),
+    (
+        "l10n_es_irnr.%s_account_tax_template_s_irpfnrue0",
+        "l10n_es.%s_account_tax_template_s_irpfnrue0",
+    ),
+    (
+        "l10n_es_irnr.%s_account_tax_template_p_irpfnrue0p",
+        "l10n_es.%s_account_tax_template_p_irpfnrue0p",
+    ),
+]
 
 
-def migrate(cr, version):
-    env = api.Environment(cr, SUPERUSER_ID, {})
+def rename_xmlids(env):
+    for company in env["res.company"].search([]):
+        taxes_to_rename_by_company = [
+            (a % company.id, b % company.id) for (a, b) in taxes_to_rename
+        ]
+        openupgrade.rename_xmlids(env.cr, taxes_to_rename_by_company)
+
+
+def migrate_tax_groups(env):
     retenciones_24 = env.ref("l10n_es.tax_group_retenciones_24")
     retenciones_19 = env.ref("l10n_es.tax_group_retenciones_19")
     retenciones_0 = env.ref("l10n_es.tax_group_retenciones_0")
@@ -30,3 +73,10 @@ def migrate(cr, version):
             .mapped("res_id")
         )
         env["account.tax"].browse(record_ids).tax_group_id = tax_write[1]
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+
+    migrate_tax_groups(env)
+    rename_xmlids(env)
