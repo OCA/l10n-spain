af661a6a3f74a366a8609649338b80adc4b01cac
Jordi-jsc <josc@odoo.com>
2025-03-10 12:57:55 +0000

    [MIG] payment_redsys: Migration to version 18.0

--- payment_redsys/__init__.py
+++ payment_redsys/__init__.py
@@ -2,3 +2,13 @@
 
 from . import models
 from . import controllers
+
+from odoo.addons.payment import reset_payment_provider, setup_provider
+
+
+def post_init_hook(env):
+    setup_provider(env, "redsys")
+
+
+def uninstall_hook(env):
+    reset_payment_provider(env, "redsys")
