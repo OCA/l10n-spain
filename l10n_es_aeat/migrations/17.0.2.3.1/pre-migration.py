1dab872d19d2398d65de31ee3eb9215af25b3e38
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2025-03-18 21:06:22 +0100

    [FIX] l10n_es_*: Fix intra/extra-community taxes refs and new exempt taxes
    Odoo screwed up the tax XML-IDs in the middle of the 17.0 lifecycle,
    introducing an unfolding of the exempt taxes, using existing ones
    that belongs to the sales intra and extra community taxes.
    
    After that, they have renamed the XML-IDs of both taxes to new ones.
    
    We need to handle this in 2 ways:
    
    - Detect and correct existing installations previous to this
      taxpocalypse for renaming them.
    - Change the reference in all places to the new XML-IDs.
    
    Besides, we have to add the unfolded exempt taxes in the reports.

---
+++ l10n_es_aeat/migrations/17.0.2.3.1/pre-migration.py
@@ -0,0 +1,20 @@
+# Copyright 2025 Tecnativa - Pedro M. Baeza
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    # Detect if the company doesn't have the taxes XML-IDs renamed (before the
+    # taxpocalypse), and if so, rename them manually
+    for comp in env["res.company"].search([]):
+        for src, dest in [("s_iva0_e", "s_iva0_g_e"), ("s_iva0_ic", "s_iva0_g_i")]:
+            if env.ref(
+                f"account.{comp.id}_account_tax_template_{src}", False
+            ) and not env.ref(f"account.{comp.id}_account_tax_template_{dest}", False):
+                env["ir.model.data"].search(
+                    [
+                        ("module", "=", "account"),
+                        ("name", "=", f"{comp.id}_account_tax_template_{src}"),
+                    ]
+                ).name = f"account.{comp.id}_account_tax_template_{dest}"
