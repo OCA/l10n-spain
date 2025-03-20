6b81f4a233e90818cf6058b093052fd86d00ddc5
Angel Rivas <angel.rivas@sygel.es>
2025-03-20 16:39:37 +0100

    [IMP] l10n_es_facturae: remove unidad tramitadora and oficina contable checks

--- l10n_es_facturae/tests/common.py
+++ l10n_es_facturae/tests/common.py
@@ -792,18 +792,6 @@ class CommonTest(CommonTestBase):
         )
         return wizard
 
-    def test_create_facturae_file_without_unidad_tramitadora(self):
-        wizard = self._create_wizard_facturae_file()
-        self.partner.unidad_tramitadora = False
-        with self.assertRaises(exceptions.ValidationError):
-            wizard.create_facturae_file()
-
-    def test_create_facturae_file_without_oficina_contable(self):
-        wizard = self._create_wizard_facturae_file()
-        self.partner.oficina_contable = False
-        with self.assertRaises(exceptions.ValidationError):
-            wizard.create_facturae_file()
-
     def test_facturae_contact_creation_directly(self):
         """Create new child partner directly instead of parent form view"""
         new_partner = self.env["res.partner"].create(
