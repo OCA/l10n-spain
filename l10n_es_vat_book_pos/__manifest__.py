df258333ec8f952376ccc215d445531d84ad5aed
Antoni Marroig Campomar <amarroig@apsl.net>
2025-04-01 11:17:04 +0200

    [ADD] l10n_es_vat_book_pos: New module l10n_es_vat_book_pos

---
+++ l10n_es_vat_book_pos/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2025 APSL-Nagarro Antoni Marroig
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Libro de IVA Adaptado al Punto de Venta",
+    "version": "17.0.1.0.0",
+    "category": "Accounting",
+    "website": "https://github.com/OCA/l10n-spain",
+    "author": "APSL-Nagarro, Odoo Community Association (OCA)",
+    "maintainers": ["peluko00"],
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["l10n_es_vat_book", "point_of_sale"],
+}
---
+++ l10n_es_vat_book_pos/readme/CONTRIBUTORS.md
@@ -0,0 +1,2 @@
+- \[APSL-Nagarro\](<https://apsl.tech>):
+  - Antoni Marroig \<<amarroig@apsl.net>\>
