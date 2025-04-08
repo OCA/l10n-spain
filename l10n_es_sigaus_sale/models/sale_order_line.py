fc302d1f93c29c57af16bbda020007a7da3ec346
sygel <alberto.martinez@sygel.es>
2025-04-08 14:00:47 +0200

    [IMP] l10n_es_sigaus modules: remove ensure_ones

--- l10n_es_sigaus_sale/models/sale_order_line.py
+++ l10n_es_sigaus_sale/models/sale_order_line.py
@@ -45,7 +45,9 @@ class SaleOrderLine(models.Model):
     def create(self, vals_list):
         lines = super().create(vals_list)
         if not self.env.context.get("avoid_line_recursion"):
-            sales = lines.filtered(lambda li: li.product_id.sigaus_has_amount).mapped('order_id')
+            sales = lines.filtered(lambda li: li.product_id.sigaus_has_amount).mapped(
+                "order_id"
+            )
             sales.automatic_sigaus_exception()
             sales.apply_sigaus()
         return lines
