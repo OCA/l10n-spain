d9cc237fedf6a3d97660078e6ca0b77e7ca66b83
LauraCForgeFlow <laura.cazorla@forgeflow.com>
2025-04-17 12:28:21 +0200

    [MIG] l10n_es_aeat_mod111: Migration to 18.0

--- l10n_es_aeat_mod111/__manifest__.py
+++ l10n_es_aeat_mod111/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "AEAT modelo 111",
-    "version": "17.0.1.0.1",
+    "version": "18.0.1.0.0",
     "development_status": "Mature",
     "category": "Localization/Accounting",
     "author": "AvanzOSC, "
