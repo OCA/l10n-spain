bc5a8628a01542c8ce2730b83a545d41e612fa37
LauraCForgeFlow <laura.cazorla@forgeflow.com>
2025-04-17 13:28:32 +0200

    [MIG] l10n_es_aeat_mod123: Migration to 18.0

--- l10n_es_aeat_mod123/__manifest__.py
+++ l10n_es_aeat_mod123/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "AEAT modelo 123",
-    "version": "17.0.1.1.1",
+    "version": "18.0.1.0.0",
     "category": "Localisation/Accounting",
     "author": "Tecnativa, "
     "Spanish Localization Team, "
