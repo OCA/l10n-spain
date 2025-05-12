81e6c591fd4246cac9ff870f2d30a3275baec054
Carlos Lopez <carlos.lopez@tecnativa.com>
2025-05-12 09:24:45 -0500

    [IMP] l10n_es_aeat: Refactor sequence code generation for reports Allow easy inheritance in other modules (ATC).

--- l10n_es_aeat/__manifest__.py
+++ l10n_es_aeat/__manifest__.py
@@ -12,7 +12,7 @@
 {
     "name": "AEAT Base",
     "summary": "Modulo base para declaraciones de la AEAT",
-    "version": "18.0.1.3.3",
+    "version": "18.0.1.3.4",
     "author": "Pexego, "
     "Acysos S.L., "
     "AvanzOSC, "
