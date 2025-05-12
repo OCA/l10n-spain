7e4e581a243ac391b2bc1c75dd24f0a3452aa972
david-banon-tecnativa <david.banon@tecnativa.com>
2025-05-12 17:18:01 +0200

    [MIG] l10n_es_aeat_mod296: Migration to version 17.0
    TT52329

--- l10n_es_aeat_mod296/__manifest__.py
+++ l10n_es_aeat_mod296/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     "name": "AEAT modelo 296",
-    "version": "16.0.1.1.0",
+    "version": "17.0.1.0.0",
     "category": "Localisation/Accounting",
     "author": "Tecnativa, AvanzOSC, Qubiq, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-spain",
@@ -14,7 +14,9 @@
     "depends": ["l10n_es_aeat", "l10n_es_aeat_mod216"],
     "data": [
         "security/ir.model.access.csv",
-        "data/tax_code_map_mod296_data.xml",
+        "data/l10n.es.aeat.map.tax.csv",
+        "data/l10n.es.aeat.map.tax.line.tax.csv",
+        "data/l10n.es.aeat.map.tax.line.csv",
         "data/aeat_export_mod296_line_data.xml",
         "data/aeat_export_mod296_data.xml",
         "views/mod296_views.xml",
