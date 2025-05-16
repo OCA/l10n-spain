e2d7f312a4b7a9869702d5a5616c866b6b2bab5c
EmilioPascual <emilio@moduon.team>
2025-05-16 11:43:36 +0200

    [IMP] l10n_es_aeat_mod216: version 2024
    Update model 216 following the new directions for 2024, https://sede.agenciatributaria.gob.es/Sede/todas-gestiones/impuestos-tasas/impuesto-sobre-renta-no-residentes/modelo-216-irnr______sos-cuenta-declaracion-ingreso_/instrucciones-modelo-216-presentacion-ejercicio-siguientes.html,  and the new register designer for 2024 https://sede.agenciatributaria.gob.es/static_files/Sede/Disenyo_registro/DR_200_299/archivos_24/DR200-2024.xls
    
    MT-5789

---
+++ l10n_es_aeat_mod216/migrations/17.0.1.1.0/pre-migrate.py
@@ -0,0 +1,51 @@
+# Copyright 2025 Moduon Team S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl-3.0)
+
+from openupgradelib import openupgrade
+
+_rename_fields = [
+    (
+        "l10n.es.aeat.mod216.report",
+        "l10n_es_aeat_mod216_report",
+        "casilla_01",
+        "casilla_01_pre_2024",
+    ),
+    (
+        "l10n.es.aeat.mod216.report",
+        "l10n_es_aeat_mod216_report",
+        "casilla_03",
+        "casilla_03_pre_2024",
+    ),
+    (
+        "l10n.es.aeat.mod216.report",
+        "l10n_es_aeat_mod216_report",
+        "casilla_04",
+        "casilla_04_pre_2024",
+    ),
+    (
+        "l10n.es.aeat.mod216.report",
+        "l10n_es_aeat_mod216_report",
+        "casilla_05",
+        "casilla_05_pre_2024",
+    ),
+    (
+        "l10n.es.aeat.mod216.report",
+        "l10n_es_aeat_mod216_report",
+        "casilla_06",
+        "casilla_06_pre_2024",
+    ),
+    (
+        "l10n.es.aeat.mod216.report",
+        "l10n_es_aeat_mod216_report",
+        "casilla_07",
+        "casilla_07_pre_2024",
+    ),
+]
+
+
+@openupgrade.migrate()
+def migrate(cr, version):
+    for model, table, oldfield, newfield in _rename_fields:
+        if not openupgrade.column_exists(cr, table, oldfield):
+            continue
+        openupgrade.rename_fields(cr, [(model, table, oldfield, newfield)])
