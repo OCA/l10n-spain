9ca6528a5ad757a5d2cac94e0ec013a52e743c37
jorgeluislrpc <jorge.lopez@processcontrol.es>
2025-06-03 16:57:01 +0200

    Añade la configuración de claves de registro y claves de impuestos a las plantillas de posiciones fiscales de la localización

---
+++ l10n_es_verifactu/models/account_fiscal_position_template.py
@@ -0,0 +1,22 @@
+from odoo import fields, models, api
+
+
+class AccountFiscalPositionTemplate(models.Model):
+    _inherit = "account.fiscal.position.template"
+
+    verifactu_tax_key = fields.Selection(
+        selection="_get_verifactu_tax_keys",
+    )
+    verifactu_registration_key = fields.Many2one(
+        "verifactu.registration.keys",
+        ondelete="restrict",
+    )
+    verifactu_active = fields.Boolean(
+        copy=False,
+        default=True,
+        help="Enable Verifactu for this fiscal position?",
+    )
+
+    @api.model
+    def _get_verifactu_tax_keys(self):
+        return self.env["account.fiscal.position"]._get_verifactu_tax_keys()
