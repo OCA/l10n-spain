97d86c901d2640fb03d63c84ab207209959d0fd9
Emilio Pascual <emilio@moduon.team>
2025-06-19 09:44:41 +0200

    [MIG] l10n_es_aeat_mod115: Migration to 18.0

--- l10n_es_aeat_mod115/__manifest__.py
+++ l10n_es_aeat_mod115/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "AEAT modelo 115",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "development_status": "Mature",
     "category": "Localisation/Accounting",
     "author": "AvanzOSC, Tecnativa, Odoo Community Association (OCA)",
@@ -15,7 +15,8 @@
     "data": [
         "security/ir.model.access.csv",
         "security/l10n_es_aeat_mod115_security.xml",
-        "data/aeat_export_mod115_data.xml",
+        "data/aeat.model.export.config.csv",
+        "data/aeat.model.export.config.line.csv",
         "data/l10n.es.aeat.map.tax.csv",
         "data/l10n.es.aeat.map.tax.line.tax.csv",
         # l10n.es.aeat.map.tax.line.tax.csv should be before
