04e9fda6112c6ee2cb80a165492df25b3279d304
Jordi Ballester Alomar <jordi.ballester@forgeflow.com>
2025-06-23 12:41:44 +0200

    [REF+IMP] l10n_es_verifactu_oca: verifactu.send.* + mocked tests
    - Refactor para enviar las facturas desde verifactu.send.queue y guardar las respuestas en verifactu.send.respose
    - Improve tests related with the chaining of invoices
    - Mandar incidencia en cabecera cuando los registros se han generado pasados 260s del envio a verifactu
    - Add tests with mocked responses from verifactu

---
+++ l10n_es_verifactu_oca/models/verifactu_send_response_line.py
@@ -0,0 +1,33 @@
+# Copyright 2025 ForgeFlow S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+VERIFACTU_SEND_STATES = [
+    ("not_sent", "Not sent"),
+    ("correct", "Sent and Correct"),
+    ("incorrect", "Sent and Incorrect"),
+    ("accepted_with_errors", "Sent and accepted with errors"),
+]
+
+
+class VerifactuSendResponseLine(models.Model):
+    _name = "verifactu.send.response.line"
+    _description = "Verifactu Send Log"
+    _order = "id desc"
+
+    send_queue_id = fields.Many2one("verifactu.send.queue", required=True)
+    send_response_id = fields.Many2one("verifactu.send.response", required=True)
+    move_id = fields.Many2one(related="send_queue_id.move_id")
+    response = fields.Text()
+    send_state = fields.Selection(
+        selection=VERIFACTU_SEND_STATES,
+        string="Verifactu send state",
+        default="not_sent",
+        readonly=True,
+        copy=False,
+        help="Indicates the state of this document in relation with the "
+        "presentation to Verifactu.",
+    )
+    verifactu_csv = fields.Text(related="send_response_id.verifactu_csv")
+    error_code = fields.Char()
