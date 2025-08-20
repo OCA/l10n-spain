3ffb378c7e32a8b0769223b0a92ca9ecb0ca4441
Víctor Martínez <victor.martinez@tecnativa.com>
2025-08-20 09:07:18 +0200

    [FIX] l10n_es_intrastat_report: Define the appropriate data in post_init_hook
    Define the appropriate data (intrastat+vat_required) in the fiscal positions
    to be consistent with what is indicated in the _get_fiscal_position() method
    of account.chart.template

--- l10n_es_intrastat_report/hooks.py
+++ l10n_es_intrastat_report/hooks.py
@@ -7,17 +7,30 @@ def post_init_hook(env):
     """Set the intrastat field of the private fiscal position records.
     This is necessary for the intrastat report generation
     """
-    items = env["ir.model.data"].search(
+    b2b_items = env["ir.model.data"].search(
         [
             ("model", "=", "account.fiscal.position"),
-            ("name", "like", "%_fp_intra%"),
+            ("name", "like", "%_fp_intra"),
+            ("module", "=", "account"),
+        ]
+    )
+    b2c_items = env["ir.model.data"].search(
+        [
+            ("model", "=", "account.fiscal.position"),
+            ("name", "like", "%_fp_intra_private"),
             ("module", "=", "account"),
         ]
     )
     # Avoid modifying fiscal positions created with accounting plans other than l10n_es
     env["account.fiscal.position"].search(
         [
-            ("id", "in", items.mapped("res_id")),
+            ("id", "in", b2b_items.mapped("res_id")),
             ("company_id.chart_template", "like", "es_%"),
         ]
     ).write({"intrastat": "b2b", "vat_required": True})
+    env["account.fiscal.position"].search(
+        [
+            ("id", "in", b2c_items.mapped("res_id")),
+            ("company_id.chart_template", "like", "es_%"),
+        ]
+    ).write({"intrastat": "b2c", "vat_required": False})
