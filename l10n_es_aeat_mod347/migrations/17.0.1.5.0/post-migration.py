3714d0e79f1b609464cde658e077e1f2a351a10d
Eduardo de Miguel <edu@moduon.team>
2025-10-22 16:01:59 +0200

    [FIX] l10n_es_aeat_mod347: Remove old_not_in_mod347 column
    Check if old_not_in_mod347 column exists before renaming.
    Remove old_not_in_mod347 column once the migration has finished.

--- l10n_es_aeat_mod347/migrations/17.0.1.5.0/post-migration.py
+++ l10n_es_aeat_mod347/migrations/17.0.1.5.0/post-migration.py
@@ -9,7 +9,10 @@ from odoo import SUPERUSER_ID, api
 def migrate(cr, version):
     if not openupgrade.column_exists(cr, "res_partner", "not_in_mod347"):
         return  # Already migrated in 16
+    if not openupgrade.column_exists(cr, "res_partner", "old_not_in_mod347"):
+        return  # Already migrated in 16
     env = api.Environment(cr, SUPERUSER_ID, {})
     openupgrade.convert_to_company_dependent(
         env, "res.partner", "old_not_in_mod347", "not_in_mod347"
     )
+    openupgrade.drop_columns(cr, [("res_partner", "old_not_in_mod347")])
--- l10n_es_aeat_mod347/migrations/17.0.1.5.0/pre-migration.py
+++ l10n_es_aeat_mod347/migrations/17.0.1.5.0/pre-migration.py
@@ -7,6 +7,8 @@ from openupgradelib import openupgrade
 def migrate(cr, version):
     if not openupgrade.column_exists(cr, "res_partner", "not_in_mod347"):
         return  # Already migrated in 16
+    if openupgrade.column_exists(cr, "res_partner", "old_not_in_mod347"):
+        return  # Already migrated in 16
     openupgrade.rename_columns(
         cr, {"res_partner": [("not_in_mod347", "old_not_in_mod347")]}
     )
