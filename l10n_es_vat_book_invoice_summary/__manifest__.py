da485ca0962258f930bcc1c42af0b653ef448773
Víctor Martínez <victor.martinez@tecnativa.com>
2025-10-27 08:43:58 +0100

    [MIG] l10n_es_vat_book_invoice_summary: Migration to 18.0
    TT57216

--- l10n_es_vat_book_invoice_summary/__manifest__.py
+++ l10n_es_vat_book_invoice_summary/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Facturas resumen en libro de IVA",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "depends": ["l10n_es_aeat_sii_invoice_summary", "l10n_es_vat_book"],
     "category": "Accounting & Finance",
     "author": "Tecnativa, Odoo Community Association (OCA)",
