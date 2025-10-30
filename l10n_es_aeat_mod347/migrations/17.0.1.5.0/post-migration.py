2772149549252b39faea97fe4f8ed381e1cf9092
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2025-10-30 12:20:23 +0100

    [OU-FIX] l10n_es_aeat_mod347: Change the remaining part
    Follow-up of #4524

--- l10n_es_aeat_mod347/migrations/17.0.1.5.0/post-migration.py
+++ l10n_es_aeat_mod347/migrations/17.0.1.5.0/post-migration.py
@@ -11,5 +11,5 @@ def migrate(cr, version):
         return  # Already migrated in 16
     env = api.Environment(cr, SUPERUSER_ID, {})
     openupgrade.convert_to_company_dependent(
-        env, "res.partner", "old_not_in_mod347", "not_in_mod347"
+        env, "res.partner", "old_not_in_mod347", "old_not_in_mod347_17"
     )
