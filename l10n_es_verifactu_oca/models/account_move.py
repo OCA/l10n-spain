8ffc23c35911634d117df844ba0f87277ba947ed
almumu <almudena.delapuente@factorlibre.com>
2025-10-31 08:00:04 +0100

    [16.0][IMP] l10n_es_verifactu_oca: change the way to get all mapped taxes to make it compatible with OSS taxes

--- l10n_es_verifactu_oca/models/account_move.py
+++ l10n_es_verifactu_oca/models/account_move.py
@@ -504,9 +504,10 @@ class AccountMove(models.Model):
     def _check_all_taxes_mapped(self):
         if not (tax_lines := self._get_aeat_tax_info()):
             return False
-        verifactu_map = self._get_verifactu_map(self._get_document_date())
-        tax_templates = verifactu_map.map_lines.taxes
-        mapped_taxes = self.company_id.get_taxes_from_templates(tax_templates)
+        date = self._get_document_date()
+        verifactu_map = self._get_verifactu_map(date)
+        codes = verifactu_map.map_lines.mapped("code")
+        mapped_taxes = self._get_verifactu_taxes_map(codes, date)
         for tax_line in tax_lines.values():
             if tax_line["tax"] not in mapped_taxes:
                 return False
