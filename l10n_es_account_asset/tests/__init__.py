cee5835193cff5c988b3b8a5e7b9882e27c5b84a
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2025-11-10 18:51:32 +0100

    [IMP] l10n_es_account_asset: Corrected amount after non deductible
    When a tax is non deductible (because it's directly non deductible VAT,
    or due to the VAT prorate), the amount to depreciate should be increased
    with this non deductible amount, which was not taken into account before
    this commit.
    
    For achieving this without requiring glue modules, we are now depending
    on `l10n_es_aeat`, which is a reasonable trade-off, as almost all the
    instances with this module will have the other installed as well.
    
    As we only know the deductible amount at invoice level, we get the
    general tax info, and then apply the percentage corresponding to each
    line.
    
    It includes a new test for covering this case.

--- l10n_es_account_asset/tests/__init__.py
+++ l10n_es_account_asset/tests/__init__.py
@@ -1,4 +1,4 @@
-# (c) 2015 Ainara Galdona - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
+from . import test_create_asset_from_invoice
 from . import test_l10n_es_account_asset
