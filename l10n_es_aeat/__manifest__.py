0755b97a547685cfb9d155ba1e4a1ef4c8d10ba6
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2025-11-11 16:47:10 +0100

    [FIX] l10n_es_aeat: Find properly the XML-ID for non deductible
    The tax groups are now by company and bound to `account` module, so
    let's change the compare condition.

--- l10n_es_aeat/__manifest__.py
+++ l10n_es_aeat/__manifest__.py
@@ -12,7 +12,7 @@
 {
     "name": "AEAT Base",
     "summary": "Modulo base para declaraciones de la AEAT",
-    "version": "18.0.1.3.5",
+    "version": "18.0.1.3.6",
     "author": "Pexego, "
     "Acysos S.L., "
     "AvanzOSC, "
