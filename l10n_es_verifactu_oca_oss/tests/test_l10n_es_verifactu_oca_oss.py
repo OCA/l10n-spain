9246c1a6cd551ad960bc7101679a8994638f0a08
LauraCForgeFlow <laura.cazorla@forgeflow.com>
2025-11-24 16:26:06 +0100

    [MIG] l10n_es_verifactu_oca_oss: Migration to 17.0

--- l10n_es_verifactu_oca_oss/tests/test_l10n_es_verifactu_oca_oss.py
+++ l10n_es_verifactu_oca_oss/tests/test_l10n_es_verifactu_oca_oss.py
@@ -1,4 +1,4 @@
-# Copyright 2025 FactorLibre - Almudena de La Puente <almudena.delapuente@factorlibre.com>
+# Copyright 2025 FactorLibre - Almudena de La Puente <almudena.delapuente@factorlibre.com>  # noqa: E501
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from odoo import Command
 
