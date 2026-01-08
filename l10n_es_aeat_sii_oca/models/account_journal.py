3253c649187d90e0e0a9bb9fd1b1ae61373d4596
christian-ramos-tecnativa <christian.ramos@tecnativa.com>
2026-01-08 15:18:58 +0000

    [IMP] l10n_es_aeat_sii_oca: multiple improvements
    En este PR se añaden las siguientes mejoras:
    
    * Si la factura está en error, al enviarla de nuevo se crea como envío
      inmediato.
    
      * Esto sobretodo es para evitar que lleguemos al límite de tiempo si
        la corrección se realiza más tarde y tenemos retardo de días.
    
    * El check de error ahora es readonly.
    
      * Esto solo debería ser modificado desde el código.
    
    * Muestreo en el dashboard de los diarios si existen facturas con error
      con enlace directo.

--- l10n_es_aeat_sii_oca/models/account_journal.py
+++ l10n_es_aeat_sii_oca/models/account_journal.py
@@ -11,3 +11,24 @@ class AccountJournal(models.Model):
         related="company_id.sii_enabled", string="Company enable SII"
     )
     sii_enabled = fields.Boolean(string="Enable SII", default=True)
+
+    def _fill_sale_purchase_dashboard_data(self, dashboard_data):
+        res = super()._fill_sale_purchase_dashboard_data(dashboard_data)
+        for journal in self.filtered(
+            lambda journal: journal.type in ("sale", "purchase")
+        ):
+            domain_not_send = [
+                ("journal_id", "=", journal.id),
+                ("aeat_state", "=", "not_sent"),
+                ("sii_enabled", "=", True),
+            ]
+            domain_fail = [
+                ("journal_id", "=", journal.id),
+                ("aeat_send_failed", "=", True),
+                ("sii_enabled", "=", True),
+            ]
+            journal_dict = dashboard_data[journal.id]
+            AccountMove = self.env["account.move"]
+            journal_dict["number_not_sent"] = AccountMove.search_count(domain_not_send)
+            journal_dict["number_aeat_failed"] = AccountMove.search_count(domain_fail)
+        return res
