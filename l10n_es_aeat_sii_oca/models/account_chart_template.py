dc0c25952da569f6c4cdbba4c981fc7d4474dcb8
FernandoRomera <fromera@astrohotel.es>
2026-01-11 17:17:29 +0100

    [18.0][FIX] l10n_es_aeat_sii_oca: CoA name has changed in 18

--- l10n_es_aeat_sii_oca/models/account_chart_template.py
+++ l10n_es_aeat_sii_oca/models/account_chart_template.py
@@ -6,8 +6,10 @@ from odoo.addons.account.models.chart_template import template
 class AccountChartTemplate(models.AbstractModel):
     _inherit = "account.chart.template"
 
-    @template("es_common", "account.fiscal.position")
-    def _get_es_facturae_account_tax(self):
+    @template("es_common_mainland", "account.fiscal.position")
+    def _get_es_common_mainland_account_fiscal_position_sii(self):
         return self._parse_csv(
-            "es_common", "account.fiscal.position", module="l10n_es_aeat_sii_oca"
+            "es_common_mainland",
+            "account.fiscal.position",
+            module="l10n_es_aeat_sii_oca",
         )
