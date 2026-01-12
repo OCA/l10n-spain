883b288ef0bf71754d04d00125b03b1f8ed23d9c
Emilio Pascual <emilio@moduon.team>
2026-01-12 12:58:17 +0100

    [IMP] l10n_es_aeat_mod190: Record design 2025
    According to https://sede.agenciatributaria.gob.es/static_files/Sede/Disenyo_registro/DR_100_199/archivos_25/DISENOS_LOGICOS_190_2025.pdf
    
    @moduon MT-13276

--- l10n_es_aeat_mod190/__manifest__.py
+++ l10n_es_aeat_mod190/__manifest__.py
@@ -11,12 +11,16 @@
     "license": "AGPL-3",
     "depends": ["l10n_es", "l10n_es_aeat"],
     "data": [
+        # 2017
+        "data/2017/aeat.model.export.config.csv",
+        "data/2017/aeat.model.export.config.line.csv",
+        # 2025
+        "data/2025/aeat.model.export.config.csv",
+        "data/2025/aeat.model.export.config.line.csv",
         "security/l10n_es_aeat_mod190_security.xml",
         "security/ir.model.access.csv",
         "data/perception_key_data.xml",
         "data/perception_subkey_data.xml",
-        "data/aeat.model.export.config.csv",
-        "data/aeat.model.export.config.line.csv",
         "data/l10n.es.aeat.map.tax.csv",
         "data/l10n.es.aeat.map.tax.line.tax.csv",  # This one should be before the next
         "data/l10n.es.aeat.map.tax.line.csv",
