e7a7b1b373dab2830f680a6dfb79039db686af98
Carlos Lopez <carlos.lopez@tecnativa.com>
2026-01-22 13:30:08 -0500

    [IMP] l10n_es_atc_mod417: Update JAR to v2.2.1
    TT60489

--- l10n_es_atc_mod417/__manifest__.py
+++ l10n_es_atc_mod417/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     "name": "ATC Modelo 417",
-    "version": "17.0.1.1.1",
+    "version": "17.0.1.2.0",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "category": "Accounting",
     "website": "https://github.com/OCA/l10n-spain",
