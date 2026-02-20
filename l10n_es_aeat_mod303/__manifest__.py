186d366260646addf521671f30f65c5997160c3b
luciaavanzosc <luciaavanzosc@gmail.com>
2026-02-20 09:55:46 +0100

    [14.0][IMP] l10n_es_aeat_mod303: New declaration format for 2026

--- l10n_es_aeat_mod303/__manifest__.py
+++ l10n_es_aeat_mod303/__manifest__.py
@@ -32,6 +32,8 @@
         "data/2024-10/aeat.model.export.config.line.csv",
         "data/2024-10/l10n.es.aeat.map.tax.csv",
         "data/2024-10/l10n.es.aeat.map.tax.line.csv",
+        "data/2026/aeat.model.export.config.csv",
+        "data/2026/aeat.model.export.config.line.csv",
         "data/tax_code_map_mod303_data.xml",
         "data/aeat_export_mod303_2018_data.xml",
         "data/aeat_export_mod303_2021_data.xml",
