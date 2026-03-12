9ecdd3c4137dd03a3d166a2f0eefae5346ab80b0
christian-ramos-tecnativa <christian.ramos@tecnativa.com>
2026-03-12 13:29:06 +0000

    [IMP] l10n_es_aeat_sii_oca: Improve dashboard search performance

--- l10n_es_aeat_sii_oca/models/account_journal.py
+++ l10n_es_aeat_sii_oca/models/account_journal.py
@@ -14,21 +14,28 @@ class AccountJournal(models.Model):
 
     def _fill_sale_purchase_dashboard_data(self, dashboard_data):
         res = super()._fill_sale_purchase_dashboard_data(dashboard_data)
-        for journal in self.filtered(
-            lambda journal: journal.type in ("sale", "purchase")
-        ):
-            domain_not_send = [
-                ("journal_id", "=", journal.id),
-                ("aeat_state", "=", "not_sent"),
-                ("sii_enabled", "=", True),
-            ]
-            domain_fail = [
-                ("journal_id", "=", journal.id),
-                ("aeat_send_failed", "=", True),
-                ("sii_enabled", "=", True),
-            ]
-            journal_dict = dashboard_data[journal.id]
-            AccountMove = self.env["account.move"]
-            journal_dict["number_not_sent"] = AccountMove.search_count(domain_not_send)
-            journal_dict["number_aeat_failed"] = AccountMove.search_count(domain_fail)
+        journals = self.filtered(lambda journal: journal.type in ("sale", "purchase"))
+        if not journals:
+            return res
+        AccountMove = self.env["account.move"]
+        domain_base = [
+            ("journal_id", "in", journals.ids),
+            ("sii_enabled", "=", True),
+            "|",
+            ("aeat_state", "=", "not_sent"),
+            ("aeat_send_failed", "=", True),
+        ]
+        all_data = AccountMove._read_group(
+            domain_base, ["journal_id", "aeat_state", "aeat_send_failed"], ["__count"]
+        )
+        for journal_id, aeat_state, aeat_send_failed, count in all_data:
+            journal_dict = dashboard_data[journal_id.id]
+            if not journal_dict.get("number_not_sent"):
+                journal_dict["number_not_sent"] = 0
+            if not journal_dict.get("number_aeat_failed"):
+                journal_dict["number_aeat_failed"] = 0
+            if aeat_state == "not_sent":
+                journal_dict["number_not_sent"] += count
+            if aeat_send_failed:
+                journal_dict["number_aeat_failed"] += count
         return res
