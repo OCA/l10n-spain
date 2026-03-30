2a9259b6d6d814d3ec708468322cd4dbfc189441
juancarlosonate-tecnativa <juancarlos.onate@tecnativa.com>
2026-03-30 13:42:10 +0200

    [IMP] website_sale_insurance_caser: move caser insurance modal to cart checkout button

--- website_sale_insurance_caser/static/src/js/checkout_insurance.esm.js
+++ website_sale_insurance_caser/static/src/js/checkout_insurance.esm.js
@@ -4,33 +4,46 @@ import {rpc} from "@web/core/network/rpc";
 publicWidget.registry.caserInsuranceCheckout = publicWidget.Widget.extend({
     selector: ".oe_website_sale",
     events: {
+        "click a[name='website_sale_main_button']": "_onCheckoutClick",
         "click #addInsuranceBtn": "_onAddInsurance",
+        "click #skipInsuranceBtn": "_onSkipInsurance",
+        "click #closeInsuranceModal": "_onSkipInsurance",
+        "click #toggleAllInsurance": "_onToggleAll",
     },
 
-    async start() {
-        await this._super(...arguments);
-        if (window.location.pathname.includes("/shop/payment")) {
-            await this._checkUninsuredProducts();
+    start() {
+        this._checkoutHref = this.el
+            .querySelector("a[name='website_sale_main_button']")
+            ?.getAttribute("href");
+        return this._super(...arguments);
+    },
+
+    async _onCheckoutClick(ev) {
+        if (!window.location.pathname.includes("/shop/cart")) {
+            return;
         }
-    },
-
-    async _checkUninsuredProducts() {
+        ev.preventDefault();
         const data = await rpc("/shop/caser_insurance/get_uninsured_products");
         if (data.products?.length) {
             this._showInsuranceModal(data.products);
+        } else {
+            window.location.href = this._checkoutHref;
         }
     },
 
     _showInsuranceModal(products) {
         const $list = this.$("#uninsured_products_list");
         $list.empty();
+        const totalUnits = products.reduce((sum, p) => sum + p.quantity, 0);
+        const hasMultiUnit = totalUnits > 1;
 
         products.forEach((product) => {
+            const checked = hasMultiUnit ? "" : "checked";
             for (let i = 0; i < product.quantity; i++) {
                 $list.append(`
                     <div class="d-flex align-items-center gap-3 py-2 border-bottom">
                         <input class="form-check-input flex-shrink-0 insurance-checkbox"
-                               type="checkbox"
+                               type="checkbox" ${checked}
                                data-line-id="${product.line_id}"
                                id="ins_${product.line_id}_${i}"/>
                         <img src="${product.image_url}" alt="${product.product_name}"
@@ -44,7 +57,25 @@ publicWidget.registry.caserInsuranceCheckout = publicWidget.Widget.extend({
             }
         });
 
+        const $toggle = this.$("#toggleAllInsurance");
+        if (hasMultiUnit) {
+            $toggle.removeClass("d-none");
+        } else {
+            $toggle.addClass("d-none");
+        }
+
         this.$("#caserInsuranceModal").modal("show");
+    },
+
+    _onToggleAll() {
+        const $checkboxes = this.$(".insurance-checkbox");
+        const allChecked = $checkboxes.length === $checkboxes.filter(":checked").length;
+        $checkboxes.prop("checked", !allChecked);
+    },
+
+    _onSkipInsurance() {
+        this.$("#caserInsuranceModal").modal("hide");
+        window.location.href = this._checkoutHref;
     },
 
     async _onAddInsurance(ev) {
