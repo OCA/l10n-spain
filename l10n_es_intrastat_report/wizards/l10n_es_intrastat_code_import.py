9f854d9b41531c4eb6210b9e6449baa9271173d4
miguelvazquez1 <miguel@comunitea.com>
2026-05-06 12:03:59 +0200

    [IMP] l10n_es_intrastat_report: 2026 NC codes

--- l10n_es_intrastat_report/wizards/l10n_es_intrastat_code_import.py
+++ l10n_es_intrastat_report/wizards/l10n_es_intrastat_code_import.py
@@ -30,16 +30,20 @@ class L10nEsPartnerImportWizard(models.TransientModel):
 
     def _import_hs_codes(self):
         code_obj = self.env["hs.code"].with_context(active_test=False)
-        path = file_path("l10n_es_intrastat_report/data/Estruc_NC2023.xlsx")
+        path = file_path("l10n_es_intrastat_report/data/CN2026_Structure.xlsx")
         wb = openpyxl.load_workbook(path, read_only=True, data_only=True)
         sheet_rows = list(wb.worksheets[0].iter_rows(min_row=2, values_only=True))
         vals_list = []
         parents = []
         prev_level = 0
         for row in sheet_rows:
-            code = str(row[2] or "").replace(" ", "")
-            description = str(row[6] or "").lstrip("-")
-            level = int(row[3] or 0)
+            code = str(row[1] or "").replace(" ", "")
+            description = str(row[4] or "")
+            nbdash = row[2]
+            if nbdash is None:
+                level = 2 if str(row[1] or "").strip().isdigit() else 1
+            else:
+                level = int(nbdash) + 3
             temp = prev_level
             while temp > level and parents:
                 del parents[-1]
@@ -54,7 +58,7 @@ class L10nEsPartnerImportWizard(models.TransientModel):
                 "description": " /".join(parents + [description]),
             }
             if not code_obj.search([("local_code", "=", code)]):
-                iu = str(row[5] or "").replace("\xa0", " ")
+                iu = str(row[5] or "").replace("\xa0", " ").strip()
                 if iu and iu != "-":  # specific unit
                     if iu in UOM_MAPPING:
                         iu_unit_id = self.env.ref(
