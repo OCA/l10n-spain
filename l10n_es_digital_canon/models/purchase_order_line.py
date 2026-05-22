2876ebe97b657aa17e11ccee9e5cbef434a58892
sergio-teruel <sergio.teruel@tecnativa.com>
2026-05-22 18:14:39 +0200

    [FIX] l10n_es_digital_canon: Digital canon taxes not take into account in _prepare_purchase_order_line TT62646

--- l10n_es_digital_canon/models/purchase_order_line.py
+++ l10n_es_digital_canon/models/purchase_order_line.py
@@ -1,6 +1,6 @@
 # Copyright 2025 Juan Carlos Oñate - Tecnativa <juancarlos.onate@tecnativa.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import models
+from odoo import api, models
 
 
 class PurchaseOrderLine(models.Model):
@@ -28,3 +28,16 @@ class PurchaseOrderLine(models.Model):
                 )
                 if tax:
                     line.taxes_id |= tax
+
+    @api.model
+    def _prepare_purchase_order_line(
+        self, product_id, product_qty, product_uom, company_id, supplier, po
+    ):
+        vals = super()._prepare_purchase_order_line(
+            product_id, product_qty, product_uom, company_id, supplier, po
+        )
+        if product_id.l10n_es_digital_canon:
+            virtual_line = self.new(vals)
+            virtual_line._compute_tax_id()
+            vals["taxes_id"] = [(6, 0, virtual_line.taxes_id.ids)]
+        return vals
