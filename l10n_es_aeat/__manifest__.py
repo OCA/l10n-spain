f09d86c6fbadc1628e799c12225bb02de2483c95
Oihane Crucelaegui <oihanecruce@gmail.com>
2026-05-27 14:49:13 +0200

    [IMP] l10n_es_aeat: mapeo de impuestos creados manualmente para modelos

--- l10n_es_aeat/__manifest__.py
+++ l10n_es_aeat/__manifest__.py
@@ -46,6 +46,7 @@
         "views/aeat_certificate_view.xml",
         "views/account_journal_view.xml",
         "views/account_move_view.xml",
+        "views/account_tax_view.xml",
     ],
     "installable": True,
     "maintainers": ["pedrobaeza"],
