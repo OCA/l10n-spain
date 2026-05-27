f0e06fa50b76d498641f9e9f7cf7741b5d475ef9
Oihane Crucelaegui <oihanecruce@gmail.com>
2026-05-27 14:49:13 +0200

    [IMP] l10n_es_aeat: mapeo de impuestos creados manualmente para modelos

--- l10n_es_aeat/models/account_tax.py
+++ l10n_es_aeat/models/account_tax.py
@@ -1,11 +1,18 @@
 # Copyright 2020 Tecnativa - Pedro M. Baeza
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import api, models
+from odoo import api, fields, models
 
 
 class AccountTax(models.Model):
     _inherit = "account.tax"
+
+    aeat_equivalent_tax_id = fields.Many2one(
+        comodel_name="account.tax",
+        string="Equivalent Tax (for AEAT mapping purposes)",
+        help="This field is for manually created taxes in order to allow to map them"
+        " for AEAT reports",
+    )
 
     def _clear_tax_id_from_tax_template_cache(self):
         Company = self.env["res.company"]
@@ -27,3 +34,15 @@ class AccountTax(models.Model):
         res = super().unlink()
         self._clear_tax_id_from_tax_template_cache()
         return res
+
+    def copy_data(self, default=None):
+        """When duplicating a tax, if it does not contain an equivalent tax,
+        takes itself as base.
+        """
+        vals_list = super().copy_data(default=default)
+        for record, vals in zip(self, vals_list, strict=False):
+            if not vals.get("aeat_equivalent_tax_id"):
+                vals["aeat_equivalent_tax_id"] = (
+                    record.aeat_equivalent_tax_id.id or record.id
+                )
+        return vals_list
