722f578003e5346826b715e0ae74438ab5a1eacc
Luis Rodriguez <luis.rodriguez@dixmit.com>
2026-08-05 10:49:24 +0200

    [MIG] l10n_es_aeat_mod369: Migration to 19.0

--- l10n_es_aeat_mod369/__manifest__.py
+++ l10n_es_aeat_mod369/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "AEAT modelo 369",
-    "version": "18.0.1.0.1",
+    "version": "19.0.1.0.0",
     "category": "Accounting",
     "author": "Studio73, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-spain",
