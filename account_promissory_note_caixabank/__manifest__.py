a7b362e97e16ea2ae42d4fe9e4ee4547e1f4366d
r.perez <r.perez@binhex.cloud>
2026-09-12 09:46:36 -0400

    [MIG] account_promissory_note_caixabank: Migration to 16.0

--- account_promissory_note_caixabank/__manifest__.py
+++ account_promissory_note_caixabank/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Impresión de pagaré CaixaBank A4",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "category": "Accounting",
